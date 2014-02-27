94b2c424225720d99d41cd477826d4d7da7c59f8
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2014-02-27 21:58:13 +0400

    Update cached check results

--- verifier-cli/src/main/java/com/jetbrains/pluginverifier/utils/DownloadUtils.java
+++ verifier-cli/src/main/java/com/jetbrains/pluginverifier/utils/DownloadUtils.java
@@ -1,11 +1,17 @@
 package com.jetbrains.pluginverifier.utils;
 
 import com.google.common.io.Files;
+import com.google.common.net.HttpHeaders;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * @author Sergey Evdokimov
@@ -56,15 +62,47 @@ public class DownloadUtils {
 
     File res = new File(checkResDir, build + ".xml");
 
-    if (!res.exists()) {
-      File currentDownload = File.createTempFile("currentDownload", ".zip", downloadDir);
-
-      FileUtils.copyURLToFile(new URL(Configuration.getInstance().getPluginRepositoryUrl() + "/files/checkResults/" + build + ".xml"), currentDownload);
-
-      Files.move(currentDownload, res);
-    }
+    updateFile(new URL(Configuration.getInstance().getPluginRepositoryUrl() + "/files/checkResults/" + build + ".xml"), res);
 
     return res;
   }
 
+  private static String toHttpDate(Date date) {
+    SimpleDateFormat httpDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+    httpDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+    return httpDateFormat.format(date);
+  }
+
+  private static void updateFile(URL url, File file) throws IOException {
+    long lastModified = file.lastModified();
+
+    if (lastModified == 0) {
+      FileUtils.copyURLToFile(url, file);
+    }
+    else {
+      HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+      connection.addRequestProperty(HttpHeaders.IF_MODIFIED_SINCE, toHttpDate(new Date(lastModified)));
+      connection.addRequestProperty(HttpHeaders.CACHE_CONTROL, "max-age=0");
+      connection.connect();
+      int responseCode = connection.getResponseCode();
+
+      try {
+        if (responseCode == 200) {
+          FileUtils.copyInputStreamToFile(connection.getInputStream(), file);
+
+        }
+        else if (responseCode == 304) {
+          // Not modified
+        }
+        else {
+          throw new IOException("Failed to download check result: " + responseCode);
+        }
+      }
+      finally {
+        connection.disconnect();
+      }
+    }
+  }
 }
