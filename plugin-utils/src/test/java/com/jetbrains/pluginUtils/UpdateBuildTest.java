a062efa014f04b45ab2a122971f33c760bbf5380
Ivan Chirkov <Ivan.Chirkov@jetbrains.com>
2014-08-19 09:01:30 +0200

    UpdateBuild test added

---
+++ plugin-utils/src/test/java/com/jetbrains/pluginUtils/UpdateBuildTest.java
@@ -0,0 +1,39 @@
+package com.jetbrains.pluginUtils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UpdateBuildTest {
+    @Test
+    public void testTypicalBuild() {
+        UpdateBuild updateBuild = new UpdateBuild("IU-138.1042");
+        Assert.assertEquals(138, updateBuild.getBranch());
+        Assert.assertEquals(1042, updateBuild.getBuild());
+        Assert.assertEquals("IU", updateBuild.getProductCode());
+        Assert.assertEquals("idea", updateBuild.getProductName());
+        Assert.assertEquals(false, updateBuild.isSnapshot());
+        Assert.assertEquals(true, updateBuild.isOk());
+    }
+
+    @Test
+    public void testSnapshotBuild() {
+        UpdateBuild updateBuild = new UpdateBuild("PS-138.SNAPSHOT");
+        Assert.assertEquals(138, updateBuild.getBranch());
+        Assert.assertEquals(Integer.MAX_VALUE, updateBuild.getBuild());
+        Assert.assertEquals("PS", updateBuild.getProductCode());
+        Assert.assertEquals("phpStorm", updateBuild.getProductName());
+        Assert.assertEquals(true, updateBuild.isSnapshot());
+        Assert.assertEquals(true, updateBuild.isOk());
+    }
+
+    @Test
+    public void testUnsupportedProduct() {
+        UpdateBuild updateBuild = new UpdateBuild("XX-138.SNAPSHOT");
+        Assert.assertEquals(0, updateBuild.getBranch());
+        Assert.assertEquals(0, updateBuild.getBuild());
+        Assert.assertEquals("", updateBuild.getProductCode());
+        Assert.assertNull(updateBuild.getProductName());
+        Assert.assertEquals(false, updateBuild.isSnapshot());
+        Assert.assertEquals(false, updateBuild.isOk());
+    }
+}
