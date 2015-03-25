69e8836a593cf299c43bd6658a28066eb2c5b0f9
Leonid Shalupov <shalupov@diverse.org.ru>
2015-03-25 22:06:34 +0100

    plugin-verifier skips checking for .class constants #2

--- verifier-cli/src/main/java/com/jetbrains/pluginverifier/verifiers/Verifiers.java
+++ verifier-cli/src/main/java/com/jetbrains/pluginverifier/verifiers/Verifiers.java
@@ -11,6 +11,7 @@ import com.jetbrains.pluginverifier.verifiers.field.FieldTypeVerifier;
 import com.jetbrains.pluginverifier.verifiers.field.FieldVerifier;
 import com.jetbrains.pluginverifier.verifiers.instruction.InstructionVerifier;
 import com.jetbrains.pluginverifier.verifiers.instruction.InvokeInstructionVerifier;
+import com.jetbrains.pluginverifier.verifiers.instruction.LdcInstructionVerifier;
 import com.jetbrains.pluginverifier.verifiers.instruction.TypeInstructionVerifier;
 import com.jetbrains.pluginverifier.verifiers.method.MethodVerifier;
 import com.jetbrains.pluginverifier.verifiers.method.OverrideNonFinalVerifier;
@@ -23,7 +24,11 @@ public class Verifiers {
   private static final ClassVerifier[] CLASS_VERIFIERS = new ClassVerifier[] {new SuperClassVerifier(), new InterfacesVerifier(), new AbstractMethodVerifier()};
   private static final MethodVerifier[] METHOD_VERIFIERS = new MethodVerifier[] {new OverrideNonFinalVerifier()};
   private static final FieldVerifier[] FIELD_VERIFIERS = new FieldVerifier[] {new FieldTypeVerifier()};
-  private static final InstructionVerifier[] INSTRUCTION_VERIFIERS = new InstructionVerifier[] {new InvokeInstructionVerifier(), new TypeInstructionVerifier()};
+  private static final InstructionVerifier[] INSTRUCTION_VERIFIERS = new InstructionVerifier[] {
+      new InvokeInstructionVerifier(),
+      new TypeInstructionVerifier(),
+      new LdcInstructionVerifier()
+  };
 
   public static final Verifier[] PLUGIN_VERIFIERS = new Verifier[]{new ReferencesVerifier()};
 
---
+++ verifier-cli/src/main/java/com/jetbrains/pluginverifier/verifiers/instruction/LdcInstructionVerifier.java
@@ -0,0 +1,28 @@
+package com.jetbrains.pluginverifier.verifiers.instruction;
+
+import com.jetbrains.pluginverifier.VerificationContext;
+import com.jetbrains.pluginverifier.problems.ClassNotFoundProblem;
+import com.jetbrains.pluginverifier.problems.ProblemLocation;
+import com.jetbrains.pluginverifier.resolvers.Resolver;
+import com.jetbrains.pluginverifier.verifiers.util.VerifierUtil;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+public class LdcInstructionVerifier implements InstructionVerifier {
+  public void verify(final ClassNode clazz, final MethodNode method, final AbstractInsnNode instr, final Resolver resolver, final VerificationContext ctx) {
+    if (!(instr instanceof LdcInsnNode)) return;
+
+    final Object constant = ((LdcInsnNode) instr).cst;
+    if (!(constant instanceof Type)) return;
+
+    final String descriptor = ((Type) constant).getDescriptor();
+    final String className = VerifierUtil.extractClassNameFromDescr(descriptor);
+
+    if (VerifierUtil.classExists(ctx.getOptions(), resolver, className)) return;
+
+    ctx.registerProblem(new ClassNotFoundProblem(className), ProblemLocation.fromMethod(clazz.name, method.name + method.desc));
+  }
+}
