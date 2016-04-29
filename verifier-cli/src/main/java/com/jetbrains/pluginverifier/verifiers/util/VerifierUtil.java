56c3ebd6659d315c9afc837420a058fab77280b2
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-04-29 20:44:41 +0300

    multiple util method moved to VerifierUtil

--- verifier-cli/src/main/java/com/jetbrains/pluginverifier/verifiers/util/VerifierUtil.java
+++ verifier-cli/src/main/java/com/jetbrains/pluginverifier/verifiers/util/VerifierUtil.java
@@ -5,14 +5,17 @@ import com.intellij.structure.resolvers.Resolver;
 import com.jetbrains.pluginverifier.location.ProblemLocation;
 import com.jetbrains.pluginverifier.problems.FailedToReadClassProblem;
 import com.jetbrains.pluginverifier.utils.FailUtil;
+import com.jetbrains.pluginverifier.utils.StringUtil;
 import com.jetbrains.pluginverifier.verifiers.VerificationContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
 import org.objectweb.asm.tree.MethodNode;
 
 import java.io.IOException;
+import java.util.Set;
 
 public class VerifierUtil {
   public static boolean classExistsOrExternal(VerificationContext ctx, final Resolver resolver, final @NotNull String className) {
@@ -65,6 +68,10 @@ public class VerifierUtil {
   }
 
 
+  /**
+   * @param descr full descriptor (may be an array type or a primitive type)
+   * @return null for primitive types and the innermost type for array types
+   */
   @Nullable // return null for primitive types
   public static String extractClassNameFromDescr(@NotNull String descr) {
     descr = prepareArrayName(descr);
@@ -95,8 +102,20 @@ public class VerifierUtil {
     return (method.access & Opcodes.ACC_PRIVATE) != 0;
   }
 
+  public static boolean isPrivate(@NotNull FieldNode field) {
+    return (field.access & Opcodes.ACC_PRIVATE) != 0;
+  }
+
   private static boolean isPublic(@NotNull MethodNode method) {
     return (method.access & Opcodes.ACC_PUBLIC) != 0;
+  }
+
+  private static boolean isPublic(@NotNull FieldNode field) {
+    return (field.access & Opcodes.ACC_PUBLIC) != 0;
+  }
+
+  public static boolean isDefaultAccess(@NotNull FieldNode field) {
+    return !isPublic(field) && !isProtected(field) && !isPrivate(field);
   }
 
   public static boolean isDefaultAccess(@NotNull MethodNode method) {
@@ -107,11 +126,57 @@ public class VerifierUtil {
     return (method.access & Opcodes.ACC_ABSTRACT) != 0;
   }
 
+  public static boolean isProtected(@NotNull FieldNode field) {
+    return (field.access & Opcodes.ACC_PROTECTED) != 0;
+  }
+
   public static boolean isProtected(@NotNull MethodNode method) {
     return (method.access & Opcodes.ACC_PROTECTED) != 0;
   }
 
   public static boolean isStatic(@NotNull MethodNode method) {
     return (method.access & Opcodes.ACC_STATIC) != 0;
+  }
+
+  public static boolean isStatic(@NotNull FieldNode field) {
+    return (field.access & Opcodes.ACC_STATIC) != 0;
+  }
+
+  public static boolean haveTheSamePackage(@NotNull ClassNode first, @NotNull ClassNode second) {
+    return StringUtil.equals(extractPackage(first.name), extractPackage(second.name));
+  }
+
+  @Nullable
+  private static String extractPackage(@Nullable String className) {
+    if (className == null) return null;
+    int slash = className.lastIndexOf('/');
+    if (slash == -1) return className;
+    return className.substring(0, slash);
+  }
+
+  public static boolean isAncestor(@NotNull ClassNode child, @NotNull ClassNode possibleParent, @NotNull Resolver resolver, @NotNull VerificationContext ctx) {
+    while (child != null) {
+      if (StringUtil.equals(possibleParent.name, child.name)) {
+        return true;
+      }
+      String superName = child.superName;
+      if (superName == null) {
+        return false;
+      }
+      child = findClass(resolver, superName, ctx);
+    }
+    return false;
+  }
+
+  public static boolean hasUnresolvedParentClass(@NotNull String clazz,
+                                                 @NotNull Resolver resolver,
+                                                 @NotNull VerificationContext ctx) {
+    ClassNode aClass = findClass(resolver, clazz, ctx);
+    if (aClass == null) {
+      return true;
+    }
+
+    Set<String> unresolvedClasses = ResolverUtil.collectUnresolvedClasses(resolver, clazz, ctx);
+    return !unresolvedClasses.isEmpty();
   }
 }
