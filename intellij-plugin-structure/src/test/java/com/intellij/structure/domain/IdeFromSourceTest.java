46f8827c7eaa0bd73f46d2f0e499025faddeb5d1
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-05-24 21:45:44 +0300

    added a TODO

--- intellij-plugin-structure/src/test/java/com/intellij/structure/domain/IdeFromSourceTest.java
+++ intellij-plugin-structure/src/test/java/com/intellij/structure/domain/IdeFromSourceTest.java
@@ -9,6 +9,8 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * TODO: rewrite test. construct the directories structure manually.
+ *
  * Created by Sergey Patrikeev
  */
 public class IdeFromSourceTest {
