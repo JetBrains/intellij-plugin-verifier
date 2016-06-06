d7d120005a16d3dea8d73d71d2c4ccf41cabc251
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-06-06 14:28:17 +0300

    Problem now implements Jsonable so it (and all its inheritors) can be saved as Json

--- verifier-problems/src/main/java/com/jetbrains/pluginverifier/problems/Problem.java
+++ verifier-problems/src/main/java/com/jetbrains/pluginverifier/problems/Problem.java
@@ -1,16 +1,14 @@
 package com.jetbrains.pluginverifier.problems;
 
-import com.jetbrains.pluginverifier.utils.Pair;
+import com.jetbrains.pluginverifier.persistence.Jsonable;
 import org.jetbrains.annotations.NotNull;
 
 import javax.xml.bind.annotation.XmlTransient;
-import java.util.List;
 
 /**
  * Problem container class.
- * Subclasses must have the default public constructor.
  */
-public abstract class Problem {
+public abstract class Problem implements Jsonable<Problem> {
 
   @XmlTransient
   @NotNull
@@ -22,35 +20,6 @@ public abstract class Problem {
   @XmlTransient
   @NotNull
   public abstract String getDescription();
-
-  /**
-   * <p>
-   * Override this method to deserialize the Problem class.
-   * <p>
-   * {@code params} are going in the order previously specified with {@link #serialize()}
-   * <p>
-   * Note: subclasses have to make an effort of deserializing its fields.
-   *
-   * @param params parameters for the construction of the derived Problem class.
-   * @return Problem instance
-   */
-  public abstract Problem deserialize(String... params);
-
-  /**
-   * <p>
-   * Override this method to serialize the Problem class.
-   * <p>
-   * It must return (key, value) pairs of the Problem content.
-   * The key represents the key name of the underlying Problem field. The value represents its value.
-   * <p>
-   * The {@link #deserialize(String...)} method will be called with the parameters
-   * according to the List order.
-   * <p>
-   * Note: subclasses have to make an effort of serialising its fields.
-   *
-   * @return (key, value) pairs of the Problem content
-   */
-  public abstract List<Pair<String, String>> serialize();
 
   @Override
   public final boolean equals(Object o) {
