47d0989cf75e3fb4bd23a0703d529331162d8b65
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-06-24 14:39:33 +0300

    problems test fixed

--- verifier-problems/src/test/java/TestProblems.java
+++ verifier-problems/src/test/java/TestProblems.java
@@ -3,15 +3,15 @@ import com.jetbrains.pluginverifier.persistence.GsonHolder;
 import com.jetbrains.pluginverifier.problems.AccessType;
 import com.jetbrains.pluginverifier.problems.IncompatibleClassChangeProblem;
 import com.jetbrains.pluginverifier.problems.Problem;
-import kotlin.Pair;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -85,47 +85,38 @@ public class TestProblems {
       if ((aClass.getModifiers() & Modifier.ABSTRACT) != 0) {
         continue;
       }
-      Method serialize = null;
-      Method deserialize = null;
-      for (Method method : aClass.getMethods()) {
-        if (method.getName().equals("serialize")) {
-          serialize = method;
-        }
-        if (method.getName().equals("deserialize")) {
-          deserialize = method;
-        }
-      }
-      Assert.assertNotNull(serialize);
-      Assert.assertNotNull(deserialize);
-
       checkConversion(aClass);
     }
 
   }
 
   private void checkConversion(Class<?> aClass) throws Exception {
-    Problem factory = (Problem) aClass.newInstance();
+    Constructor<?>[] constructors = aClass.getConstructors();
 
-    List<Pair<String, String>> pairs = factory.serialize();
-    int size = pairs.size();
-    String[] names = new String[size];
-    String[] params = new String[size];
-    for (int i = 0; i < size; i++) {
-      names[i] = pairs.get(i).getFirst();
-      if ("access".equals(names[i])) { //guess enum constant name
-        params[i] = AccessType.PACKAGE_PRIVATE.name();
-      } else if ("change".equals(names[i])) {
-        params[i] = IncompatibleClassChangeProblem.Change.CLASS_TO_INTERFACE.name();
-      } else {
-        params[i] = "TEST_PARAMETER_" + i;
+    int i = 0;
+    List<Object> pairs = new ArrayList<Object>();
+
+    for (Constructor<?> constructor : constructors) {
+      if (constructor.getParameterTypes().length > 0) {
+        for (Class<?> aClass1 : constructor.getParameterTypes()) {
+          if (aClass1.equals(AccessType.class)) {
+            pairs.add(AccessType.PACKAGE_PRIVATE);
+          } else if (aClass1.equals(IncompatibleClassChangeProblem.Change.class)) {
+            pairs.add(IncompatibleClassChangeProblem.Change.CLASS_TO_INTERFACE);
+          } else {
+            pairs.add("TEST_PARAM_" + (++i));
+          }
+        }
+        Object instance = constructor.newInstance(pairs.toArray(new Object[0]));
+        String json = GsonHolder.INSTANCE.getGSON().toJson(instance, Problem.class);
+        Problem problem = (Problem) GsonHolder.INSTANCE.getGSON().fromJson(json, instance.getClass());
+
+        Assert.assertEquals(instance, problem);
+        return;
       }
     }
 
-    Problem instance = factory.deserialize(params);
 
-    String json = GsonHolder.INSTANCE.getGSON().toJson(instance);
-    Problem problem = GsonHolder.INSTANCE.getGSON().fromJson(json, instance.getClass());
 
-    Assert.assertEquals(instance, problem);
   }
 }
