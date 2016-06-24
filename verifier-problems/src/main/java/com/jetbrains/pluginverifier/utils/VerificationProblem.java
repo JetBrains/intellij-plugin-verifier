d72e5288d05bf2e94e16b8ecca70c3aff17a5739
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-06-24 14:38:55 +0300

    cleanup

--- verifier-problems/src/main/java/com/jetbrains/pluginverifier/utils/VerificationProblem.java
+++ verifier-problems/src/main/java/com/jetbrains/pluginverifier/utils/VerificationProblem.java
@@ -1,11 +1,8 @@
 package com.jetbrains.pluginverifier.utils;
 
 import com.jetbrains.pluginverifier.problems.Problem;
-import kotlin.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 /**
  * This problem is not saved to report-file: it's only shown on verification status page.
@@ -31,18 +28,6 @@ public class VerificationProblem extends Problem {
   @Override
   public String getDescription() {
     return String.format("%s %s: %s", "failed to verify plugin", (myPlugin != null ? myPlugin : ""), myDetails);
-  }
-
-  @NotNull
-  @Override
-  public Problem deserialize(@NotNull String... params) {
-    throw new UnsupportedOperationException("The class is not intended to be serialized");
-  }
-
-  @NotNull
-  @Override
-  public List<Pair<String, String>> serialize() {
-    throw new UnsupportedOperationException("The class is not intended to be serialized");
   }
 
 }
