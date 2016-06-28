aacc980e13f623dc61796b7efb65af304f602287
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-06-28 15:32:05 +0300

    cli commands verification throught VManager

--- verifier-cli/src/main/java/com/jetbrains/pluginverifier/commands/VerifierCommand.java
+++ verifier-cli/src/main/java/com/jetbrains/pluginverifier/commands/VerifierCommand.java
@@ -3,8 +3,12 @@ package com.jetbrains.pluginverifier.commands;
 import com.intellij.structure.domain.Ide;
 import com.intellij.structure.domain.IdeManager;
 import com.intellij.structure.domain.IdeVersion;
+import com.intellij.structure.domain.Plugin;
 import com.intellij.structure.resolvers.Resolver;
+import com.jetbrains.pluginverifier.api.*;
+import com.jetbrains.pluginverifier.results.ProblemSet;
 import com.jetbrains.pluginverifier.utils.FailUtil;
+import kotlin.Pair;
 import org.apache.commons.cli.CommandLine;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,6 +17,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -41,7 +46,7 @@ public abstract class VerifierCommand {
   public abstract int execute(@NotNull CommandLine commandLine, @NotNull List<String> freeArgs) throws Exception;
 
   @NotNull
-  protected Resolver createJdkResolver(@NotNull CommandLine commandLine) throws IOException {
+  protected File getJdkDir(@NotNull CommandLine commandLine) throws IOException {
     File runtimeDirectory;
 
     if (commandLine.hasOption('r')) {
@@ -61,14 +66,14 @@ public abstract class VerifierCommand {
       }
     }
 
-    return Resolver.createJdkResolver(runtimeDirectory);
+    return runtimeDirectory;
   }
 
-  @Nullable
+  @NotNull
   protected Resolver getExternalClassPath(CommandLine commandLine) throws IOException {
     String[] values = commandLine.getOptionValues("cp");
     if (values == null) {
-      return null;
+      return Resolver.getEmptyResolver();
     }
 
     List<Resolver> pools = new ArrayList<>(values.length);
@@ -78,6 +83,29 @@ public abstract class VerifierCommand {
     }
 
     return Resolver.createUnionResolver("External classpath resolver: " + Arrays.toString(values), pools);
+  }
+
+  @NotNull
+  protected ProblemSet verify(@NotNull Plugin plugin,
+                              @NotNull Ide ide,
+                              @NotNull Resolver ideResolver,
+                              @NotNull File jdkDir,
+                              @NotNull Resolver externalClassPath,
+                              @NotNull VOptions options) throws Exception {
+    JdkDescriptor jdkDescriptor = new JdkDescriptor.ByFile(jdkDir);
+    List<Pair<PluginDescriptor, IdeDescriptor>> pairs = Collections.singletonList(new Pair<>(new PluginDescriptor.ByInstance(plugin), new IdeDescriptor.ByInstance(ide, ideResolver)));
+
+    //the exceptions are propagated
+    VResult result = VManager.INSTANCE.verify(new VParams(jdkDescriptor, pairs, options, externalClassPath)).getResults().get(0);
+
+    if (result instanceof VResult.Problems) {
+      final ProblemSet problemSet = new ProblemSet();
+      ((VResult.Problems) result).getProblems().entries().forEach(x -> problemSet.addProblem(x.getKey(), x.getValue()));
+      return problemSet;
+    } else if (result instanceof VResult.BadPlugin) {
+      throw new IllegalArgumentException(((VResult.BadPlugin) result).getReason());
+    }
+    return new ProblemSet();
   }
 
   @Nullable
