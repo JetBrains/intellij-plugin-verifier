ea8e376ae4fd7ab0cf3c0db677b5fef924be084d
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-06-28 19:22:48 +0300

    indents

--- verifier-problems/src/main/kotlin/com/jetbrains/pluginverifier/api/VResults.kt
+++ verifier-problems/src/main/kotlin/com/jetbrains/pluginverifier/api/VResults.kt
@@ -16,7 +16,10 @@ sealed class VResult() {
   /**
    * Indicates that the Plugin doesn't have compatibility problems with the checked IDE.
    */
-  class Nice(@SerializedName("plugin") val pluginDescriptor: PluginDescriptor, @SerializedName("ide") val ideDescriptor: IdeDescriptor, @SerializedName("overview") val overview: String) : VResult() {
+  class Nice(@SerializedName("plugin") val pluginDescriptor: PluginDescriptor,
+             @SerializedName("ide") val ideDescriptor: IdeDescriptor,
+             @SerializedName("overview") val overview: String) : VResult() {
+
     override fun toString(): String {
       return "VResult.Nice(plugin=$pluginDescriptor, ide=$ideDescriptor, overview='$overview')"
     }
@@ -25,7 +28,10 @@ sealed class VResult() {
   /**
    * The Plugin has compatibility problems with the IDE. They are listed in the [problems]
    */
-  class Problems(@SerializedName("plugin") val pluginDescriptor: PluginDescriptor, @SerializedName("ide") val ideDescriptor: IdeDescriptor, @SerializedName("overview") val overview: String, @SerializedName("problems") val problems: Multimap<Problem, ProblemLocation>) : VResult() {
+  class Problems(@SerializedName("plugin") val pluginDescriptor: PluginDescriptor,
+                 @SerializedName("ide") val ideDescriptor: IdeDescriptor,
+                 @SerializedName("overview") val overview: String,
+                 @SerializedName("problems") val problems: Multimap<Problem, ProblemLocation>) : VResult() {
 
     override fun toString(): String {
       return "VResult.Problems(plugin=$pluginDescriptor, ide=$ideDescriptor, overview='$overview', problems=$problems)"
@@ -37,7 +43,8 @@ sealed class VResult() {
    * The Plugin has a completely incorrect structure (missing plugin.xml, etc.).
    * The [reason] is a user-friendly description of the problem.
    */
-  class BadPlugin(@SerializedName("plugin") val pluginDescriptor: PluginDescriptor, @SerializedName("reason") val reason: String) : VResult() {
+  class BadPlugin(@SerializedName("plugin") val pluginDescriptor: PluginDescriptor,
+                  @SerializedName("reason") val reason: String) : VResult() {
 
     override fun toString(): String {
       return "VResult.BadPlugin(plugin=$pluginDescriptor, reason='$reason')"
