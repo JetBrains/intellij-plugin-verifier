b00e7b2410ace201e6ccac5a891cea29d6ab858a
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-01 17:25:00 +0300

    fixed redundant problems "overriding final method ...": hiding the static final methods in the bytecode is permissible

---
+++ verifier-cli/mock-plugin/src/main/java/mock/plugin/noproblems/staticFinalMethodNoProblem/Derived.java
@@ -0,0 +1,11 @@
+package mock.plugin.noproblems.staticFinalMethodNoProblem;
+
+import finals.Base;
+
+/**
+ * @author Sergey Patrikeev
+ */
+public class Derived extends Base {
+  final public static void method() {
+  }
+}
