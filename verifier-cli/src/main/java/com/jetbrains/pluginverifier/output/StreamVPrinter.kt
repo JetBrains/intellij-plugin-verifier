04a46b248b63740fb28ce4c78d1a09d50fe8eb4a
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-05 00:58:59 +0300

    added a stdout printer

---
+++ verifier-cli/src/main/java/com/jetbrains/pluginverifier/output/StreamVPrinter.kt
@@ -0,0 +1,30 @@
+package com.jetbrains.pluginverifier.output
+
+import com.jetbrains.pluginverifier.api.VResult
+import com.jetbrains.pluginverifier.api.VResults
+import java.io.PrintStream
+
+class StreamVPrinter(val out: PrintStream) : VPrinter {
+
+  override fun printResults(results: VResults) {
+    results.results.forEach {
+      val ideVersion = it.ideDescriptor.ideVersion
+      val plugin = "${it.pluginDescriptor.pluginId}:${it.pluginDescriptor.version}"
+      when (it) {
+        is VResult.Nice -> out.println("With the $ideVersion the plugin $plugin is OK")
+        is VResult.Problems -> {
+          val problemsCnt = it.problems.keySet().size
+          out.println("With the $ideVersion the plugin $plugin has $problemsCnt problems:")
+          it.problems.asMap().forEach {
+            out.println("    #${it.key.description}")
+            it.value.forEach {
+              out.println("        at ${it.asString()}")
+            }
+          }
+        }
+        is VResult.BadPlugin -> out.println("With the $ideVersion it is broken ${it.overview}")
+      }
+    }
+  }
+
+}
