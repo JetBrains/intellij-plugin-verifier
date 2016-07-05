665eca5c62ddcbe0eea0733fe484fb0b367b1290
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-05 15:19:07 +0300

    parse the cli-options outside of the commands

--- verifier-cli/src/main/java/com/jetbrains/pluginverifier/PluginVerifierMain.kt
+++ verifier-cli/src/main/java/com/jetbrains/pluginverifier/PluginVerifierMain.kt
@@ -1,40 +1,46 @@
 package com.jetbrains.pluginverifier
 
-import com.jetbrains.pluginverifier.commands.CheckIdeCommand
-import com.jetbrains.pluginverifier.commands.CheckPluginCommand
-import com.jetbrains.pluginverifier.commands.NewProblemsCommand
-import com.jetbrains.pluginverifier.commands.VerifierCommand
+import com.jetbrains.pluginverifier.configurations.CheckIdeConfiguration
+import com.jetbrains.pluginverifier.configurations.CheckIdeParamsParser
+import com.jetbrains.pluginverifier.configurations.CheckPluginConfiguration
+import com.jetbrains.pluginverifier.configurations.CheckPluginParamsParser
 import com.jetbrains.pluginverifier.utils.Opts
 import com.sampullara.cli.Args
 
 object PluginVerifierMain {
 
-  val COMMAND_MAP = listOf<VerifierCommand>(CheckIdeCommand(), NewProblemsCommand(), CheckPluginCommand()).associateBy { it.name }
-
   @JvmStatic fun main(args: Array<String>) {
     val opts = Opts()
-    val freeArgs = Args.parse(opts, args)
+    var freeArgs = Args.parse(opts, args)
+
 
     if (args.isEmpty()) {
-      System.err.println("The command is not specified. Should be one of ${COMMAND_MAP.keys.joinToString()}")
+      System.err.println("The command is not specified. Should be one of 'check-plugin' or 'check-ide'")
       System.exit(1)
     }
 
-    val command = args[0]
+    val command = freeArgs[0]
+    freeArgs = freeArgs.drop(1)
 
-    val verifierCommand = COMMAND_MAP[command]
-    if (verifierCommand == null) {
-      System.err.println("Unknown command $command. Should be one of ${COMMAND_MAP.keys.joinToString()}")
-      System.exit(1)
+    when (command) {
+      "check-plugin" -> {
+        val params = CheckPluginParamsParser.parse(opts, freeArgs)
+        CheckPluginConfiguration(params).execute().printResults(System.out)
+      }
+      "check-ide" -> {
+        val params = CheckIdeParamsParser.parse(opts, freeArgs)
+        CheckIdeConfiguration(params).execute().processResults(opts)
+      }
+      else -> {
+        System.err.println("Unsupported command $command")
+        System.exit(1)
+      }
     }
-    val time = System.currentTimeMillis()
-    val exitCode = verifierCommand!!.execute(opts, freeArgs.subList(1, freeArgs.size).toList())
-    println("Completed in ${(System.currentTimeMillis() - time) / 1000} seconds")
 
     //TODO: replace exitCode0 with cli-parameter
-    if (exitCode != 0 && !java.lang.Boolean.getBoolean("exitCode0")) {
-      System.exit(exitCode)
-    }
+//    if (exitCode != 0 && !java.lang.Boolean.getBoolean("exitCode0")) {
+//      System.exit(exitCode)
+//    }
   }
 
 }
