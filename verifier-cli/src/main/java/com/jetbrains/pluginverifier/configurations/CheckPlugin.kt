fb053d3edd572068e6abcd83d2d805c218c5cd09
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-07 19:17:40 +0300

    check-plugin: revert an option to check multiple plugins specified in the file

--- verifier-cli/src/main/java/com/jetbrains/pluginverifier/configurations/CheckPlugin.kt
+++ verifier-cli/src/main/java/com/jetbrains/pluginverifier/configurations/CheckPlugin.kt
@@ -1,9 +1,8 @@
 package com.jetbrains.pluginverifier.configurations
 
-import com.intellij.structure.domain.Plugin
+import com.intellij.structure.domain.IdeVersion
 import com.intellij.structure.resolvers.Resolver
 import com.jetbrains.pluginverifier.api.*
-import com.jetbrains.pluginverifier.misc.PluginCache
 import com.jetbrains.pluginverifier.output.StreamVPrinter
 import com.jetbrains.pluginverifier.repository.RepositoryManager
 import com.jetbrains.pluginverifier.utils.CmdOpts
@@ -17,31 +16,30 @@ object CheckPluginParamsParser : ParamsParser {
 
   override fun parse(opts: CmdOpts, freeArgs: List<String>): CheckPluginParams {
     if (freeArgs.size <= 1) {
-      throw RuntimeException("You must specify plugin to check and IDE, example:\n" +
+      throw RuntimeException("You must specify plugin to check and IDE(s), example:\n" +
           "java -jar verifier.jar check-plugin ~/work/myPlugin/myPlugin.zip ~/EAPs/idea-IU-117.963\n" +
           "java -jar verifier.jar check-plugin #14986 ~/EAPs/idea-IU-117.963")
     }
-    val plugin = createPlugin(freeArgs[0])
-    val ideFiles = freeArgs.drop(1).map { File(it) }.map { CmdUtil.createIde(it, opts) }.map { IdeDescriptor.ByInstance(it) }
+    val ideDescriptors = freeArgs.drop(1).map { File(it) }.map { IdeDescriptor.ByInstance(CmdUtil.createIde(it, opts)) }
+    val pluginFiles = getPluginFiles(freeArgs[0], ideDescriptors.map { it.ideVersion })
     val jdkDescriptor = JdkDescriptor.ByFile(CmdUtil.getJdkDir(opts))
     val vOptions = VOptionsUtil.parseOpts(opts)
     val externalClasspath = CmdUtil.getExternalClassPath(opts)
-    return CheckPluginParams(plugin, ideFiles, jdkDescriptor, vOptions, externalClasspath)
+    return CheckPluginParams(pluginFiles.map { PluginDescriptor.ByFile("${it.nameWithoutExtension}", "", it) }, ideDescriptors, jdkDescriptor, vOptions, externalClasspath)
   }
 
-  fun createPlugin(pluginToTestArg: String): PluginDescriptor.ByInstance {
-    if (pluginToTestArg.matches("#\\d+".toRegex())) {
+  fun getPluginFiles(pluginToTestArg: String, ideVersions: List<IdeVersion>? = null): List<File> {
+    if (pluginToTestArg.startsWith("@")) {
+      val pluginListFile = File(pluginToTestArg.substring(1))
+      val pluginPaths = pluginListFile.readLines()
+      return ideVersions!!.map { fetchPlugins(it, pluginListFile, pluginPaths) }.flatten()
+    } else if (pluginToTestArg.matches("#\\d+".toRegex())) {
       val pluginId = pluginToTestArg.substring(1)
       try {
         val updateId = Integer.parseInt(pluginId)
         val updateInfo = RepositoryManager.getInstance().findUpdateById(updateId) ?: throw RuntimeException("No such plugin $pluginToTestArg")
         val pluginFile = RepositoryManager.getInstance().getPluginFile(updateInfo) ?: throw RuntimeException("No such plugin $pluginToTestArg")
-        try {
-          val plugin = PluginCache.createPlugin(pluginFile)
-          return PluginDescriptor.ByInstance(plugin)
-        } catch (e: Exception) {
-          throw IllegalArgumentException("The plugin $pluginFile is invalid", e)
-        }
+        return listOf(pluginFile)
       } catch (e: IOException) {
         throw RuntimeException("Cannot load plugin #" + pluginId, e)
       }
@@ -50,20 +48,38 @@ object CheckPluginParamsParser : ParamsParser {
       if (!file.exists()) {
         throw IllegalArgumentException("The file $file doesn't exist")
       }
-      val plugin: Plugin
-      try {
-        plugin = PluginCache.createPlugin(file)
-      } catch(e: Exception) {
-        throw RuntimeException("The plugin $file is invalid", e)
-      }
-      return PluginDescriptor.ByInstance(plugin)
+      return listOf(file)
     }
   }
+
+  fun fetchPlugins(ideVersion: IdeVersion, pluginListFile: File, pluginPaths: List<String>): List<File> =
+      pluginPaths
+          .map { it.trim() }
+          .filter { it.isNotEmpty() }
+          .map {
+            if (it.startsWith("id:")) {
+              downloadPluginBuilds(it.substringAfter("id:"), ideVersion)
+            } else {
+              var pluginFile = File(it)
+              if (!pluginFile.isAbsolute) {
+                pluginFile = File(pluginListFile.parentFile, it)
+              }
+              if (!pluginFile.exists()) {
+                throw RuntimeException("Plugin file '" + it + "' specified in '" + pluginListFile.absolutePath + "' doesn't exist")
+              }
+              listOf(pluginFile)
+            }
+          }.flatten()
+
+  fun downloadPluginBuilds(pluginId: String, ideVersion: IdeVersion): List<File> =
+      RepositoryManager.getInstance()
+          .getAllCompatibleUpdatesOfPlugin(ideVersion, pluginId)
+          .map { RepositoryManager.getInstance().getPluginFile(it)!! }
 
 
 }
 
-class CheckPluginParams(val pluginDescriptor: PluginDescriptor,
+class CheckPluginParams(val pluginDescriptors: List<PluginDescriptor>,
                         val ideDescriptors: List<IdeDescriptor>,
                         val jdkDescriptor: JdkDescriptor,
                         val vOptions: VOptions,
@@ -78,7 +94,7 @@ class CheckPluginResults(val vResults: VResults) : Results {
 class CheckPluginConfiguration(val params: CheckPluginParams) : Configuration {
 
   override fun execute(): CheckPluginResults {
-    val pluginsToCheck = params.ideDescriptors.map { params.pluginDescriptor to it }
+    val pluginsToCheck = params.pluginDescriptors.map { p -> params.ideDescriptors.map { p to it } }.flatten()
     val vParams = VParams(params.jdkDescriptor, pluginsToCheck, params.vOptions, params.externalClasspath)
     val vResults = VManager.verify(vParams)
 
