2b5ed8a12d03987005107b8810b4faaae441fdcc
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-27 12:42:18 +0300

    CheckTrunkApi: don't verify the major IDE build if there is already a check report for this build on the server (performance optimization)

--- plugins-verifier-service/server/src/main/kotlin/org/jetbrains/plugins/verifier/service/runners/CheckTrunkApiRunner.kt
+++ plugins-verifier-service/server/src/main/kotlin/org/jetbrains/plugins/verifier/service/runners/CheckTrunkApiRunner.kt
@@ -17,6 +17,7 @@ import org.jetbrains.plugins.verifier.service.params.CheckTrunkApiRunnerParams
 import org.jetbrains.plugins.verifier.service.results.CheckTrunkApiResults
 import org.jetbrains.plugins.verifier.service.storage.IdeFilesManager
 import org.jetbrains.plugins.verifier.service.storage.JdkManager
+import org.jetbrains.plugins.verifier.service.storage.ReportsManager
 import org.jetbrains.plugins.verifier.service.util.deleteLogged
 import org.slf4j.LoggerFactory
 import java.io.File
@@ -41,42 +42,33 @@ class CheckTrunkApiRunner(val ideFile: File,
       } catch (e: Exception) {
         throw IllegalArgumentException("The supplied IDE $ideFile is broken", e)
       }
-      val majorBuildLock: IdeFilesManager.IdeLock? = firstMajorBuild(ide.version.baselineVersion)
-      if (majorBuildLock == null) {
+
+      val pluginsToCheck: List<PluginDescriptor>
+      try {
+        pluginsToCheck = RepositoryManager
+            .getInstance()
+            .getLastCompatibleUpdates(ide.version)
+            .map { PluginDescriptor.ByUpdateInfo(it.pluginId ?: "", it.version ?: "", it) }
+      } catch(e: Exception) {
+        throw RuntimeException("Unable to fetch list of plugins compatible with ${ide.version}", e)
+      }
+
+      val jdkDescriptor = JdkDescriptor.ByFile(JdkManager.getJdkHome(runnerParams.jdkVersion))
+
+      val majorVersion = IdeFilesManager.ideList().filter { it.baselineVersion == ide.version.baselineVersion }.sorted().firstOrNull()
+      if (majorVersion == null) {
         val msg = "There is no major IDE update on the Server with which to compare check results"
         LOG.error(msg)
         throw IllegalStateException(msg)
       }
 
-      try {
-        val pluginsToCheck: List<PluginDescriptor> = RepositoryManager
-            .getInstance()
-            .getLastCompatibleUpdates(majorBuildLock.ide.version)
-            .map { PluginDescriptor.ByUpdateInfo(it.pluginId ?: "", it.version ?: "", it) }
+      val majorReport = getExistingReport(majorVersion) ?: calculateMajorReport(majorVersion, jdkDescriptor, pluginsToCheck)
+      val currentReport = calculateIdeReport(ide, jdkDescriptor, pluginsToCheck)
 
-        val jdkDescriptor = JdkDescriptor.ByFile(JdkManager.getJdkHome(runnerParams.jdkVersion))
-        val majorDescriptor = IdeDescriptor.ByInstance(majorBuildLock.ide)
-        val currentDescriptor = IdeDescriptor.ByInstance(ide)
+      ReportsManager.saveReport(majorReport)
+      ReportsManager.saveReport(currentReport)
 
-        val majorParams = CheckIdeParams(majorDescriptor, jdkDescriptor, pluginsToCheck, ImmutableMultimap.of(), runnerParams.vOptions)
-        val currentParams = majorParams.copy(ideDescriptor = currentDescriptor)
-
-        LOG.debug("${presentableName()} major arguments: $majorParams, current arguments: $currentParams")
-
-        try {
-          val majorResults = CheckIdeConfiguration(majorParams).execute()
-          val currentResults = CheckIdeConfiguration(currentParams).execute()
-          return CheckTrunkApiResults(CheckIdeReport.createReport(majorResults.ideVersion, majorResults.vResults), CheckIdeReport.createReport(currentResults.ideVersion, currentResults.vResults))
-        } catch (ie: InterruptedException) {
-          throw ie
-        } catch (e: Exception) {
-          LOG.error("Failed to verify plugins", e)
-          throw e
-        }
-
-      } finally {
-        majorBuildLock.release()
-      }
+      return CheckTrunkApiResults(majorReport, currentReport)
     } finally {
       if (deleteOnCompletion) {
         ideFile.deleteLogged()
@@ -84,17 +76,36 @@ class CheckTrunkApiRunner(val ideFile: File,
     }
   }
 
-
-  private fun firstMajorBuild(baselineNumber: Int): IdeFilesManager.IdeLock? {
-    val major: IdeVersion? = IdeFilesManager
-        .ideList()
-        .filter { it.baselineVersion == baselineNumber }
-        .sorted()
-        .firstOrNull()
-    if (major != null) {
-      return IdeFilesManager.getIde(major)
+  private fun calculateIdeReport(ide: Ide, jdkDescriptor: JdkDescriptor.ByFile, pluginsToCheck: List<PluginDescriptor>): CheckIdeReport {
+    try {
+      val currentParams = CheckIdeParams(IdeDescriptor.ByInstance(ide), jdkDescriptor, pluginsToCheck, ImmutableMultimap.of(), runnerParams.vOptions)
+      LOG.debug("${presentableName()} current arguments: $currentParams")
+      return CheckIdeConfiguration(currentParams).execute().run { CheckIdeReport.createReport(ideVersion, vResults) }
+    } catch (ie: InterruptedException) {
+      throw ie
+    } catch (e: Exception) {
+      LOG.error("Failed to verify plugins", e)
+      throw e
     }
-    return null
   }
+
+  private fun calculateMajorReport(majorVersion: IdeVersion, jdkDescriptor: JdkDescriptor.ByFile, pluginsToCheck: List<PluginDescriptor>): CheckIdeReport {
+    val majorBuildLock: IdeFilesManager.IdeLock = IdeFilesManager.getIde(majorVersion)!!
+    try {
+      val majorParams = CheckIdeParams(IdeDescriptor.ByInstance(majorBuildLock.ide), jdkDescriptor, pluginsToCheck, ImmutableMultimap.of(), runnerParams.vOptions)
+      LOG.debug("${presentableName()} major arguments: $majorParams")
+      return CheckIdeConfiguration(majorParams).execute().run { CheckIdeReport.createReport(ideVersion, vResults) }
+    } catch (ie: InterruptedException) {
+      throw ie
+    } catch (e: Exception) {
+      LOG.error("Failed to verify major IDE", e)
+      throw e
+    } finally {
+      majorBuildLock.release()
+    }
+  }
+
+
+  private fun getExistingReport(ideVersion: IdeVersion): CheckIdeReport? = ReportsManager.getReport(ideVersion)?.run { CheckIdeReport.loadFromFile(this) }
 
 }
