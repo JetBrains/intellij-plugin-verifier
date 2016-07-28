2556222a94cb46f66639ea0cb2064264e1c411e6
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-28 23:14:05 +0300

    delete the plugin extracted files if exception

--- intellij-plugin-structure/src/main/java/com/intellij/structure/impl/resolvers/PluginResolver.java
+++ intellij-plugin-structure/src/main/java/com/intellij/structure/impl/resolvers/PluginResolver.java
@@ -63,7 +63,12 @@ public class PluginResolver extends Resolver {
     if (file.isDirectory() || PluginManagerImpl.isJarOrZip(file)) {
       if (StringUtil.endsWithIgnoreCase(file.getName(), ".zip")) {
         File extracted = PluginExtractor.extractPlugin(file);
-        return new PluginResolver(plugin, extracted, true);
+        try {
+          return new PluginResolver(plugin, extracted, true);
+        } catch (RuntimeException e) {
+          FileUtils.deleteQuietly(extracted);
+          throw e;
+        }
       }
       return new PluginResolver(plugin, file, false);
     }
