eeee0b00b2d546bee405ea4edb249fe322d6fd6e
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-31 01:22:14 +0300

    purified logback configuration

--- plugins-verifier-service/server/grails-app/conf/logback.groovy
+++ plugins-verifier-service/server/grails-app/conf/logback.groovy
@@ -1,13 +1,14 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.filter.ThresholdFilter
 import ch.qos.logback.core.ConsoleAppender
-import ch.qos.logback.core.FileAppender
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy
 import ch.qos.logback.core.status.OnConsoleStatusListener
-import grails.util.BuildSettings
-import grails.util.Environment
+import ch.qos.logback.core.util.FileSize
 
 statusListener OnConsoleStatusListener
 
-String timedPattern = "%d{yyyy-MM-dd'T'HH:mm:ss} [%thread] %-5level %logger{36} - %msg%n"
+String timedPattern = "%20(%d{yyyy-MM-dd'T'HH:mm:ss}) %-22([%thread]) %-5level %-30.30(%logger{0}:%method) - %msg%n"
 
 //http://logback.qos.ch/manual/groovy.html
 appender('STDOUT', ConsoleAppender) {
@@ -16,33 +17,56 @@ appender('STDOUT', ConsoleAppender) {
   }
 }
 
-appender('FILE', FileAppender) {
-  def home = System.getProperty('verifierService.homeDirectory')
-  def logName = "log"
-  file = "$home/logs/$logName"
+def home = System.getProperty('verifierService.homeDirectory')
+if (!home) {
+  throw new RuntimeException("The property 'verifierService.homeDirectory' is not set")
+}
+appender('FILE', RollingFileAppender) {
+  //the logger is always logging into this file (but rollovers the files according to fileNamePattern)
+  file = "$home/log/verifier.log"
+
+  rollingPolicy(SizeAndTimeBasedRollingPolicy) {
+    fileNamePattern = "$home/log/verifier/%d{MM/dd, aux}/verifier.%d{yyyy-MM-dd_HH}.%i.log"
+    //keep logs for 30 days
+    maxHistory = 30 * 24
+    maxFileSize = FileSize.valueOf("100MB")
+    totalSizeCap = FileSize.valueOf("2GB")
+  }
+
   append = true
   encoder(PatternLayoutEncoder) {
     pattern = timedPattern
   }
+
 }
+
+appender('ERRORS_FILE', RollingFileAppender) {
+  file = "$home/log/errors.log"
+  rollingPolicy(SizeAndTimeBasedRollingPolicy) {
+    fileNamePattern = "$home/log/errors/%d{MM/dd, aux}/errors.%d{yyyy-MM-dd}.%i.log"
+    maxHistory = 30
+    maxFileSize = FileSize.valueOf("100MB")
+    totalSizeCap = FileSize.valueOf("2GB")
+  }
+  append = true
+  encoder(PatternLayoutEncoder) {
+    pattern = timedPattern
+  }
+  filter(ThresholdFilter) {
+    level = WARN
+  }
+
+}
+
+boolean isProduction = 'true' == System.getProperty('verifierService.productionMode')
+def loggers = ['STDOUT', 'ERRORS_FILE', 'FILE']
 
 logger("com.jetbrains.pluginverifier.misc.DownloadManager", OFF)
-logger("com.intellij.structure", ERROR, ['STDOUT'])
-logger("com.jetbrains.pluginverifier.api.VManager", TRACE, ['STDOUT'], false)
-logger("com.jetbrains.pluginverifier", INFO, ['STDOUT'])
-logger("grails.app.controllers", DEBUG, ['STDOUT'], false)
-logger("grails.app.services", DEBUG, ['STDOUT'], false)
+logger("com.intellij.structure", ERROR, loggers, false)
+logger("org.jetbrains.plugins.verifier.service", isProduction ? INFO : TRACE, loggers, false)
+logger("com.jetbrains.pluginverifier.api.VManager", isProduction ? INFO : TRACE, loggers, false)
+logger("com.jetbrains.pluginverifier", isProduction ? INFO : TRACE, loggers, false)
+logger("grails.app.controllers", isProduction ? INFO : DEBUG, loggers, false)
+logger("grails.app.services", isProduction ? INFO : DEBUG, loggers, false)
 
-root(ERROR, ['STDOUT'])
-
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir) {
-  appender("FULL_STACKTRACE", FileAppender) {
-    file = "${targetDir}/stacktrace.log"
-    append = true
-    encoder(PatternLayoutEncoder) {
-      pattern = timedPattern
-    }
-  }
-  logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
-}
+root(WARN, loggers)
