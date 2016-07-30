34cc47467bb7c9acaaf0478cd1c5d16045b881b3
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-30 18:18:54 +0300

    remove inaccurate return type

--- plugins-verifier-service/rest/src/main/kotlin/org/jetbrains/plugins/verifier/service/client/util/ArchiverUtil.kt
+++ plugins-verifier-service/rest/src/main/kotlin/org/jetbrains/plugins/verifier/service/client/util/ArchiverUtil.kt
@@ -1,4 +1,4 @@
-package com.jetbrains.pluginverifier.client.util
+package org.jetbrains.plugins.verifier.service.client.util
 
 import org.codehaus.plexus.archiver.zip.ZipArchiver
 import java.io.File
@@ -7,12 +7,11 @@ import java.io.File
  * @author Sergey Patrikeev
  */
 object ArchiverUtil {
-  fun archiveDirectory(dir: File, destFile: File): File {
+  fun archiveDirectory(dir: File, destFile: File) {
     val archiver = ZipArchiver()
     archiver.destFile = destFile
     archiver.addDirectory(dir)
     archiver.createArchive()
-    return destFile
   }
 
 }
