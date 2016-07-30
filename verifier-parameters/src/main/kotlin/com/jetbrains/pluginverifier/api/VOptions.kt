fa4ea39b79807f4fe9bf51b0ba4f3360a22e6b1b
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-30 13:52:33 +0300

    merge VParams and VOptions into one kt file

--- verifier-parameters/src/main/kotlin/com/jetbrains/pluginverifier/api/VOptions.kt
+++ verifier-parameters/src/main/kotlin/com/jetbrains/pluginverifier/api/VOptions.kt
@@ -4,8 +4,78 @@ import com.google.common.collect.Multimap
 import com.google.gson.annotations.SerializedName
 import com.intellij.structure.domain.Plugin
 import com.intellij.structure.impl.utils.StringUtil
+import com.intellij.structure.resolvers.Resolver
 import com.jetbrains.pluginverifier.problems.Problem
 import java.util.regex.Pattern
+
+interface VProgress {
+  fun getProgress(): Double
+
+  fun setProgress(value: Double)
+
+  fun getText(): String
+
+  fun setText(text: String)
+}
+
+class DefaultVProgress() : VProgress {
+  @Volatile private var progress: Double = 0.0
+
+  @Volatile private var text: String = ""
+
+  override fun getProgress(): Double = progress
+
+  override fun setProgress(value: Double) {
+    progress = value
+  }
+
+  override fun getText(): String = text
+
+  override fun setText(text: String) {
+    this.text = text
+  }
+
+}
+
+/**
+ * Accumulates parameters of the upcoming verification.
+ */
+data class VParams(
+
+    /**
+     * The JDK against which the plugins will be verified.
+     */
+    val jdkDescriptor: JdkDescriptor,
+
+    /**
+     * The pairs of _(plugin, ide)_ which will be verified.
+     */
+    val pluginsToCheck: List<Pair<PluginDescriptor, IdeDescriptor>>,
+
+    /**
+     * The options for the Verifier (excluded problems, etc).
+     */
+    val options: VOptions,
+
+    /**
+     * The Resolver for external classes. The verification can refer to them.
+     */
+    val externalClassPath: Resolver = Resolver.getEmptyResolver(),
+
+    /**
+     * If set to true the plugins can refer other plugins withing the verification.
+     * It's used to check several plugins with dependencies between them.
+     */
+    val resolveDependenciesWithin: Boolean = false
+) {
+  override fun toString(): String {
+    val todo: Map<IdeDescriptor, List<PluginDescriptor>> = pluginsToCheck.groupBy { it.second }.mapValues { it.value.map { it.first } }
+    return "Jdk = ${jdkDescriptor.presentableName()}; " +
+        "(IDE -> [Plugins]) = [${todo.entries.joinToString { "${it.key.presentableName()} -> [${it.value.joinToString { it.presentableName() }}]" }}]; " +
+        "vOptions: $options; "
+  }
+}
+
 
 /**
  * @author Sergey Patrikeev
