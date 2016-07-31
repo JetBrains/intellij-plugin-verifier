fa6a4dfb7e6c1f666172ea1faffa3157b4737b90
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-31 14:01:17 +0300

    rewrite RepositoryManager properly

--- verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/RepositoryManager.java
+++ verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/RepositoryManager.java
@@ -1,23 +1,24 @@
 package com.jetbrains.pluginverifier.repository;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.intellij.structure.domain.IdeVersion;
 import com.jetbrains.pluginverifier.format.UpdateInfo;
+import com.jetbrains.pluginverifier.misc.DownloadManager;
 import com.jetbrains.pluginverifier.misc.RepositoryConfiguration;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.annotation.ThreadSafe;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.lang.reflect.Type;
 import java.net.URL;
-import java.util.ArrayList;
+import java.net.URLEncoder;
 import java.util.List;
-import java.util.StringTokenizer;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 /**
  * @author Sergey Evdokimov
@@ -25,104 +26,70 @@ import java.util.concurrent.ConcurrentMap;
 @ThreadSafe
 public class RepositoryManager implements PluginRepository {
 
+  private final static Logger LOG = LoggerFactory.getLogger(RepositoryManager.class);
+
+  private static final Type updateListType = new TypeToken<List<UpdateInfo>>() {
+  }.getType();
+
   private static final RepositoryManager INSTANCE = new RepositoryManager();
 
-  private final ImmutableList<PluginRepository> repositories;
-
-  private final ConcurrentMap<UpdateInfo, PluginRepository> update2repository = new ConcurrentHashMap<>();
-
-  private RepositoryManager() {
-
-    List<PluginRepository> reps = new ArrayList<>();
-    reps.add(new GlobalRepository(RepositoryConfiguration.getInstance().getPluginRepositoryUrl()));
-
-    String customRepositories = RepositoryConfiguration.getInstance().getCustomRepositories();
-    if (customRepositories != null && !customRepositories.isEmpty()) {
-      for (StringTokenizer tokenizer = new StringTokenizer(customRepositories, ","); tokenizer.hasMoreTokens(); ) {
-        String repositoryUrl = tokenizer.nextToken().trim();
-        try {
-          reps.add(new CustomRepository(new URL(repositoryUrl)));
-        } catch (MalformedURLException e) {
-          throw new RuntimeException("Unable to connect custom repository by " + repositoryUrl, e);
-        }
-      }
-    }
-
-    repositories = ImmutableList.copyOf(reps);
+  @NotNull
+  public static PluginRepository getInstance() {
+    return INSTANCE;
   }
 
-  public static RepositoryManager getInstance() {
-    return INSTANCE;
+  private String getRepoUrl() {
+    return RepositoryConfiguration.getInstance().getPluginRepositoryUrl();
   }
 
   @NotNull
   @Override
   public List<UpdateInfo> getLastCompatibleUpdates(@NotNull IdeVersion ideVersion) throws IOException {
-    List<UpdateInfo> res = new ArrayList<>();
+    LOG.debug("Loading list of plugins compatible with " + ideVersion + "... ");
 
-    for (PluginRepository repository : repositories) {
-      List<UpdateInfo> updates = repository.getLastCompatibleUpdates(ideVersion);
-      for (UpdateInfo update : updates) {
-        update2repository.put(update, repository);
-      }
+    URL url = new URL(getRepoUrl() + "/manager/allCompatibleUpdates/?build=" + ideVersion);
 
-      res.addAll(updates);
-    }
-
-    return res;
-  }
-
-  @NotNull
-  @Override
-  public List<UpdateInfo> getAllCompatibleUpdatesOfPlugin(@NotNull IdeVersion ideVersion, @NotNull String pluginId) throws IOException {
-    List<UpdateInfo> res = new ArrayList<>();
-
-    for (PluginRepository repository : repositories) {
-      List<UpdateInfo> updates = repository.getAllCompatibleUpdatesOfPlugin(ideVersion, pluginId);
-      for (UpdateInfo update : updates) {
-        update2repository.put(update, repository);
-      }
-
-      res.addAll(updates);
-    }
-
-    return res;
-  }
-
-
-  @Override
-  @Nullable
-  public File getPluginFile(@NotNull UpdateInfo update) throws IOException {
-    PluginRepository repository = update2repository.get(update);
-    Preconditions.checkArgument(repository != null, "Unknown update " + update + ", it should be registered in the RepositoryManager");
-    return repository.getPluginFile(update);
-  }
-
-
-  @Override
-  @Nullable
-  public UpdateInfo findUpdateById(int updateId) throws IOException {
-    for (PluginRepository repository : repositories) {
-      UpdateInfo update = repository.findUpdateById(updateId);
-      if (update != null) {
-        update2repository.put(update, repository);
-        return update;
-      }
-    }
-    return null;
+    return new Gson().fromJson(IOUtils.toString(url), updateListType);
   }
 
   @Nullable
   @Override
   public UpdateInfo getLastCompatibleUpdateOfPlugin(@NotNull IdeVersion ideVersion, @NotNull String pluginId) throws IOException {
-    for (PluginRepository repository : repositories) {
-      UpdateInfo update = repository.getLastCompatibleUpdateOfPlugin(ideVersion, pluginId);
-      if (update != null) {
-        update2repository.put(update, repository);
-        return update;
+    LOG.debug("Fetching last compatible update of plugin {} with ide {}", pluginId, ideVersion);
+
+    //search the given number in the all compatible updates
+    List<UpdateInfo> all = getAllCompatibleUpdatesOfPlugin(ideVersion, pluginId);
+    UpdateInfo result = null;
+    for (UpdateInfo info : all) {
+      if (result == null || result.getUpdateId() < info.getUpdateId()) {
+        result = info;
       }
     }
-    return null;
+
+    return result;
+  }
+
+  @NotNull
+  @Override
+  public List<UpdateInfo> getAllCompatibleUpdatesOfPlugin(@NotNull IdeVersion ideVersion, @NotNull String pluginId) throws IOException {
+    LOG.debug("Fetching list of all compatible builds of a pluginId " + pluginId + " on IDE " + ideVersion);
+
+    String urlSb = getRepoUrl() + "/manager/originalCompatibleUpdatesByPluginIds/?build=" + ideVersion +
+        "&pluginIds=" + URLEncoder.encode(pluginId, "UTF-8");
+
+    return new Gson().fromJson(IOUtils.toString(new URL(urlSb)), updateListType);
+  }
+
+  @Nullable
+  @Override
+  public File getPluginFile(@NotNull UpdateInfo update) throws IOException {
+    return getPluginFile(update.getUpdateId());
+  }
+
+  @Nullable
+  @Override
+  public File getPluginFile(int updateId) throws IOException {
+    return DownloadManager.getInstance().getOrLoadUpdate(updateId);
   }
 
 }
