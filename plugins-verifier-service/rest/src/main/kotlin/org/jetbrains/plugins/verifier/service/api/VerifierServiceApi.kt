9bdeb7b7476ea3cb329335a7eb50cc68a356842b
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-31 20:19:50 +0300

    added a check that the client is supported by the addressing server

--- plugins-verifier-service/rest/src/main/kotlin/org/jetbrains/plugins/verifier/service/api/VerifierServiceApi.kt
+++ plugins-verifier-service/rest/src/main/kotlin/org/jetbrains/plugins/verifier/service/api/VerifierServiceApi.kt
@@ -1,5 +1,25 @@
 package org.jetbrains.plugins.verifier.service.api
 
-interface VerifierServiceApi<out T> {
-  fun execute(): T
+import org.jetbrains.plugins.verifier.service.client.CLIENT_SIDE_VERSION
+import org.jetbrains.plugins.verifier.service.client.VerifierService
+import org.jetbrains.plugins.verifier.service.client.executeSuccessfully
+
+abstract class VerifierServiceApi<out T>(val host: String) {
+  internal val service = VerifierService(host)
+
+  abstract fun executeImpl(): T
+
+  fun execute(): T {
+    val supported: List<String>
+    try {
+      supported = service.statusService.getSupportedClients().executeSuccessfully().body()
+    } catch(e: Exception) {
+      throw RuntimeException("Plugin verifier service $host is not available", e)
+    }
+    if (CLIENT_SIDE_VERSION !in supported) {
+      throw IllegalStateException("Please update your rest-client distribution from version $CLIENT_SIDE_VERSION to ${supported.last()}")
+    }
+    return executeImpl()
+  }
+
 }
