3c3137b42ab4e6522c4d2d422d3e78bbc20633e5
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-31 20:19:20 +0300

    added request of supported client versions

--- plugins-verifier-service/server/grails-app/controllers/org/jetbrains/plugins/verifier/service/StatusController.groovy
+++ plugins-verifier-service/server/grails-app/controllers/org/jetbrains/plugins/verifier/service/StatusController.groovy
@@ -1,6 +1,26 @@
 package org.jetbrains.plugins.verifier.service
 
+import com.google.common.collect.ImmutableList
+import com.jetbrains.pluginverifier.persistence.GsonHolder
+
 class StatusController {
 
+  private static final def SUPPORTED_CLIENTS = ImmutableList.of("1.0")
+
   def index() {}
+
+  def supportedClients() {
+    sendJson(SUPPORTED_CLIENTS)
+  }
+
+  private sendJson(Object obj) {
+    String json
+    if (obj instanceof String) {
+      json = obj as String
+    } else {
+      json = GsonHolder.GSON.toJson(obj)
+    }
+    render(contentType: 'text/json', encoding: 'utf-8', text: json)
+  }
+
 }
