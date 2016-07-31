c9f369056dd3248d9aa283f0c460f6b60dc36851
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-07-31 14:04:06 +0300

    DownloadManager exception handling + logs + cleanUp + tests

--- verifier-repository/src/main/java/com/jetbrains/pluginverifier/misc/DownloadManager.java
+++ verifier-repository/src/main/java/com/jetbrains/pluginverifier/misc/DownloadManager.java
@@ -1,24 +1,19 @@
 package com.jetbrains.pluginverifier.misc;
 
-import com.google.common.base.Throwables;
-import com.google.common.net.HttpHeaders;
-import com.jetbrains.pluginverifier.format.UpdateInfo;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.http.annotation.ThreadSafe;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -32,7 +27,6 @@ public class DownloadManager {
 
   private static final int BROKEN_ZIP_THRESHOLD = 200;
   private static final DateFormat httpDateFormat;
-  private static final int HTTP_OK_STATUS = 200;
   private static final DownloadManager INSTANCE = new DownloadManager();
 
   static {
@@ -60,58 +54,9 @@ public class DownloadManager {
     return downloadDir;
   }
 
-  private void updateFile(URL url, File file) throws IOException {
-    long lastModified = file.lastModified();
-
-    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-    if (lastModified > 0) {
-      connection.addRequestProperty(HttpHeaders.IF_MODIFIED_SINCE, httpDateFormat.format(new Date(lastModified)));
-      connection.addRequestProperty(HttpHeaders.CACHE_CONTROL, "max-age=0");
-    }
-
-    int responseCode = connection.getResponseCode();
-
-    try {
-      if (responseCode == HTTP_OK_STATUS) {
-        String lastModifiedResStr = connection.getHeaderField(HttpHeaders.LAST_MODIFIED);
-        if (lastModifiedResStr == null) {
-          throw new IOException(HttpHeaders.LAST_MODIFIED + " header can not be null");
-        }
-
-        Date lastModifiedRes;
-
-        try {
-          lastModifiedRes = httpDateFormat.parse(lastModifiedResStr);
-        } catch (ParseException e) {
-          throw Throwables.propagate(e);
-        }
-
-        FileUtils.copyInputStreamToFile(connection.getInputStream(), file);
-
-        //noinspection ResultOfMethodCallIgnored
-        file.setLastModified(lastModifiedRes.getTime());
-
-      } else
-        //noinspection StatementWithEmptyBody
-        if (responseCode == 304) { /* Not modified */
-
-        } else {
-          throw new IOException("Failed to download check result: " + responseCode);
-        }
-    } finally {
-      connection.disconnect();
-    }
-  }
-
   @NotNull
-  private String getCacheFileName(UpdateInfo update) {
-    if (update.getUpdateId() != null) {
-      return update.getUpdateId() + ".zip";
-    } else {
-      String updateAndVersion = update.getPluginId() + ":" + update.getVersion();
-      return (updateAndVersion + '_' + Integer.toHexString(updateAndVersion.hashCode()) + ".zip").replaceAll("[^a-zA-Z0-9_\\-.]+", "_");
-    }
+  private String getCacheFileName(int updateId) {
+    return updateId + ".zip";
   }
 
   /**
@@ -136,39 +81,28 @@ public class DownloadManager {
     return url;
   }
 
-  public boolean doesUpdateExist(@NotNull UpdateInfo updateInfo, @NotNull URL url) throws IOException {
-    File cachedFile = new File(getOrCreateDownloadDir(), getCacheFileName(updateInfo));
-    if (cachedFile.exists() && cachedFile.length() > BROKEN_ZIP_THRESHOLD) {
-      //file exists in the cache
-      return true;
-    }
-
-    url = getFinalUrl(url);
-    try {
-      InputStream stream = null;
-      try {
-        stream = url.openStream();
-        return stream.read() != -1;
-      } finally {
-        IOUtils.closeQuietly(stream);
-      }
-    } catch (IOException e) {
-      return false;
-    }
+  @NotNull
+  private URL getUrlForUpdate(int updateId) throws MalformedURLException {
+    return new URL(RepositoryConfiguration.getInstance().getPluginRepositoryUrl() + "/plugin/download/?noStatistic=true&updateId=" + updateId);
   }
 
-  @NotNull
-  public File getOrLoadUpdate(@NotNull UpdateInfo update, @NotNull URL url) throws IOException {
+  @Nullable
+  public File getOrLoadUpdate(int updateId) throws IOException {
     File downloadDir = getOrCreateDownloadDir();
 
-    url = getFinalUrl(url);
+    URL url = getUrlForUpdate(updateId);
+    try {
+      url = getFinalUrl(url);
+    } catch (IOException e) {
+      throw new IOException("The repository " + url.getHost() + " problems", e);
+    }
 
-    File pluginInCache = new File(downloadDir, getCacheFileName(update));
+    File pluginInCache = new File(downloadDir, getCacheFileName(updateId));
 
     if (!pluginInCache.exists() || pluginInCache.length() < BROKEN_ZIP_THRESHOLD) {
       File currentDownload = File.createTempFile("currentDownload", ".zip", downloadDir);
 
-//      LOG.info("Downloading " + update + "... ");
+      LOG.debug("Downloading {} by url {}... ", updateId, url);
 
       boolean downloadFail = true;
       try {
@@ -178,35 +112,52 @@ public class DownloadManager {
           throw new IOException("Broken zip archive");
         }
 
-//        LOG.info("downloading " + update + " done!");
+        LOG.debug("Plugin {} is downloaded", updateId);
         downloadFail = false;
-      } catch (IOException e) {
-        LOG.error("Error loading plugin " + update + " by " + url.toExternalForm(), e);
-        throw e;
+      } catch (Exception e) {
+        LOG.error("Error loading plugin " + updateId + " by " + url.toExternalForm(), e);
+        return null;
       } finally {
         if (downloadFail) {
-          if (currentDownload.exists()) {
-            try {
-              FileUtils.forceDelete(currentDownload);
-            } catch (Exception ce) {
-              LOG.error("Unable to delete temporary file " + currentDownload, ce);
-            }
-          }
+          deleteLogged(currentDownload);
         }
       }
 
       //provides the thread safety while multiple threads attempt to load the same plugin.
       synchronized (this) {
-        if (pluginInCache.exists()) {
-          //remove the old (possible broken plugin)
-          FileUtils.forceDelete(pluginInCache);
+        try {
+          if (pluginInCache.exists()) {
+            //remove the old (possibly broken plugin)
+            FileUtils.forceDelete(pluginInCache);
+          }
+        } catch (Exception e) {
+          LOG.error("Unable to delete cached plugin file " + pluginInCache, e);
+          deleteLogged(currentDownload);
+          throw e;
         }
-        FileUtils.moveFile(currentDownload, pluginInCache);
+        try {
+          FileUtils.moveFile(currentDownload, pluginInCache);
+        } catch (Exception e) {
+          LOG.error("Unable to move downloaded plugin file " + currentDownload + " to " + pluginInCache, e);
+          deleteLogged(currentDownload);
+          deleteLogged(pluginInCache);
+          throw e;
+        }
       }
 
     }
 
     return pluginInCache;
+  }
+
+  private void deleteLogged(File file) {
+    if (file.exists()) {
+      try {
+        FileUtils.forceDelete(file);
+      } catch (Exception ce) {
+        LOG.error("Unable to delete file " + file, ce);
+      }
+    }
   }
 
 }
--- verifier-repository/src/test/java/com/jetbrains/pluginverifier/results/TestRepositoryManager.java
+++ verifier-repository/src/test/java/com/jetbrains/pluginverifier/results/TestRepositoryManager.java
@@ -1,7 +1,9 @@
 package com.jetbrains.pluginverifier.results;
 
+import com.intellij.structure.domain.IdeVersion;
 import com.jetbrains.pluginverifier.format.UpdateInfo;
 import com.jetbrains.pluginverifier.repository.RepositoryManager;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import java.io.File;
@@ -12,19 +14,35 @@ import static org.junit.Assert.*;
  * @author Sergey Patrikeev
  */
 public class TestRepositoryManager {
+
+  @Test
+  public void updatesOfPlugin() throws Exception {
+    assertTrue(RepositoryManager.getInstance().getAllCompatibleUpdatesOfPlugin(getIdeVersion(), "org.jetbrains.kotlin").size() > 0);
+  }
+
+  @Test
+  public void lastUpdate() throws Exception {
+    UpdateInfo info = RepositoryManager.getInstance().getLastCompatibleUpdateOfPlugin(getIdeVersion(), "org.jetbrains.kotlin");
+    assertNotNull(info);
+    assertTrue(info.getUpdateId() > 20000);
+  }
+
+  @NotNull
+  private IdeVersion getIdeVersion() {
+    return IdeVersion.createIdeVersion("IU-162.1132.10");
+  }
+
   @Test
   public void downloadNonExistentPlugin() throws Exception {
-    UpdateInfo id = RepositoryManager.getInstance().findUpdateById(-1000);
+    File id = RepositoryManager.getInstance().getPluginFile(-1000);
     assertNull(id);
   }
 
   @Test
   public void downloadExistentPlugin() throws Exception {
-    UpdateInfo info = RepositoryManager.getInstance().findUpdateById(25128); //.gitignore 1.3.3
+    File info = RepositoryManager.getInstance().getPluginFile(25128); //.gitignore 1.3.3
     assertNotNull(info);
-    File file = RepositoryManager.getInstance().getPluginFile(info);
-    assertNotNull(file);
-    assertTrue(file.length() > 0);
+    assertTrue(info.length() > 0);
   }
 
 }
