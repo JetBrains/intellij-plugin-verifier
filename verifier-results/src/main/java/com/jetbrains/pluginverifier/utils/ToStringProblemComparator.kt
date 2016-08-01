b0f589176f732693ca1b17d2bdec0a3d1463d233
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-08-01 19:51:16 +0300

    cleanUp unused classes

--- verifier-results/src/main/java/com/jetbrains/pluginverifier/utils/ToStringProblemComparator.kt
+++
@@ -1,9 +0,0 @@
-package com.jetbrains.pluginverifier.utils
-
-import com.jetbrains.pluginverifier.problems.Problem
-
-class ToStringProblemComparator : ToStringCachedComparator<Problem>() {
-  override fun toString(`object`: Problem): String {
-    return `object`.description
-  }
-}
