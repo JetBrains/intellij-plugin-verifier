7dfa127eec6928948eb560de3e9712151f1d1925
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-08-16 23:03:23 +0300

    proper classes resolution

--- verifier-core/src/main/java/com/jetbrains/pluginverifier/utils/VerifierUtil.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/utils/VerifierUtil.kt
@@ -1,8 +1,11 @@
 package com.jetbrains.pluginverifier.utils
 
-import com.google.common.base.Preconditions
 import com.intellij.structure.resolvers.Resolver
 import com.jetbrains.pluginverifier.api.VContext
+import com.jetbrains.pluginverifier.location.ProblemLocation
+import com.jetbrains.pluginverifier.problems.AccessType
+import com.jetbrains.pluginverifier.problems.ClassNotFoundProblem
+import com.jetbrains.pluginverifier.problems.IllegalClassAccessProblem
 import com.jetbrains.pluginverifier.warnings.Warning
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.ClassNode
@@ -10,28 +13,66 @@ import org.objectweb.asm.tree.FieldNode
 import org.objectweb.asm.tree.MethodNode
 import org.slf4j.LoggerFactory
 
+sealed class ClsResolution() {
+  object NotFound : ClsResolution()
+  object ExternalClass : ClsResolution()
+  class IllegalAccess(val accessType: AccessType) : ClsResolution()
+  class Found(val node: ClassNode) : ClsResolution()
+}
+
+fun VContext.registerMissingClass(clsName: String, problemLocation: ProblemLocation) = this.registerProblem(ClassNotFoundProblem(clsName), problemLocation)
+
 object VerifierUtil {
 
   private val LOG = LoggerFactory.getLogger(VerifierUtil::class.java)
 
-  fun classExistsOrExternal(ctx: VContext, resolver: Resolver, className: String): Boolean {
-    Preconditions.checkArgument(!className.startsWith("["), className)
-    Preconditions.checkArgument(!className.endsWith(";"), className)
-
-    return ctx.verifierOptions.isExternalClass(className) || resolver.containsClass(className)
-  }
-
-  fun classExistsOrExternal(ctx: VContext, potential: ClassNode, resolver: Resolver, descr: String): Boolean = descr == potential.name || classExistsOrExternal(ctx, resolver, descr)
-
-  fun isInterface(classNode: ClassNode): Boolean = classNode.access and Opcodes.ACC_INTERFACE != 0
-
-  fun findClass(resolver: Resolver, potential: ClassNode, className: String, ctx: VContext): ClassNode? {
-    if (className == potential.name) {
-      return potential
+  /**
+   * To resolve an unresolved symbolic reference from D to a class or interface C denoted by N, the following steps are performed:
+   * ...<JVM-related stuff>...
+   *  3) Finally, access permissions to C are checked.
+   *  If C is not accessible (§5.4.4) to D, class or interface resolution throws an IllegalAccessError.
+   */
+  private fun resolveClass(resolver: Resolver, className: String, lookup: ClassNode, ctx: VContext): ClsResolution {
+    if (ctx.verifierOptions.isExternalClass(className)) {
+      return ClsResolution.ExternalClass
     }
-    return findClass(resolver, className, ctx)
+    val node = findClassNode(resolver, className, ctx)
+    if (node != null) {
+      return if (isClassAccessibleToOtherClass(node, lookup)) {
+        ClsResolution.Found(node)
+      } else {
+        ClsResolution.IllegalAccess(getAccessType(node.access))
+      }
+    }
+    return ClsResolution.NotFound
   }
 
+  fun resolveClassOrProblem(resolver: Resolver,
+                            className: String,
+                            lookup: ClassNode,
+                            ctx: VContext,
+                            lookupLocation: (() -> ProblemLocation)): ClassNode? {
+    val resolveClass = resolveClass(resolver, className, lookup, ctx)
+    return when (resolveClass) {
+      ClsResolution.NotFound -> {
+        ctx.registerMissingClass(className, lookupLocation())
+        null
+      }
+      ClsResolution.ExternalClass -> null
+      is ClsResolution.IllegalAccess -> {
+        ctx.registerProblem(IllegalClassAccessProblem(className, resolveClass.accessType), lookupLocation())
+        null
+      }
+      is ClsResolution.Found -> resolveClass.node
+    }
+  }
+
+
+  fun checkClassExistsOrExternal(resolver: Resolver, className: String, ctx: VContext, registerMissing: (() -> ProblemLocation)) {
+    if (!ctx.verifierOptions.isExternalClass(className) && !resolver.containsClass(className)) {
+      ctx.registerProblem(ClassNotFoundProblem(className), registerMissing())
+    }
+  }
 
   /**
    * Finds a class with the given name in the given resolver
@@ -41,7 +82,7 @@ object VerifierUtil {
    * @param ctx       context to report a problem of missing class to
    * @return null if not found or exception occurs (in the last case 'failed to read' warning is reported)
    */
-  fun findClass(resolver: Resolver, className: String, ctx: VContext): ClassNode? {
+  private fun findClassNode(resolver: Resolver, className: String, ctx: VContext): ClassNode? {
     try {
       return resolver.findClass(className)
     } catch (e: Exception) {
@@ -73,11 +114,39 @@ object VerifierUtil {
 
   private fun isPrimitiveType(type: String): Boolean = type.length == 1 && type.first() in "ZIJBFSDC"
 
+  /**
+   * A method is signature polymorphic if all of the following are true:
+   * - It is declared in the java.lang.invoke.MethodHandle class.
+   * - It has a single formal parameter of type Object[].
+   * - It has a return type of Object.
+   * - It has the ACC_VARARGS and ACC_NATIVE flags set.
+   *
+   * In Java SE 8, the only signature polymorphic methods are the invoke and invokeExact methods of the class java.lang.invoke.MethodHandle.
+   */
+  fun isSignaturePolymorphic(hostClass: String, methodNode: MethodNode): Boolean =
+      "java/lang/invoke/MethodHandle" == hostClass
+          && "([Ljava/lang/Object;)Ljava/lang/Object;" == methodNode.desc
+          && isVararg(methodNode)
+          && isNative(methodNode)
+
+  fun getAccessType(accessFlags: Int): AccessType = when {
+    accessFlags and Opcodes.ACC_PUBLIC != 0 -> AccessType.PUBLIC
+    accessFlags and Opcodes.ACC_PRIVATE != 0 -> AccessType.PRIVATE
+    accessFlags and Opcodes.ACC_PROTECTED != 0 -> AccessType.PROTECTED
+    else -> AccessType.PACKAGE_PRIVATE
+  }
+
+  fun isVararg(methodNode: MethodNode): Boolean = methodNode.access and Opcodes.ACC_VARARGS != 0
+
+  fun isNative(methodNode: MethodNode): Boolean = methodNode.access and Opcodes.ACC_NATIVE != 0
+
   fun isFinal(classNode: ClassNode): Boolean = classNode.access and Opcodes.ACC_FINAL != 0
 
   fun isFinal(superMethod: MethodNode): Boolean = superMethod.access and Opcodes.ACC_FINAL != 0
 
   fun isFinal(fieldNode: FieldNode): Boolean = fieldNode.access and Opcodes.ACC_FINAL != 0
+
+  fun isInterface(classNode: ClassNode): Boolean = classNode.access and Opcodes.ACC_INTERFACE != 0
 
   fun isAbstract(clazz: ClassNode): Boolean = clazz.access and Opcodes.ACC_ABSTRACT != 0
 
@@ -85,9 +154,11 @@ object VerifierUtil {
 
   fun isPrivate(field: FieldNode): Boolean = field.access and Opcodes.ACC_PRIVATE != 0
 
-  private fun isPublic(method: MethodNode): Boolean = method.access and Opcodes.ACC_PUBLIC != 0
+  fun isPublic(classNode: ClassNode): Boolean = classNode.access and Opcodes.ACC_PUBLIC != 0
 
-  private fun isPublic(field: FieldNode): Boolean = field.access and Opcodes.ACC_PUBLIC != 0
+  fun isPublic(method: MethodNode): Boolean = method.access and Opcodes.ACC_PUBLIC != 0
+
+  fun isPublic(field: FieldNode): Boolean = field.access and Opcodes.ACC_PUBLIC != 0
 
   fun isDefaultAccess(field: FieldNode): Boolean = !isPublic(field) && !isProtected(field) && !isPrivate(field)
 
@@ -103,33 +174,28 @@ object VerifierUtil {
 
   fun isStatic(field: FieldNode): Boolean = field.access and Opcodes.ACC_STATIC != 0
 
-  fun haveTheSamePackage(first: ClassNode, second: ClassNode): Boolean = extractPackage(first.name).equals(extractPackage(second.name), false)
+  fun haveTheSamePackage(first: ClassNode, second: ClassNode): Boolean = extractPackage(first.name) == extractPackage(second.name)
 
-  private fun extractPackage(className: String?): String? {
-    if (className == null) return null
-    val slash = className.lastIndexOf('/')
-    if (slash == -1) return className
-    return className.substring(0, slash)
-  }
+  /**
+   * Access Control
+   * A class or interface C is accessible to a class or interface D if and only if either of the following is true:
+   * C is public.
+   * C and D are members of the same run-time package (§5.3).
+   */
+  fun isClassAccessibleToOtherClass(me: ClassNode, other: ClassNode): Boolean = isPublic(me) || haveTheSamePackage(me, other)
 
-  fun isAncestor(child: ClassNode, possibleParent: ClassNode, resolver: Resolver, ctx: VContext): Boolean {
-    var child1: ClassNode? = child
-    while (child1 != null) {
-      if (possibleParent.name.equals(child1.name, false)) {
+  private fun extractPackage(className: String): String = className.substringBeforeLast('/', "")
+
+  fun isSubclassOf(child: ClassNode, possibleParent: ClassNode, resolver: Resolver, ctx: VContext): Boolean {
+    var current: ClassNode? = child
+    while (current != null) {
+      if (possibleParent.name == current.name) {
         return true
       }
-      val superName = child1.superName ?: return false
-      child1 = findClass(resolver, superName, ctx)
+      val superName = current.superName ?: return false
+      current = findClassNode(resolver, superName, ctx)
     }
     return false
   }
 
-  fun hasUnresolvedParentClass(clazz: String,
-                               resolver: Resolver,
-                               ctx: VContext): Boolean {
-    findClass(resolver, clazz, ctx) ?: return true
-
-    val unresolvedClasses = ResolverUtil.collectUnresolvedClasses(resolver, clazz, ctx)
-    return !unresolvedClasses.isEmpty()
-  }
 }
