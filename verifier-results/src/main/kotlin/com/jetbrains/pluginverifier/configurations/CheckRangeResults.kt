d24595c0208bc4c45405c753e893b9b9777abe71
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-08-20 15:37:32 +0300

    add explicit list of checked ide-s

--- verifier-results/src/main/kotlin/com/jetbrains/pluginverifier/configurations/CheckRangeResults.kt
+++ verifier-results/src/main/kotlin/com/jetbrains/pluginverifier/configurations/CheckRangeResults.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.pluginverifier.configurations
 
 import com.google.gson.annotations.SerializedName
+import com.jetbrains.pluginverifier.api.IdeDescriptor
 import com.jetbrains.pluginverifier.api.PluginDescriptor
 import com.jetbrains.pluginverifier.api.VResults
 
@@ -8,4 +9,5 @@ import com.jetbrains.pluginverifier.api.VResults
  * @author Sergey Patrikeev
  */
 data class CheckRangeResults(@SerializedName("plugin") val plugin: PluginDescriptor,
+                             @SerializedName("checkedIdeList") val checkedIdeList: List<IdeDescriptor>,
                              @SerializedName("results") val vResults: VResults) : Results
