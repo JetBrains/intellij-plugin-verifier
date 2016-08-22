6a57db9f2e068c9bee871bf6fa30c939eb20b5ba
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-08-22 14:40:30 +0300

    update range results

--- verifier-results/src/main/kotlin/com/jetbrains/pluginverifier/configurations/CheckRangeResults.kt
+++ verifier-results/src/main/kotlin/com/jetbrains/pluginverifier/configurations/CheckRangeResults.kt
@@ -3,11 +3,23 @@ package com.jetbrains.pluginverifier.configurations
 import com.google.gson.annotations.SerializedName
 import com.jetbrains.pluginverifier.api.IdeDescriptor
 import com.jetbrains.pluginverifier.api.PluginDescriptor
+import com.jetbrains.pluginverifier.api.VResult
 import com.jetbrains.pluginverifier.api.VResults
 
 /**
  * @author Sergey Patrikeev
  */
 data class CheckRangeResults(@SerializedName("plugin") val plugin: PluginDescriptor,
-                             @SerializedName("checkedIdeList") val checkedIdeList: List<IdeDescriptor>,
-                             @SerializedName("results") val vResults: VResults) : Results
+                             @SerializedName("type") val resultType: ResultType,
+                             @SerializedName("badPlugin") val badPlugin: VResult.BadPlugin?,
+                             @SerializedName("checkedIdeList") val checkedIdeList: List<IdeDescriptor>?,
+                             @SerializedName("results") val vResults: VResults?) : Results {
+
+  enum class ResultType {
+    NOT_FOUND,
+    NO_COMPATIBLE_IDES,
+    BAD_PLUGIN,
+    CHECKED
+  }
+
+}
