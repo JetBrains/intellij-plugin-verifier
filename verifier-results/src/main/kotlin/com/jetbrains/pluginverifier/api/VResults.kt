d78123870c69209dcaf7f788a146fe5a8471cc99
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-08-22 14:40:59 +0300

    don't require specific ide version

--- verifier-results/src/main/kotlin/com/jetbrains/pluginverifier/api/VResults.kt
+++ verifier-results/src/main/kotlin/com/jetbrains/pluginverifier/api/VResults.kt
@@ -52,8 +52,7 @@ sealed class VResult(@SerializedName("plugin") val pluginDescriptor: PluginDescr
    * The plugin specified with [pluginDescriptor] is not found in the Repository.
    */
   class NotFound(pluginDescriptor: PluginDescriptor,
-                 ideDescriptor: IdeDescriptor,
-                 @SerializedName("reason") val reason: String) : VResult(pluginDescriptor, ideDescriptor) {
+                 @SerializedName("reason") val reason: String) : VResult(pluginDescriptor, IdeDescriptor.AnyIde) {
 
     override fun toString(): String = "VResult.NotFound(plugin=$pluginDescriptor, ideDescriptor=$ideDescriptor, reason=$reason)"
   }
