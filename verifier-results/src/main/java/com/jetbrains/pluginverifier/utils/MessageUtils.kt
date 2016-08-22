fb2570f3d484a06c7759a5b2b5bfa212d5a6eded
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-08-22 12:22:11 +0300

    rewrite without ASM

--- verifier-results/src/main/java/com/jetbrains/pluginverifier/utils/MessageUtils.kt
+++ verifier-results/src/main/java/com/jetbrains/pluginverifier/utils/MessageUtils.kt
@@ -1,7 +1,5 @@
 package com.jetbrains.pluginverifier.utils
 
-import org.objectweb.asm.Type
-
 object MessageUtils {
 
   private val COMMON_PACKAGES = setOf("java.io", "java.lang", "java.util")
@@ -14,20 +12,76 @@ object MessageUtils {
     return binaryName
   }
 
+  private fun convertDescriptor(descriptor: String): String {
+    var dims = 0
+    while (descriptor[dims] == '[') {
+      dims++
+    }
+    val elemType = descriptor.substring(dims)
+    val arrayType = when (elemType) {
+      "V" -> "void"
+      "Z" -> "boolean"
+      "C" -> "char"
+      "B" -> "byte"
+      "S" -> "short"
+      "I" -> "int"
+      "F" -> "float"
+      "J" -> "long"
+      "D" -> "double"
+      else -> {
+        require(elemType.startsWith("L") && elemType.endsWith(";") && elemType.length > 2, { elemType })
+        convertClass(elemType.substring(1, elemType.length - 1))
+      }
+    }
+    return arrayType + "[]".repeat(dims)
+  }
+
+
+  private fun convertMethodDescriptor(methodDescriptor: String): Pair<List<String>, String> {
+    require(methodDescriptor.startsWith("(") && methodDescriptor.contains(')'), { methodDescriptor })
+    val args = arrayListOf<String>()
+    var pos = 1
+    while (methodDescriptor[pos] != ')') {
+      val char = methodDescriptor[pos]
+      if (char in "ZCBSIFJD") {
+        args.add(convertDescriptor(char.toString()))
+        pos++
+      } else if (char == '[') {
+        var end = pos
+        while (methodDescriptor[end] == '[') {
+          end++
+        }
+        if (methodDescriptor[end] == 'L') {
+          end = methodDescriptor.indexOf(';', end)
+        }
+        args.add(convertDescriptor(methodDescriptor.substring(pos, end + 1)))
+        pos = end + 1
+      } else {
+        require(char == 'L')
+        val end = methodDescriptor.indexOf(';', pos)
+        args.add(convertDescriptor(methodDescriptor.substring(pos, end + 1)))
+        pos = end + 1
+      }
+    }
+    val returnType = convertDescriptor(methodDescriptor.substring(pos + 1))
+    return args to returnType
+  }
+
+
   /*
     methodName (Ljava/lang/Object;IDF)V -> void methodName(Object, int, double, float)
     className methodName (Ljava/lang/Object;IDF)V -> className.methodName(Object, int, double, float)
   */
   fun convertMethod(methodName: String, methodDescriptor: String, className: String? = null): String {
-    val methodType = Type.getMethodType(methodDescriptor)
-    val nameAndArgs = methodType.argumentTypes.map { it.className }.joinToString(prefix = "$methodName(", postfix = ")") { convertClass(it) }
-    return if (className != null) convertClass(className) + ".$nameAndArgs" else convertClass(methodType.returnType.className) + " " + nameAndArgs
+    val methodType = convertMethodDescriptor(methodDescriptor)
+    val nameAndArgs = methodType.first.joinToString(prefix = "$methodName(", postfix = ")")
+    return if (className != null) convertClass(className) + ".$nameAndArgs" else convertClass(methodType.second) + " " + nameAndArgs
   }
 
   fun convertField(fieldName: String, fieldDescriptor: String, className: String? = null): String =
       if (className != null)
         convertClass(className) + ".$fieldName"
       else
-        convertClass(Type.getType(fieldDescriptor).className) + " " + fieldName
+        convertClass(convertDescriptor(fieldDescriptor)) + " " + fieldName
 
 }
