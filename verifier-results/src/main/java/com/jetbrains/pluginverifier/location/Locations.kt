b1eee1cb2e82e58b6b8599b8844d3e8dab120093
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-08-26 13:26:54 +0300

    rewrite using the custom gson-serializer

--- verifier-results/src/main/java/com/jetbrains/pluginverifier/location/Locations.kt
+++ verifier-results/src/main/java/com/jetbrains/pluginverifier/location/Locations.kt
@@ -1,6 +1,9 @@
 package com.jetbrains.pluginverifier.location
 
-import com.google.gson.annotations.SerializedName
+import com.github.salomonbrys.kotson.jsonDeserializer
+import com.github.salomonbrys.kotson.jsonSerializer
+import com.github.salomonbrys.kotson.string
+import com.google.gson.JsonPrimitive
 import com.jetbrains.pluginverifier.utils.MessageUtils
 import org.objectweb.asm.tree.MethodNode
 
@@ -10,29 +13,49 @@ import org.objectweb.asm.tree.MethodNode
 interface ProblemLocation {
 
   companion object {
-    fun fromClass(className: String): ProblemLocation = ClassLocation(className)
+    fun fromClass(className: String): ClassLocation = ClassLocation(className)
 
-    fun fromMethod(hostClass: String, method: MethodNode): ProblemLocation = fromMethod(hostClass, method.name, method.desc)
+    fun fromMethod(hostClass: String, method: MethodNode): MethodLocation = fromMethod(hostClass, method.name, method.desc)
 
-    fun fromMethod(hostClass: String, methodName: String, methodDescriptor: String): ProblemLocation = MethodLocation(hostClass, methodName, methodDescriptor)
+    fun fromMethod(hostClass: String, methodName: String, methodDescriptor: String): MethodLocation = MethodLocation(hostClass, methodName, methodDescriptor)
 
-    fun fromField(hostClass: String, fieldName: String, fieldDescriptor: String): ProblemLocation = FieldLocation(hostClass, fieldName, fieldDescriptor)
+    fun fromField(hostClass: String, fieldName: String, fieldDescriptor: String): FieldLocation = FieldLocation(hostClass, fieldName, fieldDescriptor)
   }
+
 }
 
-data class MethodLocation(@SerializedName("host") val hostClass: String,
-                          @SerializedName("name") val methodName: String,
-                          @SerializedName("descriptor") val methodDescriptor: String) : ProblemLocation {
+data class MethodLocation(val hostClass: String,
+                          val methodName: String,
+                          val methodDescriptor: String) : ProblemLocation {
   override fun toString(): String = MessageUtils.convertMethod(methodName, methodDescriptor, hostClass)
 }
 
-
-data class FieldLocation(@SerializedName("host") val hostClass: String,
-                         @SerializedName("name") val fieldName: String,
-                         @SerializedName("descriptor") val fieldDescriptor: String) : ProblemLocation {
+data class FieldLocation(val hostClass: String,
+                         val fieldName: String,
+                         val fieldDescriptor: String) : ProblemLocation {
   override fun toString(): String = MessageUtils.convertField(fieldName, fieldDescriptor, hostClass)
 }
 
-data class ClassLocation(@SerializedName("class") val className: String) : ProblemLocation {
+data class ClassLocation(val className: String) : ProblemLocation {
   override fun toString(): String = MessageUtils.convertClass(className)
+}
+
+internal val problemLocationSerializer = jsonSerializer<ProblemLocation> {
+  val src = it.src
+  return@jsonSerializer when (src) {
+    is MethodLocation -> JsonPrimitive("M#${src.hostClass}#${src.methodName}#${src.methodDescriptor}")
+    is FieldLocation -> JsonPrimitive("F#${src.hostClass}#${src.fieldName}#${src.fieldDescriptor}")
+    is ClassLocation -> JsonPrimitive("C#${src.className}")
+    else -> throw IllegalArgumentException("Unregistered type ${it.src.javaClass.name}: ${it.src}")
+  }
+}
+
+internal val problemLocationDeserializer = jsonDeserializer<ProblemLocation> {
+  val parts = it.json.string.split('#')
+  return@jsonDeserializer when {
+    parts[0] == "M" -> ProblemLocation.fromMethod(parts[1], parts[2], parts[3])
+    parts[0] == "F" -> ProblemLocation.fromField(parts[1], parts[2], parts[3])
+    parts[0] == "C" -> ProblemLocation.fromClass(parts[1])
+    else -> throw IllegalArgumentException("Unknown type ${it.json.string}")
+  }
 }
