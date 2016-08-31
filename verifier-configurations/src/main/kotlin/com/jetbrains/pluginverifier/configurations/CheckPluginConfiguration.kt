0b366fee920cd4ea94dd7140e1504c53c6916577
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-08-31 19:05:29 +0300

    import statement

--- verifier-configurations/src/main/kotlin/com/jetbrains/pluginverifier/configurations/CheckPluginConfiguration.kt
+++ verifier-configurations/src/main/kotlin/com/jetbrains/pluginverifier/configurations/CheckPluginConfiguration.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.pluginverifier.configurations
 
+import com.jetbrains.pluginverifier.api.VManager
 import com.jetbrains.pluginverifier.api.VParams
 
 
@@ -8,7 +9,7 @@ class CheckPluginConfiguration(val params: CheckPluginParams) : Configuration {
   override fun execute(): CheckPluginResults {
     val pluginsToCheck = params.pluginDescriptors.map { p -> params.ideDescriptors.map { p to it } }.flatten()
     val vParams = VParams(params.jdkDescriptor, pluginsToCheck, params.vOptions, params.externalClasspath, params.resolveDependenciesWithin)
-    val vResults = com.jetbrains.pluginverifier.api.VManager.verify(vParams, params.progress)
+    val vResults = VManager.verify(vParams, params.progress)
 
     return CheckPluginResults(vResults)
   }
