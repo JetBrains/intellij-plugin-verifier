77e1de43456a2c03f0510fa8279e0998c06f7b5f
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-09-30 13:51:57 +0300

    revert the check-trunk-api on the service: download the missing IDE

---
+++ plugins-verifier-service/server/src/main/kotlin/org/jetbrains/plugins/verifier/service/runners/CheckTrunkApiRunner.kt
@@ -0,0 +1,137 @@
+package org.jetbrains.plugins.verifier.service.runners
+
+import com.google.common.collect.ImmutableMultimap
+import com.intellij.structure.domain.Ide
+import com.intellij.structure.domain.IdeManager
+import com.intellij.structure.domain.IdeVersion
+import com.jetbrains.pluginverifier.api.IdeDescriptor
+import com.jetbrains.pluginverifier.api.JdkDescriptor
+import com.jetbrains.pluginverifier.api.PluginDescriptor
+import com.jetbrains.pluginverifier.configurations.BundledPlugins
+import com.jetbrains.pluginverifier.configurations.CheckIdeConfiguration
+import com.jetbrains.pluginverifier.configurations.CheckIdeParams
+import com.jetbrains.pluginverifier.configurations.CheckTrunkApiResults
+import com.jetbrains.pluginverifier.misc.deleteLogged
+import com.jetbrains.pluginverifier.report.CheckIdeReport
+import com.jetbrains.pluginverifier.repository.RepositoryManager
+import org.jetbrains.plugins.verifier.service.core.BridgeVProgress
+import org.jetbrains.plugins.verifier.service.core.Progress
+import org.jetbrains.plugins.verifier.service.core.Task
+import org.jetbrains.plugins.verifier.service.core.TaskManager
+import org.jetbrains.plugins.verifier.service.params.CheckTrunkApiRunnerParams
+import org.jetbrains.plugins.verifier.service.storage.IdeFilesManager
+import org.jetbrains.plugins.verifier.service.storage.JdkManager
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * @author Sergey Patrikeev
+ */
+class CheckTrunkApiRunner(val ideFile: File,
+                          val deleteOnCompletion: Boolean,
+                          val runnerParams: CheckTrunkApiRunnerParams) : Task<CheckTrunkApiResults>() {
+  override fun presentableName(): String = "CheckTrunkApi"
+
+  private val MAX_TIME_DOWNLOADING_MISSING_IDE_MS: Long = 1000 * 60 * 60 // 1 hour
+
+  private val SLEEP_TIME_WAITING_MS: Long = 1000 * 10 //10 minutes
+
+  companion object {
+    private val LOG = LoggerFactory.getLogger(CheckTrunkApiResults::class.java)
+  }
+
+  override fun computeResult(progress: Progress): CheckTrunkApiResults {
+    try {
+      val ide: Ide
+      try {
+        ide = IdeManager.getInstance().createIde(ideFile)
+      } catch (e: Exception) {
+        throw IllegalArgumentException("The supplied IDE $ideFile is broken", e)
+      }
+
+      var majorIdeVersion: IdeVersion
+      try {
+        majorIdeVersion = IdeVersion.createIdeVersion(runnerParams.majorVersion)
+      } catch (e: Exception) {
+        try {
+          majorIdeVersion = IdeVersion.createIdeVersion(runnerParams.majorVersion.substringAfter("-"))
+        } catch (ignore: Exception) {
+          throw IllegalArgumentException("Incorrect major IDE version ${runnerParams.majorVersion}", e)
+        }
+      }
+
+      var majorLock = IdeFilesManager.getIde(majorIdeVersion)
+      if (majorLock == null) {
+        LOG.info("There is no IDE of version $majorIdeVersion; downloading from the repository")
+
+        val uploadIdeRunner = UploadIdeRunner(majorIdeVersion.asString(false, true), majorIdeVersion.productCode == "IC")
+
+        val downloadFinished = AtomicBoolean()
+
+        val downloadId = TaskManager.enqueue(uploadIdeRunner, {}, { t, st, task -> }, { st, task -> downloadFinished.set(true) })
+
+        val startTime = System.currentTimeMillis()
+
+        while (!downloadFinished.get()) {
+          LOG.debug("Waiting for the IDE #$majorIdeVersion to be loaded")
+          if (System.currentTimeMillis() - startTime > MAX_TIME_DOWNLOADING_MISSING_IDE_MS) {
+            throw RuntimeException("Too long waiting (${MAX_TIME_DOWNLOADING_MISSING_IDE_MS / 1000 / 60} minutes) " +
+                "for the IDE #$majorIdeVersion to be loaded; download taskId = $downloadId")
+          }
+          Thread.sleep(SLEEP_TIME_WAITING_MS)
+        }
+
+        majorLock = IdeFilesManager.getIde(majorIdeVersion)
+        if (majorLock == null) {
+          val status = TaskManager.get(downloadId)
+          throw IllegalStateException("Unable to download the IDE #$majorIdeVersion; error message is - ${status?.errorMessage}")
+        }
+      }
+
+      try {
+        val pluginsToCheck: List<PluginDescriptor>
+        try {
+          pluginsToCheck = RepositoryManager.getLastCompatibleUpdates(ide.version).map { PluginDescriptor.ByUpdateInfo(it) }
+        } catch(e: Exception) {
+          throw RuntimeException("Unable to fetch list of plugins compatible with ${ide.version}", e)
+        }
+
+        val jdkDescriptor = JdkDescriptor.ByFile(JdkManager.getJdkHome(runnerParams.jdkVersion))
+
+        val majorReport = calcReport(ide, jdkDescriptor, pluginsToCheck, progress)
+        val currentReport = calcReport(majorLock.ide, jdkDescriptor, pluginsToCheck, progress)
+
+        return CheckTrunkApiResults(majorReport.first, majorReport.second, currentReport.first, currentReport.second)
+      } finally {
+        majorLock.release()
+      }
+    } finally {
+      if (deleteOnCompletion) {
+        ideFile.deleteLogged()
+      }
+    }
+  }
+
+  private fun calcReport(ide: Ide,
+                         jdkDescriptor: JdkDescriptor.ByFile,
+                         pluginsToCheck: List<PluginDescriptor>,
+                         progress: Progress): Pair<CheckIdeReport, BundledPlugins> {
+    try {
+      val params = CheckIdeParams(IdeDescriptor.ByInstance(ide), jdkDescriptor, pluginsToCheck, ImmutableMultimap.of(), runnerParams.vOptions, progress = BridgeVProgress(progress))
+      LOG.debug("Check IDE arguments: $params")
+      val bundledPlugins = getBundledPlugins(ide)
+      val ideReport = CheckIdeConfiguration(params).execute().run { CheckIdeReport.createReport(ide.version, vResults) }
+      return ideReport to bundledPlugins
+    } catch (ie: InterruptedException) {
+      throw ie
+    } catch (e: Exception) {
+      LOG.error("Unable to verify IDE $ide", e)
+      throw e
+    }
+
+  }
+
+  private fun getBundledPlugins(ide: Ide): BundledPlugins = BundledPlugins(ide.bundledPlugins.map { it.pluginId }.distinct(), ide.bundledPlugins.flatMap { it.definedModules }.distinct())
+
+}
