1b555d4c3fe8502fbbd851121b1c99d13fdfc4e2
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-10-06 15:32:14 +0300

    specify default disk memory value

--- plugins-verifier-service/server/src/main/kotlin/org/jetbrains/plugins/verifier/service/setting/Settings.kt
+++ plugins-verifier-service/server/src/main/kotlin/org/jetbrains/plugins/verifier/service/setting/Settings.kt
@@ -8,7 +8,7 @@ enum class Settings(private val key: String, private val default: (() -> String?
   JDK_6_HOME("verifier.service.jdk.6.dir", { JDK_8_HOME.get() }),
   JDK_7_HOME("verifier.service.jdk.7.dir", { JDK_8_HOME.get() }),
   JDK_8_HOME("verifier.service.jdk.8.dir"),
-  MAX_DISK_SPACE_MB("verifier.service.max.disk.space.mb"),
+  MAX_DISK_SPACE_MB("verifier.service.max.disk.space.mb", { (50 * 1024).toString() }),
   PLUGIN_REPOSITORY_URL("verifier.service.plugin.repository.url", { "http://plugins.jetbrains.com" }),
   IDE_REPOSITORY_URL("verifier.service.ide.repository.url", { "http://www.jetbrains.com" });
 
