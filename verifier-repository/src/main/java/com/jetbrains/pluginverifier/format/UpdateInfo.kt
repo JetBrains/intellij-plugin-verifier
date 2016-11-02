6ea68176944ff4bdd1a1a1deebfbc60ee8df52dc
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-11-02 19:15:01 +0300

    added a workaround for broken plugins site API

--- verifier-repository/src/main/java/com/jetbrains/pluginverifier/format/UpdateInfo.kt
+++ verifier-repository/src/main/java/com/jetbrains/pluginverifier/format/UpdateInfo.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class UpdateInfo(@SerializedName("pluginId") val pluginId: String,
                       @SerializedName("pluginName") val pluginName: String,
-                      @SerializedName("version") val version: String,
+                      @SerializedName("version", alternate = arrayOf("pluginVersion")) val version: String,
                       @SerializedName("updateId") val updateId: Int) {
 
   override fun toString(): String = "$pluginId:$version (#$updateId)"
