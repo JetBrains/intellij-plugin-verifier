a4e03195e4b44146d1a1bd13d0fec41ade683ff4
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-11-04 20:25:28 +0300

    added a vendor field

--- verifier-repository/src/main/java/com/jetbrains/pluginverifier/format/UpdateInfo.kt
+++ verifier-repository/src/main/java/com/jetbrains/pluginverifier/format/UpdateInfo.kt
@@ -5,7 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class UpdateInfo(@SerializedName("pluginId") val pluginId: String,
                       @SerializedName("pluginName") val pluginName: String,
                       @SerializedName("version", alternate = arrayOf("pluginVersion")) val version: String,
-                      @SerializedName("updateId") val updateId: Int) {
+                      @SerializedName("updateId") val updateId: Int,
+                      @SerializedName("vendor") val vendor: String?) {
 
   override fun toString(): String = "$pluginId:$version (#$updateId)"
 }
