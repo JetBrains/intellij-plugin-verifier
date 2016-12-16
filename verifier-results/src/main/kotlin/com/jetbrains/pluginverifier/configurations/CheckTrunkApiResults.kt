7a5ed56eb35d961c71758d1e66a1b43346a58ad4
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2016-12-16 19:56:57 +0300

    check-trunk configuration: filter problems caused by missing plugins

--- verifier-results/src/main/kotlin/com/jetbrains/pluginverifier/configurations/CheckTrunkApiResults.kt
+++ verifier-results/src/main/kotlin/com/jetbrains/pluginverifier/configurations/CheckTrunkApiResults.kt
@@ -15,8 +15,7 @@ import com.jetbrains.pluginverifier.report.CheckIdeReport
 data class CheckTrunkApiResults(@SerializedName("majorReport") val majorReport: CheckIdeReport,
                                 @SerializedName("majorPlugins") val majorPlugins: BundledPlugins,
                                 @SerializedName("currentReport") val currentReport: CheckIdeReport,
-                                @SerializedName("currentPlugins") val currentPlugins: BundledPlugins) : Results {
-}
+                                @SerializedName("currentPlugins") val currentPlugins: BundledPlugins) : Results
 
 data class BundledPlugins(@SerializedName("pluginIds") val pluginIds: List<String>,
                           @SerializedName("moduleIds") val moduleIds: List<String>)
@@ -30,7 +29,8 @@ data class CheckTrunkApiCompareResult(@SerializedName("curVersion") val currentV
       val oldProblems = apiResults.majorReport.pluginProblems.values().toSet()
       val newProblems = Multimaps.filterValues(apiResults.currentReport.pluginProblems, { it !in oldProblems })
       val newMissingProblems = Multimaps.filterEntries(apiResults.currentReport.missingPlugins, { it !in apiResults.majorReport.missingPlugins.entries() })
-      return CheckTrunkApiCompareResult(apiResults.currentReport.ideVersion, apiResults.majorReport.ideVersion, newProblems, newMissingProblems)
+      val relatedProblems = Multimaps.filterKeys(newProblems, { !newMissingProblems.containsValue(it) })
+      return CheckTrunkApiCompareResult(apiResults.currentReport.ideVersion, apiResults.majorReport.ideVersion, relatedProblems, newMissingProblems)
     }
   }
 }
