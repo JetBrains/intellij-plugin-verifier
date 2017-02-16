5e530fc23e4ab4e01d24c864bc5f9506cee60261
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-02-16 22:21:25 +0300

    add a utility class which ensures correct encoding of delimiter characters

--- verifier-results/src/main/java/com/jetbrains/pluginverifier/reference/SymbolicReference.kt
+++ verifier-results/src/main/java/com/jetbrains/pluginverifier/reference/SymbolicReference.kt
@@ -4,6 +4,7 @@ import com.github.salomonbrys.kotson.jsonDeserializer
 import com.github.salomonbrys.kotson.jsonSerializer
 import com.github.salomonbrys.kotson.string
 import com.google.gson.JsonPrimitive
+import com.jetbrains.pluginverifier.utils.CompactJson
 import com.jetbrains.pluginverifier.utils.MessageUtils
 
 /**
@@ -43,15 +44,15 @@ data class ClassReference(val className: String) : SymbolicReference {
 internal val symbolicReferenceSerializer = jsonSerializer<SymbolicReference> {
   val src = it.src
   return@jsonSerializer when (src) {
-    is MethodReference -> JsonPrimitive("M#${src.hostClass}#${src.methodName}#${src.methodDescriptor}")
-    is FieldReference -> JsonPrimitive("F#${src.hostClass}#${src.fieldName}#${src.fieldDescriptor}")
-    is ClassReference -> JsonPrimitive("C#${src.className}")
+    is MethodReference -> JsonPrimitive(CompactJson.serialize(listOf("M", src.hostClass, src.methodName, src.methodDescriptor)))
+    is FieldReference -> JsonPrimitive(CompactJson.serialize(listOf("F", src.hostClass, src.fieldName, src.fieldDescriptor)))
+    is ClassReference -> JsonPrimitive(CompactJson.serialize(listOf("C", src.className)))
     else -> throw IllegalArgumentException("Unknwon type ${src.javaClass.name}: $src")
   }
 }
 
 internal val symbolicReferenceDeserializer = jsonDeserializer<SymbolicReference> {
-  val parts = it.json.string.split('#')
+  val parts = CompactJson.deserialize(it.json.string)
   return@jsonDeserializer when {
     parts[0] == "M" -> SymbolicReference.methodFrom(parts[1], parts[2], parts[3])
     parts[0] == "F" -> SymbolicReference.fieldFrom(parts[1], parts[2], parts[3])
