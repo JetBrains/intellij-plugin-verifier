e51f6e0c08cf3b4eb84c36a83977cfc7b582cf09
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-03-26 12:15:53 +0300

    cleanup unused classes

--- plugins-verifier-service/server/src/main/kotlin/org/jetbrains/plugins/verifier/service/util/RetrofitUtil.kt
+++ plugins-verifier-service/server/src/main/kotlin/org/jetbrains/plugins/verifier/service/util/RetrofitUtil.kt
@@ -1,31 +1,8 @@
 package org.jetbrains.plugins.verifier.service.util
 
-import com.jetbrains.pluginverifier.persistence.GsonHolder
-import okhttp3.MediaType
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.Response
-import java.io.File
 import java.io.IOException
-
-object MultipartUtil {
-
-  fun createJsonPart(partName: String, obj: Any): MultipartBody.Part =
-      createJsonPart(partName, GsonHolder.GSON.toJson(obj))
-
-  fun createJsonPart(partName: String, json: String): MultipartBody.Part =
-      MultipartBody.Part.createFormData(partName, null, RequestBody.create(MediaTypes.JSON, json))
-
-  fun createFilePart(partName: String, file: File): MultipartBody.Part =
-      MultipartBody.Part.createFormData(partName, file.name, RequestBody.create(MediaTypes.OCTET_STREAM, file))
-
-}
-
-object MediaTypes {
-  val JSON: MediaType = MediaType.parse("application/json")
-  val OCTET_STREAM: MediaType = MediaType.parse("application/octet-stream")
-}
 
 fun <T> Call<T>.executeSuccessfully(): Response<T> {
   val server = "${this.request().url().host()}:${this.request().url().port()}"
