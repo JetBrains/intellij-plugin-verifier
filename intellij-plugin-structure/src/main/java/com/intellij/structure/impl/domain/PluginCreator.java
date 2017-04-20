c637ce54408ccf7f6a65c1c69bfe680861c1a176
satamas <se.atamas@gmail.com>
2017-04-20 11:38:17 +0300

    Minor problems update.

--- intellij-plugin-structure/src/main/java/com/intellij/structure/impl/domain/PluginCreator.java
+++ intellij-plugin-structure/src/main/java/com/intellij/structure/impl/domain/PluginCreator.java
@@ -244,7 +244,7 @@ final class PluginCreator {
 
   private void validateDescription(@Nullable String description) {
     if (isEmpty(description)) {
-      registerProblem(new EmptyDescription(myDescriptorPath));
+      registerProblem(new PropertyNotSpecified(myDescriptorPath, "<description>"));
       return;
     }
 
