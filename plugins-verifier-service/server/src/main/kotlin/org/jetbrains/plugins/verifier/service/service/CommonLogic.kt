66396132dcf878521a601051c42851305ecd672b
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-05-17 19:56:46 +0300

    update kotlin

--- plugins-verifier-service/server/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/CommonLogic.kt
+++ plugins-verifier-service/server/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/CommonLogic.kt
@@ -1,6 +1,6 @@
 package org.jetbrains.plugins.verifier.service.service
 
-import com.jetbrains.pluginverifier.persistence.CompactJson
+import com.jetbrains.pluginverifier.persistence.GsonHolder
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.RequestBody
@@ -24,4 +24,4 @@ val JSON_MEDIA_TYPE: MediaType = MediaType.parse("application/json")
 
 fun createStringRequestBody(string: String): RequestBody = RequestBody.create(STRING_MEDIA_TYPE, string)
 
-fun createCompactJsonRequestBody(obj: Any): RequestBody = RequestBody.create(JSON_MEDIA_TYPE, CompactJson.toJson(obj))
+fun createCompactJsonRequestBody(obj: Any): RequestBody = RequestBody.create(JSON_MEDIA_TYPE, GsonHolder.GSON.toJson(obj))
