3ef23820c8a70f3cb74db56f184c24831407b8c2
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-05-25 15:12:52 +0300

    update versions of intellij-structure and verifier

--- verifier-parameters/src/main/kotlin/com/jetbrains/pluginverifier/plugin/PluginCreator.kt
+++ verifier-parameters/src/main/kotlin/com/jetbrains/pluginverifier/plugin/PluginCreator.kt
@@ -4,16 +4,20 @@ import com.intellij.structure.plugin.Plugin
 import com.intellij.structure.plugin.PluginCreationFail
 import com.intellij.structure.plugin.PluginCreationSuccess
 import com.intellij.structure.plugin.PluginManager
-import com.intellij.structure.problems.UnableToReadPluginClassFiles
+import com.intellij.structure.problems.PluginProblem
 import com.intellij.structure.resolvers.Resolver
 import com.jetbrains.pluginverifier.api.PluginDescriptor
 import com.jetbrains.pluginverifier.repository.FileLock
 import com.jetbrains.pluginverifier.repository.RepositoryManager
 import com.jetbrains.pluginverifier.repository.UpdateInfo
 import com.jetbrains.pluginverifier.utils.CloseIgnoringResolver
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.io.File
 
 object PluginCreator {
+
+  private val LOG: Logger = LoggerFactory.getLogger(PluginCreator::class.java)
 
   fun createPlugin(pluginDescriptor: PluginDescriptor): CreatePluginResult = when (pluginDescriptor) {
     is PluginDescriptor.ByFileLock -> createPluginByFile(pluginDescriptor.fileLock.getFile())
@@ -48,8 +52,14 @@ object PluginCreator {
     val resolver = try {
       Resolver.createPluginResolver(plugin)
     } catch (e: Exception) {
-      return CreatePluginResult.BadPlugin(PluginCreationFail(listOf(UnableToReadPluginClassFiles(plugin.originalFile))))
+      LOG.debug("Unable to read plugin $plugin class files", e)
+      return CreatePluginResult.BadPlugin(PluginCreationFail(listOf(UnableToReadPluginClassFilesProblem)))
     }
     return CreatePluginResult.OK(PluginCreationSuccess(plugin, emptyList()), resolver)
+  }
+
+  object UnableToReadPluginClassFilesProblem : PluginProblem() {
+    override val level: PluginProblem.Level = PluginProblem.Level.ERROR
+    override val message: String = "Unable to read plugin class files"
   }
 }
