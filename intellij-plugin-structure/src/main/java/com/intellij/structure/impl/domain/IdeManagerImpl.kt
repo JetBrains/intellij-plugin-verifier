64e2377e2029488c2663faa6fd75495d5e65e4d7
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-05-29 13:52:58 +0300

    convert java to kotlin

---
+++ intellij-plugin-structure/src/main/java/com/intellij/structure/impl/domain/IdeManagerImpl.kt
@@ -0,0 +1,200 @@
+package com.intellij.structure.impl.domain
+
+import com.google.common.base.Joiner
+import com.google.common.io.Files
+import com.intellij.structure.ide.Ide
+import com.intellij.structure.ide.IdeManager
+import com.intellij.structure.ide.IdeVersion
+import com.intellij.structure.impl.utils.StringUtil
+import com.intellij.structure.impl.utils.xml.JDOMXIncluder
+import com.intellij.structure.impl.utils.xml.URLUtil
+import com.intellij.structure.impl.utils.xml.XIncludeException
+import com.intellij.structure.plugin.Plugin
+import com.intellij.structure.plugin.PluginCreationFail
+import com.intellij.structure.plugin.PluginCreationSuccess
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.filefilter.TrueFileFilter
+import org.apache.commons.io.filefilter.WildcardFileFilter
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.IOException
+import java.net.URL
+import java.nio.charset.Charset
+import java.util.*
+
+/**
+ * @author Sergey Patrikeev
+ */
+class IdeManagerImpl : IdeManager() {
+
+
+  @Throws(IOException::class)
+  override fun createIde(ideDir: File): Ide = createIde(ideDir, null)
+
+  @Throws(IOException::class)
+  override fun createIde(idePath: File, version: IdeVersion?): Ide {
+    if (!idePath.exists()) {
+      throw IllegalArgumentException("IDE file $idePath is not found")
+    }
+    val bundled = if (isSourceDir(idePath)) getDummyPluginsFromSources(idePath) else getIdeaPlugins(idePath)
+    val ideVersion = version ?: if (isSourceDir(idePath)) readVersionFromSourcesDir(idePath) else readVersionFromBinaries(idePath)
+    return IdeImpl(idePath, ideVersion, bundled)
+  }
+
+  @Throws(IOException::class)
+  private fun readVersionFromBinaries(idePath: File): IdeVersion {
+    val versionFile = File(idePath, "build.txt")
+    if (!versionFile.exists()) {
+      throw IllegalArgumentException(versionFile.toString() + " is not found")
+    }
+    return readBuildNumber(versionFile)
+  }
+
+  @Throws(IOException::class)
+  private fun readVersionFromSourcesDir(idePath: File): IdeVersion {
+    val buildFile = File(idePath, "build.txt")
+    if (buildFile.exists()) {
+      return readBuildNumber(buildFile)
+    }
+    val communityBuildFile = File(idePath, "community/build.txt")
+    if (communityBuildFile.exists()) {
+      return readBuildNumber(communityBuildFile)
+    }
+    throw IllegalArgumentException("Unable to find IDE version file (build.txt or community/build.txt)")
+  }
+
+  private class PluginFromSourcePathResolver internal constructor(private val myDescriptors: Map<String, File>) : JDOMXIncluder.DefaultPathResolver() {
+
+    private fun resolveOutputDirectories(relativePath: String, base: String?): URL {
+      val normalizedPath = if (relativePath.startsWith("./")) {
+        "/META-INF/" + StringUtil.substringAfter(relativePath, "./")
+      } else {
+        relativePath
+      }
+
+      val file = myDescriptors[normalizedPath]
+      if (file != null) {
+        try {
+          return file.toURI().toURL()
+        } catch (exc: Exception) {
+          throw XIncludeException("File $file has an invalid URL presentation ", exc)
+        }
+
+      }
+      throw XIncludeException("Unable to resolve " + normalizedPath + if (base != null) " against " + base else "")
+    }
+
+    override fun resolvePath(relativePath: String, base: String?): URL {
+      try {
+        val res = super.resolvePath(relativePath, base)
+        //try the parent resolver
+        URLUtil.openStream(res)
+        return res
+      } catch (e: IOException) {
+        return resolveOutputDirectories(relativePath, base)
+      }
+
+    }
+  }
+
+  companion object {
+
+    private val LOG = LoggerFactory.getLogger(IdeManagerImpl::class.java)
+
+    @Throws(IOException::class)
+    private fun readBuildNumber(versionFile: File): IdeVersion {
+      val buildNumberString = Files.toString(versionFile, Charset.defaultCharset()).trim { it <= ' ' }
+      return IdeVersion.createIdeVersion(buildNumberString)
+    }
+
+    fun isUltimate(ideaDir: File): Boolean = File(ideaDir, "community/.idea").isDirectory && getUltimateClassesRoot(ideaDir).isDirectory
+
+    fun isCommunity(ideaDir: File): Boolean = File(ideaDir, ".idea").isDirectory && getCommunityClassesRoot(ideaDir).isDirectory
+
+    fun getUltimateClassesRoot(ideaDir: File): File = File(ideaDir, "out/classes/production")
+
+    fun getCommunityClassesRoot(ideaDir: File): File = File(ideaDir, "out/production")
+
+    @Throws(IOException::class)
+    private fun getDummyPluginsFromSources(ideaDir: File): List<Plugin> {
+      if (isUltimate(ideaDir)) {
+        return getDummyPlugins(getUltimateClassesRoot(ideaDir))
+      } else if (isCommunity(ideaDir)) {
+        return getDummyPlugins(getCommunityClassesRoot(ideaDir))
+      } else {
+        throw IllegalArgumentException("Incorrect IDEA structure: $ideaDir. It must be Community or Ultimate sources root with compiled class files.")
+      }
+    }
+
+    /**
+     * Creates plugin descriptors from the found plugin.xml files.
+     * We don't know exactly which classes correspond to which descriptors
+     * so these classes are all in one Resolver.
+     *
+     * See [com.intellij.structure.impl.resolvers.IdeResolverCreator.getIdeaResolverFromSources]
+
+     * @param root idea root directory
+     * *
+     * @return dummy (no classes) plugins
+     */
+
+    private fun getDummyPlugins(root: File): List<Plugin> {
+      val xmlFiles = FileUtils.listFiles(root, WildcardFileFilter("*.xml"), TrueFileFilter.TRUE)
+
+      val pathResolver = getFromSourcesPathResolver(xmlFiles)
+      return getDummyPlugins(xmlFiles, pathResolver)
+    }
+
+    private fun getDummyPlugins(xmlFiles: Collection<File>, pathResolver: JDOMXIncluder.PathResolver): List<Plugin> = xmlFiles
+        .filter { "plugin.xml" == it.name }
+        .map { it.absoluteFile.parentFile }
+        .filter { "META-INF" == it.name && it.isDirectory && it.parentFile != null }
+        .map { it.parentFile }
+        .filter { it.isDirectory }
+        .mapNotNull { createDummyPluginFromDirectory(it, pathResolver) }
+
+    private fun createDummyPluginFromDirectory(pluginDirectory: File, pathResolver: JDOMXIncluder.PathResolver?): Plugin? {
+      try {
+        val pluginCreator = PluginManagerImpl(pathResolver).getPluginCreatorWithResult(pluginDirectory, false)
+        pluginCreator.setOriginalFile(pluginDirectory)
+        val creationResult = pluginCreator.pluginCreationResult
+        return when (creationResult) {
+          is PluginCreationSuccess -> creationResult.plugin
+          is PluginCreationFail -> {
+            val problems = creationResult.errorsAndWarnings
+            LOG.debug("Failed to read plugin " + pluginDirectory + ". Problems: " + Joiner.on(", ").join(problems))
+            null
+          }
+        }
+      } catch (e: Exception) {
+        LOG.debug("Unable to create plugin from sources: " + pluginDirectory, e)
+      }
+
+      return null
+    }
+
+    private fun getFromSourcesPathResolver(xmlFiles: Collection<File>): JDOMXIncluder.PathResolver {
+      val xmlDescriptors = HashMap<String, File>()
+      for (file in xmlFiles) {
+        val path = file.absolutePath
+        val parts = path.split("/".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        if (parts.size >= 2) {
+          val key = "/" + parts[parts.size - 2] + "/" + parts[parts.size - 1]
+          xmlDescriptors.put(key, file)
+        }
+      }
+      return PluginFromSourcePathResolver(xmlDescriptors)
+    }
+
+    fun isSourceDir(dir: File): Boolean {
+      return File(dir, ".idea").isDirectory
+    }
+
+    @Throws(IOException::class)
+    private fun getIdeaPlugins(ideaDir: File): List<Plugin> {
+      val pluginsDir = File(ideaDir, "plugins")
+      val pluginsFiles = pluginsDir.listFiles() ?: return emptyList()
+      return pluginsFiles.filter { it.isDirectory }.mapNotNull { createDummyPluginFromDirectory(it, null) }
+    }
+  }
+}
---
+++ intellij-plugin-structure/src/main/java/com/intellij/structure/impl/resolvers/IdeResolverCreator.kt
@@ -0,0 +1,77 @@
+package com.intellij.structure.impl.resolvers
+
+import com.google.common.base.Predicate
+import com.google.common.base.Predicates
+import com.intellij.structure.ide.Ide
+import com.intellij.structure.impl.domain.IdeManagerImpl
+import com.intellij.structure.impl.utils.JarsUtils
+import com.intellij.structure.resolvers.Resolver
+import com.intellij.structure.resolvers.Resolver.createUnionResolver
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.IOException
+
+/**
+ * @author Sergey Patrikeev
+ */
+object IdeResolverCreator {
+
+  private val HARD_CODED_LIB_FOLDERS = arrayOf("community/android/android/lib", "community/plugins/gradle/lib")
+
+  private val LOG = LoggerFactory.getLogger(IdeResolverCreator::class.java)
+
+  @Throws(IOException::class)
+  @JvmStatic
+  fun createIdeResolver(ide: Ide): Resolver {
+    val idePath = ide.idePath
+    if (IdeManagerImpl.isSourceDir(idePath)) {
+      return IdeResolverCreator.getIdeaResolverFromSources(idePath)
+    } else {
+      return IdeResolverCreator.getIdeResolverFromLibraries(idePath)
+    }
+  }
+
+  @Throws(IOException::class)
+  private fun getIdeResolverFromLibraries(ideDir: File): Resolver {
+    val lib = File(ideDir, "lib")
+    if (!lib.isDirectory) {
+      throw IOException("Directory \"lib\" is not found (should be found at $lib)")
+    }
+
+    val jars = JarsUtils.collectJars(lib, Predicate<File> { file -> !file!!.name.endsWith("javac2.jar") }, false)
+
+    return JarsUtils.makeResolver("Idea `lib` dir: " + lib.canonicalPath, jars)
+  }
+
+  @Throws(IOException::class)
+  private fun getIdeaResolverFromSources(ideaDir: File): Resolver {
+    val pools = arrayListOf<Resolver>()
+
+    pools.add(getIdeResolverFromLibraries(ideaDir))
+
+    if (IdeManagerImpl.isUltimate(ideaDir)) {
+      pools.add(CompileOutputResolver(IdeManagerImpl.getUltimateClassesRoot(ideaDir)))
+      pools.add(getIdeResolverFromLibraries(File(ideaDir, "community")))
+      pools.addAll(hardCodedUltimateLibraries(ideaDir))
+    } else if (IdeManagerImpl.isCommunity(ideaDir)) {
+      pools.add(CompileOutputResolver(IdeManagerImpl.getCommunityClassesRoot(ideaDir)))
+    } else {
+      throw IllegalArgumentException("Incorrect IDEA sources: $ideaDir. It must be Community or Ultimate sources root with compiled class files")
+    }
+
+    return createUnionResolver("Idea dir: " + ideaDir.canonicalPath, pools)
+  }
+
+  private fun hardCodedUltimateLibraries(ideaDir: File): List<Resolver> = HARD_CODED_LIB_FOLDERS
+      .map { File(ideaDir, it) }
+      .filter { it.isDirectory }
+      .mapNotNull { createResolverForLibDir(it) }
+
+  private fun createResolverForLibDir(dir: File): Resolver? = try {
+    JarsUtils.makeResolver(dir.name + " `lib` dir", JarsUtils.collectJars(dir, Predicates.alwaysTrue<File>(), false))
+  } catch (e: Exception) {
+    LOG.warn("Unable to read libraries from " + dir, e)
+    null
+  }
+
+}
