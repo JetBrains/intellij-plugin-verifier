e55b5dc554204351085c2eb916303632ac9586b1
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-07-10 16:34:38 +0300

    specify list elements type

--- plugins-verifier-service/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/featureExtractor/AdaptedFeaturesResult.kt
+++ plugins-verifier-service/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/featureExtractor/AdaptedFeaturesResult.kt
@@ -1,8 +1,9 @@
 package org.jetbrains.plugins.verifier.service.service.featureExtractor
 
 import com.google.gson.annotations.SerializedName
+import com.jetbrains.intellij.feature.extractor.ExtensionPointFeatures
 
 data class AdaptedFeaturesResult(@SerializedName("updateId") val updateId: Int,
                                  @SerializedName("resultType") val resultType: FeaturesResult.ResultType,
-                                 @SerializedName("features") val features: List<Any> = emptyList(),
+                                 @SerializedName("features") val features: List<ExtensionPointFeatures> = emptyList(),
                                  @SerializedName("protocolVersion") val protocolVersion: Int)
