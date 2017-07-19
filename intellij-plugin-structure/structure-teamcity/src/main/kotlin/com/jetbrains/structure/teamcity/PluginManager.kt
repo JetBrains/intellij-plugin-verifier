efd65b765f78c34848b08c01e8eafbdf82589187
satamas <semyon.atamas@jetbrains.com>
2017-07-19 12:04:28 +0300

    Don't extract zip archive.
    (cherry picked from commit fcdf89a)

--- intellij-plugin-structure/structure-teamcity/src/main/kotlin/com/jetbrains/structure/teamcity/PluginManager.kt
+++ intellij-plugin-structure/structure-teamcity/src/main/kotlin/com/jetbrains/structure/teamcity/PluginManager.kt
@@ -7,9 +7,11 @@ import com.jetbrains.structure.plugin.PluginProblem
 import com.jetbrains.structure.problems.*
 import com.jetbrains.structure.teamcity.beans.extractPluginBean
 import com.jetbrains.structure.utils.FileUtil
-import com.jetbrains.structure.utils.ZipUtil
 import org.jdom2.input.JDOMParseException
 import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.util.zip.ZipFile
 
 private val DESCRIPTOR_NAME = "teamcity-plugin.xml"
 
@@ -20,10 +22,8 @@ fun createTeamcityPlugin(pluginFile: File): PluginCreationResult<TeamcityPlugin>
 
   return if (FileUtil.isZip(pluginFile)) {
     try {
-      ZipUtil.withExtractedZipArchive(pluginFile) { extractedArchive: File ->
-        loadDescriptorFromDirectory(extractedArchive)
-      }
-    } catch (e: Throwable){
+      loadDescriptorFromZip(ZipFile(pluginFile))
+    } catch (e: IOException) {
       return PluginCreationFail(UnableToExtractZip(pluginFile))
     }
   } else if (pluginFile.isDirectory) {
@@ -33,25 +33,33 @@ fun createTeamcityPlugin(pluginFile: File): PluginCreationResult<TeamcityPlugin>
   }
 }
 
-private fun loadDescriptorFromDirectory(pluginDirectory: File): PluginCreationResult<TeamcityPlugin> {
-  try {
-    val descriptorFile = File(pluginDirectory, DESCRIPTOR_NAME)
-    if (!descriptorFile.exists()) {
+private fun loadDescriptorFromZip(pluginFile: ZipFile): PluginCreationResult<TeamcityPlugin> {
+  val descriptorEntry = pluginFile.getEntry(DESCRIPTOR_NAME) ?:
       return PluginCreationFail(PluginDescriptorIsNotFound(DESCRIPTOR_NAME))
-    }
-    val bean = extractPluginBean(descriptorFile)
+  return loadDescriptorFromStream(pluginFile.getInputStream(descriptorEntry))
+}
 
+private fun loadDescriptorFromDirectory(pluginDirectory: File): PluginCreationResult<TeamcityPlugin> {
+  val descriptorFile = File(pluginDirectory, DESCRIPTOR_NAME)
+  if (!descriptorFile.exists()) {
+    return PluginCreationFail(PluginDescriptorIsNotFound(DESCRIPTOR_NAME))
+  }
+  return loadDescriptorFromStream(descriptorFile.inputStream())
+}
+
+private fun loadDescriptorFromStream(inputStream: InputStream): PluginCreationResult<TeamcityPlugin> {
+  try {
+    val bean = extractPluginBean(inputStream)
     val beanValidationResult = validateTeamcityPluginBean(bean)
-    if(beanValidationResult.any { it.level == PluginProblem.Level.ERROR }){
+    if (beanValidationResult.any { it.level == PluginProblem.Level.ERROR }) {
       return PluginCreationFail(beanValidationResult)
     }
-
     return PluginCreationSuccess(bean.toPlugin(), beanValidationResult)
   } catch (e: JDOMParseException) {
     val lineNumber = e.lineNumber
-    val message = if(lineNumber != -1) "unexpected element on line " + lineNumber else "unexpected elements"
+    val message = if (lineNumber != -1) "unexpected element on line " + lineNumber else "unexpected elements"
     return PluginCreationFail(UnexpectedDescriptorElements(message))
-  } catch (e: Exception){
+  } catch (e: Exception) {
     return PluginCreationFail(UnableToReadDescriptor())
   }
 }
--- intellij-plugin-structure/structure-teamcity/src/main/kotlin/com/jetbrains/structure/teamcity/beans/TeamcityPluginBeanExtractor.kt
+++ intellij-plugin-structure/structure-teamcity/src/main/kotlin/com/jetbrains/structure/teamcity/beans/TeamcityPluginBeanExtractor.kt
@@ -3,9 +3,9 @@ package com.jetbrains.structure.teamcity.beans
 import org.jdom2.Document
 import org.jdom2.input.SAXBuilder
 import org.jonnyzzz.kotlin.xml.bind.jdom.JDOM
-import java.io.File
+import java.io.InputStream
 
-fun extractPluginBean(descriptorFile: File): TeamcityPluginBean {
-  val rootElement = (SAXBuilder().build(descriptorFile) as Document).rootElement
+fun extractPluginBean(descriptor: InputStream): TeamcityPluginBean {
+  val rootElement = (SAXBuilder().build(descriptor) as Document).rootElement
   return JDOM.load(rootElement, TeamcityPluginBean::class.java)
 }
