36934aa936c0ac5621f72b7dd6ced6ab42488086
Ivan Chirkov <ivan.chirkov@jetbrains.com>
2017-07-21 18:37:57 +0300

    Remove `plugin` from `PluginCreationResult`
    (cherry picked from commit 4af71c6)

--- intellij-plugin-structure/structure-common/src/main/kotlin/com/jetbrains/structure/plugin/PluginCreationResult.kt
+++ intellij-plugin-structure/structure-common/src/main/kotlin/com/jetbrains/structure/plugin/PluginCreationResult.kt
@@ -1,18 +1,14 @@
 package com.jetbrains.structure.plugin
 
-sealed class PluginCreationResult<out PluginType : Plugin> {
-  abstract val plugin: PluginType?
-}
+sealed class PluginCreationResult<out PluginType : Plugin>
 
 data class PluginCreationFail<out PluginType : Plugin>(val errorsAndWarnings: List<PluginProblem>) : PluginCreationResult<PluginType>() {
   constructor(error: PluginProblem) : this(listOf(error))
 
-  override val plugin: PluginType? = null
-
   override fun toString(): String = "Failed: ${errorsAndWarnings.joinToString()}"
 }
 
-data class PluginCreationSuccess<out PluginType : Plugin>(override val plugin: PluginType, val warnings: List<PluginProblem>) :
+data class PluginCreationSuccess<out PluginType : Plugin>(val plugin: PluginType, val warnings: List<PluginProblem>) :
     PluginCreationResult<PluginType>() {
   override fun toString(): String = "Success" + (if (warnings.isNotEmpty()) " but warnings: " + warnings.joinToString() else "")
 }
