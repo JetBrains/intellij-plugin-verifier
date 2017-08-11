5d5b19dc71da33a4f81f82d0c04ce474c4ba0e5a
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-08-11 13:31:16 +0300

    Fix IDE downloader

--- plugins-verifier-service/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/ide/UploadIde.kt
+++ plugins-verifier-service/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/ide/UploadIde.kt
@@ -5,11 +5,8 @@ import com.jetbrains.pluginverifier.misc.deleteLogged
 import com.jetbrains.pluginverifier.repository.AvailableIde
 import com.jetbrains.pluginverifier.repository.IdeRepository
 import org.jetbrains.plugins.verifier.service.ide.IdeFilesManager
-import org.jetbrains.plugins.verifier.service.storage.FileManager
 import org.jetbrains.plugins.verifier.service.tasks.Task
 import org.jetbrains.plugins.verifier.service.tasks.TaskProgress
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 /**
  * @author Sergey Patrikeev
@@ -19,29 +16,18 @@ class UploadIdeRunner(val ideVersion: IdeVersion? = null,
                       val fromSnapshots: Boolean = false) : Task<Boolean>() {
 
   init {
-    require(ideVersion != null || availableIde != null, { "Ide version to be uploaded is not specified" })
+    require(ideVersion != null || availableIde != null, { "IDE version to be uploaded is not specified" })
   }
 
-  private val LOG: Logger = LoggerFactory.getLogger(UploadIdeRunner::class.java)
-
-  override fun presentableName(): String = "UploadIde #${availableIde?.version ?: ideVersion!!}"
+  override fun presentableName(): String = "Downloading IDE #${availableIde?.version ?: ideVersion!!}"
 
   override fun computeResult(progress: TaskProgress): Boolean {
     val artifact = getArtifactInfo() ?: throw IllegalArgumentException("Unable to find the IDE #$ideVersion in snapshots = $fromSnapshots")
 
-    val ideFile = FileManager.createTempFile(".zip")
+    val ideFile = IdeRepository.getOrDownloadIde(artifact) { progress.setFraction(it) }
 
     try {
-      try {
-        IdeRepository.getOrDownloadIde(artifact) { progress.setFraction(it) }
-      } catch(e: Exception) {
-        LOG.error("Unable to download IDE ${artifact.version} community=${artifact.isCommunity} from snapshots=${artifact.isSnapshot}", e)
-        throw e
-      }
-
-      val success = IdeFilesManager.addIde(ideFile)
-      LOG.info("IDE #${artifact.version} ${if (fromSnapshots) "from snapshots repo" else ""} has been added")
-      return success
+      return IdeFilesManager.addIde(ideFile)
     } finally {
       ideFile.deleteLogged()
     }
