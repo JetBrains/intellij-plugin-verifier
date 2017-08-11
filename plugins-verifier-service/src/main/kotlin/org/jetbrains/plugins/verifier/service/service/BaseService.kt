6405439e359c1e6e9ba0ae5ce923135a213d71ed
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-08-11 13:36:27 +0300

    Add service states and possibility to stop and resume the services

--- plugins-verifier-service/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/BaseService.kt
+++ plugins-verifier-service/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/BaseService.kt
@@ -3,17 +3,19 @@ package org.jetbrains.plugins.verifier.service.service
 import com.google.common.util.concurrent.ThreadFactoryBuilder
 import com.google.gson.Gson
 import org.jetbrains.plugins.verifier.service.setting.Settings
-import org.jetbrains.plugins.verifier.service.tasks.TaskManager
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
-abstract class BaseService(private val serviceName: String,
-                           private val initialDelay: Long,
-                           private val period: Long,
-                           private val timeUnit: TimeUnit,
-                           protected val taskManager: TaskManager) {
+abstract class BaseService(val serviceName: String,
+                           initialDelay: Long,
+                           period: Long,
+                           timeUnit: TimeUnit) {
+
+  enum class State {
+    SLEEPING, RUNNING, PAUSED, STOPPED
+  }
 
   companion object {
     val GSON: Gson = Gson()
@@ -21,7 +23,7 @@ abstract class BaseService(private val serviceName: String,
 
   protected val LOG: Logger = LoggerFactory.getLogger(serviceName)
 
-  private var isServing: Boolean = false
+  private var state: State = State.SLEEPING
 
   private val executor = Executors.newSingleThreadScheduledExecutor(
       ThreadFactoryBuilder()
@@ -30,37 +32,52 @@ abstract class BaseService(private val serviceName: String,
           .build()
   )
 
+  init {
+    executor.scheduleAtFixedRate({ tick() }, initialDelay, period, timeUnit)
+  }
+
   protected val pluginRepositoryUserName: String = Settings.PLUGIN_REPOSITORY_VERIFIER_USERNAME.get()
 
   protected val pluginRepositoryPassword: String = Settings.PLUGIN_REPOSITORY_VERIFIER_PASSWORD.get()
 
-  fun start() {
-    executor.scheduleAtFixedRate({ tick() }, initialDelay, period, timeUnit)
+  protected val taskManager = ServerInstance.taskManager
+
+  fun getState() = state
+
+  @Synchronized
+  fun pause() {
+    state = State.PAUSED
   }
 
+  @Synchronized
+  fun resume() {
+    state = State.SLEEPING
+  }
+
+  @Synchronized
   fun stop() {
-    LOG.info("Stopping $serviceName")
-    executor.shutdownNow()
+    if (state != State.STOPPED) {
+      state = State.STOPPED
+      LOG.info("Stopping $serviceName")
+      executor.shutdownNow()
+    }
   }
 
   @Synchronized
   protected fun tick() {
-    if (isServing) {
-      LOG.info("$serviceName is already in progress")
-      return
-    }
-
-    isServing = true
-    val start = System.currentTimeMillis()
-    try {
-      LOG.info("$serviceName is going to start")
-      doTick()
-    } catch (e: Throwable) {
-      LOG.error("$serviceName failed to serve", e)
-    } finally {
-      val duration = System.currentTimeMillis() - start
-      LOG.info("$serviceName has served in $duration ms")
-      isServing = false
+    if (state == State.SLEEPING) {
+      state = State.RUNNING
+      val start = System.currentTimeMillis()
+      try {
+        LOG.info("$serviceName is going to start")
+        doTick()
+      } catch (e: Throwable) {
+        LOG.error("$serviceName failed to serve", e)
+      } finally {
+        val duration = System.currentTimeMillis() - start
+        LOG.info("$serviceName has served in $duration ms")
+        state = State.SLEEPING
+      }
     }
   }
 
