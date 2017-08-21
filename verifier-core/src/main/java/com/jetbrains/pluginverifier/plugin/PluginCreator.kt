b374253681a33701625ad410b64047fd806ec905
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-08-21 13:17:20 +0300

    Catch an exception on resolver creation and return 'bad-plugin' result instead of exception

--- verifier-core/src/main/java/com/jetbrains/pluginverifier/plugin/PluginCreator.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/plugin/PluginCreator.kt
@@ -7,6 +7,7 @@ import com.intellij.structure.plugin.PluginManager
 import com.intellij.structure.problems.PluginProblem
 import com.intellij.structure.resolvers.Resolver
 import com.jetbrains.pluginverifier.api.PluginCoordinate
+import com.jetbrains.pluginverifier.misc.closeLogged
 import com.jetbrains.pluginverifier.misc.closeOnException
 import com.jetbrains.pluginverifier.repository.FileLock
 import com.jetbrains.pluginverifier.repository.RepositoryManager
@@ -24,10 +25,8 @@ object PluginCreator {
     is PluginCoordinate.ByUpdateInfo -> createPluginByUpdateInfo(pluginCoordinate.updateInfo)
   }
 
-  private fun downloadPluginByUpdateInfo(updateInfo: UpdateInfo): FileLock? = RepositoryManager.getPluginFile(updateInfo)
-
   fun createPluginByUpdateInfo(updateInfo: UpdateInfo): CreatePluginResult {
-    val pluginFileLock = downloadPluginByUpdateInfo(updateInfo)
+    val pluginFileLock = RepositoryManager.getPluginFile(updateInfo)
         ?: return CreatePluginResult.NotFound("Plugin $updateInfo is not found in the Plugin Repository")
     return createPluginByFileLock(pluginFileLock)
   }
@@ -38,32 +37,34 @@ object PluginCreator {
     override fun getFile(): File = backedFile
   }
 
-  fun createPluginByFile(pluginFile: File) = createPluginResultByFileAndRegisterLock(pluginFile, IdleFileLock(pluginFile))
+  fun createPluginByFile(pluginFile: File) = createPluginByFileLock(IdleFileLock(pluginFile))
 
-  fun createPluginByFileLock(pluginFileLock: FileLock): CreatePluginResult =
-      createPluginResultByFileAndRegisterLock(pluginFileLock.getFile(), pluginFileLock)
-
-  private fun createPluginResultByFileAndRegisterLock(pluginFile: File, pluginFileLock: FileLock): CreatePluginResult {
+  fun createPluginByFileLock(pluginFileLock: FileLock): CreatePluginResult {
     pluginFileLock.closeOnException {
-      val pluginCreationResult = PluginManager.getInstance().createPlugin(pluginFile)
-      if (pluginCreationResult is PluginCreationSuccess) {
-        val pluginResolver = Resolver.createPluginResolver(pluginCreationResult.plugin)
-        return CreatePluginResult.OK(pluginCreationResult.plugin, pluginCreationResult.warnings, pluginResolver, pluginFileLock)
+      val pluginFile = pluginFileLock.getFile()
+
+      val creationResult = PluginManager.getInstance().createPlugin(pluginFile)
+      if (creationResult is PluginCreationSuccess) {
+        val pluginResolver = createResolverByPluginOrCloseLock(creationResult.plugin, pluginFileLock) ?: return CreatePluginResult.BadPlugin(listOf(UnableToReadPluginClassFilesProblem))
+        return CreatePluginResult.OK(creationResult.plugin, creationResult.warnings, pluginResolver, pluginFileLock)
       } else {
         pluginFileLock.close()
-        return CreatePluginResult.BadPlugin((pluginCreationResult as PluginCreationFail).errorsAndWarnings)
+        return CreatePluginResult.BadPlugin((creationResult as PluginCreationFail).errorsAndWarnings)
       }
     }
   }
 
   fun createResultByExistingPlugin(plugin: Plugin): CreatePluginResult {
-    val resolver = try {
-      Resolver.createPluginResolver(plugin)
-    } catch (e: Exception) {
-      LOG.debug("Unable to read plugin $plugin class files", e)
-      return CreatePluginResult.BadPlugin(listOf(UnableToReadPluginClassFilesProblem))
-    }
+    val resolver = createResolverByPluginOrCloseLock(plugin, null) ?: return CreatePluginResult.BadPlugin(listOf(UnableToReadPluginClassFilesProblem))
     return CreatePluginResult.OK(plugin, emptyList(), resolver, IdleFileLock(File("")))
+  }
+
+  private fun createResolverByPluginOrCloseLock(plugin: Plugin, fileLock: FileLock?): Resolver? = try {
+    Resolver.createPluginResolver(plugin)
+  } catch (e: Exception) {
+    LOG.debug("Unable to read plugin $plugin class files", e)
+    fileLock?.closeLogged()
+    null
   }
 
   object UnableToReadPluginClassFilesProblem : PluginProblem() {
