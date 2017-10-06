5aa1ddf60fea85da9dee9d033e654a0ae3bcfd17
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-10-06 15:24:46 +0300

    Merge all command-line options into one file.

--- verifier-core/src/main/java/com/jetbrains/pluginverifier/options/CmdOpts.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/options/CmdOpts.kt
@@ -24,9 +24,39 @@ open class CmdOpts(
     @set:Argument("documented-problems-url", alias = "dpu", description = "The URL of the page containing documented problems that must not be reported. " +
         "By default it is $DEFAULT_DOCUMENTED_PROBLEMS_PAGE_URL that contains the sources of the page of " +
         "SDK Documentation: http://www.jetbrains.org/intellij/sdk/docs/reference_guide/api_changes_list.html")
-    var documentedProblemsPageUrl: String? = DEFAULT_DOCUMENTED_PROBLEMS_PAGE_URL
+    var documentedProblemsPageUrl: String? = DEFAULT_DOCUMENTED_PROBLEMS_PAGE_URL,
 
-) : PublicOpts() {
+    @set:Argument("runtime-dir", alias = "r", description = "The path to directory containing Java runtime jars (e.g. /usr/lib/jvm/java-8-oracle ")
+    var runtimeDir: String? = null,
+
+    @set:Argument("team-city", alias = "tc", description = "Specify this flag if you want to print the TeamCity compatible output on stdout.")
+    var needTeamCityLog: Boolean = false,
+
+    @set:Argument("tc-grouping", alias = "g", description = "How to group the TeamCity presentation of the problems: either 'plugin' to group by each plugin or 'problem_type' to group by problem type")
+    var teamCityGroupType: String? = null,
+
+    @set:Argument("plugins-to-check-all-builds", alias = "p-all", delimiter = ":", description = "The plugin ids to check with IDE. The plugin verifier will check ALL compatible plugin builds")
+    var pluginToCheckAllBuilds: Array<String> = arrayOf(),
+
+    @set:Argument("plugins-to-check-last-builds", alias = "p-last", delimiter = ":", description = "The plugin ids to check with IDE. The plugin verifier will check LAST plugin build only")
+    var pluginToCheckLastBuild: Array<String> = arrayOf(),
+
+    @set:Argument("excluded-plugins-file", alias = "epf", description = "File with list of excluded plugin builds (e.g. '<IDE-home>/lib/resources.jar/brokenPlugins.txt')")
+    var excludedPluginsFile: String? = null,
+
+    @set:Argument("dump-broken-plugin-list", alias = "d", description = "File to dump broken plugin ids. The broken plugins are those which contain at least one problem as a result of the verification")
+    var dumpBrokenPluginsFile: String? = null,
+
+    @set:Argument("html-report", description = "Create HTML report of broken plugins")
+    var htmlReportFile: String? = null,
+
+    @set:Argument("plugins-to-check-file", alias = "ptcf", description = "File that contains list of plugins to check (e.g. '<IDE-home>/lib/resources.jar/checkedPlugins.txt')")
+    var pluginsToCheckFile: String? = null,
+
+    @set:Argument("external-prefixes", alias = "ex-prefixes", delimiter = ":", description = "The prefixes of classes from the external libraries. The Verifier will not report 'No such class' for such classes.")
+    var externalClassesPrefixes: Array<String> = arrayOf()
+
+) {
   companion object {
     const val DEFAULT_DOCUMENTED_PROBLEMS_PAGE_URL = "https://raw.githubusercontent.com/JetBrains/intellij-sdk-docs/master/reference_guide/api_changes_list.md"
   }
