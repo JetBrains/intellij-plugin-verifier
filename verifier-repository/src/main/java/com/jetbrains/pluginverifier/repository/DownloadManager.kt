92ae59eaa30831ff91f2712074c0d50938a06b10
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-11-08 10:09:47 +0300

    Fix correctness of the download manager: lock the plugin file in advance to avoid race conditions when one thread attempts to get the plugin and another thread deletes it

--- verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/DownloadManager.kt
+++ verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/DownloadManager.kt
@@ -3,6 +3,7 @@ package com.jetbrains.pluginverifier.repository
 import com.google.common.primitives.Ints
 import com.google.common.util.concurrent.ThreadFactoryBuilder
 import com.jetbrains.pluginverifier.misc.bytesToMegabytes
+import com.jetbrains.pluginverifier.misc.closeOnException
 import com.jetbrains.pluginverifier.misc.deleteLogged
 import com.jetbrains.pluginverifier.network.NotFound404ResponseException
 import okhttp3.MediaType
@@ -21,18 +22,20 @@ class DownloadManager(private val downloadDir: File,
                       downloadDirMaxSpace: Long,
                       private val downloader: (UpdateInfo) -> Response<ResponseBody>) {
 
-  companion object {
-    private val LOG: Logger = LoggerFactory.getLogger(DownloadManager::class.java)
+  private companion object {
+    val LOG: Logger = LoggerFactory.getLogger(DownloadManager::class.java)
 
-    private val TEMP_DOWNLOAD_PREFIX = "plugin_"
+    val TEMP_DOWNLOAD_PREFIX = "plugin_"
 
-    private val TEMP_DOWNLOAD_SUFFIX = "_download"
+    val TEMP_DOWNLOAD_SUFFIX = "_download"
 
-    private val FORGOTTEN_LOCKS_GC_TIMEOUT_MS: Long = TimeUnit.HOURS.toMillis(8)
+    val FORGOTTEN_LOCKS_GC_TIMEOUT_MS: Long = TimeUnit.HOURS.toMillis(8)
 
-    private val BROKEN_FILE_THRESHOLD_BYTES = 200
+    val BROKEN_FILE_THRESHOLD_BYTES = 200
 
-    private val JAR_CONTENT_TYPE = MediaType.parse("application/java-archive")
+    fun File.isCorrupterPluginFile(): Boolean = length() < BROKEN_FILE_THRESHOLD_BYTES
+
+    val JAR_CONTENT_TYPE = MediaType.parse("application/java-archive")
   }
 
   private inner class FileLockImpl(val locked: File, val id: Long, val lockDate: Long) : FileLock() {
@@ -49,7 +52,7 @@ class DownloadManager(private val downloadDir: File,
             .setDaemon(true)
             .setNameFormat("download-mng-gc-%d")
             .build()
-    ).scheduleAtFixedRate({ releaseOldLocksAndDeleteUnusedPlugins() }, 5, 30, TimeUnit.SECONDS)
+    ).scheduleAtFixedRate({ removeUnusedPlugins() }, 5, 30, TimeUnit.SECONDS)
   }
 
   private var nextId: Long = 0
@@ -64,7 +67,7 @@ class DownloadManager(private val downloadDir: File,
   private val spaceWatcher = FreeDiskSpaceWatcher(downloadDir, downloadDirMaxSpace)
 
   @Synchronized
-  private fun releaseOldLocksAndDeleteUnusedPlugins() {
+  private fun removeUnusedPlugins() {
     val spaceReport = spaceWatcher.getSpaceReport()
     LOG.info("It's time to remove unused plugins from cache. Download cache usage: ${spaceReport.usedSpace.bytesToMegabytes()} Mb; " +
         "Estimated available space (Mb): ${spaceReport.availableSpace.bytesToMegabytes()}")
@@ -100,7 +103,6 @@ class DownloadManager(private val downloadDir: File,
       LOG.warn("Available space after cleanup is not sufficient! ${spaceReport.availableSpace.bytesToMegabytes()} Mb < ${spaceReport.lowSpaceThreshold.bytesToMegabytes()} Mb")
     }
   }
-
 
   private fun releaseOldLocks() {
     busyLocks.values
@@ -144,20 +146,21 @@ class DownloadManager(private val downloadDir: File,
     }
   }
 
-  private fun getCachedFile(updateInfo: UpdateInfo): File? {
+  @Synchronized
+  private fun getCachedPlugin(updateInfo: UpdateInfo): FileLock? {
     val updateId = updateInfo.updateId
     val asZip = File(downloadDir, "$updateId.zip")
-    if (asZip.exists() && asZip.length() >= BROKEN_FILE_THRESHOLD_BYTES) {
-      return asZip
+    if (asZip.exists() && !asZip.isCorrupterPluginFile()) {
+      return registerLock(asZip)
     }
     val asJar = File(downloadDir, "$updateId.jar")
-    if (asJar.exists() && asJar.length() >= BROKEN_FILE_THRESHOLD_BYTES) {
-      return asJar
+    if (asJar.exists() && !asJar.isCorrupterPluginFile()) {
+      return registerLock(asJar)
     }
     return null
   }
 
-  private fun doDownloadAndGuessFileName(updateInfo: UpdateInfo, tempFile: File): String {
+  private fun doDownloadAndGuessPluginFileName(updateInfo: UpdateInfo, tempFile: File): String {
     val updateId = updateInfo.updateId
     val response = downloader(updateInfo)
     FileUtils.copyInputStreamToFile(response.body().byteStream(), tempFile)
@@ -165,97 +168,110 @@ class DownloadManager(private val downloadDir: File,
     return "$updateId.$extension"
   }
 
-  private fun guessExtension(response: Response<ResponseBody>): String {
-    if (response.body().contentType() == JAR_CONTENT_TYPE) {
-      return "jar"
-    }
-    return "zip"
-  }
-
-  private fun downloadToTempFile(updateInfo: UpdateInfo, tempFile: File): String {
-    val updateFileName = doDownloadAndGuessFileName(updateInfo, tempFile)
-    if (tempFile.length() < BROKEN_FILE_THRESHOLD_BYTES) {
-      throw IOException("Too small update $updateInfo size: ${tempFile.length()} bytes")
-    }
-    return updateFileName
-  }
+  private fun guessExtension(response: Response<ResponseBody>): String =
+      if (response.body().contentType() == JAR_CONTENT_TYPE) {
+        "jar"
+      } else {
+        "zip"
+      }
 
   /**
    *  Provides the thread safety when multiple threads attempt to load the same plugin.
    *
-   *  @return true if tempFile has been moved to cached, false otherwise
+   *  @return true if [tempFile] has been moved to [finalFile], false otherwise
    */
   @Synchronized
-  private fun moveDownloaded(updateInfo: UpdateInfo, tempFile: File, cached: File): Boolean {
-    if (cached.exists()) {
-      if (cached.length() >= BROKEN_FILE_THRESHOLD_BYTES) {
-        LOG.debug("Update $updateInfo is concurrently loaded by another thread to $cached")
+  private fun moveDownloaded(tempFile: File, finalFile: File, updateInfo: UpdateInfo): Boolean {
+    if (finalFile.exists()) {
+      if (finalFile.isCorrupterPluginFile()) {
+        FileUtils.forceDelete(finalFile)
+      } else {
         return false
       }
-
-      if (locksAcquired.getOrDefault(cached, 0) > 0) {
-        //we can't delete the cached plugin right now
-        return false
-      }
-      FileUtils.forceDelete(cached)
     }
 
-    FileUtils.moveFile(tempFile, cached)
+    FileUtils.moveFile(tempFile, finalFile)
     return true
   }
 
-  private fun downloadUpdate(updateInfo: UpdateInfo, tempFile: File): File {
-    val cachedUpdate = downloadUpdateToTempFileAndGuessFileName(updateInfo, tempFile)
-    if (moveDownloaded(updateInfo, tempFile, cachedUpdate)) {
-      LOG.debug("Update $updateInfo is saved to $cachedUpdate")
+  private fun downloadPlugin(updateInfo: UpdateInfo, tempFile: File): FileLock {
+    val guessedFileName = doDownloadAndGuessPluginFileName(updateInfo, tempFile)
+    if (tempFile.isCorrupterPluginFile()) {
+      throw IOException(getCorruptedMessage(updateInfo, tempFile))
     }
-    return cachedUpdate
-  }
-
-  private fun downloadUpdateToTempFileAndGuessFileName(updateInfo: UpdateInfo, tempFile: File): File {
-    val updateFileName = downloadToTempFile(updateInfo, tempFile)
-    return File(downloadDir, updateFileName)
-  }
-
-  private fun downloadUpdate(updateInfo: UpdateInfo): File {
-    LOG.debug("Downloading update $updateInfo")
-    val tempFile = File.createTempFile(TEMP_DOWNLOAD_PREFIX, TEMP_DOWNLOAD_SUFFIX, downloadDir)
-    try {
-      return downloadUpdate(updateInfo, tempFile)
-    } finally {
-      if (tempFile.exists()) {
-        tempFile.deleteLogged()
-      }
-    }
-  }
-
-  fun getOrDownloadPlugin(updateInfo: UpdateInfo): DownloadPluginResult {
-    var pluginFile = getCachedFile(updateInfo)
-
-    if (pluginFile == null || pluginFile.length() < BROKEN_FILE_THRESHOLD_BYTES) {
-      try {
-        pluginFile = downloadUpdate(updateInfo)
-      } catch (e: NotFound404ResponseException) {
-        return DownloadPluginResult.NotFound("Plugin $updateInfo is not found the Plugin Repository")
-      } catch (e: Exception) {
-        val message = "Unable to download update $updateInfo" + (if (e.message != null) " " + e.message else "")
-        LOG.info(message, e)
-        return DownloadPluginResult.FailedToDownload(message + ": " + e.message)
-      }
-    }
-
+    val pluginFile = File(downloadDir, guessedFileName)
+    /*
+    We must register the file lock before moving the temp file to the final file because
+    there could be another thread which removes the final file before we managed to register a lock for it.
+    This is not the case for the temp file: we can operate with the temp file directly because only the current thread has access to it.
+     */
     val lock = registerLock(pluginFile)
+    val moved = lock.closeOnException {
+      moveDownloaded(tempFile, pluginFile, updateInfo)
+    }
+    if (moved) {
+      LOG.debug("Plugin $updateInfo is saved into $pluginFile")
+    } else {
+      LOG.debug("Another thread has downloaded the same plugin $updateInfo into $pluginFile")
+    }
+    return lock
+  }
+
+  private fun getCorruptedMessage(updateInfo: UpdateInfo, tempFile: File) =
+      "Corrupter plugin file $updateInfo: the file size is only ${tempFile.length()} bytes"
+
+  private fun downloadPlugin(updateInfo: UpdateInfo): FileLock {
+    val tempFile = File.createTempFile(TEMP_DOWNLOAD_PREFIX, TEMP_DOWNLOAD_SUFFIX, downloadDir)
+    LOG.debug("Downloading update $updateInfo to $tempFile")
+    try {
+      return downloadPlugin(updateInfo, tempFile)
+    } finally {
+      tempFile.deleteLogged()
+    }
+  }
+
+  /**
+   * Searches the plugin by [updateInfo] coordinates in the local cache and
+   * in case it isn't found there, downloads the plugin from the repository.
+   *
+   * The possible results are represented as subclasses of [DownloadPluginResult].
+   * If the plugin is found locally or successfully downloaded, the [FileLock] is registered
+   * for the plugin's file so it will be protected against deletions by other threads.
+   *
+   * This method is thread safe. In case several threads attempt to get the same plugin, only one
+   * of them will download it while others will wait for the first to complete. TODO: implement this.
+   */
+  fun getOrDownloadPlugin(updateInfo: UpdateInfo): DownloadPluginResult {
+    val cachedPlugin = getCachedPlugin(updateInfo)
+    if (cachedPlugin != null) {
+      if (cachedPlugin.getFile().isCorrupterPluginFile()) {
+        cachedPlugin.release()
+      } else {
+        return DownloadPluginResult.Found(cachedPlugin)
+      }
+    }
+
+    val downloadedPlugin = try {
+      downloadPlugin(updateInfo)
+    } catch (e: NotFound404ResponseException) {
+      return DownloadPluginResult.NotFound("Plugin $updateInfo is not found the Plugin Repository")
+    } catch (e: Exception) {
+      //todo: provide a human readable error message: maybe find a library capable of this?
+      val message = "Unable to download plugin $updateInfo" + (if (e.message != null) " " + e.message else "")
+      LOG.info(message, e)
+      return DownloadPluginResult.FailedToDownload(message + ": " + e.message)
+    }
 
     try {
       val spaceReport = spaceWatcher.getSpaceReport()
       if (spaceReport.availableSpace < spaceReport.lowSpaceThreshold) {
-        releaseOldLocksAndDeleteUnusedPlugins()
+        removeUnusedPlugins()
       }
     } catch (e: Throwable) {
-      lock.release()
+      downloadedPlugin.release()
       throw e
     }
 
-    return DownloadPluginResult.Found(lock)
+    return DownloadPluginResult.Found(downloadedPlugin)
   }
 }
