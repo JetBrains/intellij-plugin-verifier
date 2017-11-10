2570f8f9ff47435522416ce0967f4b4715ce3c58
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-11-10 16:17:57 +0300

    Super name is null means the class node is Object.

--- verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/clazz/InheritFromFinalClassVerifier.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/clazz/InheritFromFinalClassVerifier.kt
@@ -1,12 +1,15 @@
 package com.jetbrains.pluginverifier.verifiers.clazz
 
 import com.jetbrains.pluginverifier.results.problems.InheritFromFinalClassProblem
-import com.jetbrains.pluginverifier.verifiers.*
+import com.jetbrains.pluginverifier.verifiers.VerificationContext
+import com.jetbrains.pluginverifier.verifiers.fromClass
+import com.jetbrains.pluginverifier.verifiers.isFinal
+import com.jetbrains.pluginverifier.verifiers.resolveClassOrProblem
 import org.objectweb.asm.tree.ClassNode
 
 class InheritFromFinalClassVerifier : ClassVerifier {
   override fun verify(clazz: ClassNode, ctx: VerificationContext) {
-    val superClassName = clazz.superName ?: ClassParentsVisitor.JAVA_LANG_OBJECT
+    val superClassName = clazz.superName ?: return
     val supClass = ctx.resolveClassOrProblem(superClassName, clazz, { ctx.fromClass(clazz) }) ?: return
     if (supClass.isFinal()) {
       val child = ctx.fromClass(clazz)
