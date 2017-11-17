96c98bd9a0f0024e6ea1e5a302f6b7d3dfd635f1
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-11-17 17:35:30 +0300

    Remove unused parameter.

--- verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/cleanup/DiskSpaceSetting.kt
+++ verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/cleanup/DiskSpaceSetting.kt
@@ -4,6 +4,5 @@ import org.apache.commons.io.FileUtils
 
 data class DiskSpaceSetting(
     val maxSpaceUsage: Long,
-    val lowSpaceThreshold: Long = FileUtils.ONE_GB,
-    val enoughSpaceThreshold: Long = lowSpaceThreshold * 3
+    val lowSpaceThreshold: Long = FileUtils.ONE_GB
 )
