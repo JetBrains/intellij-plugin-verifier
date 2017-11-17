ee3db6ed6c1b2ad05998ec745457b8f8be65ca82
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-11-17 16:08:57 +0300

    Extract IDE downloading logic into a separate class.

--- verifier-repository/src/main/java/com/jetbrains/pluginverifier/ide/IdeDownloader.kt
+++ verifier-repository/src/main/java/com/jetbrains/pluginverifier/ide/IdeDownloader.kt
@@ -1,129 +1,73 @@
 package com.jetbrains.pluginverifier.ide
 
-import com.google.gson.Gson
 import com.jetbrains.plugin.structure.intellij.version.IdeVersion
 import com.jetbrains.pluginverifier.misc.deleteLogged
 import com.jetbrains.pluginverifier.misc.extractTo
 import com.jetbrains.pluginverifier.misc.makeOkHttpClient
+import com.jetbrains.pluginverifier.network.copyInputStreamWithProgress
 import com.jetbrains.pluginverifier.network.executeSuccessfully
+import com.jetbrains.pluginverifier.repository.downloader.DownloadResult
+import com.jetbrains.pluginverifier.repository.downloader.DownloadUrlProvider
+import com.jetbrains.pluginverifier.repository.downloader.Downloader
 import okhttp3.ResponseBody
-import org.apache.commons.io.FileUtils
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Streaming
 import retrofit2.http.Url
 import java.io.File
-import java.io.InputStream
-import java.nio.file.Files
+import java.net.URL
 import java.util.concurrent.TimeUnit
 
-class IdeDownloader(repositoryUrl: String, private val downloadDir: File) {
+private interface IdeRepositoryConnector {
+  @GET
+  @Streaming
+  fun downloadIde(@Url downloadUrl: String): Call<ResponseBody>
+}
 
-  companion object {
-    private val LOG: Logger = LoggerFactory.getLogger(IdeRepository::class.java)
-  }
 
-  private interface IdeRepositoryConnector {
+class IdeDownloader(private val ideRepositoryUrl: String,
+                    private val ideDownloadUrlProvider: DownloadUrlProvider<IdeVersion>,
+                    private val downloadProgress: (Double) -> Unit) : Downloader<IdeVersion> {
 
-    @GET
-    @Streaming
-    fun downloadIde(@Url downloadUrl: String): Call<ResponseBody>
-
-  }
-
-  private val repositoryConnector: IdeRepositoryConnector = Retrofit.Builder()
-      .baseUrl(repositoryUrl.trimEnd('/') + '/')
-      .addConverterFactory(GsonConverterFactory.create(Gson()))
+  private val repositoryConnector = Retrofit.Builder()
+      .baseUrl(ideRepositoryUrl.trimEnd('/') + '/')
       .client(makeOkHttpClient(false, 5, TimeUnit.MINUTES))
       .build()
       .create(IdeRepositoryConnector::class.java)
 
 
-  private fun getIdeDirForVersion(ideVersion: IdeVersion) = File(downloadDir, ideVersion.toString())
+  override fun download(key: IdeVersion, destination: File): DownloadResult {
+    val downloadUrl = ideDownloadUrlProvider.getDownloadUrl(key)
+        ?: return DownloadResult.NotFound("IDE $key is not found in $ideRepositoryUrl")
 
-  private fun downloadAndExtractIde(availableIde: AvailableIde, progress: (Double) -> Unit): File {
-    val tempIdeZip = File.createTempFile("ide-${availableIde.version}", ".zip", downloadDir)
+    val zippedIde = destination.resolve(destination.name + ".zip")
     try {
-      return downloadIdeToTempZipAndExtract(availableIde, tempIdeZip, progress)
-    } finally {
-      tempIdeZip.deleteLogged()
-    }
-  }
-
-  private fun downloadIdeToTempZipAndExtract(availableIde: AvailableIde, tempZip: File, progress: (Double) -> Unit): File {
-    downloadIdeZipTo(availableIde, tempZip, progress)
-
-    val tempIdeDir = Files.createTempDirectory(downloadDir.toPath(), "ide").toFile()
-    try {
-      return extractIdeZipToTempDirectoryAndMove(tempZip, tempIdeDir, availableIde)
-    } finally {
-      tempIdeDir.deleteLogged()
-    }
-  }
-
-  private fun extractIdeZipToTempDirectoryAndMove(tempZip: File, tempIdeDir: File, availableIde: AvailableIde): File {
-    tempZip.extractTo(tempIdeDir)
-    synchronized(this) {
-      val ideDir = getIdeDirForVersion(availableIde.version)
-      if (!ideDir.exists()) {
-        FileUtils.moveDirectory(tempIdeDir, ideDir)
+      try {
+        doDownloadTo(downloadUrl, zippedIde)
+      } catch (e: Exception) {
+        //todo: provide clearer messages
+        return DownloadResult.FailedToDownload("Unable to download $key: ${e.message}", e)
       }
-      return ideDir
+
+      return try {
+        zippedIde.extractTo(destination)
+        DownloadResult.Downloaded("zip")
+      } catch (e: Exception) {
+        DownloadResult.FailedToDownload("Unable to save $key: ${e.message}", e)
+      }
+    } finally {
+      zippedIde.deleteLogged()
     }
   }
 
-  fun getOrDownloadIde(availableIde: AvailableIde, progress: (Double) -> Unit): File {
-    val ideVersion = availableIde.version
-    val ideDir = getIdeDirForVersion(ideVersion)
-    return if (ideDir.isDirectory && ideDir.list().orEmpty().isNotEmpty()) {
-      LOG.info("IDE #$ideVersion is found in $ideDir")
-      ideDir
-    } else {
-      LOG.info("Downloading IDE $availableIde")
-      val downloadedIde = downloadAndExtractIde(availableIde, progress)
-      LOG.info("Successfully downloaded to $downloadedIde")
-      downloadedIde
-    }
-  }
-
-
-  private fun downloadIdeZipTo(availableIde: AvailableIde, saveTo: File, progress: ((Double) -> Unit)) {
-    val downloadUrl = availableIde.downloadUrl.toExternalForm()
-    val response = repositoryConnector.downloadIde(downloadUrl).executeSuccessfully()
+  private fun doDownloadTo(downloadUrl: URL, destination: File) {
+    val response = repositoryConnector.downloadIde(downloadUrl.toExternalForm()).executeSuccessfully()
     val expectedSize = response.body().contentLength()
-    val stream = response.body().byteStream()
-    stream.use { inputStream ->
-      copyInputStreamWithProgress(inputStream, expectedSize, saveTo, progress)
+    response.body().byteStream().use { bodyStream ->
+      copyInputStreamWithProgress(bodyStream, expectedSize, destination, downloadProgress)
     }
   }
 
-  private fun copyInputStreamWithProgress(inputStream: InputStream,
-                                          expectedSize: Long,
-                                          toFile: File,
-                                          progress: (Double) -> Unit) {
-    val buffer = ByteArray(4 * 1024)
-    if (expectedSize == 0L) {
-      throw IllegalArgumentException("File is empty")
-    }
-
-    inputStream.use { input ->
-      toFile.outputStream().buffered().use { output ->
-        var count: Long = 0
-        var n: Int
-        while (true) {
-          n = input.read(buffer)
-          if (n == -1) break
-          output.write(buffer, 0, n)
-          count += n
-          progress(count.toDouble() / expectedSize)
-        }
-      }
-    }
-  }
 
 }
-
