8e71d9f1fbd2dea7d74a4b4a8496d997cf55abca
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-11-29 17:07:08 +0300

    Increase timeout for fetching the documented problems page.

--- verifier-core/src/main/java/com/jetbrains/pluginverifier/parameters/filtering/documented/DocumentedProblemsFetcher.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/parameters/filtering/documented/DocumentedProblemsFetcher.kt
@@ -2,6 +2,7 @@ package com.jetbrains.pluginverifier.parameters.filtering.documented
 
 import org.jsoup.Connection
 import org.jsoup.Jsoup
+import java.util.concurrent.TimeUnit
 
 /**
  * @author Sergey Patrikeev
@@ -11,7 +12,7 @@ class DocumentedProblemsFetcher {
   fun fetchPage(documentedPageUrl: String): String =
       Jsoup
           .connect(documentedPageUrl)
-          .timeout(3000)
+          .timeout(TimeUnit.MINUTES.toMillis(5).toInt())
           .method(Connection.Method.GET)
           .execute()
           .body()
