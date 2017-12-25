82855d5f2b3ae6c5d0915ad7d953d10fcb92b3fb
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2017-12-25 13:30:27 +0300

    Fix the code style.

---
+++ intellij-plugin-structure/structure-teamcity/src/main/kotlin/com/jetbrains/plugin/structure/teamcity/problems/ForbiddenWordInPluginName.kt
@@ -0,0 +1,12 @@
+package com.jetbrains.plugin.structure.teamcity.problems
+
+import com.jetbrains.plugin.structure.base.plugin.PluginProblem
+
+object ForbiddenWordInPluginName : PluginProblem() {
+  val forbiddenWords = listOf("teamcity", "plugin")
+
+  override val level = PluginProblem.Level.ERROR
+
+  override val message = "Plugin name should not contain the following words: ${forbiddenWords.joinToString()}"
+
+}
