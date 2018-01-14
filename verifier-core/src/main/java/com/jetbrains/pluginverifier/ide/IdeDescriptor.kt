6055265f5a2be742680692a348a36f21e5b127ff
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2018-01-14 22:21:43 +0300

    Provide a javadoc.

--- verifier-core/src/main/java/com/jetbrains/pluginverifier/ide/IdeDescriptor.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/ide/IdeDescriptor.kt
@@ -5,10 +5,13 @@ import com.jetbrains.plugin.structure.ide.Ide
 import com.jetbrains.plugin.structure.intellij.version.IdeVersion
 import java.io.Closeable
 
+/**
+ * Holder of the [IDE] [ide] instance and its class files [resolver] [ideResolver].
+ */
 data class IdeDescriptor(val ide: Ide, val ideResolver: Resolver) : Closeable {
   val ideVersion: IdeVersion = ide.version
 
-  override fun toString(): String = "$ideVersion"
+  override fun toString() = ideVersion.toString()
 
   override fun close() = ideResolver.close()
 }
