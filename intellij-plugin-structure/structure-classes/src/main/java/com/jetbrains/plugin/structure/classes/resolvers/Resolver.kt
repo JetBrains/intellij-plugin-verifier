f610ec127f67dcc4211006f800535a6f72732827
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2018-01-15 10:39:59 +0300

    Convert java to kotlin (code conversion).

--- intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/resolvers/Resolver.kt
+++ intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/resolvers/Resolver.kt
@@ -1,82 +1,79 @@
 package com.jetbrains.plugin.structure.classes.resolvers
 
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.annotations.Nullable
 import org.objectweb.asm.tree.ClassNode
+
+import java.io.Closeable
 import java.io.File
 import java.io.IOException
-import java.util.Set
 
 /**
- * <p>Provides an access to the byte-code of a class by its name via the {@link #findClass(String)}.
- * Note that the way of constructing the {@code Resolver} affects the searching order
- * (it is similar to the Java <i>class-path</i> option)
- * <p>Note: the class is {@code Closeable} thus the {@code Resolver} requires to be closed when it is no longer needed.
- * Some resolvers may extract the classes in the temporary directory for performance reasons, so {@link #close()} will
- * clean the used disk space.</p>
+ *
+ * Provides an access to the byte-code of a class by its name via the [.findClass].
+ * Note that the way of constructing the `Resolver` affects the searching order
+ * (it is similar to the Java *class-path* option)
+ *
+ * Note: the class is `Closeable` thus the `Resolver` requires to be closed when it is no longer needed.
+ * Some resolvers may extract the classes in the temporary directory for performance reasons, so [.close] will
+ * clean the used disk space.
  */
-abstract class Resolver implements Closeable {
+abstract class Resolver : Closeable {
 
   /**
-   * Returns a class-file node with the specified name. If {@code this} resolver contains multiple
-   * instances of classes with the same name the first one in the <i>class-path</i> order will be returned.
+   * Returns the *binary* names of all the contained classes.
    *
-   * @param className class name in <i>binary</i> form (see JVM specification)
-   * @return a class-node for accessing the bytecode
-   * @throws IOException if IO error occurs, e.g. file .class-file was deleted
+   * @return all the classes names in the *binary* form.
    */
-  @Nullable
-  public abstract ClassNode findClass (@NotNull String className) throws IOException
+  abstract val allClasses: Set<String>
 
   /**
-   * Returns the resolver which contains the given class: invocation of {@link #findClass(String)} on the
-   * resulting {@code Resolver} returns the same instance of the class-node as an invocation of {@link #findClass(String)}
-   * on {@code this} instance.
-   *
-   * @param className class name for which resolver should be found (in <i>binary</i> form)
-   * @return actual class resolver or {@code null} if {@code this} resolver doesn't contain a specified class
-   */
-  @Nullable
-  public abstract Resolver getClassLocation (@NotNull String className)
-
-  /**
-   * Returns the <i>binary</i> names of all the contained classes.
-   *
-   * @return all the classes names in the <i>binary</i> form.
-   */
-  @NotNull
-  public abstract Set<String> getAllClasses ()
-
-  /**
-   * Checks whether this resolver contains any class. Classes can be obtained through {@link #getAllClasses()}.
+   * Checks whether this resolver contains any class. Classes can be obtained through [.getAllClasses].
    *
    * @return true if this resolver is not empty, false otherwise
    */
-  public abstract boolean isEmpty ()
-
-  /**
-   * Returns true if {@code this} Resolver contains the given class. It may be faster
-   * than checking {@link #findClass(String)} is not null.
-   *
-   * @param className class name in <i>binary</i> form (see JVM specification)
-   * @return {@code true} if class is in Resolver, {@code false} otherwise
-   */
-  public abstract boolean containsClass (@NotNull String className)
+  abstract val isEmpty: Boolean
 
   /**
    * Returns the roots from which this resolver loads the classes
    *
    * @return roots of the classes
    */
-  @NotNull
-  public abstract List<File> getClassPath ()
+  abstract val classPath: List<File>
 
   /**
    * Returns the resolvers that actually constitute the given resolver
    *
    * @return final resolvers
    */
-  @NotNull
-  public abstract List<Resolver> getFinalResolvers ()
+  abstract val finalResolvers: List<Resolver>
+
+  /**
+   * Returns a class-file node with the specified name. If `this` resolver contains multiple
+   * instances of classes with the same name the first one in the *class-path* order will be returned.
+   *
+   * @param className class name in *binary* form (see JVM specification)
+   * @return a class-node for accessing the bytecode
+   * @throws IOException if IO error occurs, e.g. file .class-file was deleted
+   */
+  @Throws(IOException::class)
+  abstract fun findClass(className: String): ClassNode?
+
+  /**
+   * Returns the resolver which contains the given class: invocation of [.findClass] on the
+   * resulting `Resolver` returns the same instance of the class-node as an invocation of [.findClass]
+   * on `this` instance.
+   *
+   * @param className class name for which resolver should be found (in *binary* form)
+   * @return actual class resolver or `null` if `this` resolver doesn't contain a specified class
+   */
+  abstract fun getClassLocation(className: String): Resolver?
+
+  /**
+   * Returns true if `this` Resolver contains the given class. It may be faster
+   * than checking [.findClass] is not null.
+   *
+   * @param className class name in *binary* form (see JVM specification)
+   * @return `true` if class is in Resolver, `false` otherwise
+   */
+  abstract fun containsClass(className: String): Boolean
 
 }
