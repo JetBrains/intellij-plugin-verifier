b21480925939c3909238f8e973b8ace28082f834
Ivan Chirkov <ivan.chirkov@jetbrains.com>
2018-02-17 01:58:26 +0100

    Ignore XML entities in teamcity plugin descriptors

--- intellij-plugin-structure/structure-teamcity/src/main/kotlin/com/jetbrains/plugin/structure/teamcity/beans/TeamcityPluginBeanExtractor.kt
+++ intellij-plugin-structure/structure-teamcity/src/main/kotlin/com/jetbrains/plugin/structure/teamcity/beans/TeamcityPluginBeanExtractor.kt
@@ -2,9 +2,15 @@ package com.jetbrains.plugin.structure.teamcity.beans
 
 import org.jdom2.input.SAXBuilder
 import org.jonnyzzz.kotlin.xml.bind.jdom.JDOM
+import org.xml.sax.InputSource
+import java.io.CharArrayReader
 import java.io.InputStream
 
+private val EMPTY_CHAR_ARRAY = CharArray(0)
+
 fun extractPluginBean(inputStream: InputStream): TeamcityPluginBean {
-  val rootElement = SAXBuilder().build(inputStream).rootElement
+  val saxBuilder = SAXBuilder()
+  saxBuilder.setEntityResolver({ _, _ -> InputSource(CharArrayReader(EMPTY_CHAR_ARRAY)) })
+  val rootElement = saxBuilder.build(inputStream).rootElement
   return JDOM.load(rootElement, TeamcityPluginBean::class.java)
 }
