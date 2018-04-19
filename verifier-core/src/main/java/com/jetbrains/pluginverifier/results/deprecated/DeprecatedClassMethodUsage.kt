1555012abaffc55bda190eeaa21e1dc7fecee099
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2018-04-19 12:40:25 +0300

    Use 'constructor' word instead of 'method' where appropriate.

--- verifier-core/src/main/java/com/jetbrains/pluginverifier/results/deprecated/DeprecatedClassMethodUsage.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/results/deprecated/DeprecatedClassMethodUsage.kt
@@ -12,11 +12,12 @@ import com.jetbrains.pluginverifier.results.presentation.MethodParameterTypeOpti
 import com.jetbrains.pluginverifier.results.presentation.MethodReturnTypeOption.SIMPLE_RETURN_TYPE_CLASS_NAME
 import com.jetbrains.pluginverifier.results.presentation.formatClassLocation
 import com.jetbrains.pluginverifier.results.presentation.formatMethodLocation
+import com.jetbrains.pluginverifier.results.presentation.methodOrConstructorWord
 
 data class DeprecatedClassMethodUsage(override val deprecatedElement: ClassLocation,
                                       override val usageLocation: Location,
                                       val method: MethodLocation) : DeprecatedApiUsage() {
-  override val shortDescription = "Method of a deprecated class is used ${method.formatMethodLocation(FULL_HOST_NAME, SIMPLE_PARAM_CLASS_NAME, SIMPLE_RETURN_TYPE_CLASS_NAME, WITH_PARAM_NAMES_IF_AVAILABLE)}"
+  override val shortDescription = method.methodOrConstructorWord.capitalize() + " of a deprecated class is used ${method.formatMethodLocation(FULL_HOST_NAME, SIMPLE_PARAM_CLASS_NAME, SIMPLE_RETURN_TYPE_CLASS_NAME, WITH_PARAM_NAMES_IF_AVAILABLE)}"
 
-  override val fullDescription = "Method ${method.formatMethodLocation(NO_HOST, SIMPLE_PARAM_CLASS_NAME, SIMPLE_RETURN_TYPE_CLASS_NAME, WITH_PARAM_NAMES_IF_AVAILABLE)} of the deprecated class ${deprecatedElement.formatClassLocation(FULL_NAME, WITH_GENERICS)} is used in ${usageLocation.formatDeprecatedUsageLocation()}"
+  override val fullDescription = method.methodOrConstructorWord + " ${method.formatMethodLocation(NO_HOST, SIMPLE_PARAM_CLASS_NAME, SIMPLE_RETURN_TYPE_CLASS_NAME, WITH_PARAM_NAMES_IF_AVAILABLE)} of the deprecated class ${deprecatedElement.formatClassLocation(FULL_NAME, WITH_GENERICS)} is used in ${usageLocation.formatDeprecatedUsageLocation()}"
 }
--- verifier-core/src/main/java/com/jetbrains/pluginverifier/results/deprecated/DeprecatedMethodUsage.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/results/deprecated/DeprecatedMethodUsage.kt
@@ -7,10 +7,11 @@ import com.jetbrains.pluginverifier.results.presentation.MethodParameterNameOpti
 import com.jetbrains.pluginverifier.results.presentation.MethodParameterTypeOption.SIMPLE_PARAM_CLASS_NAME
 import com.jetbrains.pluginverifier.results.presentation.MethodReturnTypeOption.SIMPLE_RETURN_TYPE_CLASS_NAME
 import com.jetbrains.pluginverifier.results.presentation.formatMethodLocation
+import com.jetbrains.pluginverifier.results.presentation.methodOrConstructorWord
 
 data class DeprecatedMethodUsage(override val deprecatedElement: MethodLocation,
                                  override val usageLocation: Location) : DeprecatedApiUsage() {
-  override val shortDescription = "Deprecated method usage ${deprecatedElement.formatMethodLocation(FULL_HOST_NAME, SIMPLE_PARAM_CLASS_NAME, SIMPLE_RETURN_TYPE_CLASS_NAME, WITH_PARAM_NAMES_IF_AVAILABLE)}"
+  override val shortDescription = "Deprecated " + deprecatedElement.methodOrConstructorWord + " usage ${deprecatedElement.formatMethodLocation(FULL_HOST_NAME, SIMPLE_PARAM_CLASS_NAME, SIMPLE_RETURN_TYPE_CLASS_NAME, WITH_PARAM_NAMES_IF_AVAILABLE)}"
 
-  override val fullDescription = "Deprecated method ${deprecatedElement.formatMethodLocation(FULL_HOST_NAME, SIMPLE_PARAM_CLASS_NAME, SIMPLE_RETURN_TYPE_CLASS_NAME, WITH_PARAM_NAMES_IF_AVAILABLE)} is used in " + usageLocation.formatDeprecatedUsageLocation()
+  override val fullDescription = "Deprecated " + deprecatedElement.methodOrConstructorWord + " ${deprecatedElement.formatMethodLocation(FULL_HOST_NAME, SIMPLE_PARAM_CLASS_NAME, SIMPLE_RETURN_TYPE_CLASS_NAME, WITH_PARAM_NAMES_IF_AVAILABLE)} is used in " + usageLocation.formatDeprecatedUsageLocation()
 }
