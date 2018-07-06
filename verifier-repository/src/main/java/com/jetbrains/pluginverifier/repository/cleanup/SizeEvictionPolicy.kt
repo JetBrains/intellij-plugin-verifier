7afce43489cfcc58a241f2117792dc00971c0563
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2018-07-07 02:43:48 +0300

    Fix ResourceCache test: guarantee stable result of evicting the resources having the same 'lastAccessTime'.

--- verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/cleanup/SizeEvictionPolicy.kt
+++ verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/cleanup/SizeEvictionPolicy.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.pluginverifier.repository.cleanup
 
+import com.jetbrains.pluginverifier.repository.resources.AvailableResource
 import com.jetbrains.pluginverifier.repository.resources.EvictionInfo
 import com.jetbrains.pluginverifier.repository.resources.EvictionPolicy
 import com.jetbrains.pluginverifier.repository.resources.ResourceWeight
@@ -8,8 +9,9 @@ import com.jetbrains.pluginverifier.repository.resources.ResourceWeight
  * Eviction policy that maintains the [maximum] [maximumSize]
  * size of the [repository] [com.jetbrains.pluginverifier.repository.resources.ResourceRepository].
  *
- * It selects for deletion the files that have not been
+ * It selects for deletion resources that have not been
  * accessed for the longest time.
+ * If the last times are equal, the resources are compared by number of access times.
  */
 class SizeEvictionPolicy<R, K>(private val maximumSize: Int) : EvictionPolicy<R, K> {
   override fun isNecessary(totalWeight: ResourceWeight) =
@@ -18,7 +20,22 @@ class SizeEvictionPolicy<R, K>(private val maximumSize: Int) : EvictionPolicy<R,
   override fun selectResourcesForEviction(evictionInfo: EvictionInfo<R, K>) =
       evictionInfo.availableResources
           .filterNot { it.isLocked }
-          .sortedBy { it.usageStatistic.lastAccessTime }
+          .sortedWith(
+              //Firstly remove resources that haven't been accessed for the longest time.
+              //Then, if they were accessed at the same time, compare by number of accesses
+              compareBy<AvailableResource<R, K>> {
+                it.usageStatistic.lastAccessTime
+              }.thenBy {
+                it.usageStatistic.timesAccessed
+              }.thenBy {
+                //As the last resort, compare by keys, to have consistent results.
+                if (it.key is Comparable<*>) {
+                  it.key
+                } else {
+                  it.toString()
+                }
+              }
+          )
           .take(howManyToRemove(evictionInfo))
 
   private fun howManyToRemove(evictionInfo: EvictionInfo<R, K>): Int {
--- verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/cache/ResourceCacheTest.kt
+++ verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/cache/ResourceCacheTest.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.pluginverifier.tests.cache
 
+import com.jetbrains.pluginverifier.repository.cache.ResourceCache
 import com.jetbrains.pluginverifier.repository.cache.ResourceCacheEntryResult
 import com.jetbrains.pluginverifier.repository.cache.createSizeLimitedResourceCache
 import com.jetbrains.pluginverifier.repository.provider.ProvideResult
@@ -7,19 +8,27 @@ import com.jetbrains.pluginverifier.repository.provider.ResourceProvider
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.Closeable
+import java.util.*
 
+/**
+ * Verifies correctness of [ResourceCache] implementation.
+ */
 class ResourceCacheTest {
 
+  /**
+   * Populates a [ResourceCache] with limited size in a single thread,
+   * verifies that all resources are disposed at proper time:
+   * 1) If there is space in the cache, don't dispose resources for no purpose.
+   * 2) If the space is out, dispose the least used resources
+   * 3) When closing the cache, dispose all the resources.
+   */
   @Test
-  fun `cache functional test`() {
-    val N = 10
-    val openedResources = hashMapOf<Int, Closeable>()
+  fun `cache resource disposition test`() {
+    val elementsNum = 10
+    val openedResources = Collections.synchronizedMap(hashMapOf<Int, Closeable>())
 
-    /**
-     * Create a resource cache
-     */
-    val resourceCache = createSizeLimitedResourceCache(
-        N,
+    val resourceCache: ResourceCache<Closeable, Int> = createSizeLimitedResourceCache(
+        elementsNum,
         object : ResourceProvider<Int, Closeable> {
           override fun provide(key: Int): ProvideResult.Provided<Closeable> {
             val closeable = Closeable {
@@ -36,18 +45,19 @@ class ResourceCacheTest {
     )
 
     //Populate the cache with keys [0; N)
-    for (i in 0 until N) {
+    for (i in 0 until elementsNum) {
       val found = resourceCache.getResourceCacheEntry(i) as ResourceCacheEntryResult.Found
       found.resourceCacheEntry.close()
     }
-    assertEquals((0 until N).toSet(), openedResources.keys)
+    //Resources [0; N) must not be closed yet because there is enough space for all of them
+    assertEquals((0 until elementsNum).toSet(), openedResources.keys)
 
     //Evict the previously inserted elements and populate the cache with keys [N; N * 2)
-    for (i in N until 2 * N) {
+    for (i in elementsNum until 2 * elementsNum) {
       val found = resourceCache.getResourceCacheEntry(i) as ResourceCacheEntryResult.Found
       found.resourceCacheEntry.close()
     }
-    assertEquals((N until 2 * N).toSet(), openedResources.keys)
+    assertEquals((elementsNum until 2 * elementsNum).toSet(), openedResources.keys)
 
     //entries of the resource cache must be closed when no more used
     resourceCache.close()
