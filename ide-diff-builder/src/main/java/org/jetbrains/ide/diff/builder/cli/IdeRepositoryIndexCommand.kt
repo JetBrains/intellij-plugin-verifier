e7dc82cba5a5f628d2f18d3db48dd011a6d7f7a6
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2018-12-04 15:53:14 +0300

    Rename option that allows to not print release IDE version.

--- ide-diff-builder/src/main/java/org/jetbrains/ide/diff/builder/cli/IdeRepositoryIndexCommand.kt
+++ ide-diff-builder/src/main/java/org/jetbrains/ide/diff/builder/cli/IdeRepositoryIndexCommand.kt
@@ -21,12 +21,12 @@ class IdeRepositoryIndexCommand : Command {
        https://www.jetbrains.com/intellij-repository/releases/ and
        https://www.jetbrains.com/intellij-repository/snapshots
 
-      ide-repository-index [-channel <option>] [-skip-release-version] [-product-code <code>] [-ascending]
+      ide-repository-index [-channel <option>] [-without-release-version] [-product-code <code>] [-ascending]
         -channel <option> := all (default) | releases-only | snapshots-only
         -channel option is used to specify a channel of the IDE repository
         to list builds from.
 
-        -skip-release-version - specify this option if you want only build numbers to be printed.
+        -without-release-version - specify this option if you want only build numbers to be printed.
         If not specified, release versions of corresponding IDE builds are printed too, like:
         IU-182.4892.20 (2018.2.5)
 
@@ -50,8 +50,8 @@ class IdeRepositoryIndexCommand : Command {
     @set:Argument("product-code", description = "Product code of IDEs to list")
     var productCode: String? = null
 
-    @set:Argument("skip-release-version", description = "If specified, the release versions will not be printed, but only build numbers")
-    var skipReleaseVersion: Boolean = false
+    @set:Argument("without-release-version", description = "If specified, the release versions will not be printed, but only build numbers")
+    var withoutReleaseVersion: Boolean = false
 
     @set:Argument("ascending", description = "If specified, the versions are printed in ascending order. Otherwise in descending order.")
     var ascendingOrder: Boolean = false
@@ -79,7 +79,7 @@ class IdeRepositoryIndexCommand : Command {
     }
 
     for (availableIde in sortedIdes) {
-      if (options.skipReleaseVersion) {
+      if (options.withoutReleaseVersion) {
         println(availableIde.version.asString())
       } else {
         println(availableIde)
