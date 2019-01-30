01a721d134b6b77856e4bb13c21720a8811ac19a
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-01-30 15:49:52 +0300

    Fix repository tests.

--- verifier-repository/src/test/java/com/jetbrains/pluginverifier/ide/IntelliJArtifactsRepositoryParserTest.kt
+++ verifier-repository/src/test/java/com/jetbrains/pluginverifier/ide/IntelliJArtifactsRepositoryParserTest.kt
@@ -4,11 +4,15 @@ import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.Gson
 import com.jetbrains.plugin.structure.intellij.version.IdeVersion
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 import java.net.URL
 
-class SnapshotsIdeRepositoryTest {
+class IntelliJArtifactsRepositoryParserTest {
+
+  companion object {
+    private const val REPO_URL = "https://cache-redirector.jetbrains.com/intellij-repository/releases"
+  }
 
   /**
    * Ensures that .json index of the IDE repository is parsed properly:
@@ -17,53 +21,55 @@ class SnapshotsIdeRepositoryTest {
   @Test
   fun `simple index parsing test`() {
     val artifactsJson = Gson().fromJson<ArtifactsJson>(
-        SnapshotsIdeRepositoryTest::class.java.getResourceAsStream("/snapshotsIdeRepositoryIndex.json").bufferedReader()
+        IntelliJArtifactsRepositoryParserTest::class.java.getResourceAsStream("/intelliJArtifactsRepositoryIndex.json").bufferedReader()
     )
 
-    val indexParser = IntelliJRepositoryIndexParser()
-    val artifacts = indexParser.parseArtifacts(artifactsJson.artifacts, false)
+    val artifacts = IntelliJRepositoryIndexParser().parseArtifacts(artifactsJson.artifacts, IntelliJIdeRepository.Channel.RELEASE)
 
     val expectedArtifacts = listOf(
         AvailableIde(
             IdeVersion.createIdeVersion("IU-181.3870.7"),
             null,
-            URL("https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/idea/ideaIU/181.3870.7/ideaIU-181.3870.7.zip")
+            URL("$REPO_URL/com/jetbrains/intellij/idea/ideaIU/181.3870.7/ideaIU-181.3870.7.zip")
         ),
 
         AvailableIde(
             IdeVersion.createIdeVersion("IC-181.3870.7"),
             null,
-            URL("https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/idea/ideaIC/181.3870.7/ideaIC-181.3870.7.zip")
+            URL("$REPO_URL/com/jetbrains/intellij/idea/ideaIC/181.3870.7/ideaIC-181.3870.7.zip")
         ),
 
         AvailableIde(
             IdeVersion.createIdeVersion("IU-173.4548.28"),
             "2017.3.4",
-            URL("https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/idea/ideaIU/2017.3.4/ideaIU-2017.3.4.zip")
+            URL("$REPO_URL/com/jetbrains/intellij/idea/ideaIU/2017.3.4/ideaIU-2017.3.4.zip")
         ),
 
         AvailableIde(
             IdeVersion.createIdeVersion("IC-173.4548.28"),
             "2017.3.4",
-            URL("https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/idea/ideaIC/2017.3.4/ideaIC-2017.3.4.zip")
+            URL("$REPO_URL/com/jetbrains/intellij/idea/ideaIC/2017.3.4/ideaIC-2017.3.4.zip")
         ),
 
         AvailableIde(
             IdeVersion.createIdeVersion("RD-173.3994.2442"),
             null,
-            URL("https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/rider/riderRD/173.3994.2442/riderRD-173.3994.2442.zip")
+            URL("$REPO_URL/com/jetbrains/intellij/rider/riderRD/173.3994.2442/riderRD-173.3994.2442.zip")
         ),
 
         AvailableIde(
             IdeVersion.createIdeVersion("MPS-181.1168"),
             "2018.1",
-            URL("https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/mps/mps/2018.1/mps-2018.1.zip")
+            URL("$REPO_URL/com/jetbrains/mps/mps/2018.1/mps-2018.1.zip")
         )
     )
 
     assertEquals(expectedArtifacts.size, artifacts.size)
     for (expectedArtifact in expectedArtifacts) {
-      assertTrue("$expectedArtifact must be parsed", expectedArtifact in artifacts)
+      val sameArtifact = artifacts.find { it.version == expectedArtifact.version }
+      assertNotNull("$expectedArtifact is not found", sameArtifact)
+      assertEquals(expectedArtifact.downloadUrl, sameArtifact!!.downloadUrl)
+      assertEquals(expectedArtifact.releaseVersion, sameArtifact.releaseVersion)
     }
   }
 
