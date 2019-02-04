61bcb56568e01021cbe369369a6eb7b68b0840dd
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-02-04 19:06:21 +0300

    Add list of known IDE packages.

---
+++ intellij-plugin-structure/structure-ide-classes/src/main/java/com/jetbrains/plugin/structure/ide/util/KnownIdePackages.kt
@@ -0,0 +1,19 @@
+package com.jetbrains.plugin.structure.ide.util
+
+/**
+ * Utility class that contains list of known IntelliJ IDE packages.
+ */
+object KnownIdePackages {
+
+  val idePackages: Set<String> by lazy(::readKnownIdePackages)
+
+  fun isKnownPackage(packageName: String): Boolean =
+      idePackages.any { packageName == it || packageName.startsWith("$it.") }
+
+  private fun readKnownIdePackages() =
+      KnownIdePackages::class.java.classLoader
+          .getResourceAsStream("com.jetbrains.plugin.structure.ide/knownIdePackages.txt")
+          .bufferedReader()
+          .readLines()
+          .toSet()
+}
