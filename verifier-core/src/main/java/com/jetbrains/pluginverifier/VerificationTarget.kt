b6228cabf74a224d2fd7396ad23e36655f6be7bd
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-03-04 15:04:02 +0300

    Cleanup class of unnecessary stuff.

--- verifier-core/src/main/java/com/jetbrains/pluginverifier/VerificationTarget.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/VerificationTarget.kt
@@ -1,32 +1,12 @@
 package com.jetbrains.pluginverifier
 
 import com.jetbrains.plugin.structure.intellij.version.IdeVersion
-import com.jetbrains.pluginverifier.misc.replaceInvalidFileNameCharacters
 import com.jetbrains.pluginverifier.repository.PluginInfo
-import java.io.Serializable
-import java.nio.file.Path
 
-sealed class VerificationTarget : Serializable {
+sealed class VerificationTarget {
 
-  data class Ide(val ideVersion: IdeVersion) : VerificationTarget() {
-    override fun toString() = ideVersion.toString()
+  data class Ide(val ideVersion: IdeVersion) : VerificationTarget()
 
-    companion object {
-      private const val serialVersionUID = 0L
-    }
-  }
-
-  data class Plugin(val plugin: PluginInfo) : VerificationTarget() {
-    override fun toString() = plugin.toString()
-
-    companion object {
-      private const val serialVersionUID = 0L
-    }
-  }
-
-  fun getReportDirectory(baseDirectory: Path): Path = when (this) {
-    is VerificationTarget.Ide -> baseDirectory.resolve(ideVersion.asString().replaceInvalidFileNameCharacters())
-    is VerificationTarget.Plugin -> baseDirectory.resolve("${plugin.pluginId} ${plugin.version}".replaceInvalidFileNameCharacters())
-  }
+  data class Plugin(val plugin: PluginInfo) : VerificationTarget()
 
 }
