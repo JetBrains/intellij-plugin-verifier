7cf7b121423b8500f22bf1fecaf0f8f96cdf8341
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-04-05 12:41:54 +0300

    Remove unnecessary inheritance from Opcodes.

--- verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/instruction/InstructionVerifier.kt
+++ verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/instruction/InstructionVerifier.kt
@@ -1,11 +1,10 @@
 package com.jetbrains.pluginverifier.verifiers.instruction
 
 import com.jetbrains.pluginverifier.verifiers.VerificationContext
-import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.AbstractInsnNode
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.MethodNode
 
-interface InstructionVerifier : Opcodes {
+interface InstructionVerifier {
   fun verify(clazz: ClassNode, method: MethodNode, instr: AbstractInsnNode, ctx: VerificationContext)
 }
