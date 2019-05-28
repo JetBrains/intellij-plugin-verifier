383138266a392b8af67e1570acef59112c434fca
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-05-28 19:01:29 +0300

    Cleanup unused class.

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/ExperimentalApiRegistrar.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/ExperimentalApiRegistrar.kt
@@ -4,8 +4,4 @@ import com.jetbrains.pluginverifier.usages.experimental.ExperimentalApiUsage
 
 interface ExperimentalApiRegistrar {
   fun registerExperimentalApiUsage(experimentalApiUsage: ExperimentalApiUsage)
-
-  object Empty : ExperimentalApiRegistrar {
-    override fun registerExperimentalApiUsage(experimentalApiUsage: ExperimentalApiUsage) = Unit
-  }
 }
