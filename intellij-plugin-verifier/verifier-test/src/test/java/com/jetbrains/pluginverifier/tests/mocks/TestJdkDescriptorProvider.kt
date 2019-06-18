e1b79eb5a0680e2c7ecbc99550f07f2d6fe9f14a
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-06-18 19:38:58 +0300

    Fix JDK path in tests.

--- intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/mocks/TestJdkDescriptorProvider.kt
+++ intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/mocks/TestJdkDescriptorProvider.kt
@@ -1,24 +1,15 @@
 package com.jetbrains.pluginverifier.tests.mocks
 
-import com.jetbrains.pluginverifier.misc.isDirectory
+import com.jetbrains.pluginverifier.misc.exists
 import java.nio.file.Path
 import java.nio.file.Paths
 
 object TestJdkDescriptorProvider {
 
   fun getJdkPathForTests(): Path {
-    return Paths.get("/usr/lib/jvm/jdk-11.0.1")
-
-    val jdk8Path = "/usr/lib/jvm/java-8-oracle"
-    val jdkPath = System.getenv("JAVA_HOME") ?: jdk8Path
-    val jdkDir = if ('9' in jdkPath) {
-      //Todo: support the java 9
-      Paths.get(jdk8Path)
-    } else {
-      Paths.get(jdkPath)
-    }
-    require(jdkDir.isDirectory)
-    return jdkDir
+    val jdkPath = Paths.get(System.getenv("JAVA_HOME") ?: "/usr/lib/jvm/java-8-oracle")
+    check(jdkPath.exists())
+    return jdkPath
   }
 
 }
