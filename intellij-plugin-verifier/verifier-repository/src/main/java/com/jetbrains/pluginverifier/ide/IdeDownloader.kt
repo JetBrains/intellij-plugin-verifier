8928948ea745a6709d8d3022c8a463e8c2964c3f
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-07-05 21:07:19 +0300

    Clarify test code of 'strip-top-level-directory' method.

--- intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/ide/IdeDownloader.kt
+++ intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/ide/IdeDownloader.kt
@@ -60,4 +60,61 @@ class IdeDownloader : Downloader<AvailableIde> {
     }
   }
 
+  companion object {
+    /**
+     * If the [directory] contains a single directory,
+     * that directory will be truncated and all its
+     * content will be moved one level up.
+     *
+     * If the only contained directory is empty, it will be removed.
+     */
+    fun stripTopLevelDirectory(directory: Path) {
+      val entries = directory.listFiles()
+      if (entries.size != 1) {
+        return
+      }
+
+      val single = entries.single()
+      if (!single.isDirectory) {
+        return
+      }
+
+      val contents = single.listFiles()
+      if (contents.isEmpty()) {
+        Files.delete(single)
+        return
+      }
+
+      var conflict: Path? = null
+      for (from in contents) {
+        if (from.simpleName == single.simpleName) {
+          conflict = from
+          continue
+        }
+
+        val to = directory.resolve(from.simpleName)
+        Files.move(from, to)
+      }
+
+      if (conflict != null) {
+        //Create a unique temporary name from the set of files.
+        //This name will be used as a destination of a conflicting name.
+        val uniqueTempName = contents.map { it.simpleName }.max()!! + ".temp"
+        val tempDestination = directory.resolve(uniqueTempName)
+
+        //Move conflict to unique location.
+        require(!tempDestination.exists())
+        Files.move(conflict, tempDestination)
+
+        //Delete empty single
+        require(single.listFiles().isEmpty())
+        single.deleteLogged()
+
+        Files.move(tempDestination, single)
+      } else {
+        single.deleteLogged()
+      }
+    }
+  }
+
 }
--- intellij-plugin-verifier/verifier-repository/src/test/java/com/jetbrains/pluginverifier/misc/StripTopLevelDirectoryTest.kt
+++ intellij-plugin-verifier/verifier-repository/src/test/java/com/jetbrains/pluginverifier/misc/StripTopLevelDirectoryTest.kt
@@ -1,5 +1,7 @@
 package com.jetbrains.pluginverifier.misc
 
+import com.jetbrains.plugin.structure.base.utils.createDir
+import com.jetbrains.pluginverifier.ide.IdeDownloader.Companion.stripTopLevelDirectory
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -12,69 +14,97 @@ class StripTopLevelDirectoryTest {
   val tempFolder = TemporaryFolder()
 
   /**
-   * f
+   * f/
    */
   @Test
   fun `empty directory`() {
-    val tmpFolder = tempFolder.newFolder()
-    stripTopLevelDirectory(tmpFolder.toPath())
-    assertArrayEquals(emptyArray(), tmpFolder.list())
+    val f = tempFolder.newFolder()
+    stripTopLevelDirectory(f.toPath())
+    assertArrayEquals(emptyArray(), f.list())
   }
 
   /**
-   * f
-   *   a
-   *   b
+   * f/
+   *   empty/
    */
   @Test
-  fun `more than one file`() {
-    val tmpFolder = tempFolder.newFolder()
-    tmpFolder.resolve("a").createDir()
-    tmpFolder.resolve("b").createDir()
-    stripTopLevelDirectory(tmpFolder.toPath())
-    assertEquals(listOf("a", "b"), tmpFolder.list().sorted())
+  fun singleEmptyDirectory() {
+    val f = tempFolder.newFolder()
+    f.resolve("empty").createDir()
+    stripTopLevelDirectory(f.toPath())
+    assertTrue(f.listFiles().orEmpty().isEmpty())
   }
 
   /**
-   * f
-   *   s
-   *     a
-   *     b
+   * f/
+   *   a/
+   *   b/
    */
   @Test
-  fun simple() {
-    val tmpFolder = tempFolder.newFolder()
-    val s = tmpFolder.resolve("s").createDir()
-    s.resolve("a").createDir()
-    s.resolve("b").writeText("42")
-    stripTopLevelDirectory(tmpFolder.toPath())
-    assertEquals(listOf("a", "b"), tmpFolder.list().sorted())
+  fun `more than one file means no change`() {
+    val f = tempFolder.newFolder()
+    f.resolve("a").createDir()
+    f.resolve("b").createDir()
+    stripTopLevelDirectory(f.toPath())
+    assertEquals(
+        listOf(f.resolve("a"), f.resolve("b")),
+        f.listFiles().orEmpty().sorted()
+    )
   }
 
   /**
-   * f
-   *   s
-   *     a
-   *     s/b.txt
+   * f/
+   *   s/
+   *     a/
+   *     b (42)
    *
    * should be
-   * f
-   *   a
+   * f/
+   *   a/
+   *   b (42)
+   */
+  @Test
+  fun stripped() {
+    val f = tempFolder.newFolder()
+    val s = f.resolve("s").createDir()
+    s.resolve("a").createDir()
+    s.resolve("b").writeText("42")
+    stripTopLevelDirectory(f.toPath())
+    assertEquals(
+        listOf(f.resolve("a"), f.resolve("b")),
+        f.listFiles().orEmpty().sorted()
+    )
+    assertEquals("42", f.resolve("b").readText())
+  }
+
+
+  /**
+   * f/
+   *   s/
+   *     a/
+   *     s/b.txt  <- "s" in "s" is conflict, which must be handled.
+   *
+   * should be
+   * f/
+   *   a/
    *   s/b.txt
    */
   @Test
   fun conflict() {
-    val tmpFolder = tempFolder.newFolder()
-    val s = tmpFolder.resolve("s").createDir()
+    val f = tempFolder.newFolder()
+    val s = f.resolve("s").createDir()
     s.resolve("a").createDir()
 
     val ss = s.resolve("s").createDir()
     ss.resolve("b.txt").writeText("42")
-    stripTopLevelDirectory(tmpFolder.toPath())
-    assertEquals(listOf("a", "s"), tmpFolder.list().sorted())
 
-    val moved = tmpFolder.resolve("s").resolve("b.txt")
-    assertTrue(moved.exists())
-    assertEquals("42", moved.readText())
+    stripTopLevelDirectory(f.toPath())
+    assertEquals(
+        listOf(f.resolve("a"), f.resolve("s")),
+        f.listFiles().orEmpty().sorted()
+    )
+
+    assertEquals("42", f.resolve("s").resolve("b.txt").readText())
   }
+
 }
