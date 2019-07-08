135997cd811811047b6cb69efc816bcad2b56cc5
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-07-08 12:09:18 +0300

    JDK creation: read JDK version from 'release' or 'version.txt' files.

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/parameters/jdk/JdkDescriptorCreator.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/parameters/jdk/JdkDescriptorCreator.kt
@@ -2,6 +2,7 @@ package com.jetbrains.pluginverifier.parameters.jdk
 
 import com.jetbrains.plugin.structure.base.utils.exists
 import com.jetbrains.plugin.structure.base.utils.readLines
+import com.jetbrains.plugin.structure.base.utils.readText
 import com.jetbrains.plugin.structure.classes.resolvers.Resolver
 import com.jetbrains.plugin.structure.classes.utils.JarsUtils
 import java.nio.file.Path
@@ -12,21 +13,46 @@ object JdkDescriptorCreator {
       createJdkDescriptor(jdkPath, Resolver.ReadMode.FULL)
 
   fun createJdkDescriptor(jdkPath: Path, readMode: Resolver.ReadMode): JdkDescriptor {
-    val releasePath = jdkPath.resolve("release")
-    require(releasePath.exists()) { "Invalid JDK: $releasePath does not exist" }
-
-    val properties = parseProperties(releasePath)
-    val javaVersion = properties["JAVA_VERSION"]
-    checkNotNull(javaVersion) { "JAVA_VERSION is not specified in " }
-
-    val majorVersion = javaVersion.substringBefore(".").toIntOrNull()
-    checkNotNull(majorVersion) { "Invalid JAVA_VERSION: $javaVersion " }
-
-    return if (majorVersion < 9) {
+    val javaVersion = readJavaVersion(jdkPath)
+    return if (javaVersion < 9) {
       createPreJava9(jdkPath, readMode)
     } else {
       createJava9Plus(jdkPath, readMode)
     }
+  }
+
+  private fun readJavaVersion(jdkPath: Path): Int {
+    val fullVersion = findFullVersion(jdkPath)
+    return parseJavaVersion(fullVersion)
+  }
+
+  private fun parseJavaVersion(fullVersion: String): Int {
+    return if (fullVersion.startsWith("1.")) {
+      fullVersion.substringAfter("1.").substringBefore(".").toIntOrNull()
+    } else {
+      fullVersion.substringBefore(".").toIntOrNull()
+    } ?: throw IllegalArgumentException("Invalid version: '$fullVersion'")
+  }
+
+  private fun findFullVersion(jdkPath: Path): String {
+    val releasePath = jdkPath.resolve("release")
+    if (releasePath.exists()) {
+      val properties = releasePath.readLines().associate {
+        val list = it.split("=")
+        list[0] to list[1].trim('"')
+      }
+      val javaVersion = properties["JAVA_VERSION"]
+      checkNotNull(javaVersion) { "JAVA_VERSION is not specified in $releasePath" }
+      return javaVersion
+    }
+
+    //Amazon Corretto JDK 8 has 'version.txt' instead.
+    val versionPath = jdkPath.resolve("version.txt")
+    if (versionPath.exists()) {
+      return versionPath.readText()
+    }
+
+    throw IllegalArgumentException("JDK version is not known: neither $releasePath nor $versionPath are available")
   }
 
   private fun createJava9Plus(jdkPath: Path, readMode: Resolver.ReadMode): JdkDescriptor {
@@ -51,11 +77,5 @@ object JdkDescriptorCreator {
     val jarResolver = JarsUtils.makeResolver(readMode, jars)
     return JdkDescriptor(jdkPath, jarResolver)
   }
-
-  private fun parseProperties(propertiesPath: Path): Map<String, String> =
-      propertiesPath.readLines().associate {
-        val list = it.split("=")
-        list[0] to list[1].trim('"')
-      }
 
 }
