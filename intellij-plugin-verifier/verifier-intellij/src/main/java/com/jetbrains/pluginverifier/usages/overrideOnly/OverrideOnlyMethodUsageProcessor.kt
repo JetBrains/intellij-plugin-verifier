6c48db4e5218b83d184c2a22f23638b36818f6ba
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-07-24 17:51:04 +0300

    Override-only method usages: move utility methods.

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/overrideOnly/OverrideOnlyMethodUsageProcessor.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/overrideOnly/OverrideOnlyMethodUsageProcessor.kt
@@ -3,6 +3,7 @@ package com.jetbrains.pluginverifier.usages.overrideOnly
 import com.jetbrains.pluginverifier.results.location.Location
 import com.jetbrains.pluginverifier.usages.ApiUsageProcessor
 import com.jetbrains.pluginverifier.verifiers.VerificationContext
+import com.jetbrains.pluginverifier.verifiers.findAnnotation
 import com.jetbrains.pluginverifier.verifiers.resolution.ClassFileMember
 import com.jetbrains.pluginverifier.verifiers.resolution.Method
 
@@ -11,5 +12,13 @@ class OverrideOnlyMethodUsageProcessor : ApiUsageProcessor {
     if (context is OverrideOnlyRegistrar && classFileMember is Method && classFileMember.isOverrideOnlyMethod()) {
       context.registerOverrideOnlyMethodUsage(OverrideOnlyMethodUsage(classFileMember.location, usageLocation))
     }
+  }
+
+  private fun Method.isOverrideOnlyMethod(): Boolean =
+      runtimeInvisibleAnnotations.findAnnotation(overrideOnlyAnnotationName) != null
+          || containingClassFile.runtimeInvisibleAnnotations.findAnnotation(overrideOnlyAnnotationName) != null
+
+  private companion object {
+    const val overrideOnlyAnnotationName = "org/jetbrains/annotations/ApiStatus\$OverrideOnly"
   }
 }
