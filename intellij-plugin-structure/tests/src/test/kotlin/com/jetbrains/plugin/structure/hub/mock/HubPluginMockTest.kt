63d1503a7fec1d73c10befa6fda51645a61f1b46
prendota <Prendota@mail.ru>
2019-08-19 12:31:04 +0300

    Add base hub validation test

---
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/hub/mock/HubPluginMockTest.kt
@@ -0,0 +1,49 @@
+package com.jetbrains.plugin.structure.hub.mock
+
+import com.jetbrains.plugin.structure.base.plugin.PluginCreationFail
+import com.jetbrains.plugin.structure.base.plugin.PluginCreationSuccess
+import com.jetbrains.plugin.structure.base.plugin.PluginProblem
+import com.jetbrains.plugin.structure.hub.HubPlugin
+import com.jetbrains.plugin.structure.hub.HubPluginManager
+import com.jetbrains.plugin.structure.intellij.utils.URLUtil
+import org.junit.Assert
+import org.junit.Test
+import java.io.File
+
+class HubPluginMockTest {
+  @Test
+  fun `hub plugin`() {
+    val pluginFile = URLUtil.urlToFile(this::class.java.getResource("/hub/mock-cat-widget.zip"))
+    testMockPluginStructureAndConfiguration(pluginFile)
+  }
+
+  private fun testMockPluginStructureAndConfiguration(pluginFile: File) {
+    val pluginCreationResult = HubPluginManager.createManager().createPlugin(pluginFile)
+    if (pluginCreationResult is PluginCreationFail) {
+      val message = pluginCreationResult.errorsAndWarnings.joinToString(separator = "\n") { it.message }
+      Assert.fail(message)
+    }
+    val pluginCreationSuccess = pluginCreationResult as PluginCreationSuccess
+
+    testMockConfigs(pluginCreationSuccess.plugin)
+    testMockWarnings(pluginCreationSuccess.warnings)
+  }
+
+  private fun testMockWarnings(problems: List<PluginProblem>) {
+    Assert.assertTrue(problems.isEmpty())
+  }
+
+  private fun testMockConfigs(plugin: HubPlugin) {
+    Assert.assertEquals("cat-widget", plugin.pluginId)
+    Assert.assertEquals("Pets", plugin.pluginName)
+    Assert.assertEquals("1.1.1", plugin.pluginVersion)
+    Assert.assertEquals("Mariya Davydova <mrs.mariya.davydova@gmail.com>", plugin.author)
+    Assert.assertEquals("https://github.com/mariyadavydova/youtrack-cats-widget", plugin.url)
+    Assert.assertEquals("Mariya Davydova", plugin.vendor)
+    Assert.assertEquals("mrs.mariya.davydova@gmail.com", plugin.vendorEmail)
+    Assert.assertEquals("Funny cats and dogs for your Dashboard!", plugin.description)
+    Assert.assertEquals("images/cat_purr.png", plugin.iconUrl)
+    Assert.assertEquals(mapOf("Hub" to ">=2018.2"), plugin.dependencies)
+    Assert.assertEquals(mapOf("Hub" to "^2018.2", "YouTrack" to "^2018.2"), plugin.products)
+  }
+}
