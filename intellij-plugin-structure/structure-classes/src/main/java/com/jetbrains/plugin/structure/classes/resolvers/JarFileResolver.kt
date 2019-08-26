193388f2afbb6da6b8e45e37287fef1b6b9fa355
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-08-26 14:10:37 +0300

    JarFileResolver: code style fixes.

--- intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/resolvers/JarFileResolver.kt
+++ intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/resolvers/JarFileResolver.kt
@@ -1,12 +1,12 @@
 package com.jetbrains.plugin.structure.classes.resolvers
 
-import com.jetbrains.plugin.structure.base.utils.closeLogged
 import com.jetbrains.plugin.structure.base.utils.closeOnException
+import com.jetbrains.plugin.structure.base.utils.exists
 import com.jetbrains.plugin.structure.base.utils.rethrowIfInterrupted
+import com.jetbrains.plugin.structure.base.utils.simpleName
 import com.jetbrains.plugin.structure.classes.utils.AsmUtil
 import org.objectweb.asm.tree.ClassNode
 import java.io.File
-import java.nio.file.Files
 import java.nio.file.Path
 import java.util.jar.JarFile
 
@@ -22,29 +22,19 @@ class JarFileResolver(
     private const val SERVICE_PROVIDERS_PREFIX = "META-INF/services/"
   }
 
-  private val jarFile: JarFile
-
   private val classes: MutableSet<String> = hashSetOf()
 
   private val packageSet = PackageSet()
 
   private val serviceProviders: MutableMap<String, Set<String>> = hashMapOf()
 
-  init {
-    if (!Files.exists(ioJarFile)) {
-      throw IllegalArgumentException("Jar file $ioJarFile doesn't exist")
-    }
-    if (!ioJarFile.fileName.toString().endsWith(".jar")) {
-      throw IllegalArgumentException("File $ioJarFile is not a jar archive")
-    }
+  private val jarFile: JarFile
 
+  init {
+    require(ioJarFile.exists()) { "Jar file $ioJarFile doesn't exist" }
+    require(ioJarFile.simpleName.endsWith(".jar")) { "File $ioJarFile is not a jar archive" }
     jarFile = JarFile(ioJarFile.toFile())
-    try {
-      readClassNamesAndServiceProviders()
-    } catch (e: Throwable) {
-      jarFile.closeLogged()
-      throw e
-    }
+    readClassNamesAndServiceProviders()
   }
 
   private fun readClassNamesAndServiceProviders() {
@@ -56,19 +46,20 @@ class JarFileResolver(
         packageSet.addPackagesOfClass(className)
       } else if (!entry.isDirectory && entryName.startsWith(SERVICE_PROVIDERS_PREFIX) && entryName.count { it == '/' } == 2) {
         val serviceProvider = entryName.substringAfter(SERVICE_PROVIDERS_PREFIX)
-        serviceProviders[serviceProvider] = readServiceImplementationNames(serviceProvider)
+        serviceProviders[serviceProvider] = readServiceImplementationNames(serviceProvider, jarFile)
       }
     }
   }
 
-  private fun readServiceImplementationNames(serviceProvider: String): Set<String> {
+  private fun readServiceImplementationNames(serviceProvider: String, jarFile: JarFile): Set<String> {
     val entry = SERVICE_PROVIDERS_PREFIX + serviceProvider
     val jarEntry = jarFile.getJarEntry(entry) ?: return emptySet()
     val lines = jarFile.getInputStream(jarEntry).reader().readLines()
     return lines.map { it.substringBefore("#").trim() }.filterNotTo(hashSetOf()) { it.isEmpty() }
   }
 
-  val implementedServiceProviders: Map<String, Set<String>> = serviceProviders
+  val implementedServiceProviders: Map<String, Set<String>>
+    get() = serviceProviders
 
   override val allPackages: Set<String>
     get() = packageSet.getAllPackages()
@@ -84,8 +75,8 @@ class JarFileResolver(
       val entryName = jarEntry.name
       if (entryName.endsWith(CLASS_SUFFIX)) {
         val className = entryName.substringBeforeLast(CLASS_SUFFIX)
-        jarFile.getInputStream(jarEntry).use { entryInputStream ->
-          val classNode = AsmUtil.readClassNode(className, entryInputStream, readMode == ReadMode.FULL)
+        jarFile.getInputStream(jarEntry).use {
+          val classNode = AsmUtil.readClassNode(className, it, readMode == ReadMode.FULL)
           if (!processor(classNode)) {
             return false
           }
@@ -103,21 +94,21 @@ class JarFileResolver(
     if (className !in classes) {
       return ResolutionResult.NotFound
     }
-    val classNode = try {
-      evaluateNode(className)
+    try {
+      val classNode = evaluateNode(className) ?: return ResolutionResult.NotFound
+      return ResolutionResult.Found(classNode, classFileOrigin)
     } catch (e: InvalidClassFileException) {
       return ResolutionResult.InvalidClassFile(e.message)
     } catch (e: Exception) {
       e.rethrowIfInterrupted()
       return ResolutionResult.FailedToReadClassFile(e.localizedMessage ?: e.javaClass.name)
-    } ?: return ResolutionResult.NotFound
-    return ResolutionResult.Found(classNode, classFileOrigin)
+    }
   }
 
   private fun evaluateNode(className: String): ClassNode? {
     val entry = jarFile.getEntry(className + CLASS_SUFFIX) ?: return null
-    jarFile.getInputStream(entry).use { inputStream ->
-      return AsmUtil.readClassNode(className, inputStream, readMode == ReadMode.FULL)
+    return jarFile.getInputStream(entry).use {
+      AsmUtil.readClassNode(className, it, readMode == ReadMode.FULL)
     }
   }
 
