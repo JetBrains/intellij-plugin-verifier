ae4a3cce2b4a4a7c5400696e6c6df5836be0130e
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2020-06-18 19:34:47 +0300

    merged conflicts

--- intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/edu/mock/EduTestData.kt
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/edu/mock/EduTestData.kt
@@ -1,17 +1,26 @@
 package com.jetbrains.plugin.structure.edu.mock
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.jetbrains.plugin.structure.edu.*
+import com.jetbrains.plugin.structure.edu.EDU_PLUGIN_VERSION
+import com.jetbrains.plugin.structure.edu.PROGRAMMING_LANGUAGE
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class EduItem(
+  var title: String? = ""
+)
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class EduPluginJsonBuilder(
   var title: String? = "key",
   var summary: String? = "summary",
   var language: String? = "en",
   var vendor: String? = "JetBrains s.r.o.",
+  @JsonProperty(PROGRAMMING_LANGUAGE)
   var programmingLanguage: String? = "kotlin",
-  var items: List<String>? = listOf("lesson1", "lesson2"),
+  var items: List<EduItem>? = listOf(EduItem("lesson1"), EduItem("lesson2")),
+  @JsonProperty(EDU_PLUGIN_VERSION)
   var version: String? = "3.7-2019.3-5266"
 ) {
 
@@ -23,5 +32,5 @@ val perfectEduPluginBuilder: EduPluginJsonBuilder
 
 
 fun getMockPluginJsonContent(fileName: String): String {
-  return object{}.javaClass.getResource("/edu/$fileName.json").readText()
+  return object {}.javaClass.getResource("/edu/$fileName.json").readText()
 }
