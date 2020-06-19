41e9a8b90c52c8ad0b8b5172cb2d24d1ce14c7db
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2020-06-19 11:52:15 +0300

    enum for the languages

--- intellij-plugin-structure/structure-edu/src/main/kotlin/com/jetbrains/plugin/structure/edu/problems/UnsupportedProgrammingLanguage.kt
+++ intellij-plugin-structure/structure-edu/src/main/kotlin/com/jetbrains/plugin/structure/edu/problems/UnsupportedProgrammingLanguage.kt
@@ -2,10 +2,19 @@ package com.jetbrains.plugin.structure.edu.problems
 
 import com.jetbrains.plugin.structure.base.plugin.PluginProblem
 
-object UnsupportedProgrammingLanguage : PluginProblem() {
-  // Important: Language name here should be the same as in Language.getID()
-  val supportedLanguages = listOf("ObjectiveC", "go", "JAVA", "JavaScript", "kotlin", "Python", "Rust", "Scala")
+// Important: Language id here should be the same as in Language.getID() from IntelliJ platform
+enum class Language(val id: String) {
+  OBJECTIVE_C("ObjectiveC"),
+  GO("go"),
+  JAVA("JAVA"),
+  JAVA_SCRIPT("JavaScript"),
+  KOTLIN("kotlin"),
+  PYTHON("Python"),
+  RUST("Rust"),
+  SCALA("Scala")
+}
 
+object UnsupportedProgrammingLanguage : PluginProblem() {
   override val level = Level.ERROR
-  override val message = "Only ${supportedLanguages.joinToString()} languages are supported"
+  override val message = "Only ${Language.values().joinToString { it.id }} languages are supported"
 }
