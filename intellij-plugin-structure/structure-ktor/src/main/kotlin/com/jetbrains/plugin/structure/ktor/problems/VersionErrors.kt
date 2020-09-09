f1df738488834b65261e35b4915df245e7a36a1a
Vadim Briliantov <vadim.briliantov@jetbrains.com>
2020-09-09 18:25:22 +0300

    fixup! Reformat VersionErrors.kt

--- intellij-plugin-structure/structure-ktor/src/main/kotlin/com/jetbrains/plugin/structure/ktor/problems/VersionErrors.kt
+++ intellij-plugin-structure/structure-ktor/src/main/kotlin/com/jetbrains/plugin/structure/ktor/problems/VersionErrors.kt
@@ -4,29 +4,26 @@
 
 package com.jetbrains.plugin.structure.ktor.problems
 
-import com.jetbrains.plugin.structure.base.problems.IncorrectPluginFile
 import com.jetbrains.plugin.structure.base.problems.InvalidDescriptorProblem
-import com.jetbrains.plugin.structure.base.problems.PluginFileError
-import com.jetbrains.plugin.structure.ktor.KtorFeaturePluginManager
 
 class IncorrectKtorVersionFormat(val ktorVersion: String) : InvalidDescriptorProblem(null) {
 
-    override val detailedMessage
-        get() = "Provided ktor version (\"$ktorVersion\") has incorrect format. " +
-                "Ktor version must be of the following format: \"X.Y.Z\" or \"X.Y.Z-w\", where X, Y, Z are numbers, w is string."
+  override val detailedMessage
+    get() = "Provided ktor version (\"$ktorVersion\") has incorrect format. " +
+        "Ktor version must be of the following format: \"X.Y.Z\" or \"X.Y.Z-w\", where X, Y, Z are numbers, w is string."
 
-    override val level
-        get() = Level.ERROR
+  override val level
+    get() = Level.ERROR
 
 }
 
 class KtorVersionDoesNotExist(val ktorVersion: String) : InvalidDescriptorProblem(null) {
 
-    override val detailedMessage
-        get() = "Provided ktor version (\"$ktorVersion\") does not exist. " +
-                "Please, visit https://mvnrepository.com/artifact/io.ktor/ktor-server-core to see possible ktor versions"
+  override val detailedMessage
+    get() = "Provided ktor version (\"$ktorVersion\") does not exist. " +
+        "Please, visit https://mvnrepository.com/artifact/io.ktor/ktor-server-core to see possible ktor versions"
 
-    override val level
-        get() = Level.ERROR
+  override val level
+    get() = Level.ERROR
 
 }
