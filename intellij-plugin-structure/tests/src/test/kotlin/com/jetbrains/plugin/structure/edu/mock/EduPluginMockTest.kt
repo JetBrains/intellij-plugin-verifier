64c59d0ae233d54b06ef6115570c618cc06c5a95
Ekaterina Smal <ekaterina.smal@jetbrains.com>
2020-10-27 15:15:30 +0300

    added lesson names to the sections in EduStat

--- intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/edu/mock/EduPluginMockTest.kt
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/edu/mock/EduPluginMockTest.kt
@@ -2,7 +2,10 @@ package com.jetbrains.plugin.structure.edu.mock
 
 import com.jetbrains.plugin.structure.base.plugin.PluginProblem
 import com.jetbrains.plugin.structure.base.utils.contentBuilder.buildZipFile
-import com.jetbrains.plugin.structure.edu.*
+import com.jetbrains.plugin.structure.edu.EduPlugin
+import com.jetbrains.plugin.structure.edu.EduPluginManager
+import com.jetbrains.plugin.structure.edu.EduPluginVersion
+import com.jetbrains.plugin.structure.edu.TaskType
 import com.jetbrains.plugin.structure.mocks.BasePluginManagerTest
 import com.jetbrains.plugin.structure.rules.FileSystemType
 import org.junit.Assert.*
@@ -42,17 +45,53 @@ class EduPluginMockTest(fileSystemType: FileSystemType) : BasePluginManagerTest<
     val eduStat = plugin.eduStat
     assertNotNull(eduStat)
     assertEquals(3, eduStat!!.lessons.size)
-    assertEquals(1, eduStat.sections.size)
+    assertEquals(0, eduStat.sections.size)
     assertEquals(4, eduStat.tasks.size)
     assertEquals(2, eduStat.tasks[TaskType.EDU.id])
     assertEquals(5, eduStat.tasks[TaskType.CHOICE.id])
-    assertEquals(2, eduStat.tasks[TaskType.THEORY.id])
+    assertEquals(1, eduStat.tasks[TaskType.THEORY.id])
     assertEquals(1, eduStat.tasks[TaskType.IDE.id])
     assertEquals(0, eduStat.tasks[TaskType.OUTPUT.id] ?: 0)
 
     assertEquals(3, eduStat.countInteractiveChallenges())
     assertEquals(5, eduStat.countQuizzes())
-    assertEquals(2, eduStat.countTheoryTasks())
+    assertEquals(1, eduStat.countTheoryTasks())
+  }
+
+  @Test
+  fun `parse edu plugin stat with sections test`() {
+    val pluginFile = buildZipFile(temporaryFolder.newFile("plugin.zip")) {
+      file(EduPluginManager.DESCRIPTOR_NAME) {
+        getMockPluginJsonContent("course_stat_sections")
+      }
+      file("courseIcon.svg", iconTestContent)
+    }
+    val pluginCreationSuccess = createPluginSuccessfully(pluginFile)
+    val plugin = pluginCreationSuccess.plugin
+
+    assertEquals("Python Course", plugin.pluginName)
+    val eduStat = plugin.eduStat
+    assertNotNull(eduStat)
+    assertEquals(3, eduStat!!.lessons.size)
+    assertEquals(2, eduStat.sections.size)
+    val section1Stat = eduStat.sections["section1"]
+    assertNotNull(section1Stat)
+    assertEquals(1, section1Stat!!.size)
+    assertEquals(listOf("lesson1 in section1"), section1Stat)
+    val section2Stat = eduStat.sections["section2"]
+    assertNotNull(section2Stat)
+    assertEquals(2, section2Stat!!.size)
+    assertEquals(listOf("lesson1 in section2", "lesson2 in section2"), section2Stat)
+    assertEquals(4, eduStat.tasks.size)
+    assertEquals(3, eduStat.tasks[TaskType.EDU.id])
+    assertEquals(1, eduStat.tasks[TaskType.CHOICE.id])
+    assertEquals(1, eduStat.tasks[TaskType.THEORY.id])
+    assertEquals(1, eduStat.tasks[TaskType.IDE.id])
+    assertEquals(0, eduStat.tasks[TaskType.OUTPUT.id] ?: 0)
+
+    assertEquals(4, eduStat.countInteractiveChallenges())
+    assertEquals(1, eduStat.countQuizzes())
+    assertEquals(1, eduStat.countTheoryTasks())
   }
 
   private fun testMockPluginStructureAndConfiguration(pluginFile: Path) {
