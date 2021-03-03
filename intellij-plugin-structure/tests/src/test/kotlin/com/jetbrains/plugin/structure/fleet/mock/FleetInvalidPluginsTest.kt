32fe67dc934106a8ee4597cd1e848a9162047dbe
Prendota <Prendota@mail.ru>
2021-03-03 10:20:53 +0300

    test(fleet): add

---
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/fleet/mock/FleetInvalidPluginsTest.kt
@@ -0,0 +1,70 @@
+package com.jetbrains.plugin.structure.fleet.mock
+
+import com.jetbrains.plugin.structure.base.plugin.PluginProblem
+import com.jetbrains.plugin.structure.base.problems.PropertyNotSpecified
+import com.jetbrains.plugin.structure.base.utils.contentBuilder.buildZipFile
+import com.jetbrains.plugin.structure.base.utils.simpleName
+import com.jetbrains.plugin.structure.fleet.FleetPlugin
+import com.jetbrains.plugin.structure.fleet.FleetPluginManager
+import com.jetbrains.plugin.structure.fleet.problems.createIncorrectFleetPluginFile
+import com.jetbrains.plugin.structure.mocks.BasePluginManagerTest
+import com.jetbrains.plugin.structure.rules.FileSystemType
+import org.junit.Test
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class FleetInvalidPluginsTest(fileSystemType: FileSystemType) : BasePluginManagerTest<FleetPlugin, FleetPluginManager>(fileSystemType) {
+  override fun createManager(extractDirectory: Path) = FleetPluginManager.createManager(extractDirectory)
+
+  @Test(expected = IllegalArgumentException::class)
+  fun `file does not exist`() {
+    assertProblematicPlugin(Paths.get("does-not-exist.zip"), emptyList())
+  }
+
+  @Test
+  fun `invalid file extension`() {
+    val incorrect = temporaryFolder.newFile("incorrect.txt")
+    assertProblematicPlugin(incorrect, listOf(createIncorrectFleetPluginFile(incorrect.simpleName)))
+  }
+
+  @Test
+  fun `name is not specified`() {
+    checkInvalidPlugin(PropertyNotSpecified("name")) { name = null }
+    checkInvalidPlugin(PropertyNotSpecified("name")) { name = "" }
+    checkInvalidPlugin(PropertyNotSpecified("name")) { name = "\n" }
+  }
+
+  @Test
+  fun `id is not specified`() {
+    checkInvalidPlugin(PropertyNotSpecified("id")) { id = null }
+    checkInvalidPlugin(PropertyNotSpecified("id")) { id = "" }
+    checkInvalidPlugin(PropertyNotSpecified("id")) { id = "\n" }
+  }
+
+  @Test
+  fun `description is not specified`() {
+    checkInvalidPlugin(PropertyNotSpecified("description")) { description = null }
+    checkInvalidPlugin(PropertyNotSpecified("description")) { description = "" }
+    checkInvalidPlugin(PropertyNotSpecified("description")) { description = "\n" }
+  }
+
+
+  @Test
+  fun `entryPoint is not specified`() {
+    checkInvalidPlugin(PropertyNotSpecified("entryPoint")) { entryPoint = null }
+    checkInvalidPlugin(PropertyNotSpecified("entryPoint")) { entryPoint = "" }
+    checkInvalidPlugin(PropertyNotSpecified("entryPoint")) { entryPoint = "\n" }
+  }
+
+
+  private fun checkInvalidPlugin(problem: PluginProblem, descriptor: FleetTestDescriptor.() -> Unit) {
+    val pluginFile = buildZipFile(temporaryFolder.newFolder().resolve("fleet.language.css-1.0.0-SNAPSHOT.zip")) {
+      file(FleetPluginManager.DESCRIPTOR_NAME) {
+        val builder = perfectFleetPluginBuilder
+        builder.descriptor()
+        builder.asString()
+      }
+    }
+    assertProblematicPlugin(pluginFile, listOf(problem))
+  }
+}
---
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/fleet/mock/FleetTestData.kt
@@ -0,0 +1,25 @@
+package com.jetbrains.plugin.structure.fleet.mock
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.jetbrains.plugin.structure.fleet.bean.FleetDependency
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class FleetTestDescriptor(
+  var id: String? = "fleet.language.css",
+  var name: String? = "CSS",
+  var version: String? = "1.0.0-SNAPSHOT",
+  var description: String? = "CSS language support",
+  var entryPoint: String? = "fleet.language.css.Css",
+  val requires: List<FleetDependency>? = null
+) {
+  fun asString(): String = jacksonObjectMapper().writeValueAsString(this)
+}
+
+val perfectFleetPluginBuilder
+  get() = FleetTestDescriptor()
+
+fun getMockPluginJsonContent(fileName: String): String {
+  return object {}.javaClass.getResource("/fleet/$fileName.json").readText()
+}
