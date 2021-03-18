76a94f71b289b6d333a230d2dfdf4782888597dc
Prendota <Prendota@mail.ru>
2021-03-18 12:53:18 +0300

    chore(fleet): add fullDescriptorJson

--- intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/fleet/mock/FleetPluginMockTest.kt
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/fleet/mock/FleetPluginMockTest.kt
@@ -95,6 +95,7 @@ class FleetPluginMockTest(fileSystemType: FileSystemType) : BasePluginManagerTes
     assertEquals("CSS language support", plugin.description)
     assertEquals("1.0.0-SNAPSHOT", plugin.pluginVersion)
     assertTrue(plugin.requires?.isNotEmpty() == true)
+    assertTrue(plugin.fullDescriptorJson.isNotEmpty())
     assertEquals("fleet.language.xml", plugin.requires?.first()?.id)
   }
 }
