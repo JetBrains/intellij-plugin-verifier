a7324722a75e9c75b21ceb83d6fa84ca47ffdcf8
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2021-05-03 14:00:28 +0300

    AppCode: cache IDE repository index.

--- plugins-verifier-service/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/ide/AppCodeIdeRepository.kt
+++ plugins-verifier-service/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/ide/AppCodeIdeRepository.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.verifier.service.service.ide
 
+import com.google.common.base.Suppliers
 import com.jetbrains.plugin.structure.ide.IntelliJPlatformProduct
 import com.jetbrains.plugin.structure.intellij.version.IdeVersion
 import com.jetbrains.pluginverifier.ide.AvailableIde
@@ -8,6 +9,7 @@ import org.jetbrains.teamcity.rest.Build
 import org.jetbrains.teamcity.rest.BuildConfigurationId
 import org.jetbrains.teamcity.rest.TeamCityInstanceFactory
 import java.net.URL
+import java.util.concurrent.TimeUnit
 
 class AppCodeIdeRepository(
   private val buildServerUrl: String,
@@ -20,7 +22,11 @@ class AppCodeIdeRepository(
 
   private val appCodeProduct = IntelliJPlatformProduct.APPCODE
 
-  override fun fetchIndex(): List<AvailableIde> {
+  private val indexCache = Suppliers.memoizeWithExpiration<List<AvailableIde>>(this::updateIndex, 5, TimeUnit.MINUTES)
+
+  override fun fetchIndex(): List<AvailableIde> = indexCache.get()
+
+  private fun updateIndex(): List<AvailableIde> {
     val allBuilds = configurationIds.asSequence().flatMap { configurationId ->
       teamCityInstance.builds()
         .fromConfiguration(BuildConfigurationId(configurationId))
