3e2c2def1af319549641ca4550077ac4bccadcf1
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2021-05-13 09:30:09 +0300

    IDE repository composite: do not fail if one repository experiences problems.

--- intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/ide/repositories/CompositeIdeRepository.kt
+++ intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/ide/repositories/CompositeIdeRepository.kt
@@ -6,12 +6,25 @@ package com.jetbrains.pluginverifier.ide.repositories
 
 import com.jetbrains.plugin.structure.intellij.version.IdeVersion
 import com.jetbrains.pluginverifier.ide.AvailableIde
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class CompositeIdeRepository(private val ideRepositories: List<IdeRepository>) : IdeRepository {
+
+  private companion object {
+    val LOG: Logger = LoggerFactory.getLogger(CompositeIdeRepository::class.java)
+  }
+
   override fun fetchIndex(): List<AvailableIde> {
     val versionToIde = hashMapOf<IdeVersion, AvailableIde>()
     for (ideRepository in ideRepositories) {
-      for (availableIde in ideRepository.fetchIndex()) {
+      val index = try {
+        ideRepository.fetchIndex()
+      } catch (e: Exception) {
+        LOG.warn("Failed to request index from $ideRepository: ${e.message}")
+        continue
+      }
+      for (availableIde in index) {
         val ide = versionToIde[availableIde.version]
         if (ide == null || availableIde.isRelease) {
           versionToIde[availableIde.version] = availableIde
