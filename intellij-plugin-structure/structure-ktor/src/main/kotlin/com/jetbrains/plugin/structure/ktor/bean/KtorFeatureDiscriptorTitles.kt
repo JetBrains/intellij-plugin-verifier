114bc93a4f6ec7734fa91a73a5d50f9aef67ea6c
Brilyantov Vadim <vadim.briliantov@jetbrains.com>
2021-05-28 13:54:19 +0300

    Update KtorFeatureDiscriptorTitles.kt

--- intellij-plugin-structure/structure-ktor/src/main/kotlin/com/jetbrains/plugin/structure/ktor/bean/KtorFeatureDiscriptorTitles.kt
+++ intellij-plugin-structure/structure-ktor/src/main/kotlin/com/jetbrains/plugin/structure/ktor/bean/KtorFeatureDiscriptorTitles.kt
@@ -87,4 +87,14 @@ const val IN_CINFIG = "application_conf"
 // Extra Files:
 const val FILE_PATH = "relative_path"
 const val FILE_CONTENT = "content"
+/**
+ * A relative path to a file of any type that can be added to a Ktor application.
+ * Only simple path scenarios are supported (not like in a complex file system).
+ * A path can only consist of one or more parts separated with "/", while each part is an alphanumerical string
+ * with small/large latin letters and/or numbers, and the last part can also have one "." symbol in the middle (file extension)
+ *
+ * Examples:
+ * a) path/to/a/file
+ * b) path/to/a/file.ext
+ * */
 val SIMPLE_PATH_REGEX = "([a-zA-Z0-9\\-_]+/)*[a-zA-Z0-9\\-_]+(\\.[a-zA-Z0-9]+)?".toRegex()
