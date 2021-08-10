a08990dc0aae9c71c8acaa835bb6926108f7e819
Prendota <Prendota@mail.ru>
2021-08-10 11:30:01 +0300

    chore(sarif): invocation converter

---
+++ plugins-verifier-service/src/main/kotlin/org/jetbrains/plugins/verifier/service/service/sarif/SarifInspectionConverter.kt
@@ -0,0 +1,82 @@
+package org.jetbrains.plugins.verifier.service.service.sarif
+
+import com.jetbrains.pluginverifier.PluginVerificationResult
+
+
+internal fun PluginVerificationResult.Verified.buildVerifiedInspections(): List<InspectionResult> {
+  val pluginStructureWarningsInspection = buildPluginStructureWarningsInspection()
+  val compatibilityWarningsInspection = buildCompatibilityWarningsInspection()
+  val compatibilityProblemsInspection = buildCompatibilityProblemInspection()
+  val apiUsagesInspections = buildApiUsageInspection()
+  return pluginStructureWarningsInspection + compatibilityWarningsInspection + compatibilityProblemsInspection + apiUsagesInspections
+}
+
+
+internal fun PluginVerificationResult.InvalidPlugin.buildPluginStructureInspections(): List<InspectionResult> {
+  return pluginStructureErrors.map {
+    InspectionResult(
+      ruleId = it.problemType,
+      level = SeverityValue.ERROR,
+      message = Message(it.message),
+      location = emptyList()
+    )
+  }
+}
+
+internal fun PluginVerificationResult.buildSingleInvocation(): List<InspectionResult> {
+  return listOf(
+    InspectionResult(
+      ruleId = this.javaClass.canonicalName,
+      level = SeverityValue.ERROR,
+      message = Message(this.verificationVerdict),
+      location = emptyList()
+    )
+  )
+}
+
+
+private fun PluginVerificationResult.Verified.buildApiUsageInspection(): List<InspectionResult> {
+  val apiUsages = deprecatedUsages + experimentalApiUsages + internalApiUsages + nonExtendableApiUsages + overrideOnlyMethodUsages
+  return apiUsages.map {
+    InspectionResult(
+      ruleId = it.javaClass.canonicalName,
+      level = SeverityValue.ERROR,
+      message = Message(it.fullDescription),
+      location = emptyList()
+    )
+  }
+}
+
+private fun PluginVerificationResult.Verified.buildCompatibilityProblemInspection(): List<InspectionResult> {
+  return compatibilityProblems.map {
+    InspectionResult(
+      ruleId = it.problemType,
+      level = SeverityValue.ERROR,
+      message = Message(it.fullDescription),
+      location = emptyList()
+    )
+  }
+}
+
+private fun PluginVerificationResult.Verified.buildCompatibilityWarningsInspection(): List<InspectionResult> {
+  return compatibilityWarnings.map {
+    InspectionResult(
+      ruleId = it.javaClass.canonicalName,
+      level = SeverityValue.WARNING,
+      message = Message(it.fullDescription),
+      location = emptyList()
+    )
+  }
+}
+
+private fun PluginVerificationResult.Verified.buildPluginStructureWarningsInspection(): List<InspectionResult> {
+  return pluginStructureWarnings.map {
+    InspectionResult(
+      ruleId = it.problemType,
+      level = SeverityValue.WARNING,
+      message = Message(it.message),
+      location = emptyList()
+    )
+  }
+}
+
