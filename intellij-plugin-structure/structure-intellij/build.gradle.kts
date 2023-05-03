b4336f5e2408f582dc712d5edcf51c576c7c26ca
Robert Novotny <robert.novotny@jetbrains.com>
2023-05-03 12:19:38 +0200

    Migrate 'intellij-structure/intellij' Gradle settings to Kotlin

---
+++ intellij-plugin-structure/structure-intellij/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+  api(project(":structure-base"))
+  api("org.jdom:jdom2:2.0.6.1")
+
+  implementation("org.jsoup:jsoup:1.15.4")
+  implementation("javax.xml.bind:jaxb-api:2.3.1")
+  implementation("org.glassfish.jaxb:jaxb-runtime:2.3.1")
+}
