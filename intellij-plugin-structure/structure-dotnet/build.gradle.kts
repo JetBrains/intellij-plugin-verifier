b22413a27e97ba5acb059b55f3b3234b2615b130
Robert Novotny <robert.novotny@jetbrains.com>
2023-05-03 12:18:58 +0200

    Migrate 'intellij-structure/dotnet' Gradle settings to Kotlin

---
+++ intellij-plugin-structure/structure-dotnet/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+  api(project(":structure-base"))
+  implementation("org.jdom:jdom2:2.0.6.1")
+  implementation("javax.xml.bind:jaxb-api:2.3.1")
+  implementation("org.glassfish.jaxb:jaxb-runtime:2.3.1")
+}
