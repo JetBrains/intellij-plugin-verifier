c329dd5aaf157d69fdf6bb98dff86a9bd0fc05e8
Robert Novotny <robert.novotny@jetbrains.com>
2023-05-04 10:25:14 +0200

    Consolidate dependency repository declarations

--- intellij-feature-extractor/build.gradle.kts
+++ intellij-feature-extractor/build.gradle.kts
@@ -18,6 +18,8 @@ allprojects {
   group = "org.jetbrains.intellij.plugins"
 
   repositories {
+    maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+    maven("https://cache-redirector.jetbrains.com/repo1.maven.org/maven2")
     mavenCentral()
     mavenLocal()
     maven { url = uri("https://www.jetbrains.com/intellij-repository/releases") }
