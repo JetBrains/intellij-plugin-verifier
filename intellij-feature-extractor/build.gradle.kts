6d923e955c32895e920c2f34f103c2cb043f7fee
Robert Novotny <robert.novotny@jetbrains.com>
2023-05-04 11:08:57 +0200

    Readd IntelliJ Releases Maven repository

--- intellij-feature-extractor/build.gradle.kts
+++ intellij-feature-extractor/build.gradle.kts
@@ -18,6 +18,7 @@ allprojects {
   group = "org.jetbrains.intellij.plugins"
 
   repositories {
+    maven("https://cache-redirector.jetbrains.com/intellij-repository/releases")
     maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
     maven("https://cache-redirector.jetbrains.com/repo1.maven.org/maven2")
     mavenCentral()
