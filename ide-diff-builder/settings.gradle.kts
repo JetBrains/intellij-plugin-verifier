a26efa056c5966e8f399e64deb72068caa054234
Robert Novotny <robert.novotny@jetbrains.com>
2023-05-09 15:06:33 +0200

    Reference shared dependency catalog

--- ide-diff-builder/settings.gradle.kts
+++ ide-diff-builder/settings.gradle.kts
@@ -1,3 +1,11 @@
 rootProject.name = "ide-diff-builder"
 include("mock-old-ide")
 include("mock-new-ide")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("sharedLibs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
