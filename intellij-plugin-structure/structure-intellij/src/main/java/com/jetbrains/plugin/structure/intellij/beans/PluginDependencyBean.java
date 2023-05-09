5a2064c996931069a86046030fc24070398dfdfd
Robert Novotny <robert.novotny@jetbrains.com>
2023-05-09 12:42:02 +0200

    Add Javadoc

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/beans/PluginDependencyBean.java
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/beans/PluginDependencyBean.java
@@ -7,6 +7,11 @@ package com.jetbrains.plugin.structure.intellij.beans;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlValue;
 
+/**
+ * Plugin dependency mapped to {@code <depends>} element in <code>plugin.xml</code>.
+ *
+ * @see <a href="https://plugins.jetbrains.com/docs/intellij/plugin-dependencies.html#3-dependency-declaration-in-pluginxml">Dependency Declaration in plugin.xml</a>
+ */
 public class PluginDependencyBean {
   @XmlAttribute(name = "optional") public Boolean optional;
   @XmlAttribute(name = "config-file") public String configFile;
