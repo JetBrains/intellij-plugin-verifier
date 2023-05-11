c279fca7cb35bbfe060698658fab9a98d6306d46
Robert Novotny <robert.novotny@jetbrains.com>
2023-05-11 22:38:34 +0200

    Add shared dependencies from catalog

--- plugins-verifier-service/settings.gradle.kts
+++ plugins-verifier-service/settings.gradle.kts
@@ -3,3 +3,11 @@ rootProject.name = "plugins-verifier-service"
 includeBuild("../intellij-feature-extractor")
 includeBuild("../intellij-plugin-verifier")
 includeBuild("../intellij-plugin-structure")
+
+dependencyResolutionManagement {
+  versionCatalogs {
+    create("sharedLibs") {
+      from(files("../gradle/libs.versions.toml"))
+    }
+  }
+}
