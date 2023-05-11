c818fc2096b0fcab6525738c28f4554430e9d659
Robert Novotny <robert.novotny@jetbrains.com>
2023-05-11 22:37:51 +0200

    Migrate Gradle settings to Kotlin DSL

---
+++ plugins-verifier-service/settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "plugins-verifier-service"
+
+includeBuild("../intellij-feature-extractor")
+includeBuild("../intellij-plugin-verifier")
+includeBuild("../intellij-plugin-structure")
