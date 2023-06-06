499151be29f2e9872a0bc93b75e11ec4616bbfe8
Robert Novotny <robert.novotny@jetbrains.com>
2023-06-06 11:01:02 +0200

    verifier-core: Declare explicit dependency on Guava

--- intellij-plugin-verifier/verifier-core/build.gradle.kts
+++ intellij-plugin-verifier/verifier-core/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
   val intellijStructureVersion : String by rootProject.extra
   implementation("org.jetbrains.intellij.plugins:structure-classes:$intellijStructureVersion")
+
+  implementation(sharedLibs.guava)
 }
