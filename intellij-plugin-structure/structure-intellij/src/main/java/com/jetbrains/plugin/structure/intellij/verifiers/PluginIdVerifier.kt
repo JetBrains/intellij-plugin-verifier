2ad14a3311163f76e0a1f7a71c2ae6812d28ea40
Robert Novotny <robert.novotny@jetbrains.com>
2023-06-07 10:34:34 +0200

    Make ProblemRegistrar a functional interface

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/verifiers/PluginIdVerifier.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/verifiers/PluginIdVerifier.kt
@@ -4,7 +4,6 @@ import com.jetbrains.plugin.structure.base.plugin.PluginProblem
 import com.jetbrains.plugin.structure.base.problems.PropertyNotSpecified
 import com.jetbrains.plugin.structure.intellij.beans.PluginBean
 import com.jetbrains.plugin.structure.intellij.plugin.CORE_PLUGIN_ID
-import com.jetbrains.plugin.structure.intellij.plugin.PluginCreator
 import com.jetbrains.plugin.structure.intellij.plugin.SPECIAL_IDEA_PLUGIN_ID
 import com.jetbrains.plugin.structure.intellij.problems.IllegalPluginIdPrefix
 import com.jetbrains.plugin.structure.intellij.problems.PropertyWithDefaultValue
@@ -24,40 +23,36 @@ val PRODUCT_ID_RESTRICTED_WORDS = listOf(
 
 class PluginIdVerifier {
 
-  fun verify(plugin: PluginBean, descriptorPath: String, problemConsumer: (PluginProblem) -> Unit) {
+  fun verify(plugin: PluginBean, descriptorPath: String, problemRegistrar: ProblemRegistrar) {
     val id = plugin.id ?: return
 
     when {
       id.isBlank() -> {
-        problemConsumer(PropertyNotSpecified("id"))
+        problemRegistrar.registerProblem(PropertyNotSpecified("id"))
       }
       "com.your.company.unique.plugin.id" == id -> {
-        problemConsumer(PropertyWithDefaultValue(descriptorPath, PropertyWithDefaultValue.DefaultProperty.ID, id))
+        problemRegistrar.registerProblem(PropertyWithDefaultValue(descriptorPath, PropertyWithDefaultValue.DefaultProperty.ID, id))
       }
       else -> {
-        validatePropertyLength("id", id, MAX_PROPERTY_LENGTH) {
-          problemConsumer(it)
-        }
-        validateNewlines("id", id) {
-          problemConsumer(it)
-        }
-        verifyPrefix(plugin, descriptorPath, problemConsumer)
+        validatePropertyLength("id", id, MAX_PROPERTY_LENGTH, descriptorPath, problemRegistrar)
+        validateNewlines("id", id, descriptorPath, problemRegistrar)
+        verifyPrefix(plugin, descriptorPath, problemRegistrar)
       }
     }
   }
 
-  private fun verifyPrefix(plugin: PluginBean, descriptorPath: String, problemConsumer: (PluginProblem) -> Unit) {
+  private fun verifyPrefix(plugin: PluginBean, descriptorPath: String, problemRegistrar: ProblemRegistrar) {
     if (isDevelopedByJetBrains(plugin)) {
       return
     }
     val id = plugin.id
     DEFAULT_ILLEGAL_PREFIXES
       .filter(id::startsWith)
-      .forEach { problemConsumer(IllegalPluginIdPrefix(id, it)) }
+      .forEach { problemRegistrar.registerProblem(IllegalPluginIdPrefix(id, it)) }
 
     id.split('.')
       .filter { idComponent -> PRODUCT_ID_RESTRICTED_WORDS.contains(idComponent) }
-      .forEach { idComponent -> problemConsumer(TemplateWordInPluginId(descriptorPath, idComponent)) }
+      .forEach { idComponent -> problemRegistrar.registerProblem(TemplateWordInPluginId(descriptorPath, idComponent)) }
   }
 
   private fun isDevelopedByJetBrains(plugin: PluginBean): Boolean {
--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/verifiers/SimpleVerifiers.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/verifiers/SimpleVerifiers.kt
@@ -4,8 +4,6 @@ import com.jetbrains.plugin.structure.base.plugin.PluginProblem
 import com.jetbrains.plugin.structure.base.problems.ContainsNewlines
 import com.jetbrains.plugin.structure.intellij.problems.TooLongPropertyValue
 
-typealias ProblemRegistrar = (PluginProblem) -> Unit
-
 const val DEFAULT_DESCRIPTOR_PATH = "plugin.xml"
 
 const val MAX_PROPERTY_LENGTH = 255
@@ -14,13 +12,13 @@ fun validateNewlines(propertyName: String, propertyValue: String,
                      descriptorPath: String = DEFAULT_DESCRIPTOR_PATH,
                      problemRegistrar: ProblemRegistrar) {
   if (propertyValue.trim().contains("\n")) {
-    problemRegistrar(ContainsNewlines(propertyName, descriptorPath))
+    problemRegistrar.registerProblem(ContainsNewlines(propertyName, descriptorPath))
   }
 }
 
 fun validatePropertyLength(propertyName: String, propertyValue: String, maxLength: Int,
                            descriptorPath: String = DEFAULT_DESCRIPTOR_PATH, problemRegistrar: ProblemRegistrar) {
   if (propertyValue.length > maxLength) {
-    problemRegistrar(TooLongPropertyValue(descriptorPath, propertyName, propertyValue.length, maxLength))
+    problemRegistrar.registerProblem(TooLongPropertyValue(descriptorPath, propertyName, propertyValue.length, maxLength))
   }
 }
