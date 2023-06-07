d8bbce3f64abf41ff63bb01f33d5ec7073619086
Robert Novotny <robert.novotny@jetbrains.com>
2023-06-07 10:53:27 +0200

    Bump license year

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/filtering/InternalApiUsageFilter.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/filtering/InternalApiUsageFilter.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2000-2023 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package com.jetbrains.pluginverifier.filtering
 
 import com.jetbrains.plugin.structure.intellij.plugin.PluginVendors
