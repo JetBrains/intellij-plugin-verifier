8427e55fdb3ece351644f5d69284ec00828c3f8d
Robert Novotny <robert.novotny@jetbrains.com>
2023-06-08 10:02:28 +0200

    Use pre-existing constant

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/verifiers/SimpleVerifiers.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/verifiers/SimpleVerifiers.kt
@@ -1,14 +1,13 @@
 package com.jetbrains.plugin.structure.intellij.verifiers
 
 import com.jetbrains.plugin.structure.base.problems.ContainsNewlines
+import com.jetbrains.plugin.structure.intellij.plugin.IdePluginManager.Companion.PLUGIN_XML
 import com.jetbrains.plugin.structure.intellij.problems.TooLongPropertyValue
-
-const val DEFAULT_DESCRIPTOR_PATH = "plugin.xml"
 
 const val MAX_PROPERTY_LENGTH = 255
 
 fun validateNewlines(propertyName: String, propertyValue: String,
-                     descriptorPath: String = DEFAULT_DESCRIPTOR_PATH,
+                     descriptorPath: String = PLUGIN_XML,
                      problemRegistrar: ProblemRegistrar) {
   if (propertyValue.trim().contains("\n")) {
     problemRegistrar.registerProblem(ContainsNewlines(propertyName, descriptorPath))
@@ -16,7 +15,7 @@ fun validateNewlines(propertyName: String, propertyValue: String,
 }
 
 fun validatePropertyLength(propertyName: String, propertyValue: String, maxLength: Int,
-                           descriptorPath: String = DEFAULT_DESCRIPTOR_PATH, problemRegistrar: ProblemRegistrar) {
+                           descriptorPath: String = PLUGIN_XML, problemRegistrar: ProblemRegistrar) {
   if (propertyValue.length > maxLength) {
     problemRegistrar.registerProblem(TooLongPropertyValue(descriptorPath, propertyName, propertyValue.length, maxLength))
   }
