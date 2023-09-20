4b3a3698c073c8ad37397546da90365d68cbbbfa
Robert Novotny <robert.novotny@jetbrains.com>
2023-09-20 08:59:44 +0200

    Treat message parsed by Plugin DevKit as a constant

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/reporting/LoggingPluginVerificationReportageAggregator.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/reporting/LoggingPluginVerificationReportageAggregator.kt
@@ -7,6 +7,12 @@ import java.nio.file.Path
 import kotlin.io.path.absolute
 
 /**
+ * This message is parsed by the Plugin DevKit plugin.
+ * If it is modified, please adjust it in the Plugin DevKit as well.
+ */
+private const val VERIFICATION_REPORTS = "Verification reports for %s saved to %s"
+
+/**
  * Stateful aggregator verification reports and their corresponding directories.
  * Allows aggregating such reports and logging them to a logger.
  */
@@ -23,9 +29,10 @@ class LoggingPluginVerificationReportageAggregator(
   fun handleAggregatedReportage() {
     messageReporters.forEach { reporter ->
       for ((result, targetDirectory) in resultsInDirectories) {
-        val message = "Verification reports for " + result.plugin + " saved to ${targetDirectory.absolute()}"
+        val message = VERIFICATION_REPORTS.format(result.plugin, targetDirectory.absolute())
         reporter.report(message)
       }
     }
   }
 }
+
