8c64cc5aedc7c25a5307ba227e63369cd2531363
Robert Novotny <robert.novotny@jetbrains.com>
2023-10-12 15:36:46 +0200

    Simplify mock task result printer

--- intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/mocks/SimpleTaskResultPrinter.kt
+++ intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/mocks/SimpleTaskResultPrinter.kt
@@ -5,9 +5,7 @@ import com.jetbrains.pluginverifier.tasks.TaskResult
 import com.jetbrains.pluginverifier.tasks.TaskResultPrinter
 
 class SimpleTaskResultPrinter : TaskResultPrinter {
-  private val taskResults : List<TaskResult> = mutableListOf()
-
   override fun printResults(taskResult: TaskResult, outputOptions: OutputOptions) {
-    taskResults + taskResult
+    // no-op
   }
 }
