016bc5e49b6899061e1f2e465f77bcd6eef25902
Robert Novotny <robert.novotny@jetbrains.com>
2023-10-26 09:58:39 +0200

    Report plugin verification time

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/reporting/telemetry/TelemetryOutputFormats.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/reporting/telemetry/TelemetryOutputFormats.kt
@@ -1,9 +1,19 @@
 package com.jetbrains.pluginverifier.reporting.telemetry
 
+import com.jetbrains.plugin.structure.base.telemetry.PLUGIN_VERIFICATION_TIME
 import com.jetbrains.plugin.structure.base.telemetry.PluginTelemetry
 import com.jetbrains.plugin.structure.base.utils.formatDuration
+import java.time.Duration
 
-fun PluginTelemetry.toPlainString(): String = buildString {
-  appendLine("Plugin descriptor parsed in: ${parsingDuration.formatDuration()}")
-  appendLine("Plugin size (bytes): ${pluginSize}")
+fun PluginTelemetry.toPlainString(): String {
+  val telemetry = this
+  return buildString {
+    appendLine("Descriptor parsed in: ${parsingDuration.formatDuration()}")
+    appendLine("Plugin size (bytes): $pluginSize")
+    telemetry[PLUGIN_VERIFICATION_TIME]?.let {
+      if (it is Duration) {
+        appendLine("Verification time: ${it.formatDuration()}")
+      }
+    }
+  }
 }
