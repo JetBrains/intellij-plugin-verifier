93415191cc95493306e44198add71f6cc07cd974
Robert Novotny <robert.novotny@jetbrains.com>
2023-10-30 10:23:12 +0100

    Add test for telemetry parsing stage

---
+++ intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/options/PluginsParsingTest.kt
@@ -0,0 +1,87 @@
+package com.jetbrains.pluginverifier.options
+
+import com.jetbrains.plugin.structure.base.telemetry.PluginTelemetry
+import com.jetbrains.plugin.structure.base.utils.contentBuilder.ContentBuilder
+import com.jetbrains.plugin.structure.base.utils.contentBuilder.buildZipFile
+import com.jetbrains.pluginverifier.reporting.PluginVerificationReportage
+import com.jetbrains.pluginverifier.reporting.telemetry.TelemetryAggregator
+import com.jetbrains.pluginverifier.repository.PluginInfo
+import com.jetbrains.pluginverifier.tests.mocks.MockPluginRepositoryAdapter
+import com.jetbrains.pluginverifier.tests.mocks.MockPluginVerificationReportage
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.nio.file.Path
+
+class PluginsParsingTest {
+  @Rule
+  @JvmField
+  val temporaryFolder = TemporaryFolder()
+
+  private val pluginRepository = MockPluginRepositoryAdapter()
+
+  @Test
+  fun `telemetry is gathered in plugin parsing`() {
+    val plugins = PluginsSet()
+    val reportage = TelemetryVerificationReportage()
+    val pluginsParsing = PluginsParsing(pluginRepository, reportage, plugins)
+
+    val pluginZip = buildPluginZipWithXml {
+      """
+        <idea-plugin>
+          $HEADER
+          <actions>
+            <action class="someClass"/>
+          </actions>
+        </idea-plugin>
+        """
+    }
+
+    pluginsParsing.addPluginFile(pluginZip, validateDescriptor = true)
+    assertEquals(1, plugins.pluginsToCheck.size)
+
+    val pluginInfo = plugins.pluginsToCheck.first()
+    val telemetry = reportage[pluginInfo]
+    if (telemetry != null) {
+      assertTrue(telemetry.pluginSize > 0)
+      assertTrue(telemetry.parsingDuration.toMillis() > 0)
+    } else {
+      fail("Plugin telemetry not found for $pluginInfo")
+    }
+  }
+
+  private fun buildPluginZipWithXml(pluginXmlContent: () -> String): Path {
+    return buildPluginZip {
+      dir("META-INF") {
+        file("plugin.xml", pluginXmlContent())
+      }
+    }
+  }
+
+  private fun buildPluginZip(pluginContentBuilder: ContentBuilder.() -> Unit): Path {
+    return buildZipFile(temporaryFolder.newFile("plugin.jar").toPath(), pluginContentBuilder)
+  }
+
+  private class TelemetryVerificationReportage(private val delegate: MockPluginVerificationReportage = MockPluginVerificationReportage()) : PluginVerificationReportage by delegate {
+    private val telemetryAggregator = TelemetryAggregator()
+
+    override fun reportTelemetry(pluginInfo: PluginInfo, telemetry: PluginTelemetry) {
+      telemetryAggregator.reportTelemetry(pluginInfo, telemetry)
+    }
+
+    operator fun get(plugin: PluginInfo): PluginTelemetry? {
+      return telemetryAggregator[plugin]
+    }
+  }
+
+  private val HEADER = """
+      <id>someId</id>
+      <name>someName</name>
+      <version>someVersion</version>
+      ""<vendor email="vendor.com" url="url">vendor</vendor>""
+      <description>this description is looooooooooong enough</description>
+      <change-notes>these change-notes are looooooooooong enough</change-notes>
+      <idea-version since-build="131.1"/>
+    """
+}
