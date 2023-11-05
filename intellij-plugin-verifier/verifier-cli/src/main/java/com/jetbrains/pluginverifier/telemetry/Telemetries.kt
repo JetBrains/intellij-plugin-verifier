a427feb156ca4c87a1591eaf020582b905be3bb2
Robert Novotny <robert.novotny@jetbrains.com>
2023-11-05 12:56:56 +0100

    Read and reconstruct telemetry from telemetry.txt format

--- intellij-plugin-verifier/verifier-cli/src/main/java/com/jetbrains/pluginverifier/telemetry/Telemetries.kt
+++ intellij-plugin-verifier/verifier-cli/src/main/java/com/jetbrains/pluginverifier/telemetry/Telemetries.kt
@@ -1,7 +1,11 @@
 package com.jetbrains.pluginverifier.telemetry
 
 import com.jetbrains.plugin.structure.base.telemetry.MutablePluginTelemetry
+import com.jetbrains.plugin.structure.base.telemetry.PLUGIN_VERIFICATION_TIME
+import com.jetbrains.plugin.structure.base.telemetry.PluginTelemetry
+import com.jetbrains.plugin.structure.base.utils.Bytes
 import java.io.File
+import java.time.Duration
 
 fun readTelemetry(telemetryFile: File): MutablePluginTelemetry {
   val telemetry = MutablePluginTelemetry()
@@ -14,4 +18,52 @@ fun readTelemetry(telemetryFile: File): MutablePluginTelemetry {
     }
   }
   return telemetry
+}
+
+fun PluginTelemetry.fromPlainStringTelemetry(): PluginTelemetry {
+  return MutablePluginTelemetry().also { newTelemetry ->
+    duration("Descriptor parsed (raw ms)") {
+      newTelemetry.parsingDuration = it
+    }
+    duration("Verification time (raw ms)") {
+      newTelemetry[PLUGIN_VERIFICATION_TIME] = it
+    }
+    bytes("Plugin size (bytes)") {
+      newTelemetry.pluginSize = it
+    }
+  }
+}
+
+private inline fun PluginTelemetry.long(key: String, whenNotNull: (Long) -> Unit) {
+  val value = this[key] ?: return
+  if (value is Long) {
+    whenNotNull(value)
+  } else {
+    val longValue = value.toString().toLongOrNull()
+    if (longValue != null) {
+      whenNotNull(longValue)
+    }
+  }
+}
+
+private inline fun PluginTelemetry.bytes(key: String, whenNotNull: (Bytes) -> Unit) {
+  val value = this[key] ?: return
+  if (value is Bytes) {
+    whenNotNull(value)
+  } else {
+    long(key) {
+      whenNotNull(it)
+    }
+  }
+}
+
+private inline fun PluginTelemetry.duration(key: String, whenNotNull: (Duration) -> Unit) {
+  val value = this[key] ?: return
+  if (value is Duration) {
+    whenNotNull(value)
+  } else {
+    long(key) {
+      whenNotNull(Duration.ofMillis(it))
+    }
+  }
 }
--- intellij-plugin-verifier/verifier-cli/src/main/java/com/jetbrains/pluginverifier/telemetry/TelemetryAggregatorCli.kt
+++ intellij-plugin-verifier/verifier-cli/src/main/java/com/jetbrains/pluginverifier/telemetry/TelemetryAggregatorCli.kt
@@ -1,33 +1,46 @@
 package com.jetbrains.pluginverifier.telemetry
 
+import com.jetbrains.plugin.structure.base.telemetry.PLUGIN_VERIFICATION_TIME
 import com.jetbrains.plugin.structure.base.telemetry.PluginTelemetry
 import com.jetbrains.plugin.structure.ide.PluginIdAndVersion
 import com.jetbrains.plugin.structure.intellij.version.IdeVersion
 import java.io.File
 import java.nio.file.Path
+import java.time.Duration
 
 const val TELEMETRY_FILE = "telemetry.txt"
 
+interface VerificationResultHandler {
+  fun beforeVerificationResult() {}
+
+  fun onVerificationResult(verificationResultDir: TelemetryAggregatorCli.LocatedTelemetry) {}
+
+  fun afterVerificationResult() { }
+}
+
 class TelemetryAggregatorCli(private val verificationOutputDir: Path) {
-  fun run(verificationResultHandler: (VerificationDir) -> Unit = {}) {
+  fun run(verificationResultHandler: VerificationResultHandler) {
     val ideDirs: Array<File> = verificationOutputDir.toFile().listFiles { child: File ->
       child.isDirectory && IdeVersion.createIdeVersionIfValid(child.name) != null
     } ?: emptyArray()
 
+    verificationResultHandler.beforeVerificationResult()
     ideDirs.forEach { ideDir ->
       val pluginsDir = ideDir.pluginDir
       if (pluginsDir != null) {
         processPluginsDir(pluginsDir, IdeVersion.createIdeVersion(ideDir.name))
-          .forEach(verificationResultHandler)
+          .forEach {
+            verificationResultHandler.onVerificationResult(it)
+          }
       }
     }
+    verificationResultHandler.afterVerificationResult()
   }
 
-  private fun processPluginsDir(pluginDir: File, ideVersion: IdeVersion): Sequence<VerificationDir> {
+  private fun processPluginsDir(pluginDir: File, ideVersion: IdeVersion): Sequence<LocatedTelemetry> {
     return pluginDir.walkBottomUp().filter {
       it.name == TELEMETRY_FILE
-    }.take(20)
-      .map { file ->
+    }.map { file ->
         file to file.path.removePrefix(pluginDir.path + File.separator)
       }
       .map { (file, name) ->
@@ -38,16 +51,16 @@ class TelemetryAggregatorCli(private val verificationOutputDir: Path) {
 
   }
 
-  data class VerificationDir(val ideVersion: IdeVersion, val pluginIdAndVersion: PluginIdAndVersion, val telemetry: PluginTelemetry)
+  data class LocatedTelemetry(val ideVersion: IdeVersion, val pluginIdAndVersion: PluginIdAndVersion, val telemetry: PluginTelemetry)
 
-  private fun parse(components: List<String>, ideVersion: IdeVersion, telemetryFile: File): VerificationDir? {
+  private fun parse(components: List<String>, ideVersion: IdeVersion, telemetryFile: File): LocatedTelemetry? {
     if (components.size != 3) {
       return null
     }
     val id = components[0]
     val version = components[1]
-    val telemetry = readTelemetry(telemetryFile)
-    return VerificationDir(ideVersion, PluginIdAndVersion(id, version), telemetry)
+    val telemetry = readTelemetry(telemetryFile).fromPlainStringTelemetry()
+    return LocatedTelemetry(ideVersion, PluginIdAndVersion(id, version), telemetry)
   }
 
   private val File.pluginDir
@@ -56,13 +69,73 @@ class TelemetryAggregatorCli(private val verificationOutputDir: Path) {
     }?.firstOrNull()
 }
 
+class CsvVerificationResultHandler(val csvOutputFile: File) : VerificationResultHandler, AutoCloseable {
+  private val SEPARATOR = ","
+
+  private val columns = listOf("IDE version", "Plugin ID", "Plugin Version",
+    "Plugin Size", "Verification Time",
+    "Description Parsing Time")
+
+  private val writer = csvOutputFile.bufferedWriter()
+
+  override fun beforeVerificationResult() {
+    writer.appendLine(columns.joinToString(SEPARATOR))
+  }
+
+  override fun onVerificationResult(verificationResult: TelemetryAggregatorCli.LocatedTelemetry) {
+    val values = listOf(
+      verificationResult.ideVersion.toString(),
+      verificationResult.pluginIdAndVersion.pluginId,
+      verificationResult.pluginIdAndVersion.version,
+      nullSafeGet { verificationResult.telemetry.pluginSize.asString() },
+      verificationResult.telemetry[PLUGIN_VERIFICATION_TIME].asString(),
+      nullSafeGet {verificationResult.telemetry.parsingDuration.asString() }
+    )
+
+    writer.appendLine(values.joinToString(SEPARATOR))
+  }
+
+  private fun Any?.asString(): String {
+    if (this == null) {
+      return ""
+    }
+    return when (this) {
+      is Duration -> this.toMillis().toString()
+      else -> this.toString()
+    }
+  }
+
+  private fun nullSafeGet(function: () -> String): String {
+    return try {
+      function()
+    } catch (e: NullPointerException) {
+      ""
+    }
+  }
+
+  override fun close() {
+    writer.close()
+  }
+}
+
 fun main(args: Array<String>) {
   require(args.isNotEmpty()) {
     "Path to the verification output directory must be set"
   }
 
   val path = Path.of(args.first())
-  TelemetryAggregatorCli(path).run {
-    println(it)
+
+  val verificationResultHandler = when {
+    args.size > 1 -> {
+      val csvOutputFile = File(args[1])
+      CsvVerificationResultHandler(csvOutputFile)
+    }
+    else -> object : VerificationResultHandler{}
+  }
+
+  TelemetryAggregatorCli(path).run(verificationResultHandler)
+
+  if (verificationResultHandler is AutoCloseable) {
+    verificationResultHandler.close()
   }
 }
