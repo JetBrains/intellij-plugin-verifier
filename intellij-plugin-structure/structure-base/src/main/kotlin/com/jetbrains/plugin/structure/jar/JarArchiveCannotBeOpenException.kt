794716c9d967050faf4986941871888df84bc0fe
Robert Novotny <robert.novotny@jetbrains.com>
2023-11-10 15:15:36 +0100

    Use additional constructor for JAR opening exceptions

--- intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/jar/JarArchiveCannotBeOpenException.kt
+++ intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/jar/JarArchiveCannotBeOpenException.kt
@@ -6,5 +6,7 @@ import java.nio.file.Path
 class JarArchiveCannotBeOpenException : JarArchiveException {
   constructor(jarUri: URI, cause: Throwable) : this("JAR file cannot be open at [$jarUri]", cause)
   constructor(jarPath: Path, cause: Throwable) : this("JAR file cannot be open at [$jarPath]", cause)
+  constructor(jarPath: Path, additionalMessage: String) : super("JAR file cannot be open at [$jarPath]: $additionalMessage")
+  constructor(jarPath: Path, resolvedJarUri: URI, cause: Throwable) : super("JAR file cannot be open at [$jarPath] (resolved URI: <$resolvedJarUri>)", cause)
   private constructor(message: String, cause: Throwable) : super(message, cause)
 }
