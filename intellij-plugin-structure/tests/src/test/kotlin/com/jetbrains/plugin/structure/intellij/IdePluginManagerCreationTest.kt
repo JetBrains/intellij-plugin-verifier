aa4529c5371e21daf5afaaec20e61740ed99439a
Robert Novotny <robert.novotny@jetbrains.com>
2023-12-20 17:08:26 +0100

    Rename unit test class

--- intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/intellij/IdePluginManagerCreationTest.kt
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/intellij/IdePluginManagerCreationTest.kt
@@ -5,18 +5,25 @@ import com.jetbrains.plugin.structure.base.plugin.Settings
 import com.jetbrains.plugin.structure.intellij.plugin.IdePluginManager
 import com.jetbrains.plugin.structure.intellij.resources.DefaultResourceResolver
 import com.jetbrains.plugin.structure.jar.DefaultJarFileSystemProvider
-import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import java.nio.file.Path
 
-class CustomIdePluginManagerPluginTest {
+class IdePluginManagerCreationTest {
   private val jarPath = Path.of("src/test/resources/resolver-jars/sample-jar-with-descriptor.jar")
+
+  @Test
+  fun `IDE plugin manager is created with default JAR file system provider`() {
+    val pluginManager = IdePluginManager.createManager()
+    val pluginCreationResult = pluginManager.createPlugin(jarPath)
+    assertThat(pluginCreationResult, instanceOf(PluginCreationSuccess::class.java))
+  }
 
   @Test
   fun `IDE plugin manager is created with explicit JAR file system provider`() {
     val pluginManager = IdePluginManager.createManager(DefaultResourceResolver, Settings.EXTRACT_DIRECTORY.getAsPath(), DefaultJarFileSystemProvider())
     val pluginCreationResult = pluginManager.createPlugin(jarPath)
-    assertThat(pluginCreationResult, CoreMatchers.instanceOf(PluginCreationSuccess::class.java))
+    assertThat(pluginCreationResult, instanceOf(PluginCreationSuccess::class.java))
   }
 }
