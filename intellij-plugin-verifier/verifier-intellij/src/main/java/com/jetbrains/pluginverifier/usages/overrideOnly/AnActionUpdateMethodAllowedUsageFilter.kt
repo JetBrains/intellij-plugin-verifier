ee6fbcf442a1962c5db3a842ba8e9f48931c936e
Robert Novotny <robert.novotny@jetbrains.com>
2024-02-29 13:18:41 +0100

    Polish flow

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/overrideOnly/AnActionUpdateMethodAllowedUsageFilter.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/overrideOnly/AnActionUpdateMethodAllowedUsageFilter.kt
@@ -10,6 +10,9 @@ import com.jetbrains.pluginverifier.verifiers.resolution.Method
 import com.jetbrains.pluginverifier.verifiers.resolution.isCallOfSuperMethod
 import org.objectweb.asm.tree.*
 
+
+typealias BinaryClassName = String
+
 class AnActionUpdateMethodAllowedUsageFilter : ApiUsageFilter {
   override fun allowMethodInvocation(methodReference: MethodReference,
                                      resolvedMethod: Method,
@@ -25,10 +28,10 @@ class AnActionUpdateMethodAllowedUsageFilter : ApiUsageFilter {
       && isCallOfSuperMethod(callerMethod, resolvedMethod, instructionNode))
   }
 
+  @Suppress("UNUSED_VARIABLE")
   private fun isAnActionUpdateDelegateCall(resolvedMethod: Method,
                                            instruction: AbstractInsnNode,
-                                           context: VerificationContext): Boolean {
-    val resolver = context.classResolver
+                                           context: VerificationContext): Boolean = with(context.classResolver) {
     val isCallingAnActionUpdateMethod = resolvedMethod.isAnActionStyleUpdateMethod()
     if (!isCallingAnActionUpdateMethod) {
       return false
@@ -37,21 +40,17 @@ class AnActionUpdateMethodAllowedUsageFilter : ApiUsageFilter {
     var ins = instruction
     val callAnActionUpdateMethod = ins.narrow<MethodInsnNode>() ?: return false
     ins = ins.previous
-    @Suppress("UNUSED_VARIABLE")
     val loadAnActionEventUpdateMethodParameter = ins.narrow<VarInsnNode>() ?: return false
     ins = ins.previous
     val getAnActionDelegateField = ins.narrow<FieldInsnNode>() ?: return false
 
-    // field 'delegate' examples:
-    //    - Lcom/intellij/openapi/actionSystem/AnAction;
-    //    - Lcom/example/plugin/DelegateAction;
-    val delegateBinaryClassName = getAnActionDelegateField.desc.extractClassNameFromDescriptor() ?: return false
-    val delegateClassNode = when (val classResolution = resolver.resolveClass(delegateBinaryClassName)) {
+    val delegateBinaryClassName = getAnActionDelegateField.fieldClass ?: return false
+    val delegateClassNode = when (val classResolution = resolveClass(delegateBinaryClassName)) {
       is ResolutionResult.Found<ClassNode> -> classResolution.value
       else -> return false
     }
     // field delegate must be a subclass of `AnAction`
-    val isSubclassOfAnAction = resolver.isSubclassOrSelf(delegateClassNode.name, "com/intellij/openapi/actionSystem/AnAction")
+    val isSubclassOfAnAction = isSubclassOrSelf(delegateClassNode.name, "com/intellij/openapi/actionSystem/AnAction")
     if (!isSubclassOfAnAction) {
       return false
     }
@@ -65,6 +64,9 @@ class AnActionUpdateMethodAllowedUsageFilter : ApiUsageFilter {
   private inline fun <reified T : AbstractInsnNode> AbstractInsnNode.narrow(): T? {
     return if (this is T) this else null
   }
+
+  private val FieldInsnNode.fieldClass: BinaryClassName?
+    get() = desc.extractClassNameFromDescriptor()
 
   private fun Method.isAnActionStyleUpdateMethod(): Boolean {
     return (name to descriptor).isAnActionStyleUpdateMethod()
