4022a4eadd538e5dc02f94dc41e8eb417f556694
Robert Novotny <robert.novotny@jetbrains.com>
2024-03-12 12:48:41 +0100

    Create a generalized allowed method invocations for override only

---
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/overrideOnly/OverrideOnlyMethodAllowedUsageFilter.kt
@@ -0,0 +1,78 @@
+package com.jetbrains.pluginverifier.usages.overrideOnly
+
+import com.jetbrains.plugin.structure.classes.resolvers.ResolutionResult
+import com.jetbrains.pluginverifier.results.reference.MethodReference
+import com.jetbrains.pluginverifier.verifiers.VerificationContext
+import com.jetbrains.pluginverifier.verifiers.extractClassNameFromDescriptor
+import com.jetbrains.pluginverifier.verifiers.filter.ApiUsageFilter
+import com.jetbrains.pluginverifier.verifiers.isSubclassOrSelf
+import com.jetbrains.pluginverifier.verifiers.resolution.Method
+import com.jetbrains.pluginverifier.verifiers.resolution.isCallOfSuperMethod
+import org.objectweb.asm.tree.*
+
+class OverrideOnlyMethodAllowedUsageFilter(private val allowedMethodDescriptor: MethodDescriptor,
+                                           private val superclassName: BinaryClassName) : ApiUsageFilter {
+
+  override fun allowMethodInvocation(methodReference: MethodReference,
+                                     resolvedMethod: Method,
+                                     instructionNode: AbstractInsnNode,
+                                     callerMethod: Method,
+                                     context: VerificationContext): Boolean {
+    return isSuperCall(callerMethod, resolvedMethod, instructionNode)
+      || isDelegateCall(resolvedMethod, instructionNode, context)
+  }
+
+  private fun isSuperCall(callerMethod: Method, resolvedMethod: Method, instructionNode: AbstractInsnNode): Boolean {
+    return (resolvedMethod.matches(allowedMethodDescriptor)
+      && isCallOfSuperMethod(callerMethod, resolvedMethod, instructionNode))
+  }
+
+  @Suppress("UNUSED_VARIABLE")
+  private fun isDelegateCall(resolvedMethod: Method,
+                             instruction: AbstractInsnNode,
+                             context: VerificationContext): Boolean = with(context.classResolver) {
+    val isCallingAllowedMethod = resolvedMethod.matches(allowedMethodDescriptor)
+    if (!isCallingAllowedMethod) {
+      return false
+    }
+
+    var ins = instruction
+    val callMethod = ins.narrow<MethodInsnNode>() ?: return false
+    ins = ins.previous
+    val loadMethodParameter = ins.narrow<VarInsnNode>() ?: return false
+    ins = ins.previous
+    val getDelegateField = ins.narrow<FieldInsnNode>() ?: return false
+
+    val delegateBinaryClassName = getDelegateField.fieldClass ?: return false
+    val delegateClassNode = when (val classResolution = resolveClass(delegateBinaryClassName)) {
+      is ResolutionResult.Found<ClassNode> -> classResolution.value
+      else -> return false
+    }
+    // field delegate must be a subclass of allowed class
+    val isSubclass = isSubclassOrSelf(delegateClassNode.name, superclassName)
+    if (!isSubclass) {
+      return false
+    }
+    if (!callMethod.matches(allowedMethodDescriptor)) {
+      return false
+    }
+    return true
+  }
+
+  private inline fun <reified T : AbstractInsnNode> AbstractInsnNode.narrow(): T? {
+    return if (this is T) this else null
+  }
+
+  private val FieldInsnNode.fieldClass: BinaryClassName?
+    get() = desc.extractClassNameFromDescriptor()
+
+}
+
+data class MethodDescriptor(private val methodName: String, private val descriptor: String)
+
+fun Method.matches(methodDescriptor: MethodDescriptor): Boolean =
+  MethodDescriptor(name, descriptor) == methodDescriptor
+
+fun MethodInsnNode.matches(methodDescriptor: MethodDescriptor): Boolean =
+  MethodDescriptor(name, desc) == methodDescriptor
+
