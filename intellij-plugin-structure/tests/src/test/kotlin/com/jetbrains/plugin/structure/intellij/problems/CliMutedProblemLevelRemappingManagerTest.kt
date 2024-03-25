162a0027fddd51dae633e107bca3e14c596d8011
Robert Novotny <robert.novotny@jetbrains.com>
2024-03-25 17:35:21 +0100

    Introduce problem level resolver for CLI muted problems

---
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/intellij/problems/CliMutedProblemLevelRemappingManagerTest.kt
@@ -0,0 +1,34 @@
+package com.jetbrains.plugin.structure.intellij.problems
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class CliMutedProblemLevelRemappingManagerTest {
+  @Test
+  fun `plugin level remapping is resolved`() {
+    val remappingDefinition = CliMutedProblemLevelRemappingManager(listOf("ForbiddenPluginIdPrefix")).initialize()
+    val cliMutedRemapping = remappingDefinition[CLI_MUTED] ?: emptyLevelRemapping(CLI_MUTED)
+    val ignoredLevel = cliMutedRemapping[ForbiddenPluginIdPrefix::class]
+    assertNotNull(ignoredLevel)
+    assertThat(ignoredLevel, `is`(IgnoredLevel))
+  }
+
+  @Test
+  fun `nonexistent plugin level remapping is not resolved`() {
+    val remappingDefinition = CliMutedProblemLevelRemappingManager().initialize()
+    val cliMutedRemapping = remappingDefinition[CLI_MUTED] ?: emptyLevelRemapping(CLI_MUTED)
+    val nonexistentRemappedLevel = cliMutedRemapping[DuplicatedDependencyWarning::class]
+    assertNull(nonexistentRemappedLevel)
+  }
+
+  @Test
+  fun `unsupported CLI problem ID is not resolved`() {
+    val remappingDefinition = CliMutedProblemLevelRemappingManager(listOf("UnsupportedProblemId")).initialize()
+    val cliMutedRemapping = remappingDefinition[CLI_MUTED] ?: emptyLevelRemapping(CLI_MUTED)
+    val nonexistentRemappedLevel = cliMutedRemapping[DuplicatedDependencyWarning::class]
+    assertNull(nonexistentRemappedLevel)
+  }
+}
