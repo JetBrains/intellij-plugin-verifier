60825f93eb97ee594bc50dbe379d086cc054ad34
Robert Novotny <robert.novotny@jetbrains.com>
2024-03-26 19:54:59 +0100

    Resolve plugin problem class by FQN

--- intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/base/problems/PluginProblems.kt
+++ intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/base/problems/PluginProblems.kt
@@ -1,6 +1,10 @@
 package com.jetbrains.plugin.structure.base.problems
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import kotlin.reflect.KClass
+
+private val LOG: Logger = LoggerFactory.getLogger(PluginProblems::class.java)
 
 fun PluginProblem.isReclassified(): Boolean = this is ReclassifiedPluginProblem
 
@@ -28,3 +32,28 @@ val PluginProblem.isInvalidDescriptorProblem: Boolean
 
 fun PluginProblem.isInstance(pluginProblemClass: KClass<*>): Boolean =
   pluginProblemClass.isInstance(unwrapped)
+
+
+object PluginProblems {
+  /**
+   * Resolves the problem ID to a fully qualified Kotlin class.
+   *
+   * The following formats are supported:
+   *
+   * - Fully qualified problem ID which corresponds to a class name, such as
+   *    `com.jetbrains.plugin.structure.intellij.problems.ForbiddenPluginIdPrefix`
+   * - Problem ID which can be resolved to a fully qualified class name in the `com.jetbrains.plugin.structure`
+   *    package prefix, such as `intellij.problems.ForbiddenPluginIdPrefix`.
+   */
+  // TODO duplicate code
+  fun resolveClass(fullyQualifiedProblemId: String): KClass<out Any>? {
+    return runCatching {
+      val pluginProblemJavaClass = Class.forName(fullyQualifiedProblemId, false, javaClass.getClassLoader())
+      val kotlin = pluginProblemJavaClass.kotlin
+
+      kotlin
+    }.onFailure { t ->
+      LOG.warn("Problem ID '$fullyQualifiedProblemId' could not be resolved to a fully qualified class corresponding to a plugin problem: {}", t.message)
+    }.getOrNull()
+  }
+}
