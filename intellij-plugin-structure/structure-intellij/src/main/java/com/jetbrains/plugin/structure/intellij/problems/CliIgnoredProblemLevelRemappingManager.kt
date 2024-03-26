24d301b61a6ae49b0071fe6b151bf4b2f343adc8
Robert Novotny <robert.novotny@jetbrains.com>
2024-03-26 19:55:20 +0100

    Load mutable plugin problem definitions from JSON

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/problems/CliIgnoredProblemLevelRemappingManager.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/problems/CliIgnoredProblemLevelRemappingManager.kt
@@ -1,9 +1,16 @@
 package com.jetbrains.plugin.structure.intellij.problems
 
+import com.fasterxml.jackson.core.exc.StreamReadException
+import com.fasterxml.jackson.databind.DatabindException
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.jetbrains.plugin.structure.base.problems.PluginProblem
+import com.jetbrains.plugin.structure.base.problems.PluginProblems
 import com.jetbrains.plugin.structure.base.problems.isInstance
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.IOException
+import java.net.URL
 import kotlin.reflect.KClass
 
 const val CLI_IGNORED = "cli-ignored"
@@ -13,11 +20,9 @@ typealias PluginProblemId = String
 private val LOG: Logger = LoggerFactory.getLogger(CliIgnoredProblemLevelRemappingManager::class.java)
 
 class CliIgnoredProblemLevelRemappingManager(ignoredProblems: List<PluginProblemId> = emptyList()) : ProblemLevelRemappingManager, ProblemSolutionHintProvider {
-  private val problemClasses = mutableMapOf<PluginProblemId, KClass<out PluginProblem>>(
-    "ForbiddenPluginIdPrefix" to ForbiddenPluginIdPrefix::class,
-    "TemplateWordInPluginId" to TemplateWordInPluginId::class,
-    "TemplateWordInPluginName" to TemplateWordInPluginName::class
-  )
+  private val ignoredProblemDefinitionLoader = CliIgnoredProblemDefinitionLoader()
+
+  private val problemClasses: List<CliIgnoredPluginProblem> = ignoredProblemDefinitionLoader.load()
 
   private val ignoredProblemLevelRemapping: Map<KClass<*>, IgnoredLevel> = ignoreProblems(ignoredProblems)
 
@@ -39,20 +44,76 @@ class CliIgnoredProblemLevelRemappingManager(ignoredProblems: List<PluginProblem
       }
       problemClass
     }
-    return ignoredProblemClasses.associateWith { IgnoredLevel }
+    return ignoredProblemClasses
+      .mapNotNull { it.pluginProblemClass }
+      .associateWith { IgnoredLevel }
+  }
+
+  private operator fun List<CliIgnoredPluginProblem>.get(pluginProblemId: PluginProblemId): CliIgnoredPluginProblem? {
+    return problemClasses.firstOrNull {
+      it.id == pluginProblemId
+    }
+  }
+
+  private fun findProblemId(problem: PluginProblem): String? {
+    val ignoredProblemDefinition = problemClasses.firstOrNull {
+      if (it.pluginProblemClass != null) {
+        problem.isInstance(it.pluginProblemClass!!)
+      } else {
+        false
+      }
+    }
+    return ignoredProblemDefinition?.id
   }
 
   override fun getProblemSolutionHint(problem: PluginProblem): String? {
-    val supportedProblems = problemClasses.filterValues { problemClass ->
-      problem.isInstance(problemClass)
-    }
-    if (supportedProblems.isEmpty()) return null
-    val problemId = supportedProblems.keys.firstOrNull() ?: return null
+    val problemId = findProblemId(problem)
 
     //FIXME novotnyr handle dates
     val message = "This plugin problem has been reported since <___>. " +
       "If the plugin was previously uploaded to the JetBrains Marketplace, it can be suppressed using the `-mute $problemId` command-line switch."
     return message
+  }
+
+}
+
+data class CliIgnoredPluginProblem(val id: String, val pluginProblemClassFqn: String, val since: String) {
+  val pluginProblemClass: KClass<*>?
+    get() {
+      return PluginProblems.resolveClass(pluginProblemClassFqn)
+    }
+}
+
+private const val CLI_IGNORED_PROBLEMS_FILE_NAME = "plugin-problems-cli-muteable.json"
+
+class CliIgnoredProblemDefinitionLoader(private val jsonUrl: URL) {
+  constructor() : this(
+    CliIgnoredProblemLevelRemappingManager::class.java.getResource(CLI_IGNORED_PROBLEMS_FILE_NAME)
+      ?: throw IOException("Definition for problems that can be ignored in the CLI switch " +
+        "cannot be found at <$CLI_IGNORED_PROBLEMS_FILE_NAME>")
+  )
+
+  private val json = ObjectMapper()
+
+  @Throws(IOException::class)
+  fun load(): List<CliIgnoredPluginProblem> {
+    return try {
+      val rawDefinitions: List<Map<String, String>> = json.readValue(jsonUrl)
+      rawDefinitions.map {
+        CliIgnoredPluginProblem(
+          it["id"] ?: throw IOException("Missing 'id' field in the definition of a CLI ignored problem"),
+          it["class"]
+            ?: throw IOException("Missing 'class' field in the definition of a CLI ignored problem"),
+          it["since"] ?: throw IOException("Missing 'since' field in the definition of a CLI ignored problem")
+        )
+      }
+    } catch (e: IOException) {
+      throw IOException("Cannot load CLI ignorable problems definitions from <$jsonUrl>", e)
+    } catch (e: StreamReadException) {
+      throw IOException("Cannot parse CLI ignorable problems definitions from JSON in <$jsonUrl>", e)
+    } catch (e: DatabindException) {
+      throw IOException("Cannot deserialize CLI ignorable problems definitions from JSON in <$jsonUrl>", e)
+    }
   }
 
 }
