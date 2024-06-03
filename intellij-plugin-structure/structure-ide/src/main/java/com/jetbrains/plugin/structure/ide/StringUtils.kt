1422bd9dc93096ebbc0a6da6ab4c43b6b457c169
Robert Novotny <robert.novotny@jetbrains.com>
2024-06-03 11:07:00 +0200

    Add path utils and string utils

--- intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/StringUtils.kt
+++ intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/StringUtils.kt
@@ -1,6 +1,8 @@
 package com.jetbrains.plugin.structure.ide
 
+import com.jetbrains.plugin.structure.intellij.platform.Layout
 import java.lang.Integer.min
+import java.nio.file.Path
 
 fun getCommonsPrefix(strings: List<String>): String {
   return when (strings.size) {
@@ -22,7 +24,53 @@ fun getCommonsPrefix(strings: List<String>): String {
       return strings[0].substring(0, min(shortestLength, firstDiff))
     }
   }
+}
 
+fun getCommonParentDirectory(paths: List<Path>): Path? {
+  return when (paths.size) {
+    0 -> null
+    1 -> paths.first()
+    else -> {
+      val shortestLength = paths.minOf { it.nameCount }
+      val longestLength = paths.maxOf { it.nameCount }
+      var firstDiff = longestLength
+
+      val firstPath = paths.first()
+      index@ for (index in 0 until shortestLength) {
+        val c = firstPath[index]
+        for (p in paths.drop(1)) {
+          if (p[index] != c) {
+            firstDiff = index
+            break@index
+          }
+        }
+      }
+      firstPath.prefixSubpath(min(shortestLength, firstDiff))
+    }
+  }
+}
+
+private fun Path.prefixSubpath(endIndex: Int): Path? {
+  if (endIndex == 0) return null
+  return this.subpath(0, endIndex)
+}
+
+private operator fun Path.get(index: Int) = getName(index)
+
+fun commonParentDirectory(classpathable: Layout.Classpathable): Path? {
+  val classpath = classpathable.getClasspath()
+  return when (classpath.size) {
+    0 -> null
+    1 -> classpath.first()
+    else -> {
+      val prefix = getCommonsPrefix(classpath.map { it.toString() })
+      if (prefix.isEmpty()) {
+        null
+      } else {
+        Path.of(prefix)
+      }
+    }
+  }
 }
 
 
--- intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/ide/StringUtilsTest.kt
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/ide/StringUtilsTest.kt
@@ -1,7 +1,10 @@
 package com.jetbrains.plugin.structure.ide
 
+import com.jetbrains.plugin.structure.intellij.platform.Layout
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
+import java.nio.file.Path
 
 class StringUtilsTest {
   @Test
@@ -45,7 +48,7 @@ class StringUtilsTest {
   }
 
   @Test
-  fun `test various corner cases`() {
+  fun `various corner cases`() {
     assertEquals("", getCommonsPrefix(listOf("", "")))
     assertEquals("", getCommonsPrefix(listOf("", "abc")))
     assertEquals("", getCommonsPrefix(listOf("abc", "")))
@@ -57,5 +60,94 @@ class StringUtilsTest {
     assertEquals("lorem ipsum ", getCommonsPrefix(listOf("lorem ipsum dolor", "lorem ipsum velit")))
   }
 
+  @Test
+  fun `common prefix as path`() {
+    val classpath = classpath(
+      "plugins/DatabaseTools/lib/database-plugin.jar",
+      "plugins/DatabaseTools/lib/jdbc-console.jar",
+      "plugins/DatabaseTools/lib/database-openapi.jar",
+      "plugins/DatabaseTools/lib/aether-dependency-resolver.jar",
+      "plugins/DatabaseTools/lib/maven-resolver-transport-file.jar",
+      "plugins/DatabaseTools/lib/maven-resolver-transport-http.jar",
+      "plugins/DatabaseTools/lib/maven-resolver-connector-basic.jar"
+    )
+    assertEquals(Path.of("plugins/DatabaseTools/lib"), commonParentDirectory(classpath))
+  }
 
+  @Test
+  fun `common prefix for single-item classpath`() {
+    val classpath = classpath(
+      "plugins/DatabaseTools/lib/database-plugin.jar",
+    )
+    assertEquals(Path.of("plugins/DatabaseTools/lib/database-plugin.jar"), commonParentDirectory(classpath))
+  }
+
+  @Test
+  fun `common prefix for empty classpath`() {
+    val classpath = classpath()
+    assertNull(commonParentDirectory(classpath))
+  }
+
+  @Test
+  fun `common parent directory for four items is found`() {
+    val paths = paths(
+      "plugins/javaee-app-servers-impl/lib/javaee-app-servers-impl.jar",
+      "plugins/javaee-app-servers-impl/lib/AppServersView.jar",
+      "plugins/javaee-app-servers-impl/lib/javaee-appServers-rt.jar",
+      "plugins/javaee-app-servers-impl/lib/javaee-appServers-cloud-rt.jar"
+    )
+
+    val commonPrefix = getCommonParentDirectory(paths)
+    assertEqualPaths("plugins/javaee-app-servers-impl/lib", commonPrefix)
+  }
+
+  @Test
+  fun `common parent directory for empty list`() {
+    val commonPrefix = getCommonParentDirectory(emptyList())
+    assertNull(commonPrefix)
+  }
+
+  @Test
+  fun `common parent directory for single item`() {
+    val paths = paths("plugins/javaee-extensions/lib/javaee-extensions.jar")
+
+    val commonPrefix = getCommonParentDirectory(paths)
+    assertEqualPaths("plugins/javaee-extensions/lib/javaee-extensions.jar", commonPrefix)
+  }
+
+  @Test
+  fun `common parent directory for two equal strings`() {
+    val paths = paths(
+      "plugins/javaee-extensions/lib/javaee-extensions.jar",
+      "plugins/javaee-extensions/lib/javaee-extensions.jar",
+    )
+
+    val commonPrefix = getCommonParentDirectory(paths)
+    assertEqualPaths("plugins/javaee-extensions/lib/javaee-extensions.jar", commonPrefix)
+  }
+
+  @Test
+  fun `various corner cases for common parent directory`() {
+    assertEqualPaths("a", getCommonParentDirectory(paths("a/b/c", "a")))
+    assertEqualPaths("a/b", getCommonParentDirectory(paths("a/b", "a/b/x/y/z")))
+    assertEqualPaths("a/b", getCommonParentDirectory(paths("a/b/c/d/e", "a/b/x/y/z")))
+    assertNull("", getCommonParentDirectory(paths("a/b/c/d/e", "x/y/z")))
+    assertNull("", getCommonParentDirectory(paths("x/y/z", "a/b/c/d/e")))
+    assertEqualPaths("lorem/ipsum/", getCommonParentDirectory(paths("lorem/ipsum/dolor", "lorem/ipsum/velit")))
+  }
+
+
+  private fun assertEqualPaths(expected: String, path: Path?) {
+    assertEquals(Path.of(expected), path)
+  }
+
+  private fun paths(vararg paths: String) = paths.map { Path.of(it) }
+
+  private fun classpath(vararg paths: String): Layout.Classpathable {
+    return object : Layout.Classpathable {
+      override fun getClasspath(): List<Path> = paths.map {
+        Path.of(it)
+      }
+    }
+  }
 }
