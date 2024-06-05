afed13958979fbf5f541b942e0111f84bdc1a6d6
Robert Novotny <robert.novotny@jetbrains.com>
2024-06-05 12:39:48 +0200

    Add detail for resolved resource
    Supports tracking JAR file names for nested filesystems

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/JarFilesResourceResolver.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/JarFilesResourceResolver.kt
@@ -1,9 +1,10 @@
 /*
- * Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2024 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.jetbrains.plugin.structure.intellij.plugin
 
+import com.jetbrains.plugin.structure.base.utils.description
 import com.jetbrains.plugin.structure.base.utils.exists
 import com.jetbrains.plugin.structure.base.utils.inputStream
 import com.jetbrains.plugin.structure.base.utils.withZipFsSeparator
@@ -13,6 +14,7 @@ import java.nio.file.FileSystems
 import java.nio.file.Path
 
 class JarFilesResourceResolver(private val jarFiles: List<Path>) : ResourceResolver {
+
   override fun resolveResource(relativePath: String, basePath: Path): ResourceResolver.Result {
     val resourceResult = DefaultResourceResolver.resolveResource(relativePath, basePath)
     if (resourceResult !is ResourceResolver.Result.NotFound) {
@@ -24,7 +26,7 @@ class JarFilesResourceResolver(private val jarFiles: List<Path>) : ResourceResol
       val foundResult = try {
         val path = jarFs.getPath(finalPath.withZipFsSeparator())
         if (path.exists()) {
-          ResourceResolver.Result.Found(path, path.inputStream(), resourceToClose = jarFs)
+          ResourceResolver.Result.Found(path, path.inputStream(), resourceToClose = jarFs, description = jarFile.description)
         } else {
           null
         }
