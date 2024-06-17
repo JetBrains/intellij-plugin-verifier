20d57bc7eb01c3e7d2f5ee881be9f949ebdb4dd4
Robert Novotny <robert.novotny@jetbrains.com>
2024-06-17 10:35:46 +0200

    Remove unnecessary methods

--- intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/StringUtils.kt
+++ intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/StringUtils.kt
@@ -1,30 +1,7 @@
 package com.jetbrains.plugin.structure.ide
 
-import com.jetbrains.plugin.structure.intellij.platform.LayoutComponent
 import java.lang.Integer.min
 import java.nio.file.Path
-
-fun getCommonsPrefix(strings: List<String>): String {
-  return when (strings.size) {
-    0 -> ""
-    1 -> strings.first()
-    else -> {
-      val shortestLength = strings.minOf { it.length }
-      val longestLength = strings.maxOf { it.length }
-      var firstDiff = longestLength
-      string@ for (index in 0 until shortestLength) {
-        val c = strings[0][index]
-        for (s in strings.drop(1)) {
-          if (s[index] != c) {
-            firstDiff = index
-            break@string
-          }
-        }
-      }
-      return strings[0].substring(0, min(shortestLength, firstDiff))
-    }
-  }
-}
 
 fun getCommonParentDirectory(paths: List<Path>): Path? {
   return when (paths.size) {
@@ -56,21 +33,5 @@ private fun Path.prefixSubpath(endIndex: Int): Path? {
 }
 
 private operator fun Path.get(index: Int) = getName(index)
-
-fun commonParentDirectory(classpathable: LayoutComponent.Classpathable): Path? {
-  val classpath = classpathable.getClasspath()
-  return when (classpath.size) {
-    0 -> null
-    1 -> classpath.first()
-    else -> {
-      val prefix = getCommonsPrefix(classpath.map { it.toString() })
-      if (prefix.isEmpty()) {
-        null
-      } else {
-        Path.of(prefix)
-      }
-    }
-  }
-}
 
 
--- intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/ide/StringUtilsTest.kt
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/ide/StringUtilsTest.kt
@@ -1,92 +1,11 @@
 package com.jetbrains.plugin.structure.ide
 
-import com.jetbrains.plugin.structure.intellij.platform.LayoutComponent
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
 import java.nio.file.Path
 
 class StringUtilsTest {
-  @Test
-  fun `common prefix for four items is found`() {
-    val strings = listOf(
-      "plugins/javaee-app-servers-impl/lib/javaee-app-servers-impl.jar",
-      "plugins/javaee-app-servers-impl/lib/AppServersView.jar",
-      "plugins/javaee-app-servers-impl/lib/javaee-appServers-rt.jar",
-      "plugins/javaee-app-servers-impl/lib/javaee-appServers-cloud-rt.jar"
-    )
-
-    val commonPrefix = getCommonsPrefix(strings)
-    assertEquals("plugins/javaee-app-servers-impl/lib/", commonPrefix)
-  }
-
-  @Test
-  fun `common prefix for empty list`() {
-    val strings = emptyList<String>()
-
-    val commonPrefix = getCommonsPrefix(strings)
-    assertEquals("", commonPrefix)
-  }
-
-  @Test
-  fun `common prefix for single item`() {
-    val strings = listOf("plugins/javaee-extensions/lib/javaee-extensions.jar")
-
-    val commonPrefix = getCommonsPrefix(strings)
-    assertEquals("plugins/javaee-extensions/lib/javaee-extensions.jar", commonPrefix)
-  }
-
-  @Test
-  fun `common prefix for two equal strings`() {
-    val strings = listOf(
-      "plugins/javaee-extensions/lib/javaee-extensions.jar",
-      "plugins/javaee-extensions/lib/javaee-extensions.jar",
-    )
-
-    val commonPrefix = getCommonsPrefix(strings)
-    assertEquals("plugins/javaee-extensions/lib/javaee-extensions.jar", commonPrefix)
-  }
-
-  @Test
-  fun `various corner cases`() {
-    assertEquals("", getCommonsPrefix(listOf("", "")))
-    assertEquals("", getCommonsPrefix(listOf("", "abc")))
-    assertEquals("", getCommonsPrefix(listOf("abc", "")))
-    assertEquals("a", getCommonsPrefix(listOf("abc", "a")))
-    assertEquals("ab", getCommonsPrefix(listOf("ab", "abxyz")))
-    assertEquals("ab", getCommonsPrefix(listOf("abcde", "abxyz")))
-    assertEquals("", getCommonsPrefix(listOf("abcde", "xyz")))
-    assertEquals("", getCommonsPrefix(listOf("xyz", "abcde")))
-    assertEquals("lorem ipsum ", getCommonsPrefix(listOf("lorem ipsum dolor", "lorem ipsum velit")))
-  }
-
-  @Test
-  fun `common prefix as path`() {
-    val classpath = classpath(
-      "plugins/DatabaseTools/lib/database-plugin.jar",
-      "plugins/DatabaseTools/lib/jdbc-console.jar",
-      "plugins/DatabaseTools/lib/database-openapi.jar",
-      "plugins/DatabaseTools/lib/aether-dependency-resolver.jar",
-      "plugins/DatabaseTools/lib/maven-resolver-transport-file.jar",
-      "plugins/DatabaseTools/lib/maven-resolver-transport-http.jar",
-      "plugins/DatabaseTools/lib/maven-resolver-connector-basic.jar"
-    )
-    assertEquals(Path.of("plugins/DatabaseTools/lib"), commonParentDirectory(classpath))
-  }
-
-  @Test
-  fun `common prefix for single-item classpath`() {
-    val classpath = classpath(
-      "plugins/DatabaseTools/lib/database-plugin.jar",
-    )
-    assertEquals(Path.of("plugins/DatabaseTools/lib/database-plugin.jar"), commonParentDirectory(classpath))
-  }
-
-  @Test
-  fun `common prefix for empty classpath`() {
-    val classpath = classpath()
-    assertNull(commonParentDirectory(classpath))
-  }
 
   @Test
   fun `common parent directory for four items is found`() {
@@ -154,11 +73,4 @@ class StringUtilsTest {
 
   private fun paths(vararg paths: String) = paths.map { Path.of(it) }
 
-  private fun classpath(vararg paths: String): LayoutComponent.Classpathable {
-    return object : LayoutComponent.Classpathable {
-      override fun getClasspath(): List<Path> = paths.map {
-        Path.of(it)
-      }
-    }
-  }
 }
