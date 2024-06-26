045ea4edf46d7ac0f755add57b19592704675906
Robert Novotny <robert.novotny@jetbrains.com>
2024-06-26 13:05:35 +0200

    Extract discovery to methods

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/annotation/AnnotationResolver.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/annotation/AnnotationResolver.kt
@@ -2,6 +2,7 @@ package com.jetbrains.pluginverifier.usages.annotation
 
 import com.jetbrains.plugin.structure.classes.resolvers.Resolver
 import com.jetbrains.pluginverifier.usages.util.MemberAnnotation
+import com.jetbrains.pluginverifier.usages.util.MemberAnnotation.*
 import com.jetbrains.pluginverifier.verifiers.findAnnotation
 import com.jetbrains.pluginverifier.verifiers.resolution.ClassFile
 import com.jetbrains.pluginverifier.verifiers.resolution.ClassFileMember
@@ -10,41 +11,44 @@ import com.jetbrains.pluginverifier.verifiers.resolution.resolveClassOrNull
 
 class AnnotationResolver(val annotation: FullyQualifiedClassName) {
   fun resolve(classFileMember: ClassFileMember, classResolver: Resolver): MemberAnnotation? {
-    if (classFileMember.isDirectlyAnnotatedWith(annotation)) {
-      return MemberAnnotation.AnnotatedDirectly(classFileMember, annotation)
+    resolveDirectAnnotation(classFileMember, classResolver)?.let { return it }
+    return when (classFileMember) {
+      is ClassFile -> {
+        if (classFileMember.name.endsWith("package-info")) return null
+        resolveInEnclosingClassName(classFileMember, classResolver)?.let { return it }
+        resolveInPackageInfo(classFileMember, classResolver)?.let { return it }
+        null
+      }
+      else -> resolveInNonClassFile(classFileMember, classResolver)
+    }
+  }
+
+  private fun resolveDirectAnnotation(classFileMember: ClassFileMember, classResolver: Resolver): MemberAnnotation? =
+    AnnotatedDirectly(classFileMember, annotation).takeIf { classFileMember.isDirectlyAnnotatedWith(annotation) }
+
+  private fun resolveInNonClassFile(classFileMember: ClassFileMember, classResolver: Resolver): MemberAnnotation? =
+    resolve(classFileMember.containingClassFile, classResolver)?.let {
+      AnnotatedViaContainingClass(classFileMember.containingClassFile, classFileMember, annotation)
     }
 
-    if (classFileMember !is ClassFile) {
-      val memberAnnotation = resolve(classFileMember.containingClassFile, classResolver)
-      return memberAnnotation?.let { MemberAnnotation.AnnotatedViaContainingClass(classFileMember.containingClassFile, classFileMember, annotation) }
-    }
-
-    if (classFileMember.name.endsWith("package-info")) {
-      return null
-    }
-
+  private fun resolveInEnclosingClassName(classFileMember: ClassFile, classResolver: Resolver): MemberAnnotation? {
     val enclosingClassName = classFileMember.enclosingClassName
     // If enclosing class name is the same as the current class name the endless loop happens
     // (since the same class will be resolved and findEffectiveMemberAnnotation call leads here)
-    if (enclosingClassName != null && enclosingClassName != classFileMember.name) {
-      val enclosingClass = classResolver.resolveClassOrNull(enclosingClassName) ?: return null
-      val memberAnnotation = resolve(enclosingClass, classResolver)
-      return memberAnnotation?.let { MemberAnnotation.AnnotatedViaContainingClass(enclosingClass, classFileMember, annotation) }
+    if (enclosingClassName == null || enclosingClassName == classFileMember.name) return null
+    val enclosingClass = classResolver.resolveClassOrNull(enclosingClassName) ?: return null
+    return resolve(enclosingClass, classResolver)?.let {
+      AnnotatedViaContainingClass(enclosingClass, classFileMember, annotation)
     }
+  }
 
+  private fun resolveInPackageInfo(classFileMember: ClassFile, classResolver: Resolver): MemberAnnotation? {
     val packageName = classFileMember.containingClassFile.packageName
-    if (packageName.isNotEmpty()) {
-      val packageInfoClass = classResolver.resolveClassOrNull("$packageName/package-info")
-      if (packageInfoClass != null) {
-        return if (packageInfoClass.isDirectlyAnnotatedWith(annotation)) {
-          MemberAnnotation.AnnotatedViaPackage(packageName, classFileMember, annotation)
-        } else {
-          null
-        }
-      }
-    }
-
-    return null
+    return packageName
+      .takeIf { it.isNotEmpty() }
+      ?.let { classResolver.resolveClassOrNull("$packageName/package-info") }
+      ?.takeIf { it.isDirectlyAnnotatedWith(annotation) }
+      ?.let { AnnotatedViaPackage(packageName, classFileMember, annotation) }
   }
 
   private fun ClassFileMember.isDirectlyAnnotatedWith(annotationName: String): Boolean =
