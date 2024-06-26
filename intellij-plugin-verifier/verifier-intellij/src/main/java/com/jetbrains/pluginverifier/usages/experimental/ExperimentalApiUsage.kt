123572eba7928402297adc3abdee00d031833030
Robert Novotny <robert.novotny@jetbrains.com>
2024-06-26 17:08:16 +0200

    Track nested resolutions locations in a usage chain via call stack

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/experimental/ExperimentalApiUsage.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/usages/experimental/ExperimentalApiUsage.kt
@@ -1,10 +1,11 @@
 /*
- * Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2024 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.jetbrains.pluginverifier.usages.experimental
 
 import com.jetbrains.plugin.structure.classes.resolvers.Resolver
+import com.jetbrains.pluginverifier.results.location.Location
 import com.jetbrains.pluginverifier.usages.ApiUsage
 import com.jetbrains.pluginverifier.usages.annotation.AnnotationResolver
 import com.jetbrains.pluginverifier.usages.annotation.isMemberEffectivelyAnnotatedWith
@@ -15,7 +16,7 @@ import com.jetbrains.pluginverifier.verifiers.resolution.ClassFileMember
  */
 abstract class ExperimentalApiUsage : ApiUsage()
 
-fun ClassFileMember.isExperimentalApi(classResolver: Resolver): Boolean =
-  isMemberEffectivelyAnnotatedWith(experimentalApiStatusResolver, classResolver)
+fun ClassFileMember.isExperimentalApi(classResolver: Resolver, usageLocation: Location): Boolean =
+  isMemberEffectivelyAnnotatedWith(experimentalApiStatusResolver, classResolver, location)
 
 private val experimentalApiStatusResolver = AnnotationResolver("org/jetbrains/annotations/ApiStatus\$Experimental")
