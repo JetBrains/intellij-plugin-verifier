77cf39bcb0da42652a2f7570ca5dabaabf32c8a3
Robert Novotny <robert.novotny@jetbrains.com>
2024-06-28 16:39:16 +0200

    Extract API usage filtering to a separate class

--- intellij-plugin-verifier/verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/filter/ApiUsageFilter.kt
+++ intellij-plugin-verifier/verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/filter/ApiUsageFilter.kt
@@ -1,6 +1,10 @@
 package com.jetbrains.pluginverifier.verifiers.filter
 
+import com.jetbrains.pluginverifier.results.reference.ClassReference
 import com.jetbrains.pluginverifier.verifiers.VerificationContext
+import com.jetbrains.pluginverifier.verifiers.resolution.ClassFile
+import com.jetbrains.pluginverifier.verifiers.resolution.ClassFileMember
+import com.jetbrains.pluginverifier.verifiers.resolution.ClassUsageType
 import com.jetbrains.pluginverifier.verifiers.resolution.Method
 import org.objectweb.asm.tree.AbstractInsnNode
 
@@ -9,4 +13,13 @@ interface ApiUsageFilter {
                             invocationInstruction: AbstractInsnNode,
                             callerMethod: Method,
                             context: VerificationContext): Boolean = true
+
+  fun allowUsage(
+    classReference: ClassReference,
+    invocationTarget: ClassFile,
+    caller: ClassFileMember,
+    usageType: ClassUsageType,
+    context: VerificationContext
+  ): Boolean = true
+
 }
