a7e21da15d4ffdc0d5b898d81a03851e724b3059
Pavel Krizskii <pavel.krizsky@gmail.com>
2024-07-05 16:17:11 +0200

    [TC Actions] Parse and validate YAML for JB Marketplace (#1091)
    * [TC Actions] Parse and validate YAML for JB Marketplace
    
    * Fix README.md
    
    * Small improvements
    
    * Code review fixes:
     * Remove ActionStep, ActionInput, and ActionRequirement from TeamCityActionPlugin, because these fields are not needed for Marketplace. We still have these fields in the spec and validate them
     * Fix actionId, temporary make it equal to the actionName, removing the action version from it. This must be fixed in the future again, because different vendors can upload actions with the same names
     * Minor renamings, tests actualization
    
    * Fix error message shown to the user when an unexpected error occurred while parsing the action descriptor. Don't show the internal exception message since it's not useful for the user

---
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/teamcity/action/ParseValidFullActionTest.kt
@@ -0,0 +1,122 @@
+package com.jetbrains.plugin.structure.teamcity.action
+
+import com.jetbrains.plugin.structure.base.utils.contentBuilder.buildZipFile
+import com.jetbrains.plugin.structure.mocks.BasePluginManagerTest
+import com.jetbrains.plugin.structure.rules.FileSystemType
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.nio.file.Path
+
+class ParseValidFullActionTest(
+  fileSystemType: FileSystemType,
+) : BasePluginManagerTest<TeamCityActionPlugin, TeamCityActionPluginManager>(fileSystemType) {
+
+  override fun createManager(extractDirectory: Path): TeamCityActionPluginManager =
+    TeamCityActionPluginManager.createManager(extractDirectory)
+
+  private val actionYaml =
+    """
+    ---
+    spec-version: 1.0.0
+    name: simple-action
+    version: 1.2.3
+    description: this is a simple action
+    inputs:
+      - some text input:
+          type: text
+          label: text input
+          default: a default text value
+          required: false
+          description: a description for the text input
+      - some select input:
+          type: select
+          label: select input
+          options:
+            - first select option
+            - second select option
+          default: first select option
+          required: true
+          description: description for select input
+      - some boolean input:
+          type: boolean
+          default: false
+    requirements:
+      - requirement 0:
+          type: exists
+      - requirement 1:
+          type: not-exists
+      - requirement 2:
+          type: equals
+          value: some-value
+      - requirement 3:
+          type: not-equals
+          value: some-value
+      - requirement 4:
+          type: more-than
+          value: 1
+      - requirement 5:
+          type: not-more-than
+          value: 1
+      - requirement 6:
+          type: less-than
+          value: 1
+      - requirement 7:
+          type: not-less-than
+          value: 1
+      - requirement 8:
+          type: starts-with
+          value: some-value
+      - requirement 9:
+          type: contains
+          value: some-value
+      - requirement 10:
+          type: does-not-match
+          value: some-value
+      - requirement 11:
+          type: version-more-than
+          value: some-value
+      - requirement 12:
+          type: version-not-more-than
+          value: some-value
+      - requirement 13:
+          type: version-less-than
+          value: some-value
+      - requirement 14:
+          type: version-not-less-than
+          value: some-value
+    steps:
+      - name: step 1
+        with: runner/maven
+        params:
+          pomLocation: pom.xml
+          goals: build
+          one more param: one more value
+      - script: echo "step 2 output"
+        name: step 2
+      - name: step 3
+        with: action/name@1.2.3
+        params:
+          text-input: passed text parameter value
+          select-input: first select option
+          boolean-input: true
+    """.trimIndent()
+
+  @Test
+  fun `parse full valid TeamCity Action from YAML`() {
+    // arrange
+    val pluginFile = buildZipFile(temporaryFolder.newFile("plugin.zip")) {
+      file("action.yaml") { actionYaml }
+    }
+
+    // act
+    val result = createPluginSuccessfully(pluginFile)
+
+    // assert
+    with(result.plugin) {
+      assertEquals("1.0.0", this.specVersion.toString())
+      assertEquals("simple-action", this.pluginName)
+      assertEquals("1.2.3", this.pluginVersion)
+      assertEquals("this is a simple action", this.description)
+    }
+  }
+}
