eb09d4715e444e46e6fbcc320e4573172f12fee8
Robert Novotny <robert.novotny@jetbrains.com>
2024-07-08 10:35:08 +0200

    Add header

--- intellij-plugin-verifier/verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/filter/SameModuleUsageFilter.kt
+++ intellij-plugin-verifier/verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/filter/SameModuleUsageFilter.kt
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2000-2024 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
 package com.jetbrains.pluginverifier.verifiers.filter
 
 import com.jetbrains.pluginverifier.verifiers.VerificationContext
