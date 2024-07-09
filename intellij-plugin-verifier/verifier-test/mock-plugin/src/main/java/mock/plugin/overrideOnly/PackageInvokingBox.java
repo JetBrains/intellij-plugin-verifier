53f0bc8ca0c23dd556c006000a70f5799296f88f
Robert Novotny <robert.novotny@jetbrains.com>
2024-07-09 17:19:07 +0200

    Fix deprecations on the mock classes

--- intellij-plugin-verifier/verifier-test/mock-plugin/src/main/java/mock/plugin/overrideOnly/PackageInvokingBox.java
+++ intellij-plugin-verifier/verifier-test/mock-plugin/src/main/java/mock/plugin/overrideOnly/PackageInvokingBox.java
@@ -1,6 +1,11 @@
 package mock.plugin.overrideOnly;
 
 public class PackageInvokingBox {
+    /*expected(DEPRECATED)
+    Deprecated method java.lang.Package.getPackage(String) invocation
+
+    Deprecated method java.lang.Package.getPackage(java.lang.String name) : java.lang.Package is invoked in mock.plugin.overrideOnly.PackageInvokingBox.getPackage(String) : Package
+    */
     @SuppressWarnings("deprecation")
     public Package getPackage(String pkg) {
         return Package.getPackage(pkg);
