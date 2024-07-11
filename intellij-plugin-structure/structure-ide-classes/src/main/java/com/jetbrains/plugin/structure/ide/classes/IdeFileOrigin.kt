c24bb98a59145cd71b57dd08cba67c51f95af9e5
Robert Novotny <robert.novotny@jetbrains.com>
2024-07-11 10:54:36 +0200

    Reorganize IDE File origin classes.
    Some origins might be aware about the IDE. Introduce a separate subclass to handle such origins.
    
    Move `IDE` variable to subclass

--- intellij-plugin-structure/structure-ide-classes/src/main/java/com/jetbrains/plugin/structure/ide/classes/IdeFileOrigin.kt
+++ intellij-plugin-structure/structure-ide-classes/src/main/java/com/jetbrains/plugin/structure/ide/classes/IdeFileOrigin.kt
@@ -10,10 +10,12 @@ import com.jetbrains.plugin.structure.ide.Ide
 sealed class IdeFileOrigin : FileOrigin {
   override val parent: FileOrigin? = null
 
-  abstract val ide: Ide
+  data class IdeLibDirectory(override val ide: Ide) : IdeAwareFileOrigin(ide)
+  data class RepositoryLibrary(override val ide: Ide) : IdeAwareFileOrigin(ide)
+  data class SourceLibDirectory(override val ide: Ide) : IdeAwareFileOrigin(ide)
+  data class CompiledModule(override val ide: Ide, val moduleName: String) : IdeAwareFileOrigin(ide)
 
-  data class IdeLibDirectory(override val ide: Ide) : IdeFileOrigin()
-  data class RepositoryLibrary(override val ide: Ide) : IdeFileOrigin()
-  data class SourceLibDirectory(override val ide: Ide) : IdeFileOrigin()
-  data class CompiledModule(override val ide: Ide, val moduleName: String) : IdeFileOrigin()
+  class BundledPlugin() : IdeFileOrigin()
+
+  abstract class IdeAwareFileOrigin(open val ide: Ide): IdeFileOrigin()
 }
