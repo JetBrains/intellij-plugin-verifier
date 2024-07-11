2dc223be951731949b68e01152b846819eb6cb57
Robert Novotny <robert.novotny@jetbrains.com>
2024-07-11 09:39:11 +0200

    Introduce file origin provider
    Allows to override file origin resolved by JARs and 'lib' directories

--- intellij-plugin-structure/structure-intellij-classes/src/main/java/com/jetbrains/plugin/structure/intellij/classes/locator/LibDirectoryLocator.kt
+++ intellij-plugin-structure/structure-intellij-classes/src/main/java/com/jetbrains/plugin/structure/intellij/classes/locator/LibDirectoryLocator.kt
@@ -4,21 +4,28 @@
 
 package com.jetbrains.plugin.structure.intellij.classes.locator
 
-import com.jetbrains.plugin.structure.base.utils.*
+import com.jetbrains.plugin.structure.base.utils.closeOnException
+import com.jetbrains.plugin.structure.base.utils.isDirectory
+import com.jetbrains.plugin.structure.base.utils.isJar
+import com.jetbrains.plugin.structure.base.utils.isZip
+import com.jetbrains.plugin.structure.base.utils.listFiles
 import com.jetbrains.plugin.structure.classes.resolvers.Resolver
 import com.jetbrains.plugin.structure.classes.resolvers.buildDirectoriesResolvers
 import com.jetbrains.plugin.structure.classes.resolvers.buildJarOrZipFileResolvers
 import com.jetbrains.plugin.structure.intellij.plugin.IdePlugin
 import java.nio.file.Path
 
-class LibDirectoryLocator(private val readMode: Resolver.ReadMode) : ClassesLocator {
+class LibDirectoryLocator(
+  private val readMode: Resolver.ReadMode,
+  private val fileOriginProvider: FileOriginProvider = LibDirectoryOriginProvider
+) : ClassesLocator {
   override val locationKey = LibDirectoryKey
 
   override fun findClasses(idePlugin: IdePlugin, pluginFile: Path): List<Resolver> {
     val pluginLib = pluginFile.resolve("lib")
     val resolvers = arrayListOf<Resolver>()
     if (pluginLib.isDirectory) {
-      val libDirectoryOrigin = PluginFileOrigin.LibDirectory(idePlugin)
+      val libDirectoryOrigin = fileOriginProvider.getFileOrigin(idePlugin, pluginFile)
       val jarsOrZips = pluginLib.listFiles().filter { file -> file.isJar() || file.isZip() }
       val directories = pluginLib.listFiles().filter { file -> file.isDirectory }
       resolvers.closeOnException {
@@ -28,11 +35,14 @@ class LibDirectoryLocator(private val readMode: Resolver.ReadMode) : ClassesLoca
     }
     return resolvers
   }
-
 }
 
 object LibDirectoryKey : LocationKey {
   override val name: String = "lib directory"
 
   override fun getLocator(readMode: Resolver.ReadMode) = LibDirectoryLocator(readMode)
+}
+
+object LibDirectoryOriginProvider: FileOriginProvider {
+  override fun getFileOrigin(idePlugin: IdePlugin, pluginFile: Path) = PluginFileOrigin.LibDirectory(idePlugin)
 }
