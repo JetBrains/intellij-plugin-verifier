0482c920ded22b2323646f87810aa4af515b3080
Robert Novotny <robert.novotny@jetbrains.com>
2024-07-15 11:52:14 +0200

    Track bundled plugin metadata with origin

--- intellij-plugin-structure/structure-ide-classes/src/main/java/com/jetbrains/plugin/structure/ide/classes/IdeFileOrigin.kt
+++ intellij-plugin-structure/structure-ide-classes/src/main/java/com/jetbrains/plugin/structure/ide/classes/IdeFileOrigin.kt
@@ -6,6 +6,7 @@ package com.jetbrains.plugin.structure.ide.classes
 
 import com.jetbrains.plugin.structure.classes.resolvers.FileOrigin
 import com.jetbrains.plugin.structure.ide.Ide
+import com.jetbrains.plugin.structure.intellij.plugin.IdePlugin
 import java.nio.file.Path
 
 sealed class IdeFileOrigin : FileOrigin {
@@ -16,7 +17,7 @@ sealed class IdeFileOrigin : FileOrigin {
   data class SourceLibDirectory(override val ide: Ide) : IdeAwareFileOrigin(ide)
   data class CompiledModule(override val ide: Ide, val moduleName: String) : IdeAwareFileOrigin(ide)
 
-  class BundledPlugin(val pluginFile: Path) : IdeFileOrigin()
+  class BundledPlugin(val pluginFile: Path, val idePlugin: IdePlugin) : IdeFileOrigin()
 
   abstract class IdeAwareFileOrigin(open val ide: Ide): IdeFileOrigin()
 }
