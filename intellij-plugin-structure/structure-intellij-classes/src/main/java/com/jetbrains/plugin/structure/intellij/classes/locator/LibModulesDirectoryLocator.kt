d544931e5dbe8e037035f97079f88fcfb9a42c76
Robert Novotny <robert.novotny@jetbrains.com>
2024-08-08 16:23:42 +0200

    Discover plugin classes in `lib/modules` directory (#1132)
    * Discover plugin classes in lib/modules
    * Discover only JAR files in lib/modules directory

---
+++ intellij-plugin-structure/structure-intellij-classes/src/main/java/com/jetbrains/plugin/structure/intellij/classes/locator/LibModulesDirectoryLocator.kt
@@ -0,0 +1,43 @@
+package com.jetbrains.plugin.structure.intellij.classes.locator
+
+import com.jetbrains.plugin.structure.base.utils.isDirectory
+import com.jetbrains.plugin.structure.base.utils.listJars
+import com.jetbrains.plugin.structure.classes.resolvers.Resolver
+import com.jetbrains.plugin.structure.classes.resolvers.buildJarOrZipFileResolvers
+import com.jetbrains.plugin.structure.intellij.plugin.IdePlugin
+import java.nio.file.Path
+
+/**
+ * Locates classes from `lib/modules` directory.
+ *
+ * Such classes correspond to V2 plugin modules with module-level libraries packaged into separate JARs.
+ */
+class LibModulesDirectoryLocator(
+  private val readMode: Resolver.ReadMode,
+  private val fileOriginProvider: FileOriginProvider = LibModulesDirectoryOriginProvider
+) : ClassesLocator {
+
+  override val locationKey = LibModulesDirectoryKey
+
+  override fun findClasses(idePlugin: IdePlugin, pluginFile: Path): List<Resolver> {
+    val modulesDir = pluginFile.resolve("lib").resolve("modules")
+    if (!modulesDir.isDirectory) {
+      return emptyList()
+    }
+
+    val origin = fileOriginProvider.getFileOrigin(idePlugin, pluginFile)
+    val jarsOrZips = modulesDir.listJars()
+
+    return buildJarOrZipFileResolvers(jarsOrZips, readMode, origin)
+  }
+}
+
+object LibModulesDirectoryKey : LocationKey {
+  override val name: String = "lib/modules directory"
+
+  override fun getLocator(readMode: Resolver.ReadMode) = LibModulesDirectoryLocator(readMode)
+}
+
+object LibModulesDirectoryOriginProvider : FileOriginProvider {
+  override fun getFileOrigin(idePlugin: IdePlugin, pluginFile: Path) = PluginFileOrigin.LibDirectory(idePlugin)
+}
---
+++ intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/mocks/PluginDescriptors.kt
@@ -0,0 +1,42 @@
+package com.jetbrains.pluginverifier.tests.mocks
+
+import com.jetbrains.plugin.structure.base.utils.contentBuilder.ContentBuilder
+import com.jetbrains.pluginverifier.verifiers.resolution.FullyQualifiedClassName
+import net.bytebuddy.ByteBuddy
+
+internal fun ideaPlugin(pluginId: String = "someid",
+                       pluginName: String = "someName",
+                       vendor: String = "vendor",
+                       sinceBuild: String = "131.1",
+                       untilBuild: String = "231.1",
+                       description: String = "this description is looooooooooong enough") = """
+    <id>$pluginId</id>
+    <name>$pluginName</name>
+    <version>someVersion</version>
+    ""<vendor email="vendor.com" url="url">$vendor</vendor>""
+    <description>$description</description>
+    <change-notes>these change-notes are looooooooooong enough</change-notes>
+    <idea-version since-build="$sinceBuild" until-build="$untilBuild"/>
+    <depends>com.intellij.modules.platform</depends>
+  """
+
+internal fun ContentBuilder.descriptor(header: String) {
+  dir("META-INF") {
+    file("plugin.xml") {
+      """
+          <idea-plugin>
+            $header
+          </idea-plugin>
+        """
+    }
+  }
+}
+
+internal fun ContentBuilder.classBytes(className: FullyQualifiedClassName, byteBuddy: ByteBuddy) {
+  val dynamicType = byteBuddy
+    .subclass(Object::class.java)
+    .name(className)
+    .make()
+  val simpleName = dynamicType.typeDescription.simpleName
+  file("$simpleName.class", dynamicType.bytes)
+}
