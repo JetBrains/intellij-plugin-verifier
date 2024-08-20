90a6f7908c7d7bf1853eb64f52ee491901b5bbde
Robert Novotny <robert.novotny@jetbrains.com>
2024-08-20 12:49:39 +0200

    Use type-safe product release version in product descriptor

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/ProductDescriptor.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/ProductDescriptor.kt
@@ -1,15 +1,35 @@
 /*
- * Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2024 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.jetbrains.plugin.structure.intellij.plugin
 
+import com.jetbrains.plugin.structure.intellij.version.ProductReleaseVersion
 import java.time.LocalDate
 
 data class ProductDescriptor(
   val code: String,
   val releaseDate: LocalDate,
-  val releaseVersion: Int,
+  val version: ProductReleaseVersion,
   val eap: Boolean,
   val optional: Boolean
-)
+) {
+
+  @Deprecated("Use version", replaceWith = ReplaceWith("ProductDescriptor(code, releaseDate, version, eap, optional)"))
+  constructor(
+    code: String,
+    releaseDate: LocalDate,
+    releaseVersion: Int,
+    eap: Boolean,
+    optional: Boolean
+  ) : this(
+    code,
+    releaseDate,
+    ProductReleaseVersion(releaseVersion),
+    eap,
+    optional
+  )
+
+  @Deprecated("Use version", replaceWith = ReplaceWith("version"))
+  val releaseVersion: Int = version.value
+}
