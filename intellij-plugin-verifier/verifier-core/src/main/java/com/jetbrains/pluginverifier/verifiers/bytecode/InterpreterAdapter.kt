53e15b52b90c298c46c13a1ecbf529ae56a3d66b
Robert Novotny <robert.novotny@jetbrains.com>
2024-08-23 16:36:56 +0200

    Refactor, cleanup and add KDoc

--- intellij-plugin-verifier/verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/bytecode/InterpreterAdapter.kt
+++ intellij-plugin-verifier/verifier-core/src/main/java/com/jetbrains/pluginverifier/verifiers/bytecode/InterpreterAdapter.kt
@@ -10,27 +10,38 @@ import org.objectweb.asm.tree.analysis.BasicInterpreter
 import org.objectweb.asm.tree.analysis.BasicValue
 import org.objectweb.asm.tree.analysis.Interpreter
 
+
+typealias Opcode = Int
+/**
+ * ASM Interpreter that offloads specific instruction to typesafe [handler](InterpreterListener).
+ */
 class InterpreterAdapter(private val interpreterListener: InterpreterListener = DefaultInterpreterListener) :
   Interpreter<BasicValue>(ASM9) {
   private val interpreter = BasicInterpreter()
 
-  override fun newValue(type: Type?): BasicValue? {
-    return interpreter.newValue(type)
-  }
+  private val singleIntOpcodes: List<Opcode> = (ICONST_M1..ICONST_5) + listOf(BIPUSH, SIPUSH)
 
+  /**
+   * Type-safe handling of:
+   *
+   * - integer handling instructions: `LDC`, `ICONST_x`, `BIPUSH` and `SIPUSH
+   * - `LDC` instruction with typesafe handling of operand
+   */
   override fun newOperation(insn: AbstractInsnNode?): BasicValue? {
     if (insn == null) throw AssertionError("Instruction cannot be null")
-    if (insn.opcode == LDC) {
-      return interpreterListener.onLdc(insn as LdcInsnNode, insn.cst, interpreter)
+    return when(insn.opcode) {
+      LDC -> interpreterListener.onLdc(insn as LdcInsnNode, insn.cst, interpreter)
+      in singleIntOpcodes -> insn.operand()?.let {
+        interpreterListener.onPushInt(insn, it, interpreter) }
+      else -> interpreter.newOperation(insn)
     }
-    return when (insn.opcode) {
-      in ICONST_M1..ICONST_5 -> insn.opcode - 3
-      BIPUSH, SIPUSH -> (insn as IntInsnNode).operand
-      else -> null
-    }?.let { interpreterListener.onPushInt(insn, it, interpreter) }
-      ?: interpreter.newOperation(insn)
   }
 
+  /**
+   * Type-safe handling of:
+   *
+   * - `INVOKESPECIAL` with type-safe passing of stack values
+   */
   override fun naryOperation(insn: AbstractInsnNode, values: MutableList<out BasicValue>?): BasicValue? {
     return if (insn.opcode == INVOKESPECIAL) {
       interpreterListener.onInvokeSpecial(insn as MethodInsnNode, values, interpreter)
@@ -39,6 +50,22 @@ class InterpreterAdapter(private val interpreterListener: InterpreterListener = 
     }
   }
 
+  /**
+   * Extract a single operand as an integer.
+   *
+   * @return the integer operand or `null` when the instruction does not provide a single integer-like operand.
+   */
+  private fun AbstractInsnNode.operand(): Int? = when (opcode) {
+    in ICONST_M1..ICONST_5 -> opcode - 3
+    BIPUSH, SIPUSH -> (this as IntInsnNode).operand
+    else -> null
+  }
+
+  // --------------------------------------------------------------------------------------------
+
+  override fun newValue(type: Type?): BasicValue? {
+    return interpreter.newValue(type)
+  }
   override fun copyOperation(insn: AbstractInsnNode?, value: BasicValue?): BasicValue? =
     interpreter.copyOperation(insn, value)
 
