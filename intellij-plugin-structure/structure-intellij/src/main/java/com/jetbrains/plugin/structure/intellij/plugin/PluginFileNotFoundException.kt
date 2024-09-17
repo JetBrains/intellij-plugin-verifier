7f05d806744d0af5d7beaf6afac545aa1ab53e1f
Robert Novotny <robert.novotny@jetbrains.com>
2024-09-17 10:50:08 +0200

    Improve copywriting

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/PluginFileNotFoundException.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/PluginFileNotFoundException.kt
@@ -2,4 +2,4 @@ package com.jetbrains.plugin.structure.intellij.plugin
 
 import java.nio.file.Path
 
-class PluginFileNotFoundException(val file: Path) : IllegalArgumentException("Plugin file $file is not found or does not exist")
+class PluginFileNotFoundException(val file: Path) : IllegalArgumentException("Plugin file $file not found or does not exist")
