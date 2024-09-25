95191cb4da64645753d3202dc111fa4192739a19
Robert Novotny <robert.novotny@jetbrains.com>
2024-09-25 10:48:47 +0200

    Use JetBrains problem remapping resolver
    This enables special support for JetBrains plugins and their specific relaxed set of plugin problems

--- intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/mocks/IdePluginManagerTest.kt
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/mocks/IdePluginManagerTest.kt
@@ -1,8 +1,11 @@
 package com.jetbrains.plugin.structure.mocks
 
+import com.jetbrains.plugin.structure.base.plugin.PluginCreationResult
 import com.jetbrains.plugin.structure.base.problems.PluginProblem
 import com.jetbrains.plugin.structure.intellij.plugin.IdePlugin
 import com.jetbrains.plugin.structure.intellij.plugin.IdePluginManager
+import com.jetbrains.plugin.structure.intellij.problems.IntelliJPluginCreationResultResolver
+import com.jetbrains.plugin.structure.intellij.problems.JetBrainsPluginCreationResultResolver
 import com.jetbrains.plugin.structure.rules.FileSystemType
 import org.junit.Assert.assertEquals
 import java.nio.file.Path
@@ -11,6 +14,10 @@ abstract class IdePluginManagerTest (fileSystemType: FileSystemType) : BasePlugi
     override fun createManager(extractDirectory: Path): IdePluginManager =
         IdePluginManager.createManager(extractDirectory)
 
+    protected fun buildPluginSuccess(expectedWarnings: List<PluginProblem>, pluginFileBuilder: () -> Path): IdePlugin {
+        return buildPluginSuccess(expectedWarnings, ::pluginFactory, pluginFileBuilder)
+    }
+
     protected fun buildPluginSuccess(expectedWarnings: List<PluginProblem>, pluginFactory: IdePluginFactory = ::defaultPluginFactory, pluginFileBuilder: () -> Path): IdePlugin {
         val pluginFile = pluginFileBuilder()
         val successResult = createPluginSuccessfully(pluginFile, pluginFactory)
@@ -18,5 +25,10 @@ abstract class IdePluginManagerTest (fileSystemType: FileSystemType) : BasePlugi
         assertEquals(expectedWarnings.toSet().sortedBy { it.message }, warnings.toSet().sortedBy { it.message })
         assertEquals(pluginFile, plugin.originalFile)
         return plugin
+    }
+
+    protected fun pluginFactory(pluginManager: IdePluginManager, pluginArtifactPath: Path): PluginCreationResult<IdePlugin> {
+        val problemResolver = JetBrainsPluginCreationResultResolver.fromClassPathJson(IntelliJPluginCreationResultResolver())
+        return pluginManager.createPlugin(pluginArtifactPath, validateDescriptor = true, problemResolver = problemResolver)
     }
 }
