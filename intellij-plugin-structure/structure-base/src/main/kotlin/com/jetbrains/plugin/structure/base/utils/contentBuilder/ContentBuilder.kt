5854c5bf34e6ff837b0a3c6c986774043b5f36d2
Robert Novotny <robert.novotny@jetbrains.com>
2024-10-02 16:26:22 +0200

    Add toString()

--- intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/base/utils/contentBuilder/ContentBuilder.kt
+++ intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/base/utils/contentBuilder/ContentBuilder.kt
@@ -4,8 +4,12 @@
 
 package com.jetbrains.plugin.structure.base.utils.contentBuilder
 
-import com.jetbrains.plugin.structure.base.utils.*
-import java.io.File
+import com.jetbrains.plugin.structure.base.utils.createDir
+import com.jetbrains.plugin.structure.base.utils.isDirectory
+import com.jetbrains.plugin.structure.base.utils.isFile
+import com.jetbrains.plugin.structure.base.utils.listFiles
+import com.jetbrains.plugin.structure.base.utils.readBytes
+import com.jetbrains.plugin.structure.base.utils.simpleName
 import java.nio.file.Path
 
 interface ContentBuilder {
@@ -16,6 +20,7 @@ interface ContentBuilder {
   fun file(name: String, localFile: Path)
   fun dir(name: String, localDirectory: Path)
   fun dir(name: String, content: ContentBuilder.() -> Unit)
+  fun dirs(name: String, content: ContentBuilder.() -> Unit)
   fun zip(name: String, content: ContentBuilder.() -> Unit)
 }
 
@@ -43,7 +48,7 @@ fun buildZipFileContent(content: ContentBuilder.() -> Unit): ContentSpec {
   return result
 }
 
-private class ContentBuilderImpl(private val result: ChildrenOwnerSpec) : ContentBuilder {
+class ContentBuilderImpl(private val result: ChildrenOwnerSpec) : ContentBuilder {
   override fun file(name: String) {
     file(name, "")
   }
@@ -67,6 +72,95 @@ private class ContentBuilderImpl(private val result: ChildrenOwnerSpec) : Conten
   override fun dir(name: String, content: ContentBuilder.() -> Unit) {
     val directorySpec = buildDirectoryContent(content)
     addChild(name, directorySpec)
+  }
+
+  override fun dirs(name: String, content: ContentBuilder.() -> Unit) {
+    val pathElements = name.split("/")
+    when (pathElements.size) {
+      0 -> throw IllegalArgumentException("Cannot have empty name")
+      1 -> dir(pathElements.first(), content)
+      else -> {
+        val firstElement = pathElements.first()
+        // /home/jetbrains/data/
+        // first ->home
+        val w = pathElements.drop(1).windowed(2).reversed()
+        val tree = w.mapIndexed { i, (parent, child) ->
+          val childSpec = if (i == 0) {
+            SingleChildSpec(child, buildDirectoryContent(content))
+          } else {
+            SingleChildSpec(child)
+          }
+          val parentSpec = SingleChildSpec(parent, childSpec)
+          parentSpec
+        }
+
+        val tree2 = ArrayList(tree).apply { add(null) }
+        for (index in 0 until tree2.size - 1) {
+          val current = tree2[index] ?: continue
+          val next = tree2[index + 1]
+          if (next != null) {
+            val n = next.copy(child = current)
+            tree2[index + 1] = n
+          } else {
+            if (index + 1 == tree2.size - 1) {
+              /*
+              current.replaceChild(content)?.let {
+                tree2[index] = it
+              }
+
+               */
+            }
+          }
+        }
+        println(tree2)
+        addChild(firstElement, tree2.dropLast(1).last())
+      }
+    }
+  }
+
+  private fun SingleChildSpec.deepestChild(): SingleChildSpec? {
+    var c = this.child
+    var result: SingleChildSpec? = null
+    while (true) {
+      if (c is SingleChildSpec) {
+        result = c
+        c = c.child
+      } else {
+        break
+      }
+    }
+    return result
+  }
+
+  private fun SingleChildSpec.replaceChild(content: ContentBuilder.() -> Unit): SingleChildSpec? {
+    return when (child) {
+      is SingleChildSpec -> {
+        val newChild = child.copy(child = buildDirectoryContent(content))
+        copy(child = newChild)
+      }
+
+      is DirectorySpec -> {
+        copy(child = buildDirectoryContent(content))
+      }
+
+      else -> {
+        null
+      }
+    }
+  }
+
+  private data class SingleChildSpec(val name: String, val child: ContentSpec) : ContentSpec {
+    constructor(name: String) : this(name, DirectorySpec())
+
+    override fun generate(target: Path) {
+      target.createDir()
+      val childFile = target.resolve(name)
+      child.generate(childFile)
+    }
+
+    override fun toString(): String {
+      return "$name/$child"
+    }
   }
 
   override fun dir(name: String, localDirectory: Path) {
