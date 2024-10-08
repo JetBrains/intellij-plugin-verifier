943b0ae731181d76dd01e4add4c62c0ff60fd336
Robert Novotny <robert.novotny@jetbrains.com>
2024-10-08 10:46:53 +0200

    Test JSON plugin classes

--- intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/JsonPluginUsageTest.kt
+++ intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/JsonPluginUsageTest.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.pluginverifier.tests
 
+import com.jetbrains.plugin.structure.base.utils.exists
 import com.jetbrains.pluginverifier.results.problems.PackageNotFoundProblem
 import com.jetbrains.pluginverifier.tests.bytecode.Dumps
 import com.jetbrains.pluginverifier.tests.mocks.IdeaPluginSpec
@@ -8,6 +9,9 @@ import com.jetbrains.pluginverifier.tests.mocks.ideaPlugin
 import com.jetbrains.pluginverifier.tests.mocks.withRootElement
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 class JsonPluginUsageTest : BaseBytecodeTest() {
   private val pluginSpec = IdeaPluginSpec("com.intellij.plugin", "JetBrains s.r.o.")
@@ -51,4 +55,51 @@ class JsonPluginUsageTest : BaseBytecodeTest() {
       }
     }
   }
+
+  @Test
+  fun `plugin uses JSON classes, JSON plugin is declared and includes classes`() {
+    val jsonPluginId = "com.intellij.modules.json"
+    val jsonPlugin = bundledPlugin {
+      id = jsonPluginId
+      descriptorContent = ideaPlugin(
+        pluginId = jsonPluginId,
+        pluginName = "JSON",
+        vendor = "JetBrains s.r.o."
+      ).withRootElement()
+      classContentBuilder = {
+        dirs("com/intellij/json") {
+          file("JsonParserDefinition.class", JsonParserDefinition())
+        }
+      }
+    }
+
+    val targetIde = buildIdeWithBundledPlugins(bundledCorePlugins = listOf(jsonPlugin))
+    assertEquals(2, targetIde.bundledPlugins.size)
+
+    assertVerified {
+      ide = targetIde
+      plugin = prepareUsage(pluginSpec, "JsonPluginUsage", Dumps.JsonPluginUsage())
+      kotlin = false
+    }.run {
+      with(compatibilityProblems) {
+        assertEquals(0, size)
+      }
+    }
+  }
+
+  fun findAfterIdeaBuildClassPath(): Path {
+    val directory = Paths.get("after-idea", "build", "classes", "java", "main")
+    return if (directory.exists()) {
+      directory
+    } else {
+      Paths.get("verifier-test").resolve(directory).also { check(it.exists()) }
+    }
+  }
+
+  private fun JsonParserDefinition(): ByteArray {
+    return findAfterIdeaBuildClassPath()
+      .resolve("com/intellij/json/JsonParserDefinition.class")
+      .let { Files.readAllBytes(it) }
+  }
+
 }
