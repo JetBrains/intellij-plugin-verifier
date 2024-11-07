496c9be2d8b140c382eea96f05d00a81613b7c4c
Robert Novotny <robert.novotny@jetbrains.com>
2024-11-07 09:22:55 +0100

    Improve tests

--- intellij-plugin-structure/structure-ide/src/test/kotlin/com/jetbrains/plugin/structure/xml/XmlTest.kt
+++ intellij-plugin-structure/structure-ide/src/test/kotlin/com/jetbrains/plugin/structure/xml/XmlTest.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.plugin.structure.xml
 
 import com.jetbrains.plugin.structure.ide.dependencies.PluginXmlDependencyFilter
+import org.intellij.lang.annotations.Language
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.BufferedReader
@@ -9,6 +10,29 @@ import java.io.ByteArrayOutputStream
 import java.io.InputStream
 
 class XmlTest {
+
+  @Test
+  fun `XML for Java plugin with XML comment embedded in DTD entity is not parsed`() {
+    // taken from plugins/grazie/lib/grazie.jar!org/languagetool/rules/en/grammar.xml
+    @Language("XML") val pluginXml = """
+      <?xml version="1.0"?>
+      <!DOCTYPE rules [
+              <!ENTITY rude_sarcastic_2 "(?:are you blind\?|are you deaf\?|bite me|I hope you're happy[\.!]|oh really\!?\?|since when\?|whoosh[\.!]|you don't say\?|you think\?)"><!-- XXX should be avoided in writing: what's new\?|what else is new| -->
+      ]>
+      <rules>
+      </rules>
+    """.trimIndent()
+
+    val expectedXml = ""
+
+    val pluginXmFilter = PluginXmlDependencyFilter()
+    val filteredXml = captureToString {
+      pluginXmFilter.filter(pluginXml.toInputStream(), this)
+    }
+
+    assertEquals(expectedXml, filteredXml)
+  }
+
   @Test
   fun `XML for Java plugin is filtered`() {
     val pluginXmFilter = PluginXmlDependencyFilter()
@@ -124,6 +148,29 @@ class XmlTest {
 
     // STaX parser has issues with wrong PIs
     val expectedXml = ""
+
+    val pluginXmFilter = PluginXmlDependencyFilter()
+    val filteredXml: String = captureToString {
+      pluginXmFilter.filter(xml.toInputStream(), this)
+    }
+
+    assertEquals(expectedXml, filteredXml)
+  }
+
+  @Test
+  fun `non-plugin-XML with just PIs is parsed`() {
+    val xml = """
+      <?xml version="1.0" encoding="UTF-8"?>
+      <?xml-stylesheet type="text/xsl" href="print.xsl" ?>
+      <?xml-stylesheet type="text/css" href="rules.css" title="Easy editing stylesheet" ?>
+      <rules>
+      </rules>
+    """.trimIndent()
+
+    val expectedXml = """<?xml-stylesheet type="text/xsl" href="print.xsl" ?>""" +
+    """<?xml-stylesheet type="text/css" href="rules.css" title="Easy editing stylesheet" ?>""" +
+    "<rules>\n" +
+    """</rules>""".trimIndent()
 
     val pluginXmFilter = PluginXmlDependencyFilter()
     val filteredXml: String = captureToString {
