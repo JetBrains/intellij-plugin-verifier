644b9b365f5a211e698166fbcc9f72dfb725c2ac
Robert Novotny <robert.novotny@jetbrains.com>
2024-11-08 09:43:45 +0100

    Rename field to be grammatically correct

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/dependencies/DiGraph.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/dependencies/DiGraph.kt
@@ -5,18 +5,18 @@
 package com.jetbrains.plugin.structure.intellij.plugin.dependencies
 
 internal class DiGraph<I, O> {
-  private val adjacents = hashMapOf<I, MutableList<O>>()
+  private val adjacency = hashMapOf<I, MutableList<O>>()
 
-  operator fun get(from: I): List<O> = adjacents[from] ?: emptyList()
+  operator fun get(from: I): List<O> = adjacency[from] ?: emptyList()
 
   fun addEdge(from: I, to: O) {
-    adjacents.getOrPut(from) { mutableListOf() } += to
+    adjacency.getOrPut(from) { mutableListOf() } += to
   }
 
-  fun contains(from: I, to: O): Boolean = adjacents[from]?.contains(to) == true
+  fun contains(from: I, to: O): Boolean = adjacency[from]?.contains(to) == true
 
   fun contains(from: I, to: I, propertyExtractor: (O) -> I?): Boolean {
-    val adj = adjacents[from] ?: emptyList<O>()
+    val adj = adjacency[from] ?: emptyList<O>()
     return adj.map { propertyExtractor(it) }.contains(to)
   }
 }
