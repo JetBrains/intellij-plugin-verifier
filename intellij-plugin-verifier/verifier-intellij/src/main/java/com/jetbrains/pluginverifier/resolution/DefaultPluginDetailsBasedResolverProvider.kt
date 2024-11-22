65767ce09a9fa5c9366cabc498d369148d0679fc
Robert Novotny <robert.novotny@jetbrains.com>
2024-11-22 11:35:02 +0100

    Introduce provider for plugin classes resolver

---
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/resolution/DefaultPluginDetailsBasedResolverProvider.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2000-2024 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package com.jetbrains.pluginverifier.resolution
+
+import com.jetbrains.pluginverifier.createPluginResolver
+import com.jetbrains.pluginverifier.plugin.PluginDetails
+
+class DefaultPluginDetailsBasedResolverProvider : PluginDetailsBasedResolverProvider {
+  override fun getPluginResolver(pluginDependency: PluginDetails) =
+    pluginDependency.pluginClassesLocations.createPluginResolver()
+}
