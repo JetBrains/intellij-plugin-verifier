8333ec3df0fe11dbf875749cae0ea315fe469cf5
Robert Novotny <robert.novotny@jetbrains.com>
2024-11-22 11:25:53 +0100

    Add name for the resolver of bundled plugin classes

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/resolution/BundledPluginClassResolverProvider.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/resolution/BundledPluginClassResolverProvider.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2000-2024 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package com.jetbrains.pluginverifier.resolution
 
 import com.jetbrains.plugin.structure.classes.resolvers.CompositeResolver
@@ -7,11 +11,11 @@ import com.jetbrains.pluginverifier.filtering.MainClassesSelector
 import com.jetbrains.pluginverifier.plugin.PluginDetails
 
 class BundledPluginClassResolverProvider {
-    private val bundledClassesSelectors = listOf(MainClassesSelector.forBundledPlugin(), ExternalBuildClassesSelector())
+  private val bundledClassesSelectors = listOf(MainClassesSelector.forBundledPlugin(), ExternalBuildClassesSelector())
 
-    fun getResolver(pluginDetails: PluginDetails): Resolver {
-      val classLocations = pluginDetails.pluginClassesLocations
-      return bundledClassesSelectors.flatMap { it.getClassLoader(classLocations) }
-        .let { CompositeResolver.create(it) }
-    }
+  fun getResolver(pluginDetails: PluginDetails): Resolver {
+    val classLocations = pluginDetails.pluginClassesLocations
+    return bundledClassesSelectors.flatMap { it.getClassLoader(classLocations) }
+      .let { CompositeResolver.create(it, pluginDetails.pluginInfo.pluginId) }
   }
+}
