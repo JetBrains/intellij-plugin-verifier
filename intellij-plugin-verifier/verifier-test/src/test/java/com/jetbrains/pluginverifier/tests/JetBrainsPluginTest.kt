9e4af3aa4bedbebcae81f73396236258a39a6ef1
Robert Novotny <robert.novotny@jetbrains.com>
2024-11-26 01:13:46 +0100

    Treat 'ReleaseVersionWrongFormat' error as a warning for JetBrains plugins

--- intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/JetBrainsPluginTest.kt
+++ intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/JetBrainsPluginTest.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.pluginverifier.tests
 
 import com.jetbrains.plugin.structure.intellij.problems.PluginCreationResultResolver
+import com.jetbrains.plugin.structure.intellij.problems.ReleaseVersionWrongFormat
 import com.jetbrains.plugin.structure.intellij.problems.remapping.JsonUrlProblemLevelRemappingManager
 import com.jetbrains.pluginverifier.PluginVerificationResult
 import com.jetbrains.pluginverifier.options.PluginParsingConfiguration
@@ -64,6 +65,35 @@ class JetBrainsPluginTest : BasePluginTest() {
       assertEmpty("Compatibility Problems", verifiedResult.compatibilityProblems)
       assertEmpty("Plugin Structure Warnings", verifiedResult.pluginStructureWarnings)
       assertEmpty("Plugin Warnings", warnings)
+    }
+  }
+
+  @Test
+  fun `plugin has a JetBrains vendor and zero-prefixed release version as a paid plugin`() {
+    val ide = buildIde("IU-242.21829.142")
+
+    val pluginId = "org.jetbrains.somePlugin"
+    val ideaPlugin = ideaPlugin(pluginId = pluginId, vendor = "JetBrains s.r.o.")
+    val creationResult = buildPluginWithResult(problemResolver) {
+      dir("META-INF") {
+        file("plugin.xml") {
+          """
+            <idea-plugin>
+              $ideaPlugin
+               <product-descriptor code="GZL" release-date="20240626" release-version="03" />
+            </idea-plugin>
+          """
+        }
+      }
+    }
+
+    assertSuccess(creationResult) {
+      val verificationResult = VerificationRunner().runPluginVerification(ide, plugin)
+      assertTrue(verificationResult is PluginVerificationResult.Verified)
+      val verifiedResult = verificationResult as PluginVerificationResult.Verified
+
+      assertEmpty("Compatibility Problems", verifiedResult.compatibilityProblems)
+      assertContainsWarning<ReleaseVersionWrongFormat>("Invalid plugin descriptor 'plugin.xml'. The <release-version> parameter (03) format is invalid. Ensure it is an integer with at least two digits.")
     }
   }
 
