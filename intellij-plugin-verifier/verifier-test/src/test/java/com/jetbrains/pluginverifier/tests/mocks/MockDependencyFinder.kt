00325efa757bc70043c9ea608119e227befa548d
Robert Novotny <robert.novotny@jetbrains.com>
2024-12-02 23:12:31 +0100

    Allow custom naming for mock dependency finder

--- intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/mocks/MockDependencyFinder.kt
+++ intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/mocks/MockDependencyFinder.kt
@@ -1,9 +1,12 @@
+/*
+ * Copyright 2000-2024 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package com.jetbrains.pluginverifier.tests.mocks
 
 import com.jetbrains.pluginverifier.dependencies.resolution.DependencyFinder
 
-class MockDependencyFinder : DependencyFinder {
-  override val presentableName: String = "Mock Dependency Finder"
+class MockDependencyFinder(override val presentableName: String = "Mock Dependency Finder") : DependencyFinder {
 
   override fun findPluginDependency(dependencyId: String, isModule: Boolean) =
     DependencyFinder.Result.NotFound("Mock Dependency Finder does not support any dependencies")
