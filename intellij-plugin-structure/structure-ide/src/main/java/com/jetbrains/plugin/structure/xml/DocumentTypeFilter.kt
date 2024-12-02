9ef3dd6f9d25653ad7e2e9f7a269996f45373038
Robert Novotny <robert.novotny@jetbrains.com>
2024-12-02 12:47:11 +0100

    Extract XML event deduplication logic to a separate class

--- intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/xml/DocumentTypeFilter.kt
+++ intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/xml/DocumentTypeFilter.kt
@@ -4,29 +4,17 @@
 
 package com.jetbrains.plugin.structure.xml
 
-import com.jetbrains.plugin.structure.xml.DocumentTypeFilter.EventProcessing.Seen
-import com.jetbrains.plugin.structure.xml.DocumentTypeFilter.EventProcessing.Unseen
 import javax.xml.namespace.QName
 import javax.xml.stream.EventFilter
 import javax.xml.stream.events.StartElement
 import javax.xml.stream.events.XMLEvent
 
-class DocumentTypeFilter(private val supportedRootElements: Set<String>, private val delegateEventFilter: EventFilter) : EventFilter {
-  private var lastEvent: EventProcessing = Unseen
-
+class DocumentTypeFilter(private val supportedRootElements: Set<String>, private val delegateEventFilter: EventFilter) : DeduplicatingEventFilter() {
   private var rootElement: QName? = null
 
   private var isPassThruMode: Boolean = false
 
-  override fun accept(event: XMLEvent): Boolean {
-    event.onAlreadySeen { return it }
-
-    return doAccept(event).also {
-      lastEvent = Seen(event, it)
-    }
-  }
-
-  private fun doAccept(event: XMLEvent): Boolean {
+  override fun doAccept(event: XMLEvent): Boolean {
     if (event is StartElement) {
       if (rootElement == null) {
         rootElement = event.name
@@ -41,18 +29,6 @@ class DocumentTypeFilter(private val supportedRootElements: Set<String>, private
       }
     } else {
       return if (isPassThruMode) true else delegateEventFilter.accept(event)
-    }
-  }
-
-  private sealed class EventProcessing {
-    object Unseen : EventProcessing()
-    data class Seen(val event: XMLEvent, val resolution: Boolean) : EventProcessing()
-  }
-
-  private inline fun XMLEvent.onAlreadySeen(seenHandler: (Boolean) -> Boolean): Boolean {
-    return when(val lastEvent = this@DocumentTypeFilter.lastEvent) {
-      is Seen -> if (lastEvent.event === this) seenHandler(lastEvent.resolution) else false
-      is Unseen -> false
     }
   }
 }
