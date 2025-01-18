84efc5ca90948a10ad9f0037705de8a40ef5e3e2
Robert Novotny <robert.novotny@jetbrains.com>
2025-01-18 20:23:22 +0100

    Introduce an InputStream-based plugin descriptor resource

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/beans/PluginModuleBean.java
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/beans/PluginModuleBean.java
@@ -1,11 +1,13 @@
 /*
- * Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2025 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.jetbrains.plugin.structure.intellij.beans;
 
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlValue;
 
 public class PluginModuleBean {
   @XmlAttribute(name = "name") public String moduleName;
+  @XmlValue public String value;
 }
