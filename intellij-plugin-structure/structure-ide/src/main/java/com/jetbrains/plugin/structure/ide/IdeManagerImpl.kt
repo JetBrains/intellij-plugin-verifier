018aa2425db56b46453af67daa0e525e39581783
Robert Novotny <robert.novotny@jetbrains.com>
2025-02-03 23:14:19 +0100

    Increase visibility of PlatformResourceResolver to integrate with Product Info-based IDE managers

--- intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/IdeManagerImpl.kt
+++ intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/IdeManagerImpl.kt
@@ -96,7 +96,7 @@ class IdeManagerImpl : IdeManager() {
    * But rarely `nearby.xml` may reside in another platform's jar file. Apparently, IDE handles such a case accidentally:
    * an ugly fallback hack is used `com.intellij.util.io.URLUtil.openResourceStream(URL)`.
    */
-  private class PlatformResourceResolver(platformJarFiles: List<Path>) : ResourceResolver {
+  class PlatformResourceResolver(platformJarFiles: List<Path>) : ResourceResolver {
     private val jarFilesResourceResolver = JarFilesResourceResolver(platformJarFiles)
 
     override fun resolveResource(relativePath: String, basePath: Path): ResourceResolver.Result {
