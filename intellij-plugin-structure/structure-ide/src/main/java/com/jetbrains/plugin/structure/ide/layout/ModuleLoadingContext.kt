ec8a4e564421e7f4b0168372bf3c330ab6d9c6ba
Robert Novotny <robert.novotny@jetbrains.com>
2025-02-04 08:28:57 +0100

    Do not load module when the resource root does not exist

--- intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/layout/ModuleLoadingContext.kt
+++ intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/layout/ModuleLoadingContext.kt
@@ -1,5 +1,8 @@
 package com.jetbrains.plugin.structure.ide.layout
 
+import com.jetbrains.plugin.structure.base.utils.exists
 import java.nio.file.Path
 
-internal data class ModuleLoadingContext(val artifactPath: Path, val descriptorName: String)
+internal data class ModuleLoadingContext(val artifactPath: Path, val descriptorName: String) {
+  fun exists(): Boolean = artifactPath.exists()
+}
