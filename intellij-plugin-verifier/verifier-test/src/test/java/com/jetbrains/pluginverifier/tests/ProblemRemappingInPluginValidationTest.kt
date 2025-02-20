e856b6aefde11a61d5369fd547956838048d56e7
Robert Novotny <robert.novotny@jetbrains.com>
2025-02-20 11:49:09 +0100

    Promote test to a regular one

---
+++ intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/tests/ProblemRemappingInPluginValidationTest.kt
@@ -0,0 +1,57 @@
+package com.jetbrains.pluginverifier.tests
+
+import com.jetbrains.plugin.structure.base.utils.contentBuilder.buildZipFile
+import com.jetbrains.plugin.structure.intellij.plugin.IdePluginManager
+import com.jetbrains.plugin.structure.intellij.problems.IntelliJPluginCreationResultResolver
+import com.jetbrains.plugin.structure.intellij.problems.JetBrainsPluginCreationResultResolver
+import com.jetbrains.plugin.structure.intellij.problems.ReleaseVersionWrongFormat
+import com.jetbrains.plugin.structure.intellij.problems.remapping.JsonUrlProblemLevelRemappingManager
+import com.jetbrains.plugin.structure.intellij.problems.remapping.RemappingSet
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class ProblemRemappingInPluginValidationTest : BasePluginTest() {
+  @Test
+  fun `plugin with JetBrains vendor is valid because plugin problems is remapped according to JSON rules`() {
+    val intellijProblemResolver = IntelliJPluginCreationResultResolver()
+
+    val mgr = JsonUrlProblemLevelRemappingManager.fromClassPathJson()
+    val levelRemapping = mgr.getLevelRemapping(RemappingSet.JETBRAINS_PLUGIN_REMAPPING_SET)
+    val resolver = JetBrainsPluginCreationResultResolver(intellijProblemResolver, levelRemapping)
+
+    val pluginFile = buildZipFile(temporaryFolder.newFile("plugin.jar").toPath()) {
+      dir("META-INF") {
+        file("plugin.xml") {
+          """
+            <idea-plugin package="com.intellij.grazie.pro" require-restart="true">
+              <idea-version since-build="242.20224" />
+              <product-descriptor code="GZL" release-date="20240626" release-version="03" optional="true" />
+              <version>0.3.359</version>
+              <id>com.intellij.grazie.pro</id>
+              <name>Grazie Pro</name>
+              <vendor>JetBrains</vendor>
+              <dependencies>
+                <module name="intellij.dev.psiViewer" />
+                <plugin id="org.intellij.plugins.markdown" />
+              </dependencies>
+              <description>
+                Enhances the bundled Grazie Lite plugin with advanced natural language writing assistance
+                Grazie Pro is a result of the latest developments in deep learning and natural language processing.
+                By default, it processes everything locally, keeping your data private and secure,
+                and provides the following features for English text
+              </description>                            
+            </idea-plugin>
+          """
+        }
+      }
+    }
+    val pluginResult = IdePluginManager.createManager().createPlugin(pluginFile, validateDescriptor = true, problemResolver = resolver)
+    assertSuccess(pluginResult) {
+      assertEmpty("Structure Unacceptable Warnings", unacceptableWarnings)
+      assertEquals(1, warnings.size)
+      assertContainsWarning<ReleaseVersionWrongFormat>("Invalid plugin descriptor 'plugin.xml'. " +
+        "The <release-version> parameter (03) format is invalid. " +
+        "Ensure it is an integer with at least two digits.")
+    }
+  }
+}
