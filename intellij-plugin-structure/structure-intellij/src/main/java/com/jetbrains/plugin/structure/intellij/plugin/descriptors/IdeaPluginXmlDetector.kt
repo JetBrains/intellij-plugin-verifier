8aabc0e95cc0885cc37bc0a1b511e4443e5dfa4e
Robert Novotny <robert.novotny@jetbrains.com>
2025-03-03 15:25:13 +0100

    Resolve all 'idea-plugin' descriptors in JAR root and META-INF

---
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/descriptors/IdeaPluginXmlDetector.kt
@@ -0,0 +1,75 @@
+package com.jetbrains.plugin.structure.intellij.plugin.descriptors
+
+import com.jetbrains.plugin.structure.base.utils.closeAll
+import com.jetbrains.plugin.structure.base.utils.inputStream
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.Closeable
+import java.nio.file.Path
+import javax.xml.stream.XMLEventReader
+import javax.xml.stream.XMLInputFactory
+import javax.xml.stream.XMLStreamException
+import javax.xml.stream.events.StartElement
+import javax.xml.stream.events.XMLEvent
+
+private val LOG: Logger = LoggerFactory.getLogger(IdeaPluginXmlDetector::class.java)
+
+private const val IDEA_PLUGIN_ROOT_ELEMENT = "idea-plugin"
+
+class IdeaPluginXmlDetector {
+  fun isPluginDescriptor(descriptorPath: Path): Boolean {
+    val closeables = mutableListOf<Closeable>()
+    try {
+      val inputFactory: XMLInputFactory = newXmlInputFactory()
+      val eventReader = inputFactory.newEventReader(descriptorPath).also { closeables += it }
+
+      while (eventReader.hasNextEvent()) {
+        val event: XMLEvent = eventReader.nextEvent()
+        if (event.isIdeaPluginElement()) {
+          return true
+        }
+      }
+      return false
+    } catch (e: Exception) {
+      LOG.warn("Unable to read plugin descriptor $descriptorPath", e)
+      return false
+    } finally {
+      closeables.closeAll()
+    }
+  }
+
+  private fun XMLEvent.isIdeaPluginElement(): Boolean =
+    this is StartElement && name.localPart == IDEA_PLUGIN_ROOT_ELEMENT
+
+  private fun XMLInputFactory.newEventReader(descriptorPath: Path): CloseableXmlEventReader {
+    return CloseableXmlEventReader(createXMLEventReader(descriptorPath.inputStream()))
+  }
+
+
+  // FIXME duplicate with XmlStreamEventFilter
+  private fun newXmlInputFactory() = XMLInputFactory.newInstance().apply {
+    setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, false)
+    setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false)
+  }
+
+  // FIXME duplicate with XmlStreamings
+  class CloseableXmlEventReader(private val delegate: XMLEventReader) : XMLEventReader by delegate, Closeable {
+    @Throws(XMLStreamException::class)
+    override fun close() {
+      delegate.close()
+    }
+  }
+
+  // FIXME duplicate with XmlStreamEventFilter
+  private fun CloseableXmlEventReader.hasNextEvent(): Boolean {
+    return try {
+      hasNext()
+    } catch (e: XMLStreamException) {
+      LOG.error("Cannot retrieve next event", e)
+      false
+    } catch (e: RuntimeException) {
+      LOG.error("Cannot retrieve next event", e)
+      false
+    }
+  }
+}
---
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/base/utils/PluginJarDescriptorTest.kt
@@ -0,0 +1,68 @@
+package com.jetbrains.plugin.structure.base.utils
+
+import com.jetbrains.plugin.structure.base.utils.contentBuilder.buildZipFile
+import com.jetbrains.plugin.structure.intellij.plugin.descriptors.IdeaPluginXmlDetector
+import com.jetbrains.plugin.structure.jar.PluginJar
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.nio.file.Path
+
+class PluginJarDescriptorTest {
+  @Rule
+  @JvmField
+  val temporaryFolder = TemporaryFolder()
+
+  private lateinit var pluginJarPath: Path
+
+  private val ideaPluginXmlDetector = IdeaPluginXmlDetector()
+
+  @Before
+  fun setUp() {
+    pluginJarPath = buildZipFile(temporaryFolder.newFile("plugin.jar").toPath()) {
+      dir("META-INF") {
+        file("plugin.xml") {
+          """
+          <idea-plugin>
+            <name>JSON</name>
+            <id>com.intellij.modules.json</id>
+            <version>251.21418.62</version>
+          </idea-plugin>            
+          """.trimIndent()
+        }
+      }
+      file("intellij.json.xml") {
+        """
+        <idea-plugin>
+          <dependencies>
+            <module name="intellij.json.split"/>
+          </dependencies>
+        </idea-plugin>                    
+        """.trimIndent()
+      }
+    }
+  }
+
+  @Test
+  fun `descriptors in META-INF and in roots are resolved`() {
+    val descriptors = PluginJar(pluginJarPath).resolveDescriptors()
+    assertEquals(2, descriptors.size)
+    assertTrue(descriptors.any { it.hasFileName("plugin.xml") })
+    assertTrue(descriptors.any { it.hasFileName("intellij.json.xml") })
+  }
+
+  @Test
+  fun `descriptors in META-INF and in roots are resolved with 'idea-plugin' detectiom`() {
+    val descriptors = PluginJar(pluginJarPath).resolveDescriptors(ideaPluginXmlDetector::isPluginDescriptor)
+    assertEquals(2, descriptors.size)
+    assertTrue(descriptors.any { it.hasFileName("plugin.xml") })
+    assertTrue(descriptors.any { it.hasFileName("intellij.json.xml") })
+  }
+
+  private fun Path.hasFileName(fileName: String): Boolean {
+    return fileName.toString() == fileName
+  }
+}
