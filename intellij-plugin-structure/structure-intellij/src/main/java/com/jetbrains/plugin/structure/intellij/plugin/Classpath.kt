553327cd50fc2e69c39a209855205d669c4b2fad
Robert Novotny <robert.novotny@jetbrains.com>
2025-03-12 15:50:22 +0100

    Add toString() implementations

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/Classpath.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/Classpath.kt
@@ -15,9 +15,13 @@ class Classpath private constructor(val entries: List<ClasspathEntry> = emptyLis
   }
 
   val size: Int = entries.size
+
+  override fun toString(): String = entries.joinToString(separator = ":", prefix = "[", postfix = "]")
 }
 
-class ClasspathEntry(val path: Path, val origin: ClasspathOrigin = IMPLICIT)
+class ClasspathEntry(val path: Path, val origin: ClasspathOrigin = IMPLICIT) {
+  override fun toString() = "$path ($origin)"
+}
 
 enum class ClasspathOrigin {
   IMPLICIT,
