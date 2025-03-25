c5666f21a39d72cbd74758ce152b991c7a264292
Robert Novotny <robert.novotny@jetbrains.com>
2025-03-25 10:42:05 +0100

    Establish a standalone JAR parser

---
+++ intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/resolvers/jar/Jar.kt
@@ -0,0 +1,95 @@
+package com.jetbrains.plugin.structure.classes.resolvers.jar
+
+import com.jetbrains.plugin.structure.base.utils.toSystemIndependentName
+import com.jetbrains.plugin.structure.classes.resolvers.PackageSet
+import com.jetbrains.plugin.structure.classes.utils.getBundleBaseName
+import com.jetbrains.plugin.structure.classes.utils.getBundleNameByBundlePath
+import com.jetbrains.plugin.structure.jar.JarFileSystemProvider
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.streams.asSequence
+
+private const val CLASS_SUFFIX = ".class"
+
+private const val PROPERTIES_SUFFIX = ".properties"
+
+private const val SERVICE_PROVIDERS_PREFIX = "META-INF/services/"
+
+class Jar(
+  val jarPath: Path,
+  private val fileSystemProvider: JarFileSystemProvider
+) {
+
+  private val _classes = mutableSetOf<String>()
+  val classes: Set<String> get() = _classes
+
+  private val packageSet = PackageSet()
+
+  val packages: Set<String>
+    get() = packageSet.getAllPackages()
+
+  private val _bundleNames = mutableMapOf<String, MutableSet<String>>()
+  val bundleNames: Map<String, Set<String>> get() = _bundleNames
+
+  private val _serviceProviders = mutableMapOf<String, Set<String>>()
+  val serviceProviders: Map<String, Set<String>> get() = _serviceProviders
+
+  fun init(): Jar = apply {
+    fileSystemProvider.getFileSystem(jarPath).use { jarFs ->
+      val jarRoot = jarFs.rootDirectories.single()
+      Files.walk(jarRoot).use { paths ->
+        paths.forEach { scan(it) }
+      }
+    }
+  }
+
+  fun containsPackage(packageName: String) = packageSet.containsPackage(packageName)
+
+  fun containsClass(className: String) = className in classes
+
+  private fun scan(path: Path) {
+    val pathInJar = getPathInJar(path)
+    when {
+      pathInJar.endsWith(CLASS_SUFFIX) -> handleClass(pathInJar)
+      pathInJar.endsWith(PROPERTIES_SUFFIX) -> handleProperties(pathInJar)
+      pathInJar.hasServiceProviders() -> handleServiceProvider(pathInJar, path)
+    }
+  }
+
+  private fun handleClass(classInJar: String) {
+    val className = classInJar.substringBeforeLast(CLASS_SUFFIX)
+    _classes += className
+    packageSet.addPackagesOfClass(className)
+  }
+
+  private fun handleProperties(propertiesInJar: String) {
+    val fullBundleName = getBundleNameByBundlePath(propertiesInJar)
+    _bundleNames.getOrPut(getBundleBaseName(fullBundleName)) { mutableSetOf() } += fullBundleName
+  }
+
+  private fun handleServiceProvider(serviceProvidersInJar: String, metaInfServiceProviderPath: Path) {
+    val serviceProvider = serviceProvidersInJar.substringAfter(SERVICE_PROVIDERS_PREFIX)
+    _serviceProviders[serviceProvider] = readServiceImplementationNames(metaInfServiceProviderPath)
+  }
+
+  private fun readServiceImplementationNames(metaInfServiceProviderPath: Path): Set<String> {
+    return Files.lines(metaInfServiceProviderPath).use { lines ->
+      lines.asSequence()
+        .mapNotNull { parseServiceImplementationLine(it) }
+        .toSet()
+    }
+  }
+
+  private fun parseServiceImplementationLine(line: String): String? {
+    val serviceImplementation = line.substringBefore("#").trim()
+    return serviceImplementation.takeIf { it.isNotEmpty() }
+  }
+
+  private fun String.hasServiceProviders() = startsWith(SERVICE_PROVIDERS_PREFIX) && count { it == '/' } == 2
+
+  private fun getPathInJar(entry: Path): String =
+    entry.toString().trimStart('/').toSystemIndependentName()
+
+
+}
+
