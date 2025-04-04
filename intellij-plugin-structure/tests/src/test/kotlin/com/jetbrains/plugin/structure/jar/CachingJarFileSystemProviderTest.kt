11ec0721881f27dd39998b1a1184df4385afa97c
Robert Novotny <robert.novotny@jetbrains.com>
2025-04-04 12:27:13 +0200

    Rename and cleanup the unit test

--- intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/jar/CachingJarFileSystemProviderTest.kt
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/jar/CachingJarFileSystemProviderTest.kt
@@ -1,7 +1,7 @@
 package com.jetbrains.plugin.structure.jar
 
 import com.jetbrains.plugin.structure.base.utils.writeText
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
@@ -9,14 +9,14 @@ import java.net.URI
 import java.nio.file.FileSystems
 import java.nio.file.Path
 
-class SingletonCachingJarFileSystemProviderTest {
+class CachingJarFileSystemProviderTest {
 
   @Rule
   @JvmField
   val tempFolder = TemporaryFolder()
 
   @Test
-  fun `Jar Uris are normalized`() {
+  fun `JAR URIss are normalized`() {
     val jarPath = tempFolder.root.toPath().resolve("test.jar")
     FileSystems.newFileSystem(URI.create("jar:${jarPath.toUri()}"), mapOf<String, Any>("create" to true)).use {
       it.getPath("hello.txt").writeText("Hello World")
@@ -24,22 +24,22 @@ class SingletonCachingJarFileSystemProviderTest {
 
     val fileSystemProvider = CachingJarFileSystemProvider(retentionTimeInSeconds = 0)
 
-    //Getting two filesystems with different path that point to the same file.
+    // Getting two filesystems with different paths that point to the same file.
     val fs1 = fileSystemProvider.getFileSystem(jarPath)
     val fs2 = fileSystemProvider.getFileSystem(
       Path.of("${tempFolder.root.absolutePath}/../${tempFolder.root.name}/test.jar")
     )
 
-    Assert.assertSame(fs1, fs2)
+    assertSame(fs1, fs2)
 
-    //After jar is closed for the first time, filesystem remains open
+    // After the JAR is closed for the first time, the filesystem remains open, as there is still one user active.
     fileSystemProvider.close(jarPath)
-    Assert.assertTrue(fs1.isOpen)
-    Assert.assertTrue(fs2.isOpen)
+    assertTrue(fs1.isOpen)
+    assertTrue(fs2.isOpen)
 
-    //After closed for second time, the filesystem has no users and is closed
+    // After the JAR is closed for the second time, the filesystem has no users and is closed
     fileSystemProvider.close(jarPath)
-    Assert.assertFalse(fs1.isOpen)
-    Assert.assertFalse(fs2.isOpen)
+    assertFalse(fs1.isOpen)
+    assertFalse(fs2.isOpen)
   }
 }
