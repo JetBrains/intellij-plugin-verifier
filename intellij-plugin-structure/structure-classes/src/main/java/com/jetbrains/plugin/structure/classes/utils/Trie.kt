95c1d1213dae4f44fde29921cce790b889a8bf64
Robert Novotny <robert.novotny@jetbrains.com>
2025-04-08 13:00:58 +0200

    Introduce trie to store package names

---
+++ intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/utils/Trie.kt
@@ -0,0 +1,78 @@
+package com.jetbrains.plugin.structure.classes.utils
+
+
+class Trie<V>(val defaultValue: V? = null) {
+  private data class Node<V>(
+    val children: MutableMap<Char, Node<V>> = mutableMapOf(),
+    var value: V? = null
+  )
+
+  private fun empty(value: V?) = Node<V>(mutableMapOf(), value)
+
+  private val root = empty(defaultValue)
+
+  val isEmpty: Boolean get() = root.children.isEmpty()
+
+  fun find(word: CharSequence): Boolean {
+    var n = root
+    for (c in word) {
+      n = n.children[c] ?: return false
+    }
+    return true
+  }
+
+  fun insert(key: CharSequence, value: V? = defaultValue): Boolean {
+    var isInserted = false
+    var currentNode = root
+    for (char in key) {
+      currentNode = currentNode.children.getOrPut(char) { empty(defaultValue).also { isInserted = true } }
+    }
+    currentNode.value = value
+    return isInserted
+  }
+
+  fun getAllWords(): Set<String> {
+    return findAllWords(value = null)
+  }
+
+  fun findAllWords(value: V?, wordSeparator: Char = '.'): Set<String> {
+    return mutableSetOf<String>().apply {
+      visit(root, "", wordSeparator) { word, visitedValue, leaf ->
+        if (leaf || value == visitedValue) {
+          add(word)
+        }
+      }
+    }
+  }
+
+  fun visitWords(wordSeparator: Char, visitor: Visitor<V>) {
+    visit(root, "", wordSeparator, visitor)
+  }
+
+  private fun visit(node: Node<V>, prefix: String, wordSeparator: Char, visitor: Visitor<V>) {
+    if (node.children.isEmpty()) {
+      visitor.visit(prefix, node.value, true)
+    } else {
+      for ((char, child) in node.children) {
+        if (char == wordSeparator) {
+          visitor.visit(prefix, node.value, false)
+        }
+        visit(child, prefix + char, wordSeparator, visitor)
+      }
+    }
+  }
+
+  val length: Int
+    get() {
+      var size = 0
+      visitWords('.') { _, _, leaf ->
+        if (leaf) size++
+      }
+      return size
+    }
+
+  fun interface Visitor<V> {
+    fun visit(word: String, value: V?, isLeaf: Boolean)
+  }
+}
+
---
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/classes/utils/TrieTest.kt
@@ -0,0 +1,126 @@
+package com.jetbrains.plugin.structure.classes.utils
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class TrieTest {
+
+  private fun newTrie(): Trie<Boolean> {
+    return Trie<Boolean>(defaultValue = false)
+  }
+
+  @Test
+  fun `all words are retrieved`() {
+    val packages = newTrie()
+    packages.insert("com.example.foo")
+    packages.insert("com.example.bar")
+    packages.insert("com.example.bar.zap")
+    packages.insert("com.jetbrains.foo")
+    packages.insert("com.jetbrains.cli")
+    packages.insert("com.jetbrains.cli.impl")
+
+    val expected = setOf(
+      "com.example.bar.zap",
+      "com.example.foo",
+      "com.jetbrains.cli.impl",
+      "com.jetbrains.foo"
+    )
+    val words = packages.getAllWords()
+    assertEquals(expected, words)
+  }
+
+  @Test
+  fun `all words are retrieved with explicit values`() {
+    val packages = newTrie().apply {
+      insert("com.example.foo", true)
+      insert("com.example.bar", true)
+      insert("com.example.bar.zap", true)
+      insert("com.jetbrains.foo", true)
+      insert("com.jetbrains.cli", true)
+      insert("com.jetbrains.cli.impl", true)
+    }
+
+    val expected = setOf(
+      "com.example.foo",
+      "com.example.bar",
+      "com.example.bar.zap",
+      "com.jetbrains.foo",
+      "com.jetbrains.cli",
+      "com.jetbrains.cli.impl",
+    )
+    val words = packages.findAllWords(true)
+    assertEquals(expected, words)
+  }
+
+  @Test
+  fun `all dot separated words are retrieved`() {
+    val packages = newTrie()
+    packages.insert("com.example.foo")
+    packages.insert("com.example.bar")
+    packages.insert("com.example.bar.zap")
+    packages.insert("com.jetbrains.foo")
+    packages.insert("com.jetbrains.cli")
+    packages.insert("com.jetbrains.cli.impl")
+
+    val expected = setOf(
+      "com", "com.example", "com.example.foo", "com.example.bar", "com.example.bar.zap",
+      "com.jetbrains", "com.jetbrains.foo",
+      "com.jetbrains.cli", "com.jetbrains.cli.impl"
+    )
+    val visitedWords = mutableSetOf<String>()
+    packages.visitWords('.') { word,_,  _ ->
+      visitedWords += word
+    }
+    assertEquals(expected, visitedWords)
+  }
+
+  @Test
+  fun `word in trie is found, others are not`() {
+    val packages = Trie<Boolean>(defaultValue = false)
+    packages.insert("com.example.foo")
+    packages.insert("com.example.bar")
+    packages.insert("com.example.bar.zap")
+    packages.insert("com.jetbrains.foo")
+    packages.insert("com.jetbrains.cli")
+    packages.insert("com.jetbrains.cli.impl")
+
+    assertTrue(packages.find("com.example"))
+    assertTrue(packages.find("com.example.bar.zap"))
+    assertFalse(packages.find("com.unavailable"))
+    assertTrue(packages.find(""))
+  }
+
+  @Test
+  fun `empty trie`() {
+    val emptyTrie = newTrie()
+    assertTrue(emptyTrie.isEmpty)
+  }
+
+  @Test
+  fun `duplicate insertions are tracked`() {
+    val packages = newTrie()
+    assertTrue(packages.insert("com.example.foo"))
+    assertFalse(packages.insert("com.example.foo"))
+    assertTrue(packages.insert("com.example.foo.zap"))
+    assertFalse(packages.insert("com.example.foo.zap"))
+
+    assertFalse(packages.insert("com.example"))
+  }
+
+  @Test
+  fun `trie size is calculated`() {
+    val packages = newTrie()
+    packages.insert("com.example.foo")
+    assertEquals(1, packages.length)
+    packages.insert("com.example.bar")
+    assertEquals(2, packages.length)
+    packages.insert("com.example.bar.zap")
+    assertEquals(2, packages.length)
+    packages.insert("com.jetbrains.foo")
+    assertEquals(3, packages.length)
+    packages.insert("com.jetbrains.cli")
+    assertEquals(4, packages.length)
+    packages.insert("com.jetbrains.cli.impl")
+    assertEquals(4, packages.length)
+  }
+}
