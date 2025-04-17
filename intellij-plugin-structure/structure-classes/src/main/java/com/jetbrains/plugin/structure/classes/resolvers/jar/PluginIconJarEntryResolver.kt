1fc7239d8fecdc8a336bef0112efece6916ae839
Robert Novotny <robert.novotny@jetbrains.com>
2025-04-17 15:06:42 +0200

    Support additional JAR entry resolvers

---
+++ intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/resolvers/jar/PluginIconJarEntryResolver.kt
@@ -0,0 +1,29 @@
+package com.jetbrains.plugin.structure.classes.resolvers.jar
+
+import com.jetbrains.plugin.structure.classes.utils.componentAt
+import com.jetbrains.plugin.structure.classes.utils.occurrences
+import java.nio.CharBuffer
+import java.util.zip.ZipEntry
+
+private const val PATH_SEPARATOR = '/'
+
+//FIXME duplicate with Jar
+private val NO_SUFFIX: CharBuffer = CharBuffer.allocate(0)
+
+
+class PluginIconJarEntryResolver : JarEntryResolver<CharSequence> {
+  override val key: JarEntryResolver.Key<CharSequence> = JarEntryResolver.Key("PluginIcon", CharSequence::class.java)
+
+  override fun resolve(path: PathInJar, zipEntry: ZipEntry): CharSequence? {
+    val descriptorPath = resolve(path, PATH_SEPARATOR, NO_SUFFIX)
+    if (descriptorPath.startsWith("META-INF/") && descriptorPath.occurrences(PATH_SEPARATOR) == 1
+      && descriptorPath.endsWith(".svg", ignoreCase = true)
+      ) {
+      val iconFileName = descriptorPath.componentAt(1, PATH_SEPARATOR)
+      if (iconFileName != null) {
+        return iconFileName
+      }
+    }
+    return null
+  }
+}
