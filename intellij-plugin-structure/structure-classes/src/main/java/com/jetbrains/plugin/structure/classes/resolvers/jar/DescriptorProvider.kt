dd38f974eb2b58e17a0ef418609ad8c87a067584
Robert Novotny <robert.novotny@jetbrains.com>
2025-04-17 15:45:42 +0200

    Remove unused DescriptorProviders

--- intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/resolvers/jar/DescriptorProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.plugin.structure.classes.resolvers.jar
-
-import com.jetbrains.plugin.structure.jar.PluginDescriptorResult
-import java.nio.file.Path
-
-interface DescriptorProvider {
-  fun <T> resolveFromJar(jarFile: Path, descriptorPath: PathInJar, onSuccess: (PluginDescriptorResult.Found) -> T): T?
-}
