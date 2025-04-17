f2383a0fd2bb4f3dddb4a00bfc3eabc6b43d14a0
Robert Novotny <robert.novotny@jetbrains.com>
2025-04-17 15:06:24 +0200

    Add helper class for CharSequences

---
+++ intellij-plugin-structure/structure-classes/src/test/java/CharSequencesTest.kt
@@ -0,0 +1,16 @@
+import com.jetbrains.plugin.structure.classes.utils.componentAt
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+
+class CharSequencesTest {
+  @Test
+  fun `specific component is retrieved`() {
+    val s: CharSequence = "/var/lib/example"
+    assertEquals("var", s.componentAt(1, '/'))
+    assertNull(s.componentAt(5, '/'))
+    assertEquals(s, s.componentAt(0, '|'))
+    assertNull(s.componentAt(1, '|'))
+  }
+}
