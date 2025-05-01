dc13b3e4348b53d54488edf43d6ebbc72173e1f9
Robert Novotny <robert.novotny@jetbrains.com>
2025-05-01 23:55:43 +0200

    Pass configuration when creating a filesystem

--- intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/jar/JarFileSystemProvider.kt
+++ intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/jar/JarFileSystemProvider.kt
@@ -6,4 +6,13 @@ import java.nio.file.Path
 interface JarFileSystemProvider {
   @Throws(JarArchiveException::class)
   fun getFileSystem(jarPath: Path): FileSystem
+
+  @Throws(JarArchiveException::class)
+  fun getFileSystem(jarPath: Path, configuration: Configuration): FileSystem = getFileSystem(jarPath)
+
+  data class Configuration(val expectedClients: Int = DEFAULT_EXPECTED_CLIENTS)
+
+  companion object {
+    const val DEFAULT_EXPECTED_CLIENTS = 1
+  }
 }
