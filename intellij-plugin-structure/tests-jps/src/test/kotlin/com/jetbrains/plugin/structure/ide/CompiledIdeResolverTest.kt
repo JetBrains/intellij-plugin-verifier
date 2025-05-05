e0b93299a6eb651d445340fc62aae8e5449b6ede
Robert Novotny <robert.novotny@jetbrains.com>
2025-05-05 12:57:12 +0200

    Test compiled IDE resolver

---
+++ intellij-plugin-structure/tests-jps/src/test/kotlin/com/jetbrains/plugin/structure/ide/CompiledIdeResolverTest.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2000-2025 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package com.jetbrains.plugin.structure.ide
+
+import com.jetbrains.plugin.structure.classes.resolvers.Resolver.ReadMode.FULL
+import com.jetbrains.plugin.structure.ide.classes.IdeResolverConfiguration
+import com.jetbrains.plugin.structure.ide.jps.CompiledIdeResolverCreator
+import com.jetbrains.plugin.structure.intellij.version.IdeVersion
+import com.jetbrains.plugin.structure.mocks.MockIde
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class CompiledIdeResolverTest {
+
+  @Rule
+  @JvmField
+  val temporaryFolder = TemporaryFolder()
+
+  @Test
+  fun `compiled IDE resolvers are provided`() {
+    val (idePath, _) = getCompiledIde(temporaryFolder)
+
+    val ideVersion = IdeVersion.createIdeVersion("IU-163.1")
+    val ide = MockIde(ideVersion, idePath, bundledPlugins = emptyList())
+
+    val ideResolverCreator = CompiledIdeResolverCreator()
+    val ideResolver = ideResolverCreator.createIdeResolver(ide, IdeResolverConfiguration(readMode = FULL))
+    with(ideResolver.allClassNames) {
+      assertTrue(any { "com/example/LibPluginService".contentEquals(it) })
+      assertTrue(any { "com/example/somePlugin/SomePluginService".contentEquals(it) })
+    }
+  }
+}
