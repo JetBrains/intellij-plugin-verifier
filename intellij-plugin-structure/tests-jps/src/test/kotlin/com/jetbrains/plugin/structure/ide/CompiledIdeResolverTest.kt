fe362bb39eb19da4de60e20835a3174fe32ebe51
Robert Novotny <robert.novotny@jetbrains.com>
2025-05-05 15:38:54 +0200

    Add parameterized tests

--- intellij-plugin-structure/tests-jps/src/test/kotlin/com/jetbrains/plugin/structure/ide/CompiledIdeResolverTest.kt
+++ intellij-plugin-structure/tests-jps/src/test/kotlin/com/jetbrains/plugin/structure/ide/CompiledIdeResolverTest.kt
@@ -5,20 +5,48 @@
 package com.jetbrains.plugin.structure.ide
 
 import com.jetbrains.plugin.structure.classes.resolvers.Resolver.ReadMode.FULL
+import com.jetbrains.plugin.structure.ide.CompiledIdeResolverTest.IdeResolverType.*
 import com.jetbrains.plugin.structure.ide.classes.IdeResolverConfiguration
+import com.jetbrains.plugin.structure.ide.classes.IdeResolverCreator
+import com.jetbrains.plugin.structure.ide.classes.IdeResolverProvider
 import com.jetbrains.plugin.structure.ide.jps.CompiledIdeResolverProvider
 import com.jetbrains.plugin.structure.intellij.version.IdeVersion
 import com.jetbrains.plugin.structure.mocks.MockIde
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import java.nio.file.Path
 
-class CompiledIdeResolverTest {
+@RunWith(Parameterized::class)
+class CompiledIdeResolverTest(private val ideResolverType: IdeResolverType) {
+
+  companion object {
+    @JvmStatic
+    @Parameterized.Parameters(name = "ide-resolver={0}")
+    fun ideResolverType(): List<Array<IdeResolverType>> = IdeResolverType.values().map { arrayOf(it) }
+  }
 
   @Rule
   @JvmField
   val temporaryFolder = TemporaryFolder()
+
+  private lateinit var ideResolverProvider: IdeResolverProvider
+
+  @Before
+  fun setUp() {
+    ideResolverProvider = when (ideResolverType) {
+      DEFAULT -> IdeResolverCreatorAdapter
+      JPS -> CompiledIdeResolverProvider()
+      SERVICE_LOADED_JPS -> {
+        IdeResolverProviders.loadCompiledIdeResolverProvider()
+          ?: throw IllegalStateException("IdeResolverProvider cannot be loaded via ServiceLoader")
+      }
+    }
+  }
 
   @Test
   fun `compiled IDE resolvers are provided`() {
@@ -27,11 +55,23 @@ class CompiledIdeResolverTest {
     val ideVersion = IdeVersion.createIdeVersion("IU-163.1")
     val ide = MockIde(ideVersion, idePath, bundledPlugins = emptyList())
 
-    val ideResolverCreator = CompiledIdeResolverProvider()
-    val ideResolver = ideResolverCreator.getIdeResolver(ide, IdeResolverConfiguration(readMode = FULL))
+    val ideResolver = ideResolverProvider.getIdeResolver(ide, IdeResolverConfiguration(readMode = FULL))
     with(ideResolver.allClassNames) {
       assertTrue(any { "com/example/LibPluginService".contentEquals(it) })
       assertTrue(any { "com/example/somePlugin/SomePluginService".contentEquals(it) })
     }
+  }
+
+  enum class IdeResolverType {
+    DEFAULT,
+    JPS,
+    SERVICE_LOADED_JPS
+  }
+
+  object IdeResolverCreatorAdapter : IdeResolverProvider {
+    override fun getIdeResolver(ide: Ide, configuration: IdeResolverConfiguration
+    ) = IdeResolverCreator.createIdeResolver(ide, configuration)
+
+    override fun supports(idePath: Path) = false
   }
 }
