50ca8bb8944d674eb96664b5a113296a68e1dbfc
Robert Novotny <robert.novotny@jetbrains.com>
2025-05-15 09:43:12 +0200

    Add method to create a minimalistic TAR.GZ

--- intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/base/utils/Zips.kt
+++ intellij-plugin-structure/tests/src/test/kotlin/com/jetbrains/plugin/structure/base/utils/Zips.kt
@@ -1,8 +1,15 @@
 package com.jetbrains.plugin.structure.base.utils
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream
 import java.io.ByteArrayOutputStream
+import java.nio.file.Path
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
+
+private const val TAR_GZ_CONTENT_FILE_NAME = "a"
+private const val TAR_GZ_CONTENT_FILE_SIZE = 0
 
 internal fun createZipBufferWithSingleEmptyFile(): ByteArray {
   val byteOut = ByteArrayOutputStream()
@@ -13,4 +20,25 @@ internal fun createZipBufferWithSingleEmptyFile(): ByteArray {
     zipOut.closeEntry()
   }
   return byteOut.toByteArray()
+}
+
+/**
+ * Creates a single-file TAR.GZ.
+ *
+ * The file inside this archive will have a single-character name and no content (0 bytes).
+ */
+internal fun createMinimalisticTarGz(tarGzPath: Path) {
+  val fileContent = ByteArray(TAR_GZ_CONTENT_FILE_SIZE)
+  tarGzPath.outputStream().use { tarGzPathStream ->
+    GzipCompressorOutputStream(tarGzPathStream).use { gzipOut ->
+      TarArchiveOutputStream(gzipOut).use { tarOut ->
+        val entry = TarArchiveEntry(TAR_GZ_CONTENT_FILE_NAME)
+        entry.size = TAR_GZ_CONTENT_FILE_SIZE.toLong()
+
+        tarOut.putArchiveEntry(entry)
+        tarOut.write(fileContent)
+        tarOut.closeArchiveEntry()
+      }
+    }
+  }
 }
