6ca200d33d6fb60b7dbe2f0e884f9d4fb78a53a3
Robert Novotny <robert.novotny@jetbrains.com>
2025-05-27 01:21:52 +0200

    Introduce a Source that provides layout component names

---
+++ intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/layout/ProductInfoLayoutComponentNameSource.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2000-2025 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package com.jetbrains.plugin.structure.ide.layout
+
+import com.jetbrains.plugin.structure.intellij.platform.ProductInfo
+
+/**
+ * Provide layout component names from the `name` key in the `layout` section
+ * of the Product Info JSON (`product-info.json`).
+ */
+class ProductInfoLayoutComponentNameSource : LayoutComponentNameSource<ProductInfo> {
+  override fun getNames(resource: ProductInfo): List<String> {
+    return resource.layout.map { it.name }
+  }
+}
