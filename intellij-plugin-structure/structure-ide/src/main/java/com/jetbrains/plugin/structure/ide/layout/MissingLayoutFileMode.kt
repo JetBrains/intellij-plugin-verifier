60a9374a87c830238086587a83bd53e368ecb438
Robert Novotny <robert.novotny@jetbrains.com>
2025-05-26 21:04:16 +0200

    Add KDoc

--- intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/layout/MissingLayoutFileMode.kt
+++ intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/layout/MissingLayoutFileMode.kt
@@ -5,9 +5,32 @@
 package com.jetbrains.plugin.structure.ide.layout
 
 enum class MissingLayoutFileMode {
+  /**
+   * Do not perform any validation of layout component classpath.
+   *
+   * The IDE will be created anyway, but its structure might be incomplete or semantically incorrect.
+    */
   IGNORE,
+
+  /**
+   * When any classpath element is missing, skip it but retain the available classpath elements.
+   */
   SKIP_CLASSPATH,
+  /**
+   * When any classpath element is missing, skip all classpath elements in the layout component.
+   * Don't log anything.
+   */
   SKIP_SILENTLY,
+  /**
+   * When any classpath element is missing, skip all classpath elements in the layout component.
+   * Warn about missing classpath elements in the log.
+   */
   SKIP_AND_WARN,
+  /**
+   * When any classpath element is missing, throw an exception.
+   *
+   * It is up to the caller to handle the exception appropriately and decide whether
+   * to construct the IDE model.
+   */
   FAIL
 }
