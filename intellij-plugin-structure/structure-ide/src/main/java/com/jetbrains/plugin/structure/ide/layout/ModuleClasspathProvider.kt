94d7e9358d74154ece8b84c4822f7c3da23d61c5
Robert Novotny <robert.novotny@jetbrains.com>
2025-05-27 23:19:32 +0200

    Remove ProductInfoClasspathProvider
    Replaced by LayoutComponentsClasspathProvider

--- intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/layout/ModuleClasspathProvider.kt
+++ intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/layout/ModuleClasspathProvider.kt
@@ -1,21 +1,9 @@
 package com.jetbrains.plugin.structure.ide.layout
 
-import com.jetbrains.plugin.structure.intellij.platform.LayoutComponent
-import com.jetbrains.plugin.structure.intellij.platform.ProductInfo
 import java.nio.file.Path
 
 fun interface ModuleClasspathProvider {
   fun getClasspath(moduleName: String): List<Path>
-}
-
-class ProductInfoClasspathProvider(private val productInfo: ProductInfo) : ModuleClasspathProvider {
-  override fun getClasspath(moduleName: String): List<Path> {
-    return productInfo.layout
-      .find { it.name == moduleName && it is LayoutComponent.Classpathable }
-      ?.let { it as LayoutComponent.Classpathable }
-      ?.getClasspath()
-      ?: emptyList()
-  }
 }
 
 class LayoutComponentsClasspathProvider(private val layoutComponents: LayoutComponents) : ModuleClasspathProvider {
