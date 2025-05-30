88d0fb84a0288f2fd5dde061688f8bb3b28b1ccb
Robert Novotny <robert.novotny@jetbrains.com>
2025-05-31 01:33:49 +0200

    Simplify code flow

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/loaders/ContentModuleLoader.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/loaders/ContentModuleLoader.kt
@@ -5,6 +5,7 @@
 package com.jetbrains.plugin.structure.intellij.plugin.loaders
 
 import com.jetbrains.plugin.structure.intellij.plugin.IdePlugin
+import com.jetbrains.plugin.structure.intellij.plugin.Module
 import com.jetbrains.plugin.structure.intellij.plugin.Module.FileBasedModule
 import com.jetbrains.plugin.structure.intellij.plugin.Module.InlineModule
 import com.jetbrains.plugin.structure.intellij.plugin.ModuleDescriptor
@@ -18,11 +19,7 @@ import com.jetbrains.plugin.structure.intellij.plugin.module.InlineModuleDescrip
 import com.jetbrains.plugin.structure.intellij.problems.AnyProblemToWarningPluginCreationResultResolver
 import com.jetbrains.plugin.structure.intellij.problems.PluginCreationResultResolver
 import com.jetbrains.plugin.structure.intellij.resources.ResourceResolver
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import java.nio.file.Path
-
-private val LOG: Logger = LoggerFactory.getLogger(ContentModuleLoader::class.java)
 
 class ContentModuleLoader internal constructor(pluginLoader: PluginLoader) {
   private val fileBasedModuleDescriptorResolver = FileBasedModuleDescriptorResolver(pluginLoader)
@@ -36,24 +33,40 @@ class ContentModuleLoader internal constructor(pluginLoader: PluginLoader) {
   ) {
     if (currentPlugin.isSuccess) {
       val contentModules = currentPlugin.plugin.contentModules
-      contentModules.map { module ->
-        when (module) {
-          is FileBasedModule -> resolveFileBasedModule(
-            module,
-            pluginFile,
-            currentPlugin,
-            resourceResolver,
-            problemResolver
-          )
+      contentModules
+        .map { resolveContentModule(it, pluginFile, currentPlugin, resourceResolver, problemResolver) }
+        .map {
+          when (it) {
+            is Found -> currentPlugin.addModuleDescriptor(it.resolvedContentModule, it.moduleDescriptor)
+            is Failed -> currentPlugin.registerProblem(it.error)
+          }
+        }
+    }
+  }
 
-          is InlineModule -> resolveInlineModule(module, pluginFile, currentPlugin, resourceResolver)
-        }
-      }.map { resolution ->
-        when (resolution) {
-          is Found -> currentPlugin.addModuleDescriptor(resolution.resolvedContentModule, resolution.moduleDescriptor)
-          is Failed -> currentPlugin.registerProblem(resolution.error)
-        }
-      }
+  private fun resolveContentModule(
+    moduleReference: Module,
+    pluginFile: Path,
+    currentPlugin: PluginCreator,
+    resourceResolver: ResourceResolver,
+    problemResolver: PluginCreationResultResolver
+  ): ResolutionResult {
+    return when (moduleReference) {
+      is FileBasedModule -> fileBasedModuleDescriptorResolver.resolveDescriptor(
+        pluginFile,
+        currentPlugin,
+        moduleReference,
+        resourceResolver,
+        problemResolver
+      )
+
+      is InlineModule -> inlineModuleDescriptorResolver.resolveDescriptor(
+        pluginFile,
+        currentPlugin,
+        moduleReference,
+        resourceResolver,
+        AnyProblemToWarningPluginCreationResultResolver
+      )
     }
   }
 
@@ -62,36 +75,5 @@ class ContentModuleLoader internal constructor(pluginLoader: PluginLoader) {
     plugin.definedModules.add(moduleDescriptor.name)
 
     mergeContent(resolvedContentModule)
-  }
-
-  private fun resolveFileBasedModule(
-    module: FileBasedModule,
-    pluginFile: Path,
-    currentPlugin: PluginCreator,
-    resourceResolver: ResourceResolver,
-    problemResolver: PluginCreationResultResolver
-  ): ResolutionResult {
-    return fileBasedModuleDescriptorResolver.resolveDescriptor(
-      pluginFile,
-      currentPlugin,
-      module,
-      resourceResolver,
-      problemResolver
-    )
-  }
-
-  private fun resolveInlineModule(
-    module: InlineModule,
-    pluginFile: Path,
-    currentPlugin: PluginCreator,
-    resourceResolver: ResourceResolver
-  ): ResolutionResult {
-    return inlineModuleDescriptorResolver.resolveDescriptor(
-      pluginFile,
-      currentPlugin,
-      module,
-      resourceResolver,
-      AnyProblemToWarningPluginCreationResultResolver
-    )
   }
 }
