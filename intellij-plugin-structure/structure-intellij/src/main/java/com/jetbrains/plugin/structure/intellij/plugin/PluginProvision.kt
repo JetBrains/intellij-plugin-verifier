4ab24be29fbeb61bc7b8a1df8d40512799ed29f7
Robert Novotny <robert.novotny@jetbrains.com>
2025-06-06 15:07:16 +0200

    Add description to plugin provisions

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/PluginProvision.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/PluginProvision.kt
@@ -4,12 +4,19 @@ package com.jetbrains.plugin.structure.intellij.plugin
  * Copyright 2000-2025 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 sealed class PluginProvision {
-  enum class Source {
-    ID,
-    NAME,
-    ALIAS
+  enum class Source(val description: String) {
+    ID("ID"),
+    NAME("name"),
+    ALIAS("plugin alias"),
+    CONTENT_MODULE_ID("content module ID");
+
+    override fun toString() = description
   }
 
-  class Found(val plugin: IdePlugin, val source: Source) : PluginProvision()
+  class Found(val plugin: IdePlugin, val source: Source) : PluginProvision() {
+    override fun toString(): String {
+      return "Found via ${source.description}"
+    }
+  }
   object NotFound : PluginProvision()
 }
