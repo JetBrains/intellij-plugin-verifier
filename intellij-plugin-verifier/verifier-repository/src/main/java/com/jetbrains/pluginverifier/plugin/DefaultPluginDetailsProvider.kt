45d3901015a8d3470eb442e8e74b1b12daeae187
Robert Novotny <robert.novotny@jetbrains.com>
2025-06-12 22:33:22 +0200

    Collect open resources in the details provider and make it stateless

--- intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/plugin/DefaultPluginDetailsProvider.kt
+++ intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/plugin/DefaultPluginDetailsProvider.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.pluginverifier.plugin
 
 import com.jetbrains.plugin.structure.base.plugin.PluginCreationResult
+import com.jetbrains.plugin.structure.base.plugin.PluginCreationSuccess
 import com.jetbrains.plugin.structure.intellij.classes.locator.CompileServerExtensionKey
 import com.jetbrains.plugin.structure.intellij.classes.plugin.BundledPluginClassesFinder
 import com.jetbrains.plugin.structure.intellij.classes.plugin.IdePluginClassesLocations
@@ -15,15 +16,18 @@ import com.jetbrains.pluginverifier.repository.repositories.dependency.Dependenc
 import java.nio.file.Path
 
 /**
- * Provides plugin details with explicit support for bundled plugins that are provided by the Platform.
+ * Provides plugin details with explicit support for bundled plugins that are provided by the Platform
+ * or downloaded as a dependency from JetBrains Marketplace.
  *
- * Non-bundled plugins are handled by the delegate [PluginDetailsProviderImpl].
+ * Non-bundled plugins that aren't dependencies are handled by the delegate [PluginDetailsProviderImpl].
  */
-class DefaultPluginDetailsProvider(extractDirectory: Path) : AbstractPluginDetailsProvider(extractDirectory) {
+class DefaultPluginDetailsProvider(extractDirectory: Path) : AbstractPluginDetailsProvider(extractDirectory), AutoCloseable {
   private val nonBundledPluginDetailsProvider: PluginDetailsProviderImpl = PluginDetailsProviderImpl(extractDirectory)
 
   private val dependencyProblemResolver: PluginCreationResultResolver =
     JetBrainsPluginCreationResultResolver.fromClassPathJson(IntelliJPluginCreationResultResolver())
+
+  private val closeableResources: MutableList<AutoCloseable> = mutableListOf()
 
   override fun readPluginClasses(pluginInfo: PluginInfo, idePlugin: IdePlugin): IdePluginClassesLocations {
     return if (pluginInfo is BundledPluginInfo) {
@@ -35,9 +39,25 @@ class DefaultPluginDetailsProvider(extractDirectory: Path) : AbstractPluginDetai
 
   override fun createPlugin(pluginInfo: PluginInfo, pluginFileLock: FileLock): PluginCreationResult<IdePlugin> {
     return if (pluginInfo is DependencyPluginInfo) {
-      idePluginManager.createPlugin(pluginFileLock.file, validateDescriptor = false, problemResolver = dependencyProblemResolver)
+      idePluginManager
+        .createPlugin(
+          pluginFileLock.file,
+          validateDescriptor = false,
+          problemResolver = dependencyProblemResolver,
+          deleteExtractedDirectory = false
+        ).registerCloseableResources()
     } else {
       super.createPlugin(pluginInfo, pluginFileLock)
     }
+  }
+
+  private fun PluginCreationResult<IdePlugin>.registerCloseableResources() = apply {
+    if (this is PluginCreationSuccess) {
+      closeableResources += resources
+    }
+  }
+
+  override fun close() {
+    closeableResources.forEach { it.close() }
   }
 }
