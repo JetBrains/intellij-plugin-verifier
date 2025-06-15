698564e04e36fc93a1f9a88f9cefd90b07730fbd
Robert Novotny <robert.novotny@jetbrains.com>
2025-06-15 14:33:23 +0200

    Introduce a deletable interface along with Closeable adapter

---
+++ intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/base/utils/Deletable.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2000-2025 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package com.jetbrains.plugin.structure.base.utils
+
+import java.io.IOException
+
+interface Deletable {
+  @Throws(IOException::class)
+  fun delete()
+}
