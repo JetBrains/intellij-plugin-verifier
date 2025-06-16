739958aba79d3c664bdfdd3be0ac59b14ab834c9
Robert Novotny <robert.novotny@jetbrains.com>
2025-06-16 23:02:16 +0200

    Prevent race condition when a new filesystem has been created between 'get' and 'new' calls

--- intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/jar/UriJarFileSystemProvider.kt
+++ intellij-plugin-structure/structure-base/src/main/kotlin/com/jetbrains/plugin/structure/jar/UriJarFileSystemProvider.kt
@@ -5,6 +5,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.net.URI
 import java.nio.file.FileSystem
+import java.nio.file.FileSystemAlreadyExistsException
 import java.nio.file.FileSystemNotFoundException
 import java.nio.file.FileSystems
 import java.nio.file.Path
@@ -23,9 +24,15 @@ class UriJarFileSystemProvider(private val pathToUri: (Path) -> URI = { it.toUri
         FileSystems.getFileSystem(jarUri).also {
           log.debug("Reusing JAR filesystem from JVM cache <{}>", jarUri)
         }
-      } catch (e: FileSystemNotFoundException) {
-        FileSystems.newFileSystem(jarUri, emptyMap<String, Any>()).also {
-          log.debug("JAR filesystem not found. Creating a new one for <{}>", jarUri)
+      } catch (_: FileSystemNotFoundException) {
+        try {
+          FileSystems.newFileSystem(jarUri, emptyMap<String, Any>()).also {
+            log.debug("JAR filesystem not found. Creating a new one for <{}>", jarUri)
+          }
+        } catch (_: FileSystemAlreadyExistsException) {
+          FileSystems.getFileSystem(jarUri).also {
+            log.debug("Reusing JAR filesystem from JVM cache <{}> (2nd attempt)", jarUri)
+          }
         }
       }
     } catch (e: JarArchiveCannotBeOpenException) {
