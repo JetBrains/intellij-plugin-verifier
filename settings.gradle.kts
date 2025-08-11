53d20dbfa3c5614bc9cd7bfe4c2b6cf4c6595f1c
Robert Novotny <robert.novotny@jetbrains.com>
2025-08-11 11:22:22 +0200

    Reorganize top-level Gradle and IDEA settings

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "intellij-plugin-verifier-parent"
+
+includeBuild("intellij-plugin-structure")
+includeBuild("intellij-plugin-verifier")
+
+includeBuild("ide-diff-builder")
+includeBuild("intellij-feature-extractor")
+
+includeBuild("plugins-verifier-service")
