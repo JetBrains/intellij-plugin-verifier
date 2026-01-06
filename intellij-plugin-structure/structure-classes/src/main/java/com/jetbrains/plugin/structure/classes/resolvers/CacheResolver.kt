b42b3ba6ad5f034c21f7fea7d7f88486555c3710
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2026-01-06 11:55:41 +0100

    [memory] `CacheResolver`: use soft caches, adjustible cache size

--- intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/resolvers/CacheResolver.kt
+++ intellij-plugin-structure/structure-classes/src/main/java/com/jetbrains/plugin/structure/classes/resolvers/CacheResolver.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2026 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.jetbrains.plugin.structure.classes.resolvers
@@ -13,18 +13,20 @@ import java.util.concurrent.ExecutionException
 
 class CacheResolver(
   private val delegate: Resolver,
-  cacheSize: Int = DEFAULT_CACHE_SIZE
+  cacheSize: Int = getDefaultCacheSize()
 ) : Resolver() {
 
   private data class BundleCacheKey(val baseName: String, val locale: Locale)
 
-  private val classCache: LoadingCache<String, ResolutionResult<ClassNode>> =
+  private val classCache: LoadingCache<BinaryClassName, ResolutionResult<ClassNode>> =
     Caffeine.newBuilder()
+      .softValues()
       .maximumSize(cacheSize.toLong())
       .build { key -> delegate.resolveClass(key) }
 
   private val propertyBundleCache: LoadingCache<BundleCacheKey, ResolutionResult<PropertyResourceBundle>> =
     Caffeine.newBuilder()
+      .softValues()
       .maximumSize(cacheSize.toLong())
       .build { key -> delegate.resolveExactPropertyResourceBundle(key.baseName, key.locale) }
 
@@ -49,14 +51,12 @@ class CacheResolver(
     get() = delegate.readMode
 
   @Deprecated("Use 'resolveClass(BinaryClassName)' instead")
-  override fun resolveClass(className: String): ResolutionResult<ClassNode> = try {
+  override fun resolveClass(className: String): ResolutionResult<ClassNode> = resolveClass(className as BinaryClassName)
+
+  override fun resolveClass(className: BinaryClassName): ResolutionResult<ClassNode> = try {
     classCache.get(className)
   } catch (e: ExecutionException) {
     throw e.cause ?: e
-  }
-
-  override fun resolveClass(className: BinaryClassName): ResolutionResult<ClassNode> {
-    return resolveClass(className.toString())
   }
 
   override fun resolveExactPropertyResourceBundle(baseName: String, locale: Locale): ResolutionResult<PropertyResourceBundle> = try {
@@ -84,6 +84,7 @@ class CacheResolver(
     delegate.processAllClasses(processor)
 
   private companion object {
-    private const val DEFAULT_CACHE_SIZE = 1024
+    fun getDefaultCacheSize(): Int =
+      System.getProperty("intellij.structure.classes.cache.size", "1024").toIntOrNull() ?: 1024
   }
 }
