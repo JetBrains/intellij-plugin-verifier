647763ae0866d0056eb53a2767f8964cd633fa57
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2026-01-06 11:55:41 +0100

    [performance] Concurrency refactoring in ResourceRepositoryImpl
    Separate storage class is not needed.
    Using fewer synchronizations, threads should not wait for `cleanup`.
    Also adjusted generic parameters nullability.

--- intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/provider/ProvideResult.kt
+++ intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/provider/ProvideResult.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2026 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.jetbrains.pluginverifier.repository.provider
@@ -7,22 +7,22 @@ package com.jetbrains.pluginverifier.repository.provider
 /**
  * Represents possible [provision] [ResourceProvider.provide] outcomes.
  */
-sealed class ProvideResult<out R> {
+sealed class ProvideResult<out R : Any> {
   /**
    * The [resource] is successfully provided.
    */
-  data class Provided<out R>(val resource: R) : ProvideResult<R>()
+  data class Provided<out R : Any>(val resource: R) : ProvideResult<R>()
 
   /**
    * Provision is failed because the specified resource is not found
    * in a place where it used to be.
    * The actual reason can be observed in [reason].
    */
-  data class NotFound<out R>(val reason: String) : ProvideResult<R>()
+  data class NotFound<out R : Any>(val reason: String) : ProvideResult<R>()
 
   /**
    * The resource was not provided due to network or other failure,
    * reason of which is the [reason] and a thrown exception is [error].
    */
-  data class Failed<out R>(val reason: String, val error: Exception) : ProvideResult<R>()
+  data class Failed<out R : Any>(val reason: String, val error: Exception) : ProvideResult<R>()
 }
--- intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/resources/ResourceLockImpl.kt
+++ intellij-plugin-verifier/verifier-repository/src/main/java/com/jetbrains/pluginverifier/repository/resources/ResourceLockImpl.kt
@@ -1,18 +1,18 @@
 /*
- * Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2026 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.jetbrains.pluginverifier.repository.resources
 
 import java.time.Instant
 
-internal class ResourceLockImpl<R, K, W : ResourceWeight<W>>(
+internal class ResourceLockImpl<R : Any, K : Any, W : ResourceWeight<W>>(
   lockTime: Instant,
-  resourceInfo: ResourceInfo<R, W>,
   val key: K,
   private val lockId: Long,
-  private val repository: ResourceRepositoryImpl<R, K, W>
-) : ResourceLock<R, W>(lockTime, resourceInfo) {
+  private val repository: ResourceRepositoryImpl<R, K, W>,
+  val value: ResourceRepositoryImpl.Stored<R, K, W>
+) : ResourceLock<R, W>(lockTime, value.info) {
 
   override fun release() = repository.releaseLock(this)
 
