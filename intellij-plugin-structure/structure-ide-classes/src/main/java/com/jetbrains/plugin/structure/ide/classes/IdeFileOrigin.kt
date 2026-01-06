817df2832a5dacb9fc921c4a35081da237e9d26c
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2026-01-06 21:28:00 +0100

    [memory] Drop duplicated fields from subclasses of `IdeFileOrigin`

--- intellij-plugin-structure/structure-ide-classes/src/main/java/com/jetbrains/plugin/structure/ide/classes/IdeFileOrigin.kt
+++ intellij-plugin-structure/structure-ide-classes/src/main/java/com/jetbrains/plugin/structure/ide/classes/IdeFileOrigin.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2026 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.jetbrains.plugin.structure.ide.classes
@@ -12,12 +12,14 @@ import java.nio.file.Path
 sealed class IdeFileOrigin : FileOrigin {
   override val parent: FileOrigin? = null
 
-  data class IdeLibDirectory(override val ide: Ide) : IdeAwareFileOrigin(ide)
-  data class RepositoryLibrary(override val ide: Ide) : IdeAwareFileOrigin(ide)
-  data class SourceLibDirectory(override val ide: Ide) : IdeAwareFileOrigin(ide)
-  data class CompiledModule(override val ide: Ide, val moduleName: String) : IdeAwareFileOrigin(ide)
+  data class IdeLibDirectory(override val ide: Ide) : IdeAwareFileOrigin()
+  data class RepositoryLibrary(override val ide: Ide) : IdeAwareFileOrigin()
+  data class SourceLibDirectory(override val ide: Ide) : IdeAwareFileOrigin()
+  data class CompiledModule(override val ide: Ide, val moduleName: String) : IdeAwareFileOrigin()
 
-  class BundledPlugin(val pluginFile: Path, val idePlugin: IdePlugin) : IdeFileOrigin()
+  data class BundledPlugin(val pluginFile: Path, val idePlugin: IdePlugin) : IdeFileOrigin()
 
-  abstract class IdeAwareFileOrigin(open val ide: Ide): IdeFileOrigin()
+  abstract class IdeAwareFileOrigin: IdeFileOrigin() {
+    abstract val ide: Ide
+  }
 }
