ac833e150defbd42ce63e1d877d99bf800d1b46e
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2026-01-25 22:50:16 +0100

    [performance] Fewer string operations in `isDiscouragingJdkClass`

--- intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/verifiers/resolution/DiscouragingJdkClassUtil.kt
+++ intellij-plugin-verifier/verifier-intellij/src/main/java/com/jetbrains/pluginverifier/verifiers/resolution/DiscouragingJdkClassUtil.kt
@@ -1,33 +1,37 @@
 /*
- * Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2026 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.jetbrains.pluginverifier.verifiers.resolution
 
 private val JDK_8_SPECIFIC_PACKAGES = listOf(
-  "javax.activation",
-  "javax.xml.bind",
-  "javax.annotation",
-  "com.sun.activation",
-  "com.sun.istack",
-  "com.sun.xml.bind",
-  "com.sun.xml.fastinfoset",
-  "com.sun.xml.txw2",
-  "org.jvnet.fastinfoset",
-  "org.jvnet.staxex",
+  "javax/activation",
+  "javax/xml/bind",
+  "javax/annotation",
+  "com/sun/activation",
+  "com/sun/istack",
+  "com/sun/xml/bind",
+  "com/sun/xml/fastinfoset",
+  "com/sun/xml/txw2",
+  "org/jvnet/fastinfoset",
+  "org/jvnet/staxex",
 
-  "javax.jws",
-  "javax.xml.soap",
-  "javax.xml.ws",
-  "javax.activity",
-  "javax.rmi",
-  "org.omg",
-  "javax.transaction",
+  "javax/jws",
+  "javax/xml/soap",
+  "javax/xml/ws",
+  "javax/activity",
+  "javax/rmi",
+  "org/omg",
+  "javax/transaction",
 
+  // For tests
   "discouragingJdkClass"
 )
 
 fun ClassFile.isDiscouragingJdkClass(): Boolean {
-  val packageName = javaPackageName
-  return JDK_8_SPECIFIC_PACKAGES.any { packageName == it || packageName.startsWith("$it.") }
+  val className = name
+  return JDK_8_SPECIFIC_PACKAGES.any {
+    // starts with it and slash
+    (className.length > it.length + 1 && className[it.length] == '/' && className.startsWith(it))
+  }
 }
