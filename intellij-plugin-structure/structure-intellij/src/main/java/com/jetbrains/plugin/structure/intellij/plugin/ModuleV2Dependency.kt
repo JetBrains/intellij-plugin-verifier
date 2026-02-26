b38f8853acd3394c884e8803c23d882072ea6026
Robert Novotny <robert.novotny@jetbrains.com>
2026-02-26 22:00:37 +0100

    Add KDoc

--- intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/ModuleV2Dependency.kt
+++ intellij-plugin-structure/structure-intellij/src/main/java/com/jetbrains/plugin/structure/intellij/plugin/ModuleV2Dependency.kt
@@ -4,9 +4,18 @@
 
 package com.jetbrains.plugin.structure.intellij.plugin
 
+/**
+ * Represents a mandatory dependency on a module in a Plugin V2 model.
+ * This maps to the `<dependencies>/<module>` element in the plugin descriptor.
+ */
 data class ModuleV2Dependency(override val id: String, override var isOptional: Boolean = false) : PluginDependency {
   override val isModule = true
 
+  /**
+   * Returns a copy of this dependency with [isOptional] flag set to true.
+   * Note that this is an artificial case, as per Plugin V2 model, the module dependency
+   * is always mandatory and cannot be optional.
+   */
   override fun asOptional(): ModuleV2Dependency = copy(isOptional = true)
 
   override fun toString() = "$id (module, v2)"
