7ce1380ad841d452e31f6f04feb0c9a26431d81a
Robert Novotny <robert.novotny@jetbrains.com>
2026-02-27 00:12:40 +0100

    Update copyrights

--- intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/layout/ModuleFactory.kt
+++ intellij-plugin-structure/structure-ide/src/main/java/com/jetbrains/plugin/structure/ide/layout/ModuleFactory.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2000-2026 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package com.jetbrains.plugin.structure.ide.layout
 
 import com.jetbrains.plugin.structure.ide.layout.PluginWithArtifactPathResult.Failure
