80538ac4ecba84549b060b91b1f06e0d79b3d5b1
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2026-05-27 09:53:33 +0200

    Propagate companion annotations to @JvmStatic methods and fields (IJPL-245371)

--- intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/usages/experimental/ExperimentalApiUsageTest.kt
+++ intellij-plugin-verifier/verifier-test/src/test/java/com/jetbrains/pluginverifier/usages/experimental/ExperimentalApiUsageTest.kt
@@ -2,6 +2,7 @@ package com.jetbrains.pluginverifier.usages.experimental
 
 import com.jetbrains.plugin.structure.classes.resolvers.EMPTY_RESOLVER
 import com.jetbrains.plugin.structure.classes.resolvers.FileOrigin
+import com.jetbrains.plugin.structure.classes.resolvers.FixedClassesResolver
 import com.jetbrains.pluginverifier.usages.util.MemberAnnotation
 import com.jetbrains.pluginverifier.verifiers.resolution.ClassFileAsm
 import org.junit.Assert.*
@@ -9,6 +10,7 @@ import org.junit.Test
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.AnnotationNode
 import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.FieldNode
 import org.objectweb.asm.tree.InsnNode
 import org.objectweb.asm.tree.MethodNode
 
@@ -38,6 +40,62 @@ class ExperimentalApiUsageTest {
 
     assertEquals(expectedAnnotation.annotationName, annotation.annotationName)
     assertEquals(expectedAnnotation.member, annotation.member)
+  }
+
+  @Test
+  fun `Companion field is detected as experimental when companion object is annotated`() {
+    val resolver = FixedClassesResolver.create(
+      listOf(mockClassNodeWithCompanionField, mockExperimentalCompanionClassNode),
+      origin
+    )
+    val classFile = ClassFileAsm(mockClassNodeWithCompanionField, origin)
+    val companionField = classFile.fields.find { it.name == "Companion" }
+    assertNotNull(companionField)
+    companionField!!
+
+    assertTrue(companionField.isExperimentalApi(resolver))
+  }
+
+  @Test
+  fun `JvmStatic method is detected as experimental when companion object is annotated`() {
+    val resolver = FixedClassesResolver.create(
+      listOf(mockClassNodeWithJvmStaticMethod, mockExperimentalCompanionClassNode),
+      origin
+    )
+    val classFile = ClassFileAsm(mockClassNodeWithJvmStaticMethod, origin)
+    val jvmStaticMethod = classFile.methods.find { it.name == JVM_STATIC_METHOD_NAME }
+    assertNotNull(jvmStaticMethod)
+    jvmStaticMethod!!
+
+    assertTrue(jvmStaticMethod.isExperimentalApi(resolver))
+  }
+
+  @Test
+  fun `JvmStatic method is detected as experimental when companion method is annotated`() {
+    val resolver = FixedClassesResolver.create(
+      listOf(mockClassNodeWithJvmStaticMethod, mockCompanionClassNodeWithExperimentalMethod),
+      origin
+    )
+    val classFile = ClassFileAsm(mockClassNodeWithJvmStaticMethod, origin)
+    val jvmStaticMethod = classFile.methods.find { it.name == JVM_STATIC_METHOD_NAME }
+    assertNotNull(jvmStaticMethod)
+    jvmStaticMethod!!
+
+    assertTrue(jvmStaticMethod.isExperimentalApi(resolver))
+  }
+
+  @Test
+  fun `JvmStatic default method is detected as experimental when companion object is annotated`() {
+    val resolver = FixedClassesResolver.create(
+      listOf(mockClassNodeWithJvmStaticDefaultMethod, mockExperimentalCompanionClassNodeWithDefaultMethod),
+      origin
+    )
+    val classFile = ClassFileAsm(mockClassNodeWithJvmStaticDefaultMethod, origin)
+    val jvmStaticDefaultMethod = classFile.methods.find { it.name == JVM_STATIC_DEFAULT_METHOD_NAME }
+    assertNotNull(jvmStaticDefaultMethod)
+    jvmStaticDefaultMethod!!
+
+    assertTrue(jvmStaticDefaultMethod.isExperimentalApi(resolver))
   }
 
   private val mockClassNode: ClassNode
@@ -71,6 +129,95 @@ class ExperimentalApiUsageTest {
       methods.add(experimentalApiMethod)
     }
 
+  private val mockClassNodeWithCompanionField: ClassNode
+    get() = ClassNode(Opcodes.ASM9).apply {
+      version = Opcodes.V1_8
+      access = Opcodes.ACC_PUBLIC
+      name = HOLDER_CLASS_NAME
+      superName = "java/lang/Object"
+
+      fields.add(
+        FieldNode(
+          Opcodes.ACC_PUBLIC or Opcodes.ACC_STATIC or Opcodes.ACC_FINAL,
+          "Companion",
+          "L$COMPANION_CLASS_NAME;",
+          null,
+          null
+        )
+      )
+    }
+
+  private val mockClassNodeWithJvmStaticMethod: ClassNode
+    get() = ClassNode(Opcodes.ASM9).apply {
+      version = Opcodes.V1_8
+      access = Opcodes.ACC_PUBLIC
+      name = HOLDER_CLASS_NAME
+      superName = "java/lang/Object"
+
+      methods.add(staticMethod(JVM_STATIC_METHOD_NAME, "()V"))
+    }
+
+  private val mockClassNodeWithJvmStaticDefaultMethod: ClassNode
+    get() = ClassNode(Opcodes.ASM9).apply {
+      version = Opcodes.V1_8
+      access = Opcodes.ACC_PUBLIC
+      name = HOLDER_CLASS_NAME
+      superName = "java/lang/Object"
+
+      methods.add(staticMethod(JVM_STATIC_DEFAULT_METHOD_NAME, "(ILjava/lang/Object;)V"))
+    }
+
+  private val mockExperimentalCompanionClassNode: ClassNode
+    get() = companionClassNode(
+      annotations = listOf(AnnotationNode("L$EXPERIMENTAL_API_ANNOTATION_NAME;")),
+      methods = listOf(instanceMethod(JVM_STATIC_METHOD_NAME, "()V"))
+    )
+
+  private val mockCompanionClassNodeWithExperimentalMethod: ClassNode
+    get() = companionClassNode(
+      methods = listOf(
+        instanceMethod(
+          JVM_STATIC_METHOD_NAME,
+          "()V",
+          annotations = listOf(AnnotationNode("L$EXPERIMENTAL_API_ANNOTATION_NAME;"))
+        )
+      )
+    )
+
+  private val mockExperimentalCompanionClassNodeWithDefaultMethod: ClassNode
+    get() = companionClassNode(
+      annotations = listOf(AnnotationNode("L$EXPERIMENTAL_API_ANNOTATION_NAME;")),
+      methods = listOf(staticMethod(JVM_STATIC_DEFAULT_METHOD_NAME, "(L$COMPANION_CLASS_NAME;ILjava/lang/Object;)V"))
+    )
+
+  private fun companionClassNode(
+    annotations: List<AnnotationNode> = emptyList(),
+    methods: List<MethodNode> = emptyList()
+  ): ClassNode = ClassNode(Opcodes.ASM9).apply {
+    version = Opcodes.V1_8
+    access = Opcodes.ACC_PUBLIC or Opcodes.ACC_FINAL
+    name = COMPANION_CLASS_NAME
+    superName = "java/lang/Object"
+    outerClass = HOLDER_CLASS_NAME
+    visibleAnnotations = annotations
+    this.methods.addAll(methods)
+  }
+
+  private fun staticMethod(name: String, descriptor: String): MethodNode =
+    MethodNode(Opcodes.ACC_PUBLIC or Opcodes.ACC_STATIC, name, descriptor, null, null).apply {
+      instructions.add(InsnNode(Opcodes.RETURN))
+    }
+
+  private fun instanceMethod(
+    name: String,
+    descriptor: String,
+    annotations: List<AnnotationNode> = emptyList()
+  ): MethodNode =
+    MethodNode(Opcodes.ACC_PUBLIC or Opcodes.ACC_FINAL, name, descriptor, null, null).apply {
+      visibleAnnotations = annotations
+      instructions.add(InsnNode(Opcodes.RETURN))
+    }
+
   private val origin = object : FileOrigin {
     override val parent = null
   }
@@ -78,4 +225,7 @@ class ExperimentalApiUsageTest {
 
 private const val EXPERIMENTAL_API_METHOD_NAME = "experimentalApiMethod"
 private const val EXPERIMENTAL_API_ANNOTATION_NAME = "org/jetbrains/annotations/ApiStatus\$Experimental"
-
+private const val HOLDER_CLASS_NAME = "com/jetbrains/Holder"
+private const val COMPANION_CLASS_NAME = "$HOLDER_CLASS_NAME\$Companion"
+private const val JVM_STATIC_METHOD_NAME = "jvmStaticMethod"
+private const val JVM_STATIC_DEFAULT_METHOD_NAME = "jvmStaticMethod\$default"
