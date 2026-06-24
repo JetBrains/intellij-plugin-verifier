ce565ae22d075c086a2fe896c90e5315422be029
Sergey.Kesarev <sergey.kesarev@jetbrains.com>
2026-06-24 16:05:34 +0200

    expose asm libs for verifier core

--- intellij-plugin-verifier/verifier-core/build.gradle.kts
+++ intellij-plugin-verifier/verifier-core/build.gradle.kts
@@ -2,5 +2,9 @@ dependencies {
   val intellijStructureVersion : String by rootProject.extra
   implementation("org.jetbrains.intellij.plugins:structure-classes:$intellijStructureVersion")
 
+  api(sharedLibs.asm.root)
+  api(sharedLibs.asm.tree)
+  api(sharedLibs.asm.analysis)
+
   implementation(sharedLibs.caffeine)
 }
